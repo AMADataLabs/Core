b40bf61af19cb8507b3a173ffedbf49f182630d6
Peter Lane <peter.lane@ama-assn.org>
2020-02-06 15:24:08 -0600

    Move test packages under datalabs.test package. Add initial unit test for conda2pipenv script.

--- Source/Python/datalabs/environment/convert.py
+++ Source/Python/datalabs/environment/convert.py
@@ -19,33 +19,33 @@ class CondaEnvironmentConverter(ABC):
         converted_dependencies = None
 
         try:
-            conda_dependencies = self.read_conda_dependencies(self._conda_package_list_filename)
+            conda_dependencies = self._read_conda_dependencies(self._conda_package_list_filename)
         except FileNotFoundError as fnfe:
             logger.exception(f'Unable to find conda package list file {self._conda_package_list_filename}.')
 
         try:
-            template = self.read_template(self._template_filename)
+            template = self._read_template(self._template_filename)
         except FileNotFoundError as fnfe:
             logger.exception(f'Unable to find template file {self._template_filename}.')
 
         if conda_dependencies and pipfile_template:
-            converted_dependencies = self.render_template(template, conda_dependencies)
+            converted_dependencies = self._render_template(template, conda_dependencies)
 
         return converted_dependencies
 
-    def read_conda_dependencies(cls, conda_package_list_filename):
+    def _read_conda_dependencies(cls, conda_package_list_filename):
         dependencies = {}
 
         with open(conda_package_list_filename) as file:
             for line in file:
-                variable, value = cls.parse_conda_dependency(line)
+                variable, value = cls._parse_conda_dependency(line)
 
                 if variable:
                     dependencies[variable] = value
 
         return dependencies
 
-    def read_pipfile_template(cls, template_filename):
+    def _read_pipfile_template(cls, template_filename):
         template = None
 
         with open(template_filename) as file:
@@ -53,7 +53,7 @@ class CondaEnvironmentConverter(ABC):
 
         return template
 
-    def parse_conda_dependency(cls, dependency):
+    def _parse_conda_dependency(cls, dependency):
         variable = None
         value = None
 
@@ -63,11 +63,11 @@ class CondaEnvironmentConverter(ABC):
         return variable, value
 
     @abstractmethod
-    def render_template(template: jinja2.Template, conda_dependencies: dict) -> str:
+    def _render_template(template: jinja2.Template, conda_dependencies: dict) -> str:
         pass
 
 class Conda2PipenvEnvrionmentConverter(CondaEnvironmentConverter):
-    def render_template(template, conda_dependencies):
+    def _render_template(template, conda_dependencies):
     names = sorted(conda_dependencies.keys())
     pipfile_dependencies = []
     python_version
---
+++ Test/Python/datalabs/test/environment/test_convert.py
@@ -0,0 +1,37 @@
+import logging
+import pytest
+
+from   datalabs.environment.convert import Conda2PipenvEnvrionmentConverter
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def test_dependency_dict_matches_conda_package_list(conda_package_list_filename, expected_conda_packages):
+    converter = Conda2PipenvEnvrionmentConverter(None, None)
+
+    dependencies = converter._read_conda_dependencies(cls, conda_package_list_filename)
+
+    assert expected_conda_packages == dependencies
+
+
+@pytest.fixture
+def conda_package_list_filename():
+    return 'Test/datalabs/environment/conda_package_list.txt'
+
+
+@pytest.fixture
+def pipfile_template_filename():
+    return 'Test/datalabs/environment/Pipfile_template.txt'
+
+
+@pytest.fixture
+def expected_conda_packages():
+    return dict(
+        conda=4.8.2,
+        numpy=1.18.1,
+        pandas=1.0.0,
+        python=3.7.6,
+        scipy=1.3.2,
+    )
