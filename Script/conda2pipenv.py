41b2a0c1fb149b8d086b4c318c63e3b78ce2940a
Peter Lane <peter.lane@ama-assn.org>
2020-02-07 15:50:37 -0600

    De-tabbed conda2pipenv script. Revised exception handling in conda2pipenv.

--- Script/conda2pipenv.py
+++ Script/conda2pipenv.py
@@ -17,33 +17,33 @@ def main(args):
         converted_dependencies=args['out'],
         whitelist=args.get('whitelist')
     )
-	converter = Conda2PipenvEnvrionmentConverter(filenames)
-	return_code = 0
+    converter = Conda2PipenvEnvrionmentConverter(filenames)
+    return_code = 0
 
-	try:
-		converter.convert()
-	except Exception as e:
-		logger.exception('Failed to convert conda package list to Pipfile.')
-		return_code = 1
+    try:
+        converter.convert()
+    except Exception as e:
+        logger.exception('Failed to convert conda package list to Pipfile.')
+        return_code = 1
 
-	return return_code
+    return return_code
 
 
 def write_pipfile(pipfile_filename, pipfile):
-	with open(pipfile_filename, 'w') as file:
-		file.write(pipfile)
+    with open(pipfile_filename, 'w') as file:
+        file.write(pipfile)
 
 
 if __name__ == '__main__':
-	ap = argparse.ArgumentParser()
+    ap = argparse.ArgumentParser()
 
-	ap.add_argument('-i', '--in', required=True, help='Path to the Conda package list file.')
-	ap.add_argument('-o', '--out', required=True, help='Path to the Pipenv package list file (Pipfile).')
-	ap.add_argument('-t', '--template', required=True, help='Path to the Pipfile template.')
-	ap.add_argument('-w', '--whitelist', required=False, help='Path to the whitelist file.')
+    ap.add_argument('-i', '--in', required=True, help='Path to the Conda package list file.')
+    ap.add_argument('-o', '--out', required=True, help='Path to the Pipenv package list file (Pipfile).')
+    ap.add_argument('-t', '--template', required=True, help='Path to the Pipfile template.')
+    ap.add_argument('-w', '--whitelist', required=False, help='Path to the whitelist file.')
 
-	args = vars(ap.parse_args())
+    args = vars(ap.parse_args())
 
-	return_code = main(args)
+    return_code = main(args)
 
-	exit(return_code)
+    exit(return_code)
