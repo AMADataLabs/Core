254f0f2c2965795119f22f4e4831f513c6ffa3fa
Garrett Lappe <garrett.lappe@ama-assn.org>
2020-03-04 10:16:18 -0600

    Edited to get going on my laptop

--- Sandbox/CommonModelCode/get_entity_ppd_info.py
+++ Sandbox/CommonModelCode/get_entity_ppd_info.py
@@ -1,18 +1,20 @@
-90# Kari Palmier    8/20/19    Created
+# Kari Palmier    8/20/19    Created
 #
 #############################################################################
 import pandas as pd
 import datetime
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # Get path of general (common) code and add it to the python path variable
 import sys
 import os
+
 curr_path = os.path.abspath(__file__)
 slash_ndx = [i for i in range(len(curr_path)) if curr_path.startswith('\\', i)]
-base_path = curr_path[:slash_ndx[-2]+1]
+base_path = curr_path[:slash_ndx[-2] + 1]
 gen_path = base_path + 'Common_Model_Code\\'
 sys.path.insert(0, gen_path)
 
@@ -21,159 +23,176 @@ from rename_entity_cols import rename_phn_cols, rename_fone_zr_cols
 
 
 def assign_lic_end_dates(license_df):
-    
     null_inact_ndx = license_df['lic_exp_dt'].isna() & license_df['lic_rnw_dt'].isna() & \
-        license_df['lic_sts'].isin(['I', 'X', 'D'])
+                     license_df['lic_sts'].isin(['I', 'X', 'D'])
     lic_notnull_df = license_df[~null_inact_ndx]
-    
+
     lic_notnull_df['anlys_end_dt'] = lic_notnull_df['lic_exp_dt']
     end_null_ndx = lic_notnull_df['anlys_end_dt'].isna()
     lic_notnull_df.loc[end_null_ndx, 'anlys_end_dt'] = lic_notnull_df.loc[end_null_ndx, 'lic_rnw_dt']
-    
+
     lic_no_end_ndx = lic_notnull_df['anlys_end_dt'].isna()
     lic_notnull_df.loc[lic_no_end_ndx, 'anlys_end_dt'] = datetime.datetime.now()
-    lic_notnull_df['anlys_end_dt'] = pd.to_datetime(lic_notnull_df['anlys_end_dt'], errors = 'coerce')
+    lic_notnull_df['anlys_end_dt'] = pd.to_datetime(lic_notnull_df['anlys_end_dt'], errors='coerce')
     lic_notnull_df = lic_notnull_df[lic_notnull_df['anlys_end_dt'].notnull()]
 
-    lic_notnull_df['lic_issue_dt'] = pd.to_datetime(lic_notnull_df['lic_issue_dt'], errors = 'coerce')
+    lic_notnull_df['lic_issue_dt'] = pd.to_datetime(lic_notnull_df['lic_issue_dt'], errors='coerce')
     lic_notnull_df = lic_notnull_df[lic_notnull_df['lic_issue_dt'].notnull()]
-    
+
     return lic_notnull_df
-    
+
+
+# made by Garrett, attempting to simplify date cleaning process (commented block of code in create_ent_comm_dates below)
+def clean_date(text):
+    if not isinstance(text, str):
+        return text
+
+    invalids = '[]().,'
+    result = []
+    for c in text:
+        if c not in invalids:
+            result.append(c)
+    result = ''.join(result)
+
+    if '(null)' in result or result == '.':
+        return datetime.datetime.now()
+
+    try:
+        result = pd.to_datetime(result)  # might throw error
+        return result
+    except:
+        return datetime.datetime.now()
+
 
 def create_ent_comm_dates(orig_data, date_var):
-    
-    orig_data[date_var] = orig_data[date_var].replace('.',datetime.datetime.now())
-
-    orig_data[date_var] = orig_data[date_var].astype('str') 
-    orig_data[date_var] = orig_data[date_var].apply(lambda x: x[0:10] \
-             if x.find('-') > 0 else x[0:9])
-    
-    orig_data[date_var] = orig_data[date_var].apply(lambda x: pd.to_datetime(x, format = '%Y-%m-%d') \
-             if x.find('-') > 0 else pd.to_datetime(x, format = '%d%b%Y'))
-
+    print('CREATE_ENT_COMM_DATES')
+    ###assert len(orig_data) > 0
+    ###orig_data[date_var] = orig_data[date_var].replace('.', datetime.datetime.now())
+    ###orig_data[date_var] = orig_data[date_var].astype('str')
+    ###orig_data[date_var] = orig_data[date_var].apply(lambda x: x[0:10] \
+    ###         if x.find('-') > 0 else x[0:9])
+    ###
+    ###orig_data[date_var] = orig_data[date_var].apply(lambda x: pd.to_datetime(x, format = '%Y-%m-%d') \
+    ###         if x.find('-') > 0 else pd.to_datetime(x, format = '%d%b%Y'))
+    ###return orig_data
+    print('cleaning date')
+    orig_data[date_var] = orig_data[date_var].apply(lambda x: clean_date(x))
+    print('done cleaning date')
+    assert len(orig_data) > 0
     return orig_data
 
 
 def set_entity_dates(ent_data_df, begin_var, end_var):
-
+    print('SET_ENTITY_DATES')
+    assert len(ent_data_df) > 0
+    print(begin_var)
     ent_data_df = create_ent_comm_dates(ent_data_df, begin_var)
-    
+
     ent_no_end_ndx = ent_data_df[end_var].isna()
     ent_data_df.loc[ent_no_end_ndx, end_var] = datetime.datetime.now()
-    
-    
+
+    print(end_var)
     ent_data_df = create_ent_comm_dates(ent_data_df, end_var)
-    
+    print('END SET_ENTITY_DATES')
     return ent_data_df
 
-    
+
 def rename_ent_ppd_addr_cols(data_df):
-    
-    ppd_rename_dict = {'POLO_MAILING_LINE_1':'ORIG_POLO_MAILING_LINE_1', 
-                   'POLO_MAILING_LINE_2':'ORIG_POLO_MAILING_LINE_2', 'POLO_CITY':'ORIG_POLO_CITY',  
-                   'POLO_STATE':'ORIG_POLO_STATE', 'POLO_ZIP':'ORIG_POLO_ZIP'}
-    data_df = data_df.rename(columns = ppd_rename_dict)
-    
-    ent_to_ppd_dict = {'post_addr_line1':'POLO_MAILING_LINE_1', 
-                   'post_addr_line2':'POLO_MAILING_LINE_2', 'post_city_cd':'POLO_CITY',  
-                   'post_state_cd':'POLO_STATE', 'post_zip':'POLO_ZIP'}   
-    data_df = data_df.rename(columns = ent_to_ppd_dict)
-    
+    ppd_rename_dict = {'POLO_MAILING_LINE_1': 'ORIG_POLO_MAILING_LINE_1',
+                       'POLO_MAILING_LINE_2': 'ORIG_POLO_MAILING_LINE_2', 'POLO_CITY': 'ORIG_POLO_CITY',
+                       'POLO_STATE': 'ORIG_POLO_STATE', 'POLO_ZIP': 'ORIG_POLO_ZIP'}
+    data_df = data_df.rename(columns=ppd_rename_dict)
+
+    ent_to_ppd_dict = {'post_addr_line1': 'POLO_MAILING_LINE_1',
+                       'post_addr_line2': 'POLO_MAILING_LINE_2', 'post_city_cd': 'POLO_CITY',
+                       'post_state_cd': 'POLO_STATE', 'post_zip': 'POLO_ZIP'}
+    data_df = data_df.rename(columns=ent_to_ppd_dict)
+
     return data_df
 
 
 def create_general_key(data_df, var1, var2, key_var):
-    
     data_df = data_df[data_df[var1].notnull()]
     data_df[var1] = data_df[var1].astype(str)
-    
+
     data_df = data_df[data_df[var2].notnull()]
     data_df[var2] = data_df[var2].astype(str)
-    
+
     data_df[key_var] = data_df[var1].map(str) + '-' + data_df[var2].map(str)
-    
+
     return data_df
-    
+
 
 def clean_addr_data(post_addr_df):
-    
     post_addr_cols = ['comm_id', 'addr_line2', 'addr_line1', 'addr_line0', 'city_cd',
                       'state_cd', 'zip', 'plus4']
     post_addr_df = post_addr_df[post_addr_cols]
     post_addr_df = rename_post_cols(post_addr_df)
-    
-    post_addr_df = post_addr_df[(post_addr_df['post_addr_line2'].notnull()) & \
-                                      (post_addr_df['post_city_cd'].notnull()) & \
-                                      (post_addr_df['post_state_cd'].notnull()) & \
-                                      (post_addr_df['post_zip'].notnull())]
-    
-    
+
+    post_addr_df = post_addr_df[(post_addr_df['post_addr_line2'].notnull()) &
+                                (post_addr_df['post_city_cd'].notnull()) &
+                                (post_addr_df['post_state_cd'].notnull()) &
+                                (post_addr_df['post_zip'].notnull())]
+
     return post_addr_df
 
 
 def clean_ent_comm_data(ent_comm_df):
-    
-    ent_comm_cols = ['entity_id', 'comm_type', 'begin_dt', 'comm_id', 'end_dt', 'src_cat_code']    
+    ent_comm_cols = ['entity_id', 'comm_type', 'begin_dt', 'comm_id', 'end_dt', 'src_cat_code']
     ent_comm_df = ent_comm_df[ent_comm_cols]
     ent_comm_df = rename_comm_cols(ent_comm_df)
-    
+
     return ent_comm_df
 
 
 def clean_email_data(ent_comm_df):
-    
-    ent_comm_cols = ['comm_id', 'user_nm', 'domain']    
+    ent_comm_cols = ['comm_id', 'user_nm', 'domain']
     ent_comm_df = ent_comm_df[ent_comm_cols]
     ent_comm_df = rename_comm_cols(ent_comm_df)
-    
+
     return ent_comm_df
 
 
 def clean_ent_usg_data(ent_usg_df):
-    
-    ent_usg_cols = ['entity_id', 'comm_type', 'comm_usage', 'usg_begin_dt', 'comm_id', 'comm_type', 
-                    'end_dt', 'src_cat_code']    
+    ent_usg_cols = ['entity_id', 'comm_type', 'comm_usage', 'usg_begin_dt', 'comm_id', 'comm_type',
+                    'end_dt', 'src_cat_code']
     ent_usg_df = ent_usg_df[ent_usg_cols]
     ent_usg_df = rename_usg_cols(ent_usg_df)
-    
+
     return ent_usg_df
 
 
 def create_ent_me_data(ent_key_df):
-    
     ent_me_df = ent_key_df[ent_key_df['key_type'] == 'ME']
-    ent_me_df = ent_me_df.rename(columns = {'key_type_val':'ent_me'})
-    
+    ent_me_df = ent_me_df.rename(columns={'key_type_val': 'ent_me'})
+
     return ent_me_df
-    
+
 
 def clean_phn_data(phone_df):
-    
     phone_cols = ['comm_id', 'area_cd', 'exchange', 'phone_nbr']
     phone_df = phone_df[phone_cols]
     phone_df = rename_phn_cols(phone_df)
-    
-    phone_df = phone_df[(phone_df['phn_area_cd'].notnull()) & \
-                                      (phone_df['phn_exchange'].notnull()) & \
-                                      (phone_df['phn_phone_nbr'].notnull())]
-    
+
+    phone_df = phone_df[(phone_df['phn_area_cd'].notnull()) &
+                        (phone_df['phn_exchange'].notnull()) &
+                        (phone_df['phn_phone_nbr'].notnull())]
+
     phone_df['aims_phone'] = phone_df['phn_area_cd'].astype(str) + \
-        phone_df['phn_exchange'].astype(str) + \
-        phone_df['phn_phone_nbr'].astype(str)
-    
+                             phone_df['phn_exchange'].astype(str) + \
+                             phone_df['phn_phone_nbr'].astype(str)
+
     return phone_df
 
 
 def clean_fone_zr_data(fone_zr_df):
-    
     fone_zr_cols = ['AREA_CD', 'STATE_CD', 'FIPS_CD', 'ZIP_CODE1', 'ZIP_CODE2', 'ZIP_CODE3']
     fone_zr_df = fone_zr_df[fone_zr_cols]
     fone_zr_df = rename_fone_zr_cols(fone_zr_df)
-    
-    fone_zr_df = fone_zr_df.drop_duplicates(subset = ['FNZR_AREA_CD', 'FNZR_STATE_CD'])
-    
+
+    fone_zr_df = fone_zr_df.drop_duplicates(subset=['FNZR_AREA_CD', 'FNZR_STATE_CD'])
+
     return fone_zr_df
-    
+
 
 
--- Sandbox/PoloRankModel/rank_ppd_polo_addrs.py
+++ Sandbox/PoloRankModel/rank_ppd_polo_addrs.py
@@ -4,20 +4,21 @@
 #############################################################################
 import tkinter as tk
 from tkinter import filedialog
-import pickle 
+import pickle
 import datetime
 import pandas as pd
 
 # Get path of general (common) code and add it to the python path variable
 import sys
 import os
+
 curr_path = os.path.abspath(__file__)
 slash_ndx = [i for i in range(len(curr_path)) if curr_path.startswith('\\', i)]
-base_path = curr_path[:slash_ndx[-2]+1]
+base_path = curr_path[:slash_ndx[-2] + 1]
 gen_path = base_path + 'Common_Code\\'
 sys.path.insert(0, gen_path)
 
-from get_ppd import get_latest_ppd_data
+# from get_ppd import get_latest_ppd_data
 from capitalize_column_names import capitalize_column_names
 
 gen_path = base_path + 'Common_Model_Code\\'
@@ -28,86 +29,103 @@ from class_model_creation import get_prob_info, get_pred_info
 from create_addr_model_input_data import create_ppd_scoring_data
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
-    
 root = tk.Tk()
 root.withdraw()
 
 # Get model file needed
-model_file = filedialog.askopenfilename(initialdir="U:\\Source Files\\Data Analytics\\Data-Science\\Data\\Polo_Rank_Model\\",
-                                        title="Choose the current POLO address rank model sav file...")
-
+# model_file = filedialog.askopenfilename(initialdir="U:\\Source Files\\Data Analytics\\Data-Science\\Data\\Polo_Rank_Model\\",
+#                                        title="Choose the current POLO address rank model sav file...")
+model_file = 'U:\Source Files\Data Analytics\Data-Science\Data\Polo_Rank_Model\Address_POLO_Rank_Class_Model.sav'
 # Get model file needed
-model_var_file = filedialog.askopenfilename(title="Choose the current POLO address rank model feature list sav file...")
+# model_var_file = filedialog.askopenfilename(title="Choose the current POLO address rank model feature list sav file...")
+model_var_file = 'U:\\Source Files\\Data Analytics\\Data-Science\Data\\Polo_Rank_Model\\Address_POLO_Rank_Class_Model_FeatureList.sav'
 
-print('1 - Choose PPD csv file')
-print('2 - Use latest PPD')
-ppd_str = input('Enter PPD choice ([1]/2): ')    
-if ppd_str.find('2') < 0:
-    ppd_str = '1'
+# print('1 - Choose PPD csv file')
+# print('2 - Use latest PPD')
+# ppd_str = 1
+# ppd_str = input('Enter PPD choice ([1]/2): ')
+# if ppd_str.find('2') < 0:
+#    ppd_str = '1'
+#
+# if ppd_str == '1':
+#    # Get model file needed
+#    ppd_file = filedialog.askopenfilename(initialdir="U:\\Source Files\\Data Analytics\\Data-Science\\Data\\PPD\\",
+#                                          title="Choose the PPD file desired...")
+ppd_file = 'C:\\Users\\glappe\\Documents\\udrive\\Data\PPD\\ppd_data_20200222.csv'
 
-if ppd_str == '1':
-    # Get model file needed
-    ppd_file = filedialog.askopenfilename(initialdir="U:\\Source Files\\Data Analytics\\Data-Science\\Data\\PPD\\",
-                                          title="Choose the PPD file desired...")
+init_save_dir = 'C:\\Users\\glappe\\Documents\\udrive\\Data\\Polo_Rank_Model\\'
+# init_save_dir = 'U:\\Source Files\\Data Analytics\\Data-Science\\Data\\Polo_Rank_Model\\'
+# ppd_score_out_dir = filedialog.askdirectory(initialdir = init_save_dir,
+#                                            title="Choose directory to save the scored PPD in...")
+# ppd_score_out_dir = ppd_score_out_dir.replace("/", "\\")
+# ppd_score_out_dir += "\\"
+ppd_score_out_dir = init_save_dir
 
-    
-init_save_dir = 'U:\\Source Files\\Data Analytics\\Data-Science\\Data\\Polo_Rank_Model\\'
-ppd_score_out_dir = filedialog.askdirectory(initialdir = init_save_dir,
-                                            title="Choose directory to save the scored PPD in...")
-ppd_score_out_dir = ppd_score_out_dir.replace("/", "\\")
-ppd_score_out_dir += "\\"
-
-ppd_archive_dir = ppd_score_out_dir + '_Archived\\'  
+ppd_archive_dir = ppd_score_out_dir + '_Archived\\'
 if not os.path.exists(ppd_archive_dir):
     os.mkdir(ppd_archive_dir)
-  
-  
-# Get entity data
-init_ent_comm_dir = 'C:\\'
-ent_comm_file = filedialog.askopenfilename(initialdir=init_ent_comm_dir,
-                                           title="Choose the entity_comm_at data csv file...")
 
-ent_comm_usg_file = filedialog.askopenfilename(title="Choose the entity_comm_usg_at data csv file...")
+##### # Get entity data
+##### init_ent_comm_dir = 'C:\\'
+##### ent_comm_file = filedialog.askopenfilename(initialdir=init_ent_comm_dir,
+#####                                            title="Choose the entity_comm_at data csv file...")
+#####
+##### ent_comm_usg_file = filedialog.askopenfilename(title="Choose the entity_comm_usg_at data csv file...")
+#####
+##### post_addr_file = filedialog.askopenfilename(title="Choose the post_addr_at data csv file...")
+#####
+##### license_file = filedialog.askopenfilename(title="Choose the license_lt data csv file...")
+#####
+##### ent_key_file = filedialog.askopenfilename(title="Choose the entity_key_et data csv file...")
 
-post_addr_file = filedialog.askopenfilename(title="Choose the post_addr_at data csv file...")
 
-license_file = filedialog.askopenfilename(title="Choose the license_lt data csv file...")
-
-ent_key_file = filedialog.askopenfilename(title="Choose the entity_key_et data csv file...")
-    
-    
 current_time = datetime.datetime.now()
 start_time_str = current_time.strftime("%Y-%m-%d")
 
-
 # Get PPD data
+ppd_str = '1'
 if ppd_str == '1':
-    ppd_df = pd.read_csv(ppd_file, delimiter=",", index_col = None, header=0, dtype=str)
-    under_ndx = under_ndx = [i for i in range(len(ppd_file)) if ppd_file.startswith('_', i)]
-    dot_ndx = ppd_file.find('.')
-    ppd_date_str = ppd_file[under_ndx[-1] + 1:dot_ndx]
+    print('Loading PPD')
+    ppd_df = pd.read_csv(ppd_file, dtype=str)
+    # under_ndx = under_ndx = [i for i in range(len(ppd_file)) if ppd_file.startswith('_', i)]
+    under_ndx = ppd_file.rindex('_')
+    # dot_ndx = ppd_file.find('.')
+    dot_ndx = ppd_file.index('.')
+    # ppd_date_str = ppd_file[under_ndx[-1] + 1:dot_ndx]
+    ppd_date_str = ppd_file[under_ndx + 1:dot_ndx]
 else:
-    ppd_df, ppd_date_str = get_latest_ppd_data()
-    
+    # ppd_df, ppd_date_str = get_latest_ppd_data()
+    quit()
 ppd_date = datetime.datetime.strptime(ppd_date_str, '%Y%m%d')
-
 
 # Load entity data
 print('Loading entity data')
-ent_comm_df = pd.read_csv(ent_comm_file, delimiter=",", index_col=None, header=0, dtype=str)
-ent_comm_df = ent_comm_df[ent_comm_df['comm_cat'] == 'A']
+# ent_comm_df = pd.read_csv(ent_comm_file, delimiter=",", index_col=None, header=0, dtype=str)
+ent_comm_df = pd.read_csv('C:\\Users\\glappe\\Documents\\udrive\\Data\\entity_data\\2020-02-25\\str_entity_comm_at.csv',
+                          dtype=str, na_values=['', '(null)'])
 
-ent_comm_usg_df = pd.read_csv(ent_comm_usg_file, delimiter=",", index_col = None, header=0, dtype=str)
-ent_comm_usg_df = ent_comm_usg_df[ent_comm_usg_df['comm_cat'] == 'A']
-
-post_addr_df = pd.read_csv(post_addr_file, delimiter=",", index_col=None, header=0, dtype=str)
-
-license_df = pd.read_csv(license_file, delimiter=",", index_col=None, header=0, dtype=str)
-
-ent_key_df = pd.read_csv(ent_key_file, delimiter=",", index_col=None, header=0, dtype=str)
-
+# ent_comm_df['comm_cat'] = ent_comm_df['comm_cat'].apply(str.strip)
+# ent_comm_df = ent_comm_df[ent_comm_df['comm_cat'] == 'A']
+assert len(ent_comm_df) > 0
+# ent_comm_usg_df = pd.read_csv(ent_comm_usg_file, delimiter=",", index_col = None, header=0, dtype=str)
+ent_comm_usg_df = pd.read_csv(
+    'C:\\Users\\glappe\\Documents\\udrive\\Data\\entity_data\\2020-02-25\\str_entity_comm_usg_at.csv',
+    dtype=str, na_values=['', '(null)'])
+# ent_comm_usg_df['comm_cat'] = ent_comm_usg_df['comm_cat'].apply(str.strip)
+# ent_comm_usg_df = ent_comm_usg_df[ent_comm_usg_df['comm_cat'] == 'A']
+assert len(ent_comm_usg_df) > 0
+# post_addr_df = pd.read_csv(post_addr_file, delimiter=",", index_col=None, header=0, dtype=str)
+post_addr_df = pd.read_csv('C:\\Users\\glappe\\Documents\\udrive\\Data\\entity_data\\2020-02-25\\str_post_addr_at.csv',
+                           dtype=str, na_values=['', '(null)'])
+# license_df = pd.read_csv(license_file, delimiter=",", index_col=None, header=0, dtype=str)
+license_df = pd.read_csv('C:\\Users\\glappe\\Documents\\udrive\\Data\\entity_data\\2020-02-25\\str_license_lt.csv',
+                         dtype=str, na_values=['', '(null)'])
+# ent_key_df = pd.read_csv(ent_key_file, delimiter=",", index_col=None, header=0, dtype=str)
+ent_key_df = pd.read_csv('C:\\Users\\glappe\\Documents\\udrive\\Data\\entity_data\\2020-02-25\\str_entity_key_et.csv',
+                         dtype=str, na_values=['', '(null)'])
 
 # Get latest model and variables
 print('Loading model and variables')
@@ -115,7 +133,7 @@ model_vars = pickle.load(open(model_var_file, 'rb'))
 model = pickle.load(open(model_file, 'rb'))
 
 print('Creating scoring data')
-ppd_scoring_df = create_ppd_scoring_data(ppd_df, ppd_date, ent_comm_df, ent_comm_usg_df, post_addr_df, 
+ppd_scoring_df = create_ppd_scoring_data(ppd_df, ppd_date, ent_comm_df, ent_comm_usg_df, post_addr_df,
                                          license_df, ent_key_df)
 print(len(ppd_scoring_df))
 ppd_entity_file = ppd_archive_dir + start_time_str + '_PPD_' + ppd_date_str + '_Polo_Addr_Rank_PPD_Entity_Data.csv'
@@ -133,7 +151,6 @@ model_pred_df.to_csv('U:\\Source Files\\Data Analytics\\Data-Science\\Data\\Polo
 model_pred_df['RANK_ROUND'] = model_pred_df['PRED_PROBABILITY'].apply(lambda x: round((x * 10)))
 zero_ndx = model_pred_df['RANK_ROUND'] == 0
 model_pred_df.loc[zero_ndx, 'RANK_ROUND'] = 1
-
 
 print('Lenght of model_pred_df: {}'.format(len(model_pred_df)))
 get_prob_info(model_pred_df['PRED_PROBABILITY'])
