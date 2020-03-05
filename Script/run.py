c22d7c2ee2be18a2707904865540d9097f7a412a
plane <peter.lane@ama-assn.org>
2020-03-05 09:45:36 -0600

    Add dotenv to dependencies. Add CommonCode and CommonModelCode to PYTHONPATH in the run.py script.
    Former-commit-id: 6eb13bf30ddcf6797ed5e20e90881b127ca74fc4

--- Script/run.py
+++ Script/run.py
@@ -1,20 +1,46 @@
+#!/usr/bin/env python3
+
 import os
 from   pathlib import Path
 import subprocess
 import sys
 
 
-if __name__ == '__main__':
-    script_path = Path(sys.argv[0])
+def main(args):
+    script_path = Path(args[0])
+    args = args[1:]
+
+    environment = setup_pythonpath(script_path)
+
+    run_args(args, environment)
+
+
+def setup_pythonpath(script_path):
+    datalabs_pythonpath = generate_datalabs_pythonpath(script_path)
     environment = os.environ.copy()
-    path = script_path.parent.joinpath('../Source/Python').resolve()
-    args = sys.argv[1:]
+    current_pythonpath = environment.get('PYTHONPATH')
 
-    if environment.get('PYTHONPATH'):
-        environment['PYTHONPATH'] = str(path) + ':' + environment['PYTHONPATH']
+    if current_pythonpath:
+        environment['PYTHONPATH'] = os.pathsep.join([datalabs_pythonpath, current_pythonpath])
     else:
-        environment['PYTHONPATH'] = str(path)
+        environment['PYTHONPATH'] = datalabs_pythonpath
 
+    return environment
+
+
+def run_args(args, environment):
     return_code = subprocess.call(args, env=environment)
 
     exit(return_code)
+
+
+def generate_datalabs_pythonpath(script_path):
+    shared_source_path = str(script_path.parent.joinpath('../Source/Python').resolve())
+    common_code_path = str(script_path.parent.joinpath('../Sandbox/CommonCode').resolve())
+    common_model_code_path = str(script_path.parent.joinpath('../Sandbox/CommonModelCode').resolve())
+
+    return os.pathsep.join([shared_source_path, common_code_path, common_model_code_path])
+
+
+if __name__ == '__main__':
+    main(sys.argv)
