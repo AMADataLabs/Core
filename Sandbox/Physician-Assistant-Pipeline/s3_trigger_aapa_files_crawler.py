8fca6f9dfd3bc14cc7049b3996df15e96300c19e
Nikhil Khatri <nikhil.khatri@ama-assn.org>
2020-03-16 08:46:36 -0500

    Made Several changes after Pull request comments

--- Sandbox/Physician-Assistant-Pipeline/s3_trigger_aapa_files_crawler.py
+++ Sandbox/Physician-Assistant-Pipeline/s3_trigger_aapa_files_crawler.py
@@ -7,7 +7,7 @@ logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
-glue_client = boto3.client('glue')
+GLUE_CLIENT = boto3.client('glue')
 
 def lambda_handler(event, context):
     """ trigger the glue crawler that creates a data catalog from state physician assistant license files """
@@ -31,7 +31,7 @@ def start_glue_crawler_if_ready(crawler_name):
 
 
 def get_glue_crawler_state(crawler_name):
-    crawler_metadata = glue_client.get_crawler(Name=crawler_name)
+    crawler_metadata = GLUE_CLIENT.get_crawler(Name=crawler_name)
 
     return crawler_metadata.get('Crawler').get('State')
 
@@ -39,7 +39,7 @@ def get_glue_crawler_state(crawler_name):
 def start_glue_crawler(crawler_name):
     LOGGER.info(f'Crawler {crawler_name} is ready, so we are starting it...')
 
-    response = glue_client.start_crawler(Name=crawler_name)
+    response = GLUE_CLIENT.start_crawler(Name=crawler_name)
 
     LOGGER.debug(f'Crawler {crawler_name} start response: {response}')
 
