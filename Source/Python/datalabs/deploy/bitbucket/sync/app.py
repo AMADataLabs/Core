f24021b928de14a9b48d6560149e1862232dff8e
plane <peter.lane@ama-assn.org>
2020-03-20 17:13:25 -0500

    Initial BitBucket sync service infrastructure.
    Former-commit-id: b5efeddeaf2e6375f3bcad7175cdc075b7e4c8e4

---
+++ Source/Python/datalabs/deploy/bitbucket/sync/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, session, abort, jsonify
+
+
+def start():
+    app = Flask(__name__)
+
+    register_blueprints(app)
+
+    return app
+
+
+def register_blueprints(app):
+    import datalabs.deploy.bitbucket.sync.trigger as trigger
+
+    app.register_blueprint(trigger.routes, url_prefix='/trigger')
+
+if __name__ == '__main__':
+    start()
