6fc442e6682427cfefdb6b1b3cfed9cee3c0b634
plane <peter.lane@ama-assn.org>
2020-03-25 08:19:54 -0500

    Renamed .env as a template since the .env will now contain a BitBucket webhook secret key. Added untested request signature varification.
    Former-commit-id: 7503b585dc5e55c69641ff319f0e4d276aa97972

--- Source/Python/datalabs/deploy/bitbucket/sync/trigger.py
+++ Source/Python/datalabs/deploy/bitbucket/sync/trigger.py
@@ -1,8 +1,12 @@
+import base64
 import logging
 import os
 import flask
+import hashlib
+import hmac
 
 import settings
+import werkzeug.exceptions as exceptions
 
 import datalabs.deploy.bitbucket.sync.sync as sync
 
@@ -15,15 +19,37 @@ routes = flask.Blueprint('trigger', __name__)
 
 @routes.route('/', methods=['POST'])
 def sync_bitbucket():
-    data = flask.request.json
-    config = _generate_sync_configuration()
-    synchronizer = sync.BitBucketSynchronizer(config)
+    _verify_secret_key(flask.request.data, flask.request.headers.get('X-Hub-Signature'))
 
-    LOGGER.debug('Trigger Request: %s', data)
-    response = synchronizer.sync(data)
+    response = _sync_on_prem_with_cloud_bitbucket(flask.request.json)
 
     return flask.jsonify(response)
 
+
+def _verify_secret_key(request_data, request_signature):
+    secret_key = os.environ.get('SECRET_KEY')
+
+    calculated_signature = _sign_request_data(request_data, secret_key)
+    LOGGER.debug('Request signature: %s', request_signature)
+    LOGGER.debug('Calculated signature: %s', calculated_signature)
+
+    if request_signature != calculated_signature:
+        raise exceptions.BadRequest('The request data signature is invalid.')
+
+
+def _sync_on_prem_with_cloud_bitbucket(request_data)
+    config = _generate_sync_configuration()
+    synchronizer = sync.BitBucketSynchronizer(config)
+
+    LOGGER.debug('Trigger Request: %s', request_data)
+    return synchronizer.sync(request_data)
+
+
+def _sign_request_data(request_data, secret_key):
+    message = bytes(request_data, 'utf-8')
+    secret = bytes(secret_key, 'utf-8')
+
+    return base64.b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())
 
 def _generate_sync_configuration():
     return sync.Configuration(
