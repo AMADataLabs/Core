4bbb6f5694cd5a67f766eb391fe3f89b22ea4033
plane <peter.lane@ama-assn.org>
2020-03-26 11:29:22 -0500

    Fix a number of errors caught by Pylint. Modified MODUL.txt parser to keep track of modifier data.
    Former-commit-id: c8de47b0be79fdb32eecac64dbaa3b0eca737e62

--- Source/Python/datalabs/curate/cpt/modifier.py
+++ Source/Python/datalabs/curate/cpt/modifier.py
@@ -1,3 +1,5 @@
+""" File parser for CPT'S MODUL.txt file using a state machine pattern. """
+
 from abc import ABC, abstractmethod
 from collections import namedtuple
 from enum import Enum
@@ -29,85 +31,116 @@ class Event(Enum):
     LevelIIModifiers = 8
 
 
+class ModifierType(Enum):
+    Regular = 'regular'
+    Physical = 'physical'
+    LevelOne = 'level_one'
+    CategoryTwo = 'category_two'
+    LevelTwo = 'level_two'
+
+
 Context = namedtuple('Context', 'state event data')
 
 
-class StateProcessor(ABC):
-    @abstractmethod
-    def handle_event(self, context, line):
-        pass
-
-
-class AppendixAProcessor(EventHandler):
-    def handle_event(self, data, line):
-        event = Event.Start
-        data = {}
-
-        if line == 'Appendix A':
-            event = Event.RegularModifier
-
-        return Context(state=State.AppendixAHandler, event=event, data=data)
-
-
-class RegularModifierProcessor(EventHandler):
-    def handle_event(self, context, line):
-        event = None
-        data = {}
-        match = re.match('[1-9][0-9] ..*')
-
-        if line == 'Anesthesia Physical Status':
-            event = Event.AnesthesiaPhysicalStatus
-        elif line == '':
-            event = Event.Blank
-        elif match:
-            event,data = self._process_match(context, match)
-
-        return Context(state=context.state, event=event, data=data)
-
-    def _process_match(self, context, match):
-        pass
-
-
 class FileParser():
+    # pylint: disable=line-too-long,bad-whitespace
     TRANSITION_TABLE = [
         # Start           Blank                      Text                       Modifier                   AppendixA              AnesthesiaPhysicalStatus  CPTLevelIModifiers      CategoryIIModifiers       LevelIIModifiers
-        [State.Unknown,   State.Unkown,              State.Unkown,              State.Unkown,              State.Unkown,          State.Unkown,             State.Unkown,           State.Unkown,              State.Unkown]            # Unknown
-        [State.Beginning, State.Beginning,           State.Beginning,           State.Unkown,              State.RegularModifier, State.Unkown,             State.Unkown,           State.Unkown,              State.Unknown]           # Beginning
-        [State.Unknown,   State.RegularModifier,     State.RegularModifier,     State.RegularModifier,     State.Unknown,         State.PhysicalModifier,   State.Unknown,          State.Unknown,             State.Unknown]           # RegularModifier
-        [State.Unknown,   State.PhysicalModifier,    State.PhysicalModifier,    PhysicalModifier,          State.Unknown,         State.Unknown,            State.LevelOneModifier, State.Unknown,             State.Unknown]           # PhysicalModifier
-        [State.Unknown,   State.LevelOneModifier,    State.LevelOneModifier,    State.LevelOneModifier,    State.Unknown,         State.Unknown,            State.Unknown,          State.CategoryTwoModifier, State.Unknown]           # LevelOneModifier
-        [State.Unknown,   State.CategoryTwoModifier, State.CategoryTwoModifier, State.CategoryTwoModifier, State.Unknown,         State.Unknown,            State.Unknown,          State.Unknown,             State.LevelTwoModifier]  # CategoryTwoModifier
-        [State.Unknown,   State.LevelTwoModifier,    State.LevelTwoModifier,    State.LevelTwoModifier,    State.Unknown,         State.Unknown,            State.Unknown,          State.Unknown,             State.Unknown]           # LevelTwoModifier
+        [State.Unknown,   State.Unknown,             State.Unknown,             State.Unknown,             State.Unknown,         State.Unknown,            State.Unknown,          State.Unknown,             State.Unknown],           # Unknown
+        [State.Beginning, State.Beginning,           State.Beginning,           State.Unknown,             State.RegularModifier, State.Unknown,            State.Unknown,          State.Unknown,             State.Unknown],           # Beginning
+        [State.Unknown,   State.RegularModifier,     State.RegularModifier,     State.RegularModifier,     State.Unknown,         State.PhysicalModifier,   State.Unknown,          State.Unknown,             State.Unknown],           # RegularModifier
+        [State.Unknown,   State.PhysicalModifier,    State.PhysicalModifier,    State.PhysicalModifier,    State.Unknown,         State.Unknown,            State.LevelOneModifier, State.Unknown,             State.Unknown],           # PhysicalModifier
+        [State.Unknown,   State.LevelOneModifier,    State.LevelOneModifier,    State.LevelOneModifier,    State.Unknown,         State.Unknown,            State.Unknown,          State.CategoryTwoModifier, State.Unknown],           # LevelOneModifier
+        [State.Unknown,   State.CategoryTwoModifier, State.CategoryTwoModifier, State.CategoryTwoModifier, State.Unknown,         State.Unknown,            State.Unknown,          State.Unknown,             State.LevelTwoModifier],  # CategoryTwoModifier
+        [State.Unknown,   State.LevelTwoModifier,    State.LevelTwoModifier,    State.LevelTwoModifier,    State.Unknown,         State.Unknown,            State.Unknown,          State.Unknown,             State.Unknown],           # LevelTwoModifier
     ]
 
-    STATE_PROCESSORS = [
-        AppendixAProcessor(),
-        RegularModifierProcessor(),
-        # ...
-    ]
+    def __init__(self):
+        self._modifiers = {
+            ModifierType.Regular: {},
+            ModifierType.Physical: {},
+            ModifierType.LevelOne: {},
+            ModifierType.CategoryTwo: {},
+            ModifierType.LevelTwo: {},
+        }
+        self._state_processors = [
+            AppendixAProcessor(self),
+            RegularModifierProcessor(self),
+            # ...
+        ]
 
-    @classmethod
-    def parse(cls, input_filename: str, output_filename: str) -> dict:
-        lines = _read_file(input_filename)
+    def add_modifier(self, modifier_type: ModifierType, code: str, description: str):
+        self._modifiers[modifier_type][code] = description
 
-        data = _parse_file(lines)
+    def parse(self, input_filename: str, output_filename: str) -> dict:
+        lines = self._read_file(input_filename)
 
-        _write_data(data, output_filename)
+        self._parse_file(lines)
+
+        self._write_data(output_filename)
 
     @classmethod
     def _read_file(cls, input_filename):
         with open(input_filename) as file:
             return file.readlines()
 
-    @classmethod
-    def _parse_file(cls, lines):
-        context = Context(state=State.InitialModifiers, event=Event.Start, data={})
+    def _parse_file(self, lines):
+        context = Context(state=State.Beginning, event=Event.Start, data={})
 
         for line in lines:
-            context = cls._handle_event(context, line.strip())
+            context = self._process_event(context, line.strip())
 
     @classmethod
-    def _process_event(cls, context, line):
-        state = TRANSITION_TABLE[context.state.value][context.event.value]
+    def _write_data(cls, output_filename):
+        # TODO: convert self._modifiers dict to Pandas DataFrame or something
+        #       and write data as CSV or push to DB
+        pass
 
-        return STATE_PROCESSORS[state.value].process_line(context, line)
+    def _process_event(self, context, line):
+        state = self.TRANSITION_TABLE[context.state.value][context.event.value]
+
+        _, event, data = self._state_processors[state.value].process_line(context, line)
+
+        return Context(state=state, event=event, data=data)
+
+
+class StateProcessor(ABC):
+    def __init__(self, parser: FileParser):
+        self._parser = parser
+
+    @abstractmethod
+    def process_line(self, context: Context, line: str) -> Context:
+        pass
+
+
+class AppendixAProcessor(StateProcessor):
+    def process_line(self, context, line):
+        event = Event.Start
+        data = {}
+
+        if line == 'Appendix A':
+            event = Event.AppendixA
+
+        return Context(state=None, event=event, data=data)
+
+
+class RegularModifierProcessor(StateProcessor):
+    def process_line(self, context, line):
+        event = Event.Modifier
+        data = {}
+        match = re.match('[1-9][0-9] ..*', line)
+
+        if line == 'Anesthesia Physical Status':
+            event = Event.AnesthesiaPhysicalStatus
+        elif line == '':
+            event = Event.Blank
+        elif match:
+            self._process_match(match)
+
+        return Context(state=None, event=event, data=data)
+
+    def _process_match(self, match):
+        modifier_line = match.group(0)
+        code, description = modifier_line.split(' ', 1)
+
+        self._parser.add_modifier(ModifierType.Regular, code, description)
