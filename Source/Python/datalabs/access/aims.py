ccd518bad7f10da39d7761fbe044882572b450b3
plane <peter.lane@ama-assn.org>
2020-04-06 13:02:59 -0500

    Strip ME numbers in DataFrame.
    Former-commit-id: e32e93f9b7a985fd80039f5ebb9754b72c60262e

--- Source/Python/datalabs/access/aims.py
+++ Source/Python/datalabs/access/aims.py
@@ -4,6 +4,7 @@ import logging
 import pandas
 
 import datalabs.access.database as db
+import datalabs.curate.dataframe as df
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -16,13 +17,19 @@ class AIMS(db.Database):
 
         return int(record_count.iloc[0,0])
 
-    def get_me_entity_map(self, chunk_size=1000, order_by='me'):
-        data = []
+    def get_me_entity_map(self, chunk_size=100000, order_by='me'):
+        chunks = self._get_me_entity_map_chunks(chunk_size, order_by)
+        data = pandas.concat(chunks, ignore_index=True)
+
+        return df.strip(data)
+
+    def _get_me_entity_map_chunks(self, chunk_size, order_by):
+        chunks = []
         chunk = pandas.DataFrame([True])
         offset = 0
 
         while not chunk.empty:
-            LOGGER.debug(f'Chunk offset: {offset}')
+            LOGGER.debug(f'Get ME/entity ID map chunk at offset: {offset}')
             chunk = self.read(
                 f"SELECT SKIP {offset} FIRST {chunk_size} key_type_val as me, entity_id "
                 f"FROM entity_key_et WHERE key_type='ME' "
@@ -30,8 +37,7 @@ class AIMS(db.Database):
             )
 
             if not chunk.empty:
-                data.append(chunk)
+                chunks.append(chunk)
                 offset += chunk_size
 
-
-        return pandas.concat(data, ignore_index=True)
+        return chunks
