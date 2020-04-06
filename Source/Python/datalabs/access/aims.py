76703ea0c598756c712a44bbe6ba586b1b59caf7
plane <peter.lane@ama-assn.org>
2020-04-06 12:33:57 -0500

    Add ME/entity ID map method to AIMS.
    Former-commit-id: 044122e199c3165de0bc448e6d651f6cd93ef2a8

--- Source/Python/datalabs/access/aims.py
+++ Source/Python/datalabs/access/aims.py
@@ -1,7 +1,28 @@
 """ Database object for AIMS """
+import pandas
 
 import datalabs.access.database as db
 
 
 class AIMS(db.Database):
-    pass
+    def get_me_entity_count(self):
+        record_count_data = self.read("SELECT count(key_type_val) FROM entity_key_et WHERE key_type='ME'")
+
+        return int(record_count.iloc[0,0])
+
+    def get_me_entity_map(self, chunk_size=1000, order_by='me'):
+        data = []
+        chunk = pandas.DataFrame([True])
+        offset = 0
+
+        while not chunk.empty:
+            chunk = self.read(
+                f"SELECT SKIP {offset} FIRST {chunk_size} key_type_val as me, entity_id "
+                f"FROM entity_key_et WHERE key_type='ME' "
+                f"ORDER BY {order_by}"
+            )
+
+            if not chunk.empty:
+                data.append(chunk)
+
+        return pandas.concat(data, ignore_index=True)
