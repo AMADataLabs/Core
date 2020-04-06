c2106ec3c669bd5c2b96a6826ef126f67a70ff3e
plane <peter.lane@ama-assn.org>
2020-04-06 12:46:07 -0500

    Get all chunks and concatenate instead of providing an iterator.
    Former-commit-id: 4532a399a5053b563758b0298c2b449f7599eb1e

--- Source/Python/datalabs/access/aims.py
+++ Source/Python/datalabs/access/aims.py
@@ -1,12 +1,18 @@
 """ Database object for AIMS """
+import logging
+
 import pandas
 
 import datalabs.access.database as db
 
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
 
 class AIMS(db.Database):
     def get_me_entity_count(self):
-        record_count_data = self.read("SELECT count(key_type_val) FROM entity_key_et WHERE key_type='ME'")
+        record_count = self.read("SELECT count(key_type_val) FROM entity_key_et WHERE key_type='ME'")
 
         return int(record_count.iloc[0,0])
 
@@ -16,6 +22,7 @@ class AIMS(db.Database):
         offset = 0
 
         while not chunk.empty:
+            LOGGER.debug(f'Chunk offset: {offset}')
             chunk = self.read(
                 f"SELECT SKIP {offset} FIRST {chunk_size} key_type_val as me, entity_id "
                 f"FROM entity_key_et WHERE key_type='ME' "
@@ -24,5 +31,7 @@ class AIMS(db.Database):
 
             if not chunk.empty:
                 data.append(chunk)
+                offset += chunk_size
+
 
         return pandas.concat(data, ignore_index=True)
