0956b2b0b337601a82a5b00f20e638854cd921bb
Nikhil Khatri <nikhil.khatri@ama-assn.org>
2020-04-08 14:43:10 -0500

    Template Only
    Former-commit-id: a9f271b1168fa658951dd91bd5f5518aca2a3e63

--- Sandbox/Medscape/automate_scraping_matching_medscape.py
+++ Sandbox/Medscape/automate_scraping_matching_medscape.py
@@ -1,7 +1,7 @@
 '''
 This script scrapes the Medscape In Memorium feature
 '''
-from datetime import datetime
+from datetime import datetime, date
 import pandas as pd
 from nameparser import HumanName
 #from medscape_scrape import scrape
@@ -10,15 +10,15 @@ import json
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import os
 
 #Set today
-TODAY = str(datetime.now()).split('.')[0].replace(' ', '_').replace(':', '')
-
+TODAY = str(date.today())
 #Set Output Directory
-OUT_DIRECTORY = 'U:/Source Files/Data Analytics/Data-Science/Data/Medscape/'
-
+OUT_DIRECTORY = f'C:/Users/nkhatri/OneDrive - American Medical Association/Documents/Task-Scheduled/{TODAY}'
+os.mkdir(OUT_DIRECTORY)
 #Define ppd
-PPD_FILE = 'U:/Source Files/Data Analytics/Data-Science/Data/PPD/ppd_data_20200404.csv'
+PPD_FILE = 'C:/Users/nkhatri/OneDrive - American Medical Association/Documents/Task-Scheduled/ppd_data_20200404.csv'
 print('Reading PPD...')
 PPD = pd.read_csv(PPD_FILE)
 
@@ -29,7 +29,7 @@ def scrape():
     soup = BeautifulSoup(response.content, 'html.parser')
     all_text = soup.text
 
-    with open(f'{OUT_DIRECTORY}Memorium_Text_{TODAY}.txt', 'w') as outfile:
+    with open(f'{OUT_DIRECTORY}/Memorium_Text_{TODAY}.txt', 'w') as outfile:
         json.dump(all_text, outfile)
 
     all_pars = soup.find_all('p')
@@ -125,8 +125,8 @@ print('Scraping...')
 DICT_LIST = scrape()
 ALL_DATA = pd.DataFrame(DICT_LIST)
 USA_DATA = ALL_DATA[ALL_DATA.COUNTRY == 'USA']
-ALL_DATA.to_csv(f'{OUT_DIRECTORY}Memorium_{TODAY}.csv', index=False)
-USA_DATA.to_csv(f'{OUT_DIRECTORY}Memorium_USA_{TODAY}.csv', index=False)
+ALL_DATA.to_csv(f'{OUT_DIRECTORY}/Memorium_{TODAY}.csv', index=False)
+USA_DATA.to_csv(f'{OUT_DIRECTORY}/Memorium_USA_{TODAY}.csv', index=False)
 
 def split_names(roster_df):
     '''Splits name column into components'''
@@ -185,5 +185,93 @@ def append_me(roster_df):
 
 print('Matching and appending ME numbers...')
 USA_DATA_SPLIT, USA_DATA_ME = append_me(USA_DATA)
-USA_DATA_ME.to_csv(f'{OUT_DIRECTORY}Memorium_USA_Physicians_{TODAY}.csv', index=False)
-USA_DATA_SPLIT.to_csv(f'{OUT_DIRECTORY}Memorium_USA_ME_{TODAY}.csv', index=False)
+USA_DATA_ME.to_csv(f'{OUT_DIRECTORY}/Memorium_USA_Physicians_{TODAY}.csv', index=False)
+USA_DATA_SPLIT.to_csv(f'{OUT_DIRECTORY}/Memorium_USA_ME_{TODAY}.csv', index=False)
+
+def newest(path):
+    '''Grabs newest filename'''
+    files = os.listdir(path)
+    paths = [os.path.join(path, basename) for basename in files if 'USA_ME' in basename]
+    return max(paths, key=os.path.getctime)
+
+RESULT_DIR = newest(OUT_DIRECTORY)
+print(RESULT_DIR)
+DATA = pd.read_csv(RESULT_DIR)
+
+def get_counts(dataframe):
+    '''GET COUNTS'''
+    nurse = 0
+    tech = 0
+    assistant = 0
+    admin = 0
+    other = 0
+    phys = 0
+    eight = 0
+    seven = 0
+    six = 0
+    five = 0
+    four = 0
+    three = 0
+    two = 0
+    unk = 0
+
+    for row in dataframe.itertuples():
+        if 'Nurse' in row.SPECIALTY:
+            nurse += 1
+        elif 'Tech' in row.SPECIALTY:
+            tech += 1
+        elif 'Assistant' in row.SPECIALTY:
+            assistant += 1
+        elif 'Admin' in row.SPECIALTY:
+            admin += 1
+        elif row.ME != 'None':
+            phys += 1
+        else:
+            other += 1
+        if row.AGE.isnumeric():
+            age = int(row.AGE)
+            if age >= 80:
+                eight += 1
+            elif age >= 70:
+                seven += 1
+            elif age >= 60:
+                six += 1
+            elif age >= 50:
+                five += 1
+            elif age >= 40:
+                four += 1
+            elif age >= 30:
+                three += 1
+            else:
+                two += 1
+        else:
+            unk += 1
+    role_df = pd.DataFrame({'Role':['Physician', 'Nurse', 'Technician', 'PA', 'Administration',
+                                    'Other'], 'Count':[phys, nurse, tech, assistant, admin, other]})
+    role_plt = role_df.plot.bar(x='Role', y='Count', rot=0,
+                                title='COVID-19 Healthcare Worker Fatalities by Role',
+                                color='darkorchid')
+    age_df = pd.DataFrame({'Age':['80 and older', '70-79', '60-69', '50-59', '40-49', '30-39',
+                                  'under 30', 'Unknown'], 'Count':[eight, seven, six, five, four,
+                                                                   three, two, unk]})
+    age_plt = age_df.plot.bar(x='Age', y='Count', rot=0,
+                              title='COVID-19 Healthcare Worker Fatalities by Age',
+                              color='darkorchid')
+    return(role_df, role_plt, age_df, age_plt)
+
+ROLE_DF, ROLE_PLT, AGE_DF, AGE_PLT = get_counts(DATA)
+STATE_DF = DATA.groupby('STATE').count()['NAME']
+STATE_PLT = STATE_DF.plot.bar(title='COVID-19 Healthcare Worker Fatalities by State',
+                              color='darkorchid')
+
+with pd.ExcelWriter(f'{OUT_DIRECTORY}/USA_Stats_{TODAY}.xlsx') as writer:
+    STATE_DF.to_excel(writer, sheet_name='By State')
+    ROLE_DF.to_excel(writer, sheet_name='By Role', index=False)
+    AGE_DF.to_excel(writer, sheet_name='By Age', index=False)
+
+FIG_1 = AGE_PLT.get_figure()
+FIG_1.savefig(f'{OUT_DIRECTORY}/AGE_{TODAY}.png')
+FIG_2 = ROLE_PLT.get_figure()
+FIG_2.savefig(f'{OUT_DIRECTORY}/ROLE_{TODAY}.png')
+FIG_3 = STATE_PLT.get_figure()
+FIG_3.savefig(f'{OUT_DIRECTORY}/STATE_{TODAY}.png')
