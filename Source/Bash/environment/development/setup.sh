e9e2355b0ad0124aeb05fb0584c14d9f57d195ef
plane <peter.lane@ama-assn.org>
2020-05-04 13:07:14 -0500

    Reorganize wipe-development-environment. Put low-level functions in setup.sh.
    Former-commit-id: bc020d97308082e0e0e4e2249e44f5b0588c4df0

--- Source/Bash/environment/development/setup.sh
+++ Source/Bash/environment/development/setup.sh
@@ -11,6 +11,16 @@ install_msodbcsql17_driver() {
 }
 
 
+remove_msodbcsql17_driver() {
+    apt remove -y msodbcsql17
+
+    rm /etc/apt/sources.list.d/mssql-release.list
+
+    keyid=$(apt-key list | grep -B 1 'Microsoft (Release signing) <gpgsecurity@microsoft.com>' | grep -v uid)
+    apt-key del $keyid
+}
+
+
 install_core_dependencies() {
     apt update
 
@@ -20,12 +30,15 @@ install_core_dependencies() {
 }
 
 
+remove_core_dependencies() {
+    apt remove -y software-properties-common build-essential curl
+}
+
+
 install_python_tools() {
     install_pip
 
     install_python3_7
-
-    configure_default_python3
 
     install_venv
 
@@ -37,7 +50,26 @@ install_python_tools() {
 }
 
 
+remove_python_tools() {
+    # remove_base_virtual_environments
+
+    remove_dev_libraries
+
+    # remove_pipenv
+
+    remove_venv
+
+    remove_python3_7
+
+    remove_pip
+}
+
+
 install_node() {
+    echo "Function 'remove_node not implemented'"
+
+    return 0
+
     curl -sL https://deb.nodesource.com/setup_10.x -o /tmp/nodesource_setup.sh
 
     bash /tmp/nodesource_setup.sh
@@ -48,6 +80,11 @@ install_node() {
 }
 
 
+remove_node() {
+    echo "Function 'remove_node not implemented'"
+}
+
+
 install_aws_tools() {
     install_aws_cli
 
@@ -55,10 +92,22 @@ install_aws_tools() {
 }
 
 
+remove_aws_tools() {
+    remove_aws_cli
+
+    remove_terraform
+}
+
+
 install_pip() {
     apt install -y python3-pip
 
     /usr/bin/python3.7 -m pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --upgrade pip
+}
+
+
+remove_pip() {
+    apt remove -y python3-pip
 }
 
 
@@ -70,13 +119,34 @@ install_python3_7() {
 }
 
 
+remove_python3_7() {
+    apt remove -y python3.7
+    apt -y autoremove
+
+    add-apt-repository -r -y ppa:deadsnakes/ppa
+
+    apt remove -y software-properties-common
+}
+
+
 install_venv() {
     apt install -y python3.7-venv
 }
 
 
+remove_venv() {
+    apt remove -y python3.6-venv
+    apt remove -y python3.7-venv
+}
+
+
 install_pipenv() {
     /usr/bin/python3.7 -m pip install pipenv
+}
+
+
+remove_pipenv() {
+    python3 -m pip uninstall -y pipenv
 }
 
 
@@ -86,15 +156,32 @@ install_dev_libraries() {
 }
 
 
+remove_dev_libraries() {
+    apt remove -y python3.7-dev unixodbc-dev
+}
+
+
 create_base_virtual_environment() {
     echo "### Setting up master Python virtual environment ###"
     setup_python_virtual_environment Environment/Master
 }
 
 
+remove_base_virtual_environments() {
+    rm -rf /home/lane/Development/Python3.6
+    rm -rf /home/lane/Development/Python3.7
+}
+
+
 install_aws_cli() {
     echo "### Installing AWS CLI ###"
     apt install -y awscli
+}
+
+
+remove_aws_cli() {
+    echo "### Removing AWS CLI ###"
+    apt remove -y awscli
 }
 
 
@@ -105,4 +192,10 @@ install_terraform() {
     apt update
 
     apt install -y terraform
+}
+
+
+remove_terraform() {
+    echo "### Removing Terraform ###"
+    apt remove -y terraform
 }
