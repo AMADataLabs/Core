a7832d88c9648c234ffc7481cf29952aef07d38f
plane <peter.lane@ama-assn.org>
2020-05-05 19:45:48 -0500

    Add missing variables TF.
    Former-commit-id: 0077718807a137cce4d0959461a0903e2e37acc6

---
+++ Source/Terraform/module/state/variables.tf
@@ -0,0 +1,10 @@
+variable "environment" {
+    description = "AWS Account Environment"
+    type        = string
+}
+
+
+variable "contact" {
+    description = "Email address of the Data Labs contact."
+    type        = string
+}
