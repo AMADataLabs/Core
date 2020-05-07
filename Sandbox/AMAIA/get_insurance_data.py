3fbdcf405b2d3047844129636aac339471199455
Victoria Grose <vigrose@ama-assn.org>
2020-05-07 15:00:26 -0500

    some triangulation, becker scrape work
    Former-commit-id: d4684e6dbd1b068fa3ef524a2e09624064ea6d7b

--- Sandbox/AMAIA/get_insurance_data.py
+++ Sandbox/AMAIA/get_insurance_data.py
@@ -1,45 +1,50 @@
 '''
 This script reads the latest insurance addresses from EDW
 '''
-import pyodbc
-import pandas as pd
+import settings
+from datalabs.access.edw import EDW
 
-CONNECTION = "DSN=PRDDW; UID={}; PWD={}".format(username, password)
-AMAEDW = pyodbc.connect(CONNECTION)
+def get_query():
+    '''Define insurance query'''
+    query = \
+        """
+        SELECT DISTINCT
+        A.PARTY_ID,
+        K.KEY_VAL AS ME,
+        A.POST_CD_ID,
+        A.FROM_DT,
+        C.PURPOSE_TYPE_DESC AS ADDR_TYPE,
+        P.ADDR_1,
+        P.ADDR_2,
+        P.ADDR_3,
+        P.CITY,
+        S.SRC_STATE_CD AS STATE_CD,
+        P.POST_CD AS ZIP
+        FROM
+        AMAEDW.PARTY_KEY K, AMAEDW.PARTY_ADDR A, AMAEDW.CONT_PURPOSE_TYPE C, AMAEDW.POST_CD P,
+        AMAEDW.STATE S
+        WHERE
+        A.THRU_DT IS NULL
+        AND
+        K.PARTY_ID = A.PARTY_ID
+        AND
+        K.KEY_TYPE_ID = 18
+        AND
+        K.ACTIVE_IND = 'Y'
+        AND
+        C.PURPOSE_TYPE_ID IN (1)
+        AND
+        A.POST_CD_ID = P.POST_CD_ID
+        AND
+        P.STATE_ID = S.STATE_ID
+        ORDER BY A.FROM_DT DESC;
+        """
+    return query
 
-AMAIA_SQL_QUERY = \
-    """
-    SELECT DISTINCT
-    A.PARTY_ID,
-    K.KEY_VAL AS ME,
-    A.POST_CD_ID,
-    A.FROM_DT,
-    C.PURPOSE_TYPE_DESC AS ADDR_TYPE,
-    P.ADDR_1,
-    P.ADDR_2,
-    P.ADDR_3,
-    P.CITY,
-    S.SRC_STATE_CD AS STATE_CD,
-    P.POST_CD AS ZIP
-    FROM
-    AMAEDW.PARTY_KEY K, AMAEDW.PARTY_ADDR A, AMAEDW.CONT_PURPOSE_TYPE C, AMAEDW.POST_CD P,
-    AMAEDW.STATE S
-    WHERE
-    A.THRU_DT IS NULL
-    AND
-    K.PARTY_ID = A.PARTY_ID
-    AND
-    K.KEY_TYPE_ID = 18
-    AND
-    K.ACTIVE_IND = 'Y'
-    AND
-    C.PURPOSE_TYPE_ID IN (1)
-    AND
-    A.POST_CD_ID = P.POST_CD_ID
-    AND
-    P.STATE_ID = S.STATE_ID
-    ORDER BY A.FROM_DT DESC;
-    """
-
-INSURANCE = pd.read_sql(con=AMAEDW, sql=AMAIA_SQL_QUERY)
-INSURANCE = INSURANCE.drop_duplicates(subset='PARTY_ID', keep='first')
+def get_insurance():
+    '''Get insurance data from EDW'''
+    amaia_query = get_query()
+    with EDW() as edw:
+        insurance = edw.read(amaia_query)
+    insurance = insurance.drop_duplicates(subset='PARTY_ID', keep='first')
+    return insurance
---
+++ Sandbox/AddressQualityTriangulation/reverse_phone_lookup.py
@@ -0,0 +1,126 @@
+'''This script can be run to call the GetPhoneInfo API on a list of phone numbers'''
+import os
+import json
+import pandas as pd
+import requests
+import xmltodict
+import settings
+
+#Reverse lookup a single phone number:
+def reverse_lookup(phone_number, apikey):
+    '''Lookup one phone number'''
+
+    #Create an empty dictionary:
+    info_dict = {}
+
+    #Call api with given parameters and save xml as dictionary:
+    base_url = "https://ws.serviceobjects.com/gppl2/api.svc/GetPhoneInfo"
+    parameters = {'PhoneNumber': phone_number, 'LicenseKey': apikey}
+    response =  requests.get(base_url, params=parameters)
+    results = xmltodict.parse(response.content)
+
+    #Select phone info section of resulting dictionary:
+    phone_results = results["PhoneInfoResponse"]["PhoneInfo"]
+
+    #Save phone number:
+    info_dict['Phone_Number'] = phone_number
+
+    #Save relevant values in results in smaller dictionary. Save null value if errors:
+    try:
+        info_dict["Name"] = phone_results["Contacts"]['Contact']["Name"]
+    except:
+        info_dict["Name"] = 'N/A'
+    try:
+        info_dict["Address"] = phone_results["Contacts"]['Contact']["Address"]
+    except:
+        info_dict["Address"] = 'None'
+    try:
+        info_dict["City"] = phone_results["Contacts"]['Contact']["City"]
+    except:
+        info_dict["City"] = 'N/A'
+    try:
+        info_dict["State"] = phone_results["Contacts"]['Contact']["State"]
+    except:
+        info_dict["State"] = 'N/A'
+    try:
+        info_dict["PhoneType"] = phone_results["Contacts"]['Contact']["PhoneType"]
+    except:
+        info_dict["PhoneType"] = 'N/A'
+    try:
+        info_dict["QualityScore"] = phone_results["Contacts"]['Contact']["QualityScore"]
+    except:
+        info_dict["QualityScore"] = 'N/A'
+    try:
+        info_dict["Date"] = phone_results["DateOfPorting"]
+    except:
+        info_dict["Date"] = 'none'
+    try:
+        info_dict["Notes"] = phone_results["NoteDescriptions"]
+    except:
+        info_dict["Notes"] = 'None'
+    try:
+        info_dict["Provider"] = phone_results['Provider']['Name']
+    except:
+        info_dict["Provider"] = 'N/A'
+    try:
+        info_dict["SICCode"] = phone_results["Contacts"]['Contact']['SICCode']
+    except:
+        info_dict["SICCode"] = 'None'
+    try:
+        info_dict["SICDesc"] = phone_results["Contacts"]['Contact']['SICDesc']
+    except:
+        info_dict["SICDesc"] = 'None'
+
+    #Return resulting dictionary and full dictionary:
+    return(info_dict, results)
+
+
+#Test all number in a dataframe:
+def test_numbers(phone_list, apikey):
+    '''Many numbers'''
+
+    #Create two empty lists:
+    results_dict_list = []
+    fun_massive_list = []
+
+    #Iterate through dataframe and call reverse_lookup on each number:
+    for phone in phone_list:
+        new_dict = {}
+        try:
+            new_dict, phone_results = reverse_lookup(phone, apikey)
+            results_dict_list.append(new_dict)
+            fun_massive_list.append(phone_results)
+        except:
+            pass
+
+    #Save lists of dictionaries as dataframes:
+    new_df = pd.DataFrame(results_dict_list)
+
+    #Return dataframes:
+    return(fun_massive_list, new_df)
+
+#Export results to files:
+def export_results(phone_csv, xml_location, df_location, phone_column):
+    '''Run and save results'''
+    #Input variables from command line:
+    key = os.environ.get('GETPHONE_KEY')
+
+    #Create dataframe from csv:
+    phone_df = pd.DataFrame(phone_csv)
+    phones = list(phone_df[phone_column])
+
+    #Call test_numbers on dataframe:
+    fun_massive_list, final_df = test_numbers(phones, key)
+
+    #Export results:
+    final_df.to_csv(df_location)
+    try:
+        with open(f'{xml_location}_data.txt', 'w') as outfile:
+            json.dump(fun_massive_list, outfile)
+    except:
+        pass
+
+    print("Files saved")
+
+# if __name__ == "__main__":
+#     export_results()
