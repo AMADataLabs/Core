0c182e7fd87956985c6e40ed43bcc2b6bd025d84
plane <peter.lane@ama-assn.org>
2020-05-08 10:57:49 -0500

    Incremental cleaning of data_quality_check.py.
    Former-commit-id: 6a078e38222cf26832f31fd78965aa344322c62e

--- Sandbox/Clean/data_quality_check.py
+++ Sandbox/Clean/data_quality_check.py
@@ -1,120 +1,74 @@
-import os
+""" Check the quality of disciplinary action data. """
+# CLEAN CODE NOTE: Core Python imports go first in alphabetical order
 import csv
-import PyPDF2
-from PyPDF2 import PdfFileWriter, PdfFileReader
+from   dataclasses import dataclass
 import datetime
-from time import strftime
 import glob
+import json
+import os
 import re
+from   time import strftime
+
+# CLEAN CODE NOTE: External dependencies go next.
+#   Make sure these get added to the master dependencies list (Build/Master/requirements.txt).
+from PyPDF2 import PdfFileReader
+
+# CLEAN CODE NOTE: Local/repo imports go last.
+import settings
+
+
+# CLEAN CODE NOTE: To Do lists are fine for branch code, but they should be removed when merging to master. If you are not
+#   done, create stories for the required work or keep track of it in the comments of the current story.
 
 # TODO:
 # 1. function check name format 3.5
 # 2. get statistic data for weekly report and get PowerBI Dashboard
 
-# Part 1: hard coding
+# CLEAN CODE NOTE: Avoid global variables if at all possible. If you must, use the PEP8 standard of all-caps names.
+#   This makes it clear in the code that a variable is global and can be altered by ANY function or method.
+
+# CLEAN CODE NOTE: Never hard-code paths. Use the repo standard of settings module plus .env. 
+
 # path of the directory of results
-#directory = '/Users/elaineyao/Desktop/QAtest/'
-directory = os.environ.get('INPUT_DATA_DIRECTORY')
+DIRECTORY = os.environ.get('INPUT_DATA_DIRECTORY')
 
-count_log_path = os.environ.get('COUNT_LOG')
-file_log_path  = os.environ.get('FILE_LOG')
+COUNT_LOG_PATH = os.environ.get('COUNT_LOG')
+FILE_LOG_PATH = os.environ.get('FILE_LOG')
 
-date = str(datetime.datetime.now().date())
+DATE = str(datetime.datetime.now().date())
+
+# CLEAN CODE NOTE: Large data definitions should be treated as such and put in a data file. Python natively supports
+#   JSON, so don't be afraid of creating such data as JSON files.
+
+DOC_CHECK_DICT = json.load(os.environ.get('TRACKED_FILES'))
 
 
-# dictionary of each folder shouldhave what files:
-doc_check_dic = {'SL&BO': ['CA_MD_SummaryList',
-                           'FL_MD_SummaryList',
-                           'FL_DO_SummaryList',
-                           'OR_SummaryList',
-                           'VA_SummaryList',
-                           'IN_SummaryList',
-                           'WV_MD_SummaryList',
-                           'WV_DO_SummaryList',
-                           'MO_SummaryList',
-                           'MA_SummaryList',
-                           'KY_SummaryList',
-                           'DEA_SummaryList',
-                           'MD_SummaryList',
-                           'KS_SummaryList',
-                           'AL_SummaryList',
-                           'NY_SummaryList',
-                           'AZ_DO_SummaryList',
-                           'AZ_MD_SummaryList',
-                           'CO_SummaryList',
-                           'CT_SummaryList',
-                           'DE_SummaryList',
-                           'GA_SummaryList',
-                           'HI_SummaryList',
-                           'IA_SummaryList',
-                           'ID_SummaryList',
-                           'LA_SummaryList',
-                           'MS_SummaryList',
-                           'NC_SummaryList',
-                           'NH_SummaryList',
-                           'OH_SummaryList',
-                           'RI_SummaryList',
-                           'SD_SummaryList',
-                           'WI_SummaryList',
-                           'NV_DO_SummaryList',
-                           'NV_MD_SummaryList',
-                           'ME_MD_SummaryList',
-                           'ME_DO_SummaryList',
-                           'VT_MD_SummaryList',
-                           'VT_DO_SummaryList',
-                           'NJ_SummaryList',
-                           'DC_SummaryList',
-                           'ND_SummaryList',
-                           'NM_MD_SummaryList',
-                           'NM_DO_SummaryList',
-                           'MN_SummaryList'],
-                 'SL': ['CA_DO_SummaryList',
-                        'AR_SummaryList',
-                        'DHHS_SummaryList',
-                        'AK_SummaryList',
-                        'OK_DO_SummaryList',
-                        'WY_SummaryList'],
-                 'NL&BO': ['OR_Newsletter',
-                           'MI_MD_Newsletter',
-                           'MI_DO_Newsletter',
-                           'CT_Newsletter',
-                           'IL_Newsletter',
-                           'OK_MD_Newsletter'],
-                 'NL': ['CA_MD_Newsletter',
-                        'WV_MD_Newsletter',
-                        'WV_DO_Newsletter',
-                        'KY_Newsletter',
-                        'AL_Newsletter',
-                        'PA_MD_Newsletter',
-                        'PA_DO_Newsletter',
-                        'UT_MD_Newsletter',
-                        'UT_DO_Newsletter',
-                        'TX_Newsletter',
-                        'NV_MD_Newsletter']}
+# CLEAN CODE NOTE: Used a dataclass or collections.namedtuple for grouping related data.
+
+@dataclass
+class FailureCounts:
+    total: int = 0
+    name_format: int = 0
+    completeness: int = 0
+    file_exists: int = 0
+    file_quality: int = 0
+    no_data_pdf: int = 0
+    no_data_duplicate: int = 0
 
 
 def main():
-    # Part 3: workflow:
-    # parameters for weekly report
-    failcount                   = 0
-    failcount_nameformat        = 0
-    failcount_completeness      = 0
-    failcount_fileexist         = 0
-    failcount_filequality       = 0
-    failcount_nodatapdf         = 0
-    failcount_nodataduplicate   = 0
+    failure_counts = FailureCounts()
 
+    modtime_unix = os.path.getmtime(DIRECTORY)
 
-    modtime_unix = os.path.getmtime(directory)
-
-    # Step 0: check if there is new result in directory, determine whether it is new procurement or re-baseline procurement
-    if not check_delivery(directory):
-        # modtime_unix = os.path.getmtime(directory)
+    # Step 0: check if there is new result in DIRECTORY, determine whether it is new procurement or re-baseline procurement
+    if not check_delivery(DIRECTORY):
+        # modtime_unix = os.path.getmtime(DIRECTORY)
         moddate = datetime.datetime.utcfromtimestamp(modtime_unix).strftime('%Y-%m-%d')
         print(f'no new folders uploaded, latest folder uploaded at {moddate}')
     else:
         # have new result updated, get path of new result and list of contents
-        path, folders = get_latest_folder_path(directory)
+        path, folders = get_latest_folder_path(DIRECTORY)
 
         print(f'new folders uploaded: {path}')
 
@@ -124,10 +78,10 @@ def main():
             # Step 1: Validate Completeness:
             if not ValidateCompleteness(path):
 
-                failcount_completeness += 1
+                failure_counts.completeness += 1
             else:  # ValidateCompleteness is True
                 if not check_files_exist(path):  # no_data folder is deleted in this function
-                    failcount_fileexist += 1
+                    failure_counts.file_exists += 1
                 else:  # check_files_exist is True
                     print('folders are correct, next step: check mandatory files contents in the folder')
 
@@ -140,39 +94,39 @@ def main():
                             # get the type of file: BO, SL, NL, QA, M (json)
                             type = get_doc_type(file)
                             # check name format:
-                            if not check_file_nameformat(file, type):
+                            if not check_file_name_format(file, type):
                                 print(f'File Format is not right: {file} as type {type}')
-                                failcount_nameformat += 1
-                                log_file_failure(file_log_path=file_log_path,
+                                failure_counts.name_format += 1
+                                log_file_failure(FILE_LOG_PATH=FILE_LOG_PATH,
                                                  date=date,
                                                  file=file,
-                                                 failure_type='nameformat - file type')
+                                                 failure_type='name_format - file type')
 
                             # check csv file:
                             if type == 'QA':
                                 if not check_qa_file(fullpath):
                                     print(f'QA file {file} is not right')
-                                    failcount_filequality += 1
-                                    log_file_failure(file_log_path=file_log_path,
+                                    failure_counts.file_quality += 1
+                                    log_file_failure(FILE_LOG_PATH=FILE_LOG_PATH,
                                                      date=date,
                                                      file=file,
-                                                     failure_type='filequality - QA')
+                                                     failure_type='file_quality - QA')
                             # check board orders file:
                             elif type == 'BO' or type == 'SL' or type == 'NL':
                                 if not check_bo_pdf(fullpath):
                                     print(f'PDF file {file} has blank page')
-                                    failcount_filequality += 1
-                                    log_file_failure(file_log_path=file_log_path,
+                                    failure_counts.file_quality += 1
+                                    log_file_failure(FILE_LOG_PATH=FILE_LOG_PATH,
                                                      date=date,
                                                      file=file,
-                                                     failure_type='filequality - blank page')
+                                                     failure_type='file_quality - blank page')
                                 #else:
                                 #    print('')
 
 
         else:
             print('This folder is re-baselined folder')
-            #updatefolders = [f for f in os.listdir(directory + 'results_04_08_2020_09_10PM') if not f.startswith('.')]
+            #updatefolders = [f for f in os.listdir(DIRECTORY + 'results_04_08_2020_09_10PM') if not f.startswith('.')]
             #updatefolders = [f for f in os.listdir(path) if not f.startswith('.')]
             validupdatefolders = [f for f in folders if f != 'no_data']
             for fold in validupdatefolders:
@@ -182,42 +136,44 @@ def main():
                     type = get_doc_type(file)
 
                     # check name format:
-                    if check_file_nameformat(file, type):
+                    if check_file_name_format(file, type):
                         print('File format is right.')
                     else:
                         print(f'File Format is not right: {file}')
-                        failcount_nameformat += 1
-                        log_file_failure(file_log_path=file_log_path,
+                        failure_counts.name_format += 1
+                        log_file_failure(FILE_LOG_PATH=FILE_LOG_PATH,
                                          date=date,
                                          file=file,
-                                         failure_type='nameformat')
+                                         failure_type='name_format')
 
                     # check csv file:
                     if type == 'QA':
                         # if not check_qa_file(fullpath):  # define the function to check qa csv file
                         if not check_qa_file(fullpath):
                             print(f'QA file {file} is not right')
-                            failcount_filequality += 1
-                            log_file_failure(file_log_path=file_log_path,
+                            failure_counts.file_quality += 1
+                            log_file_failure(FILE_LOG_PATH=FILE_LOG_PATH,
                                              date=date,
                                              file=file,
-                                             failure_type='filequality - QA')
+                                             failure_type='file_quality - QA')
                     # check board orders file:
                     elif type == 'BO':
                         if not check_bo_pdf(fullpath):  # define the function to check board orders
                             print(f'BO file {file} is not right')
-                            failcount_filequality += 1
-                            log_file_failure(file_log_path=file_log_path,
+                            failure_counts.file_quality += 1
+                            log_file_failure(FILE_LOG_PATH=FILE_LOG_PATH,
                                              date=date,
                                              file=file,
-                                             failure_type='filequality - BO')
+                                             failure_type='file_quality - BO')
 
 
-    with open(count_log_path, 'a') as count_log_file:
-        count_log_file.write('\n{},{},{},{},{},{}, {}'.format(date, failcount,
-                                                              failcount_nameformat, failcount_completeness,
-                                                              failcount_fileexist, failcount_filequality,
-                                                              failcount_nodatapdf, failcount_nodataduplicate))
+    with open(COUNT_LOG_PATH, 'a') as count_log_file:
+        count_log_file.write(
+            '\n{},{},{},{},{},{}, {}'.format(
+                date, failcount, failure_counts.name_format, failure_counts.completeness, failure_counts.file_exists,
+                failure_counts.file_quality, failure_counts.no_data_pdf, failure_counts.no_data_duplicate
+            )
+        )
 
 
 # Part 2: User Define Functions
@@ -286,7 +242,7 @@ def check_in_udrive(path, UdrivePath) -> bool:
 
 
 # 3.5 check file format: Agency_Date-Time_SL/NL.pdf, Agency-Physician_Name-Date_Time_BO.pdf
-def check_file_nameformat(filename, type) -> bool:
+def check_file_name_format(filename, type) -> bool:
     if type == 'SL':
         pattern = r'[A-Z_]*_[0-9]*-[0-9]*-[0-9]*_SL.pdf'
         if re.match(pattern, filename) is not None:
@@ -340,22 +296,22 @@ def check_files_exist(path) -> bool:
                 type = i.split('.')[0].rsplit('_', 2)[-2]
             filetype.append(type)
         # check if all necessary files are in the folder:
-        if keyvalue in doc_check_dic['SL&BO']:
+        if keyvalue in DOC_CHECK_DICT['SL&BO']:
             if set(filetype) == {'M', 'QA', 'SL', 'BO'}:
                 return True
             else:
                 print(f'only have {filetype}')
-        elif keyvalue in doc_check_dic['SL']:
+        elif keyvalue in DOC_CHECK_DICT['SL']:
             if set(filetype) == {'M', 'QA', 'SL'}:
                 return True
             else:
                 print(f'only have {filetype}')
-        elif keyvalue in doc_check_dic['NL&BO']:
+        elif keyvalue in DOC_CHECK_DICT['NL&BO']:
             if set(filetype) == {'M', 'QA', 'NL', 'BO'}:
                 return True
             else:
                 print(f'only have {filetype}')
-        elif keyvalue in doc_check_dic['NL']:
+        elif keyvalue in DOC_CHECK_DICT['NL']:
             if set(filetype) == {'M', 'QA', 'NL'}:
                 return True
             else:
@@ -397,8 +353,8 @@ def get_doc_type(filename) -> str:
     return type
 
 
-def log_file_failure(file_log_path, date, file, failure_type):
-    with open(file_log_path, 'a') as f:
+def log_file_failure(FILE_LOG_PATH, date, file, failure_type):
+    with open(FILE_LOG_PATH, 'a') as f:
         f.write(f'\n{date}, {file}, {failure_type}')
 
 
