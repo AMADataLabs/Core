bc56340f1abc9dd09db39c47b9dacce61e7322b0
Nikhil Khatri <nikhil.khatri@ama-assn.org>
2020-05-15 15:37:44 -0500

    tf.apply has been done. Code is complete.
    Former-commit-id: 65cb85f7a9f8ea4bf77b77467b3a5b85f410486f

--- Source/Terraform/DataLake/main.tf
+++ Source/Terraform/DataLake/main.tf
@@ -25,6 +25,29 @@ resource "aws_s3_bucket" "datalake_ingestion_bucket" {
     }
 }
 
+resource "aws_s3_bucket" "datalake_processed_bucket" {
+    bucket = local.processed_bucket
+
+    lifecycle {
+        prevent_destroy = true
+    }
+
+    tags = {
+        Name = "Data Labs Data Lake Processed Bucket"
+        Env                 = var.environment
+        Contact             = var.contact
+        SystemTier          = local.system_tier
+        DRTier              = local.na
+        DataClassification  = local.na
+        BudgetCode          = local.budget_code
+        Owner               = local.owner
+        Notes               = local.notes
+        OS                  = local.na
+        EOL                 = local.na
+        MaintenanceWindow   = local.na
+    }
+}
+
 
 resource "aws_s3_bucket_public_access_block" "datalake_ingestion_bucket_public_access_block" {
     bucket = local.ingestion_bucket
@@ -35,6 +58,14 @@ resource "aws_s3_bucket_public_access_block" "datalake_ingestion_bucket_public_a
     restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_public_access_block" "datalake_processed_bucket_public_access_block" {
+    bucket = local.processed_bucket
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+}
 
 variable "environment" {
     description = "AWS Account Environment"
