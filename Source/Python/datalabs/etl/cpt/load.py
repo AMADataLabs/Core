47b066cfec879aab4be74ebc634b6021d1f89dbf
plane <peter.lane@ama-assn.org>
2020-05-26 17:07:31 -0500

    Reorder consumer and clinician descriptor in ETL processing. Add skeleton ConvertCPT ETL loader.
    Former-commit-id: 64fee6f9f89b65586bac1519ba601e5bbee75bd5

--- Source/Python/datalabs/etl/cpt/load.py
+++ Source/Python/datalabs/etl/cpt/load.py
@@ -1,1 +1,20 @@
 """ CPT ETL Loader classes """
+from   dataclasses import dataclass
+import io
+import logging
+
+import pandas
+
+from   datalabs.plugin import import_plugin
+from   datalabs.etl.load import Loader
+import datalabs.etl.cpt.dbmodel as model
+import datalabs.etl.cpt.transform as transform
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+
+class CPTRelationalTableLoader(Loader):
+    def load(self, data):
+        pass
