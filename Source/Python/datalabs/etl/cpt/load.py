ef16bbb15cf5b8d6e9c86f3f4d5c726b0119d566
plane <peter.lane@ama-assn.org>
2020-05-28 21:08:11 -0500

    Add loading of CPT codes, descriptors, and consumer descriptors.
    Former-commit-id: afc4bfdf913bbfb5d77efe2127e8b764be7578dc

--- Source/Python/datalabs/etl/cpt/load.py
+++ Source/Python/datalabs/etl/cpt/load.py
@@ -1,15 +1,10 @@
 """ CPT ETL Loader classes """
 from   dataclasses import dataclass
-import io
 import logging
 
-import pandas
-import sqlalchemy
+# import pandas
 
-from datalabs.access.credentials import Credentials
-from datalabs.access.database import Configuration
 from datalabs.access.orm import Database
-from   datalabs.plugin import import_plugin
 from   datalabs.etl.load import Loader
 import datalabs.etl.cpt.dbmodel as model
 import datalabs.etl.cpt.transform as transform
@@ -19,20 +14,26 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
 
 
+@dataclass
+class IDs:
+    old: list
+    new: list
+
+
 class CPTRelationalTableLoader(Loader):
     def __init__(self, configuration):
         super().__init__(configuration)
 
     def load(self, data: transform.OutputData):
-        with Database(key='ORM') as database:
+        with Database(key=self._configuration['DATABASE']) as database:
            self._session = database.session
 
-            self._update_tables(data)
+           self._update_tables(data)
 
-    def _update_tables(self, session, data: transform.OutputData)
-            self._update_codes(data.code)
+    def _update_tables(self, data: transform.OutputData):
+            codes = self._update_codes(data.code)
 
-            self._update_short_descriptors(data.short_descriptor)
+            self._update_short_descriptors(codes, data.short_descriptor)
 
             self._update_medium_descriptors(data.medium_descriptor)
 
@@ -44,9 +45,52 @@ class CPTRelationalTableLoader(Loader):
 
             self._update_consumer_descriptors(data.consumer_descriptor)
 
-            self._update_clinician_descriptors(data.clinician_descriptor)
+            # self._update_clinician_descriptors(data.clinician_descriptor)
 
-            self._update_clinician_descriptor_code_mappings(data.clinician_descriptor_code_mapping)
+            # self._update_clinician_descriptor_code_mappings(data.clinician_descriptor_code_mapping)
 
-    def _update_codes(self, table_data):
-        current_codes = self._session.query(model.Code).all()
+            self._session.commit()
+
+    def _update_codes(self, codes):
+        query = self._session.query(model.Code)
+        current_code_rows = {row.code:row for row in query.all()}
+        old_codes = [code for code in codes.code if code in current_code_rows]
+        new_codes = [code for code in codes.code if code not in current_code_rows]
+
+        for code in new_codes:
+            self._session.add(model.Code(code=code))
+
+        return IDs(old_codes, new_codes)
+
+    def _update_short_descriptors(self, codes, descriptors):
+        self._update_descriptors(model.ShortDescriptor, codes, descriptors)
+
+    def _update_medium_descriptors(self, codes, descriptors):
+        self._update_descriptors(model.MediumDescriptor, codes, descriptors)
+
+    def _update_long_descriptors(self, codes, descriptors):
+        self._update_descriptors(model.LongDescriptor, codes, descriptors)
+
+    def _update_modifier_types(self, modifier_types):
+        pass
+
+    def _update_modifiers(self, modifiers):
+        pass
+
+    def _update_consumer_descriptors(self, codes, descriptors):
+        self._update_descriptors(model.ConsumerDescriptor, codes, descriptors)
+
+    def _update_descriptors(self, model_class, codes, descriptors):
+        query = self._session.query(model_class)
+        current_descriptor_rows = {row.code:row for row in query.all()}
+
+        for code in codes.old:
+            descriptor = descriptors.descriptor[descriptors.code == code].iloc[0]
+
+            if current_descriptor_rows[code] != descriptor:
+                current_descriptor_rows[code].descriptor = descriptor
+
+        for code in codes.new:
+            descriptor = descriptors.descriptor[descriptors.code == code].iloc[0]
+
+            self._session.add(model_class(code=code, descriptor=descriptor))
