7e91a2fad57622e17e5e8b641ef24516f841bc0b
plane <peter.lane@ama-assn.org>
2020-06-12 16:14:00 -0500

    Finish generation of release_type table data.
    Former-commit-id: 845ec4b97357b8fa344120e81d8b8ef76a23fa74

--- Source/Python/datalabs/etl/cpt/extract.py
+++ Source/Python/datalabs/etl/cpt/extract.py
@@ -1,5 +1,7 @@
 """ Extractor class for CPT standard release text data from the S3 ingestion bucket. """
-from datetime import datetime
+from   datetime import date, datetime
+import json
+
 import pandas
 
 from datalabs.etl.s3.extract import S3WindowsTextExtractor
@@ -8,27 +10,44 @@ from datalabs.etl.s3.extract import S3WindowsTextExtractor
 class CPTTextDataExtractor(S3WindowsTextExtractor):
     def extract(self):
         data = super().extract()
-        release_date = self._get_release_date()
+        release_date = self._extract_release_date()
+        release_schedule = json.loads(self._configuration['RELEASE_SCHEDULE'])
 
-        data.append(self._extract_release_type(release_date))
-        data.append(self._extract_release_details(release_date))
+        data.append(self._generate_release_types(release_schedule))
+        data.append(self._generate_release_details(release_schedule, release_date))
 
         return data
 
-    def _get_release_date(self):
+    def _extract_release_date(self):
         latest_release_path = self._get_latest_path()
         release_datestamp = latest_release_path.rsplit('/', 1)[1]
 
         return datetime.strptime(release_datestamp, '%Y%m%d').date()
 
-    def _extract_release_type(self):
-        release_schedule = self._configuration['RELEASE_SCHEDULE']
+    def _generate_release_types(self, release_schedule):
+        release_types = list(release_schedule.keys())
 
+        release_types.append('OTHER')
+
+        return pandas.DataFrame(dict(type=release_types))
+
+    def _generate_release_details(self, release_schedule, release_date):
+        release_type = self._get_release_type(release_schedule, release_date)
+
+        return pandas.DataFrame()
+
+
+    def _get_release_type(self, release_schedule, release_date):
+        release_date_for_lookup = date(1900, release_date.month, release_date.day)
+        release_types = self._map_release_dates_to_types(release_schedule)
+
+        return release_types.get(release_date_for_lookup, 'OTHER')
+
+    def _map_release_dates_to_types(self, release_schedule):
         for release_type in release_schedule:
             release_schedule[release_type] = self._convert_datestamps_to_dates(release_schedule[release_type])
 
-    def _extract_release_details(self):
-        pass
+        return {dates[0]:type for type, dates in release_schedule.items()}
 
     @classmethod
     def _convert_datestamps_to_dates(cls, datestamps):
--- Test/Python/test/datalabs/etl/cpt/test_extract.py
+++ Test/Python/test/datalabs/etl/cpt/test_extract.py
@@ -1,8 +1,10 @@
 """ source: datalabs.etl.cpt.extract """
 from datetime import date
+import json
 import logging
 
 import mock
+import pytest
 
 from   datalabs.etl.cpt.extract import CPTTextDataExtractor
 
@@ -21,11 +23,40 @@ def test_datestamp_conversion():
 
 
 @mock.patch('datalabs.etl.cpt.extract.CPTTextDataExtractor._get_latest_path')
-def test_get_release_date(get_latest_path):
+def test_extract_release_date(get_latest_path):
     get_latest_path.return_value = 'AMA/CPT/20200131'
     extractor = CPTTextDataExtractor(None)
     expected_release_date = date(2020, 1, 31)
-    release_date = extractor._get_release_date()
+    release_date = extractor._extract_release_date()
 
     assert get_latest_path.call_count == 1
     assert release_date == expected_release_date
+
+def test_generate_release_types(release_schedule):
+    extractor = CPTTextDataExtractor(dict(RELEASE_SCHEDULE=json.dumps(release_schedule)))
+    release_types = extractor._generate_release_types(release_schedule)
+
+    assert len(release_types.columns.values) == 1
+    assert 'type' in release_types
+    assert len(release_types) == 6
+    assert release_types.type.to_list() == ['ANNUAL', 'Q1', 'Q2', 'Q3', 'Q4', 'OTHER']
+
+def test_get_release_type(release_schedule):
+    extractor = CPTTextDataExtractor(dict(RELEASE_SCHEDULE=json.dumps(release_schedule)))
+    release_date = date(2020, 7, 1)
+    expected_release_type = 'Q3'
+    release_type = extractor._get_release_type(release_schedule, release_date)
+
+    assert release_type == expected_release_type
+
+
+
+@pytest.fixture
+def release_schedule():
+    return {
+        "ANNUAL": ["1-Sep", "1-Jan"],
+        "Q1": ["1-Jan", "1-Apr"],
+        "Q2": ["1-Apr", "1-Jul"],
+        "Q3": ["1-Jul", "1-Oct"],
+        "Q4": ["1-Oct", "1-Jan"]
+    }
