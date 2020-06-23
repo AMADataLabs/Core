9cce56b728a0c8a5fbbcfa03eb652206cdc86bb1
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-06-23 12:18:56 -0500

    "Updated consumer endpoints"
    Former-commit-id: 6a8c153fa079b193040ff4d830c72dda08afdd4a

---
+++ Source/Python/datalabs/access/cpt/api/consumer_descriptor_code.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datalabs.etl.cpt.dbmodel import ConsumerDescriptor
+from datalabs.access.database import Database
+import json
+
+
+def lambda_handler(event, context):
+    session = create_database_connection()
+
+    query = query_for_descriptor(session, event.get('pathParameters', None))
+    status_code, response = get_content_from_query_output(query)
+
+    return {'statusCode': status_code, 'body': json.dumps(response)}
+
+
+def create_database_connection():
+    engine = create_engine(Database.url)
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+
+def query_for_descriptor(session, path_parameter):
+    query = None
+
+    if path_parameter is not None:
+        query = session.query(ConsumerDescriptor)
+        query = query.filter(ConsumerDescriptor.code == path_parameter['code'])
+
+    return query
+
+
+def get_content_from_query_output(query):
+    if query is not None and query.count() != 0:
+        rows = []
+        for row in query.all():
+            record = {
+                'code': row.code,
+                'description': row.descriptor
+            }
+            rows.append(record)
+        status_code = 200
+
+    else:
+        status_code = 404
+        rows = {"Code": "Invalid"}
+
+    return status_code, rows
