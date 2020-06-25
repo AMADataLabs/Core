63d29e437b7e5c432fbd8901a40774de6ba37ee5
plane <peter.lane@ama-assn.org>
2020-06-25 16:11:14 -0500

    Initial modifications for adding the CPT API Gateway resource on AWS Sandbox.
    Former-commit-id: 61c5859d54b32681ff1fa06159fc04a4cf464670

--- Source/Terraform/CPT/main.tf
+++ Source/Terraform/CPT/main.tf
@@ -20,7 +20,7 @@ resource "aws_db_instance" "cpt_api_database" {
     password                      = data.aws_ssm_parameter.database_password.value
 
     tags = {
-        Name = "Data Labs Datalake Terraform State Bucket"
+        Name = "CPT API Database"
         Env                 = data.aws_ssm_parameter.account_environment.value
         Contact             = data.aws_ssm_parameter.contact.value
         SystemTier          = local.system_tier
@@ -53,42 +53,108 @@ resource "aws_iam_role" "cpt_lambda_role" {
     ]
 }
 EOF
-}
 
-
-resource "aws_lambda_function" "convert_cpt_etl" {
-    filename        = "../../../Build/CPT/app.zip"
-    function_name   = "ConvertCPT"
-    role            = aws_iam_role.cpt_lambda_role.arn
-    handler         = "datalabs.etl.run.lambda_handler"
-    runtime         = "python3.7"
-    environment {
-        variables = {
-            ETL_CONVERTCPT_LAMBDA_FUNCTION=data.aws_ssm_parameter.ETL_CONVERTCPT_LAMBDA_FUNCTION.value
-            ETL_CONVERTCPT_APP=data.aws_ssm_parameter.ETL_CONVERTCPT_APP.value
-            ETL_CONVERTCPT_EXTRACTOR=data.aws_ssm_parameter.ETL_CONVERTCPT_EXTRACTOR.value
-            ETL_CONVERTCPT_EXTRACTOR_BUCKET=data.aws_ssm_parameter.ETL_CONVERTCPT_EXTRACTOR_BUCKET.value
-            ETL_CONVERTCPT_EXTRACTOR_BASE_PATH=data.aws_ssm_parameter.ETL_CONVERTCPT_EXTRACTOR_BASE_PATH.value
-            ETL_CONVERTCPT_EXTRACTOR_FILES=data.aws_ssm_parameter.ETL_CONVERTCPT_EXTRACTOR_FILES.value
-            ETL_CONVERTCPT_TRANSFORMER=data.aws_ssm_parameter.ETL_CONVERTCPT_TRANSFORMER.value
-            ETL_CONVERTCPT_TRANSFORMER_PARSERS=data.aws_ssm_parameter.ETL_CONVERTCPT_TRANSFORMER_PARSERS.value
-            ETL_CONVERTCPT_LOADER=data.aws_ssm_parameter.ETL_CONVERTCPT_LOADER.value
-            ETL_CONVERTCPT_LOADER_BUCKET=data.aws_ssm_parameter.ETL_CONVERTCPT_LOADER_BUCKET.value
-            ETL_CONVERTCPT_LOADER_FILES=data.aws_ssm_parameter.ETL_CONVERTCPT_LOADER_FILES.value
-            ETL_CONVERTCPT_LOADER_BASE_PATH=data.aws_ssm_parameter.ETL_CONVERTCPT_LOADER_BASE_PATH.value
-
-            # TODO: These need to be moved to the RDS loading ETL Lambda resource
-            ETL_LOADCPT_LAMBDA_FUNCTION=data.aws_ssm_parameter.ETL_LOADCPT_LAMBDA_FUNCTION.value
-            ETL_LOADCPT_APP=data.aws_ssm_parameter.ETL_LOADCPT_APP.value
-            ETL_LOADCPT_EXTRACTOR=data.aws_ssm_parameter.ETL_LOADCPT_EXTRACTOR.value
-            ETL_LOADCPT_EXTRACTOR_BUCKET=data.aws_ssm_parameter.ETL_LOADCPT_EXTRACTOR_BUCKET.value
-            ETL_LOADCPT_EXTRACTOR_BASE_PATH=data.aws_ssm_parameter.ETL_LOADCPT_EXTRACTOR_BASE_PATH.value
-            ETL_LOADCPT_EXTRACTOR_FILES=data.aws_ssm_parameter.ETL_LOADCPT_EXTRACTOR_FILES.value
-            ETL_LOADCPT_TRANSFORMER=data.aws_ssm_parameter.ETL_LOADCPT_TRANSFORMER.value
-            ETL_LOADCPT_LOADER=data.aws_ssm_parameter.ETL_LOADCPT_LOADER.value
-        }
+    tags = {
+        Name = "CPT API Lambda function execution role"
+        Env                 = data.aws_ssm_parameter.account_environment.value
+        Contact             = data.aws_ssm_parameter.contact.value
+        SystemTier          = local.system_tier
+        DRTier              = local.na
+        DataClassification  = local.na
+        BudgetCode          = local.budget_code
+        Owner               = local.owner
+        Notes               = local.notes
+        OS                  = local.na
+        EOL                 = local.na
+        MaintenanceWindow   = local.na
     }
 }
+
+
+resource "aws_api_gateway_rest_api" "cpt_api_gateway" {
+    name = local.spec_title
+    description = local.spec_description
+
+    tags = {
+        Name = "CPT API Gateway"
+        Env                 = data.aws_ssm_parameter.account_environment.value
+        Contact             = data.aws_ssm_parameter.contact.value
+        SystemTier          = local.system_tier
+        DRTier              = local.na
+        DataClassification  = local.na
+        BudgetCode          = local.budget_code
+        Owner               = local.owner
+        Notes               = local.notes
+        OS                  = local.na
+        EOL                 = local.na
+        MaintenanceWindow   = local.na
+    }
+}
+
+
+resource "aws_api_gateway_rest_api" "cpt_api_gateway_test" {
+    name = local.spec_title
+    description = local.spec_description
+    body = templatefile(
+        "${path.module}/../../../Build/CPT/api.yaml",
+        {
+            title = local.spec_title,
+            description = local.spec_description,
+            region = "us-east-1",
+            lambda_return404_arn = "arn:aws:lambda:us-east-1:644454719059:function:Return404",
+        }
+    )
+
+    tags = {
+        Name = "CPT API Gateway"
+        Env                 = data.aws_ssm_parameter.account_environment.value
+        Contact             = data.aws_ssm_parameter.contact.value
+        SystemTier          = local.system_tier
+        DRTier              = local.na
+        DataClassification  = local.na
+        BudgetCode          = local.budget_code
+        Owner               = local.owner
+        Notes               = local.notes
+        OS                  = local.na
+        EOL                 = local.na
+        MaintenanceWindow   = local.na
+    }
+}
+
+
+# resource "aws_lambda_function" "convert_cpt_etl" {
+#     filename        = "../../../Build/CPT/app.zip"
+#     function_name   = "ConvertCPT"
+#     role            = aws_iam_role.cpt_lambda_role.arn
+#     handler         = "datalabs.etl.run.lambda_handler"
+#     runtime         = "python3.7"
+#     environment {
+#         variables = {
+#             ETL_CONVERTCPT_LAMBDA_FUNCTION=data.aws_ssm_parameter.ETL_CONVERTCPT_LAMBDA_FUNCTION.value
+#             ETL_CONVERTCPT_APP=data.aws_ssm_parameter.ETL_CONVERTCPT_APP.value
+#             ETL_CONVERTCPT_EXTRACTOR=data.aws_ssm_parameter.ETL_CONVERTCPT_EXTRACTOR.value
+#             ETL_CONVERTCPT_EXTRACTOR_BUCKET=data.aws_ssm_parameter.ETL_CONVERTCPT_EXTRACTOR_BUCKET.value
+#             ETL_CONVERTCPT_EXTRACTOR_BASE_PATH=data.aws_ssm_parameter.ETL_CONVERTCPT_EXTRACTOR_BASE_PATH.value
+#             ETL_CONVERTCPT_EXTRACTOR_FILES=data.aws_ssm_parameter.ETL_CONVERTCPT_EXTRACTOR_FILES.value
+#             ETL_CONVERTCPT_TRANSFORMER=data.aws_ssm_parameter.ETL_CONVERTCPT_TRANSFORMER.value
+#             ETL_CONVERTCPT_TRANSFORMER_PARSERS=data.aws_ssm_parameter.ETL_CONVERTCPT_TRANSFORMER_PARSERS.value
+#             ETL_CONVERTCPT_LOADER=data.aws_ssm_parameter.ETL_CONVERTCPT_LOADER.value
+#             ETL_CONVERTCPT_LOADER_BUCKET=data.aws_ssm_parameter.ETL_CONVERTCPT_LOADER_BUCKET.value
+#             ETL_CONVERTCPT_LOADER_FILES=data.aws_ssm_parameter.ETL_CONVERTCPT_LOADER_FILES.value
+#             ETL_CONVERTCPT_LOADER_BASE_PATH=data.aws_ssm_parameter.ETL_CONVERTCPT_LOADER_BASE_PATH.value
+
+#             # TODO: These need to be moved to the RDS loading ETL Lambda resource
+#             ETL_LOADCPT_LAMBDA_FUNCTION=data.aws_ssm_parameter.ETL_LOADCPT_LAMBDA_FUNCTION.value
+#             ETL_LOADCPT_APP=data.aws_ssm_parameter.ETL_LOADCPT_APP.value
+#             ETL_LOADCPT_EXTRACTOR=data.aws_ssm_parameter.ETL_LOADCPT_EXTRACTOR.value
+#             ETL_LOADCPT_EXTRACTOR_BUCKET=data.aws_ssm_parameter.ETL_LOADCPT_EXTRACTOR_BUCKET.value
+#             ETL_LOADCPT_EXTRACTOR_BASE_PATH=data.aws_ssm_parameter.ETL_LOADCPT_EXTRACTOR_BASE_PATH.value
+#             ETL_LOADCPT_EXTRACTOR_FILES=data.aws_ssm_parameter.ETL_LOADCPT_EXTRACTOR_FILES.value
+#             ETL_LOADCPT_TRANSFORMER=data.aws_ssm_parameter.ETL_LOADCPT_TRANSFORMER.value
+#             ETL_LOADCPT_LOADER=data.aws_ssm_parameter.ETL_LOADCPT_LOADER.value
+#         }
+#     }
+# }
 
 
 data "aws_ssm_parameter" "database_username" {
@@ -112,6 +178,8 @@ data "aws_ssm_parameter" "contact" {
 
 
 locals {
+    spec_title          = "CPT API"
+    spec_description    = "CPT API Phase I"
     system_tier         = "Application"
     na                  = "N/A"
     budget_code         = "PBW"
