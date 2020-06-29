d47098627c6d24203b44e964bc30e7fabf5596f2
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-06-29 13:58:43 -0500

    "Updated generate release table"
    Former-commit-id: cb3ced5538d947cdb30bd1c1d80e83f91a64cd75

---
+++ Source/Python/datalabs/access/cpt/api/all_release_table.py
@@ -0,0 +1,57 @@
+import json
+from   sqlalchemy import create_engine
+from   sqlalchemy.orm import sessionmaker
+from   datalabs.etl.cpt.dbmodel import Release
+from   datalabs.access.database import Database
+
+
+def lambda_handler(event, context):
+    session = create_database_connection()
+    query = query_for_release(session)
+
+    status_code, response = get_response_from_query(query, event.get('queryStringParameters', None))
+
+    return {
+        "statusCode": status_code,
+        "body": json.dumps(response)
+    }
+
+
+def create_database_connection():
+    engine = create_engine(Database.url)
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+
+def query_for_release(session):
+    query = session.query(Release)
+    return query
+
+
+def get_response_from_query(query, query_parameter):
+    status_code = 200
+
+    if query_parameter is not None:
+        results = query_parameter['results']
+
+        if results.isdigit():
+            query = query.limit(results).all()
+            response_data = get_content_from_rows(query)
+        else:
+            response_data = {'results': 'invalid'}
+            status_code = 400
+
+    else:
+        query = query.all()
+        response_data = get_content_from_rows(query)
+
+    return status_code, response_data
+
+
+def get_content_from_rows(query):
+    response_rows = [dict(id=row.id,
+                          publish_date=str(row.publish_date),
+                          effective_date=str(row.effective_date),
+                          type=row.type) for row in query.all()]
+
+    return response_rows
