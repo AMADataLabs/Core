683204eb20826b10b600be3151beeda34e04b939
plane <peter.lane@ama-assn.org>
2020-07-23 20:52:10 -0500

    Fix password variable name. Revert to no looping forever in job script.
    Former-commit-id: 0326d6f959c2afef01088f500d04bef5d4df961a

--- Build/ScheduledJobPoC/put_foo.py
+++ Build/ScheduledJobPoC/put_foo.py
@@ -5,6 +5,6 @@ import pysftp
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
 
-with pysftp.Connection('eft.ama-assn.org', username=os.environ['SFTP_USERNAME'], password=os.environ['SFTP_USERNAME'], cnopts=cnopts) as sftp:
+with pysftp.Connection('eft.ama-assn.org', username=os.environ['SFTP_USERNAME'], password=os.environ['SFTP_PASSWORD'], cnopts=cnopts) as sftp:
     with sftp.cd('Data Analytics/Peter'):
         sftp.put('foo')
