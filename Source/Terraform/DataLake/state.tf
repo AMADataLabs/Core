3dd9334d85df80724489ff038521b02ccc57b455
Peter G Lane <muffinspawn@gmail.com>
2020-07-24 15:11:26 -0500

    Remove old DataLake terraform config.
    Former-commit-id: 2a322ba541796d6f29013edc5a03b141037de68b

--- Source/Terraform/DataLake/state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-        # bucket          = "ama-hsg-datalabs-datalake-terraform-state-<account environment>"  (i.e. sandbox or production)
-        # key             = "DataLake/<deploy environment>.tfstate"  (i.e. dev, test, stage, prod)
-        region          = "us-east-1"
-        dynamodb_table  = "hsg-datalabs-terraform-locks"
-    }
-}
