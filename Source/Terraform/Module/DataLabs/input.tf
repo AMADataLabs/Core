abe940f432557de44d579be521b8821e2e891dc2
Peter G Lane <muffinspawn@gmail.com>
2020-07-24 12:25:05 -0500

    Consolidated Sandbox-specific DataLabs AWS resources to .../Terraform/Sandbox/DataLabs.
    Former-commit-id: 16519c0e8db06ad55ce672e44de07a84dd936570

--- Source/Terraform/Module/DataLabs/input.tf
+++ Source/Terraform/Module/DataLabs/input.tf
@@ -1,29 +1,3 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-
-resource "aws_s3_bucket" "datalabs_lambda_code_bucket" {
-    bucket = data.aws_ssm_parameter.lambda_code_bucket.value
-
-    lifecycle {
-        prevent_destroy = true
-    }
-
-    tags = merge(local.tags, {Name = "Data Labs Lambda Code Bucket"})
-}
-
-
-resource "aws_s3_bucket_public_access_block" "datalabs_lambda_code_bucket_public_access_block" {
-    bucket = data.aws_ssm_parameter.lambda_code_bucket.value
-
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-}
-
-
 data "aws_ssm_parameter" "account_environment" {
     name = "/DataLabs/account_environment"
 }
@@ -31,6 +5,16 @@ data "aws_ssm_parameter" "account_environment" {
 
 data "aws_ssm_parameter" "contact" {
     name = "/DataLabs/contact"
+}
+
+
+data "aws_ssm_parameter" "terraform_state_bucket" {
+    name = "/DataLabs/Terraform/state_bucket"
+}
+
+
+data "aws_ssm_parameter" "terraform_locks_database" {
+    name = "/DataLabs/Terraform/locks_database"
 }
 
 
@@ -53,7 +37,6 @@ locals {
         DataClassification  = local.na
         BudgetCode          = local.budget_code
         Owner               = local.owner
-        Notes               = local.notes
         OS                  = local.na
         EOL                 = local.na
         MaintenanceWindow   = local.na
--- Source/Terraform/Sandbox/DataLabs/state.tf
+++ Source/Terraform/Sandbox/DataLabs/state.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket          = "ama-hsg-datalabs-datalake-terraform-state-sandbox"
-        key             = "DataLabs/parameters.tfstate"
+        key             = "DataLabs/sandbox.tfstate"
         region          = "us-east-1"
         dynamodb_table  = "hsg-datalabs-terraform-locks"
     }
