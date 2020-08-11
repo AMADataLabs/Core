2d5b6bbc322141cfd16400e468855f53186c101a
Garrett Lappe <garrett.lappe@ama-assn.org>
2020-08-11 14:31:05 -0500

    Addressing style/formatting comments and changing the MarkLogic class initializations
    Former-commit-id: 103342b5c59ccd0e0c213898cb60260eeb8e1134

--- Source/Python/datalabs/access/sanctions/marklogic.py
+++ Source/Python/datalabs/access/sanctions/marklogic.py
@@ -6,12 +6,11 @@ from datalabs.access import marklogic as ml
 
 class MarkLogic(ml.MarkLogic):
     def __init__(self, key=None, host=None):
-        if key:
-            host = os.environ[f'MARKLOGIC_HOST_{key.upper()}']
         if host is None:
-            raise ValueError(f"Host '{host}' not found in server aliases.")
+            host = os.environ[f'DATABASE_{self._key.upper()}_HOST']
+
         self.url = f'http://{host}:8000/LATEST'
-        super().__init__()
+        super().__init__(credentials=None, key=key)
         self.auth = HTTPDigestAuth(self._credentials.username, self._credentials.password)
 
     def set_license_number(self, uri, license_number, json_file=None, database='PhysicianSanctions'):
@@ -24,13 +23,13 @@ class MarkLogic(ml.MarkLogic):
         self._set_file(uri=uri, data=json.dumps(json_file))
         return
 
-    def set_me_number(self, uri, me_nbr, json_file=None, database='PhysicianSanctions'):
+    def set_me_number(self, uri, me_number, json_file=None, database='PhysicianSanctions'):
         if json_file is None:
             json_file = self.get_file(uri=uri, database=database)
 
         # Fill ME number
-        json_file['sanction']['physician']['me'] = me_nbr
-        json_file['app']['assignment']['me'] = me_nbr
+        json_file['sanction']['physician']['me'] = me_number
+        json_file['app']['assignment']['me'] = me_number
 
         self._set_file(uri=uri, data=json.dumps(json_file))
 
