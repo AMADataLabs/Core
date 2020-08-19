145f20b9f6e3c4f465befbcd40b6a7dedef78f03
Nikhil Khatri <nikhil.khatri@ama-assn.org>
2020-08-19 15:36:08 -0500

    modified the main to enable versioning. policy has been amended.
    Former-commit-id: 0de5e4bb9dcdc3a8415de61873dd88a25419c9b1

--- Source/Terraform/Module/DataLake/policy.tf
+++ Source/Terraform/Module/DataLake/policy.tf
@@ -13,14 +13,25 @@ resource "aws_s3_bucket_policy" "datalake_ingestion_bucket" {
                 "AWS": "arn:aws:iam::655665223663:root"
             },
             "Action": [
-                "s3:GetBucketVersioning",
-                "s3:PutBucketVersioning",
                 "s3:ReplicateObject",
                 "s3:ReplicateDelete",
                 "s3:ObjectOwnerOverrideToBucketOwner"
             ],
             "Resource": [
                 "${aws_s3_bucket.datalake_ingestion_bucket.arn}/IVL/HospitalGroupAffiliation/*"
+            ]
+        },
+        {
+            "Sid": "S3PolicyStmt-DO-NOT-MODIFY-1597230941297",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::655665223663:root"
+            },
+            "Action": [
+                "s3:GetBucketVersioning"
+            ],
+            "Resource": [
+                "${aws_s3_bucket.datalake_ingestion_bucket.arn}"
             ]
         }
     ]
