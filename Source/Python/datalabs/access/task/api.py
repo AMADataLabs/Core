041cf050cf20c39e59ece6b5e842e1b56f71d0fe
Peter Lane <peter.lane@ama-assn.org>
2020-08-20 12:19:29 -0500

    Revert env var reference.
    Former-commit-id: 99d19a35de55e492bcb629f689f7531a8aacad68

--- Source/Python/datalabs/access/task/api.py
+++ Source/Python/datalabs/access/task/api.py
@@ -89,7 +89,7 @@ class APIEndpointParametersGetterMixin(task.TaskWrapper):
             ),
             bucket=dict(
                 name=os.getenv('BUCKET_NAME'),
-                base_path=os.getenv('BUCKET_PATH'),
+                base_path=os.getenv('BUCKET_BASE_PATH'),
                 url_duration=os.getenv('BUCKET_URL_DURATION'),
             )
         )
