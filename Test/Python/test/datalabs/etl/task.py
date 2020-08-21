a57c79c2e877e8ed68a266fee2d4276e2fb3fe3f
Peter Lane <peter.lane@ama-assn.org>
2020-08-21 15:12:25 -0500

    Add missing test classes.
    Former-commit-id: ab3c1bb56773f1cfea8a7a088ebd7e62579f650b

---
+++ Test/Python/test/datalabs/etl/task.py
@@ -0,0 +1,27 @@
+from   datalabs.etl.task import ETLTaskWrapper
+
+
+class ETLTaskWrapper(ETLTaskWrapper):
+    def __init__(self, task_class, parameters=None):
+        super().__init__(task_class, parameters=parameters)
+
+        self._exception
+        self._data = None
+
+    @propery
+    def exception(self):
+        return self._exception
+
+    @property
+    def data(self):
+        return self._data
+
+    def _handle_exception(self, exception: ETLException):
+        self._exception = exception
+
+    def _generate_response(self):
+        self._data = dict(
+            extractor=self._extractor.data,
+            transformer=self._transformer.data,
+            loader=self._loader.data
+        )
