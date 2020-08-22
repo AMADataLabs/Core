7e0b510c471c0a29aff659cbdad8a28e52b7ff8f
Peter Lane <peter.lane@ama-assn.org>
2020-08-21 23:42:27 -0500

    Remove uids. Add graph clearing commands.
    Former-commit-id: 11b7810a979ca30bd58878392f50d203d5045b87

--- Test/Python/test/datalabs/access/test_lineage.py
+++ Test/Python/test/datalabs/access/test_lineage.py
@@ -8,32 +8,22 @@ from neptune_python_utils.gremlin_utils import GremlinUtils
 
 def test_connection(lineage):
     raw_dataset = lineage.addV('dataset').property(
-        'uid', 'f90dah024r90-fhd80ap8y4-hfd8p89r'
-    ).property(
         'location', 's3://hsg-datalabs-datalake-ingestion-sandbox/AMA/CPT/20200131'
     ).next()
 
     pdf1 = lineage.addV('data').property(
-        'uid', 'vsf0hq08-fh9r0wfurw0-f8w9ahpfr89'
-    ).property(
         'location', 's3://ama-hsg-datalabs-datalake-ingestion-sandbox/AMA/CPT/20200131/CPT Link Release Notes 20200131.pdf'
     ).next()
 
     pdf2 = lineage.addV('data').property(
-        'uid', 'hv8daphf8r9-nv8r9a8r7-vnr89ahr8'
-    ).property(
         'location', 's3://ama-hsg-datalabs-datalake-ingestion-sandbox/AMA/CPT/20200131/standard/AnesthesiaGuidelines.pdf'
     ).next()
 
     processed_dataset = lineage.addV('dataset').property(
-        'uid', 'vf80eaphgr989-hf8e9ahgr89e-hgf89eagr89'
-    ).property(
         'location', 's3://hsg-datalabs-datalake-processed-sandbox/AMA/CPT/20200820'
     ).next()
 
     pdf_zip = lineage.addV('data').property(
-        'uid', 'hfd890ahf89p-hf8dphf8-h8f9ah8fr9w'
-    ).property(
         'location', 's3://ama-hsg-datalabs-datalake-processed-sandbox/AMA/CPT/20200820/pdfs.zip'
     ).next()
 
@@ -53,13 +43,16 @@ def test_connection(lineage):
         'timestamp', '20200820T21:38:32+00:00'
     ).iterate()
 
-    lineage.V(Bindings.of('id', raw_dataset)).addE('ParentOf').to(pdf2).property(
+    lineage.V(Bindings.of('id', raw_dataset)).addE('Contains').to(pdf2).property(
         'timestamp', '20200820T21:38:32+00:00'
     ).iterate()
 
     lineage.V(Bindings.of('id', processed_dataset)).addE('Contains').to(pdf_zip).property(
         'timestamp', '20200820T21:38:32+00:00'
     ).iterate()
+
+    lineage.V().drop().iterate()
+    lineage.E().drop().iterate()
 
 @pytest.fixture
 def lineage():
