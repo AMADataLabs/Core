3ee1aae2943a1f4563d92d96050f98760ad97c73
Peter Lane <peter.lane@ama-assn.org>
2020-09-18 10:25:03 -0500

    Moved Data Lake SSM parameters to a separate module. Fixed the Data Lake bastion AMI ID.
    Former-commit-id: 7b24643ec06dfaba57282437e4bc5f25e1090e2f

--- Source/Terraform/Sandbox/DataLake/Parameters/main.tf
+++ Source/Terraform/Sandbox/DataLake/Parameters/main.tf
@@ -4,9 +4,19 @@ provider "aws" {
 }
 
 
-module "datalabs_terraform_state" {
-    source  = "../../Module/DataLake"
-    project = "DataLake"
+resource "aws_ssm_parameter" "ingestion_bucket" {
+    name  = "/DataLabs/${local.project}/ingestion_bucket"
+    type  = "String"
+    value = format("ama-hsg-datalabs-%s-ingestion-sandbox", lower(local.project))
+    tags = local.tags
+}
+
+
+resource "aws_ssm_parameter" "processed_bucket" {
+    name  = "/DataLabs/${local.project}/processed_bucket"
+    type  = "String"
+    value = format("ama-hsg-datalabs-%s-processed-sandbox", lower(local.project))
+    tags = local.tags
 }
 
 
