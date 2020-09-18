ac525efd880b839d5ff679d33a4f769dcf2690be
Peter Lane <peter.lane@ama-assn.org>
2020-09-18 11:55:07 -0500

    Fix remote state file name.
    Former-commit-id: 840c383b2842b23a340b27974c15a90cc4305399

--- Source/Terraform/Sandbox/DataLake/Parameters/state.tf
+++ Source/Terraform/Sandbox/DataLake/Parameters/state.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket          = "ama-hsg-datalabs-datalake-terraform-state-sandbox"
-        key             = "DataLake/parametes.tfstate"
+        key             = "DataLake/parameter.tfstate"
         region          = "us-east-1"
         dynamodb_table  = "hsg-datalabs-terraform-locks"
     }
