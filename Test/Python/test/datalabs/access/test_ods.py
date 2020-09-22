d13152be3346fdb8e5720cde54c1164b9ac1c958
Peter Lane <peter.lane@ama-assn.org>
2020-09-22 11:29:24 -0500

    Fix S3 extractor class name in local CPT .env templates.
    Former-commit-id: 7892f6b75f06a54ec9fb944af468db3aaecd2778

---
+++ Test/Python/test/datalabs/access/test_ods.py
@@ -0,0 +1,40 @@
+import jaydebeapi
+import pytest
+import sqlalchemy as sa
+
+
+@pytest.mark.skip(reason="Example JDBC Usage")
+def test_jdbc_connction():
+    """ Prerequsites:
+            JDK: sudo apt install default-jre
+            jaydebeapi: pip install jaydebeapi
+            DB2 JDBC Driver: Put db2jcc4.jar in run directory (download from https://dbschema.com/jdbc-driver/Db2.html).
+    """
+    ods = jaydebeapi.connect(
+        'com.ibm.db2.jcc.DB2Jcc',
+        'jdbc:db2://rdbp1190.ama-assn.org:54150/eprdods',
+        ['AMA_DOMAIN_USERNAME', 'AMA_DOMAIN_PASSWORD'],
+        './db2jcc4.jar'
+    )
+
+    curs = ods.cursor()
+    # curs.execute('select schemaname from syscat.schemata')
+    # curs.execute("select name from sysibm.systables where type='T' and creator='ODS'")
+    curs.execute('select * from ODS.ODS_PPD_FILE LIMIT 10')
+
+    results = curs.fetchall()
+
+    print(results)
+    assert False
+
+
+@pytest.mark.skip(reason="Example SQLAlchemy Usage")
+def test_sqlalchemy_connection():
+    engine = sa.create_engine("ibmi://AMA_DOMAIN_USERNAME:AMA_DOMAIN_PASSWORD@rdbp1190.ama-assn.org:54150/eprdods")
+    connection = engine.connect()
+    metadata = sa.MetaData()
+
+    query = connection.execute("select * from ODS.ODS_PPD_FILE LIMIT 10")
+    result = result.fetchall()
+
+    print(result)
