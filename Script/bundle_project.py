376d6cbbfc3a06a9b4f4ff2d3e4b7ac79b072a4c
Peter Lane <peter.lane@ama-assn.org>
2020-10-02 14:01:22 -0500

    Handle compound table keys.
    Former-commit-id: ccb86cf92d54dc3cd9b2e33ad3e17dda9560a098

--- Script/bundle_project.py
+++ Script/bundle_project.py
@@ -74,7 +74,8 @@ class LocalProjectBundler(ProjectBundler):
         shutil.copy(os.path.join(self._build_path, 'Master', 'task.py'), os.path.join(target_path, 'task.py'))
 
     def _zip_bundle_directory(self, project, target_path):
-        archive_path = target_path.with_suffix('.zip')
+
+        archive_path = target_path.resolve().with_suffix('.zip')
 
         if os.path.exists(archive_path):
             os.remove(archive_path)
