1dd9af9eba40fc68899d49317c58d6f3f76cef0f
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-10-05 13:01:36 -0500

    Added initial jdbc config and loader config
    Former-commit-id: 686cdeae473a531eeea2f9bd2957ad4924dd54b9

--- Source/Python/datalabs/etl/oneview/ppd/extract.py
+++ Source/Python/datalabs/etl/oneview/ppd/extract.py
@@ -1,18 +1,20 @@
 """ Oneview PPD Extractor from ods"""
+import jaydebeapi
 import pandas
+
 import settings
 
-from   datalabs.access.ods import ODS
+from   datalabs.access.jdbc import JDBC
 from   datalabs.etl.extract import ExtractorTask
 from   datalabs.etl.task import ETLException
 
 
-class PPDExtractorODS(ODS, ExtractorTask):
+class PPDExtractorODS(JDBC, ExtractorTask):
     def __init__(self):
         self.query = '''SELECT * FROM ODS.ODS_PPD_FILE PE'''
 
     def _extract(self):
-        with ODS() as ods:
-            ppd = ods.read(self.query)
+        with JDBC() as jdbc:
+            ppd = jdbc.read(self.query)
 
         return ppd
--- Source/Python/datalabs/etl/oneview/ppd/load.py
+++ Source/Python/datalabs/etl/oneview/ppd/load.py
@@ -1,14 +1,21 @@
 """Oneview PPD Loader Task"""
+import logging
 
 from datalabs.etl.load import LoaderTask
 from datalabs.etl.task import ETLException
+from datalabs.etl.load import ConsoleLoaderTask
 from datalabs.etl.fs import LocalUnicodeTextFileLoaderTask
 from datalabs.etl.s3 import S3UnicodeTextFileLoaderTask
-
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
 
-class PPDLocalFileLoader(LocalUnicodeTextFileLoaderTask):
+
+class PPDLocalFileLoader(LoaderTask):
     def _load(self):
+        try:
+            for datum in self._parameters.data:
+                self._logger.info(datum)
+        except:
+            self._logger.info(self._parameters.data)
