1ab11c1d0c2c853b942630e36083580345780da7
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-10-16 14:24:00 -0500

    Updated parser
    Former-commit-id: 0a1d2ac5fab3e6b9ebd8ec06b9f763c342c47e6c

--- Source/Python/datalabs/curate/oneview/ppd/parse.py
+++ Source/Python/datalabs/curate/oneview/ppd/parse.py
@@ -2,10 +2,10 @@
 import pandas
 
 from datalabs.curate.parse import Parser
-import datalabs.curate.oneview.ppd.columns as column
+import datalabs.curate.oneview.ppd.columns as columns
 
 
 class ExpandedPPDParser(Parser):
     def parse(self, data: pandas.DataFrame) -> pandas.DataFrame:
-        data.column = column
+        data.columns = columns
         return data
