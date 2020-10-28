88552a6a6ac961e63b9ad2928bd52522932053f6
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-10-28 11:33:42 -0500

    Transformer for Iqvia
    Former-commit-id: afbb8dbd37f3e62623573c90fc3ffc84895f5abc

---
+++ Source/Python/datalabs/etl/oneview/iqvia/transform.py
@@ -0,0 +1,19 @@
+""" Oneview PPD Transformer"""
+import logging
+
+from datalabs.etl.oneview.ppd.column import columns
+from datalabs.etl.oneview.transform import TransformerTask
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+
+class IQVIADataFramesToCSVText(TransformerTask):
+    def _transform(self):
+        csv = super()._transform()
+
+        return csv
+
+    def _get_columns(self):
+        return columns
