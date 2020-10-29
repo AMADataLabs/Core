d8c0c9fc22a7134626f0f5b8cb7824774f1cbde9
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-10-29 12:00:45 -0500

    Refactoring oneview loader
    Former-commit-id: c763332d1c1d06e9989df5d1cb07fc7b7372d191

--- Source/Python/datalabs/etl/oneview/load.py
+++ Source/Python/datalabs/etl/oneview/load.py
@@ -1,56 +1,21 @@
 """Oneview PPD Loader Task"""
+import boto3
 import logging
-
-from io import BytesIO
-from minio import Minio
 
 from datalabs.etl.load import LoaderTask
 from datalabs.etl.task import ETLException
-from datalabs.etl.s3.load import S3UnicodeTextFileLoaderTask
+from datalabs.etl.s3.load import S3FileLoaderTask
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
 
 
-class PPDFileLoader(LoaderTask):
-    def _load(self):
-        self._logger.info(self._parameters.data)
-        client = self._connect_to_client()
-
-        try:
-            self._upload_file(client)
-
-        except Exception as exception:
-            raise ETLException('Invalid Data') from exception
-
-    def _connect_to_client(self):
-        minioClient = Minio(self._parameter.database['ENDPOINT'],
-                            access_key=self._parameters.database['ACCESS_KEY'],
-                            secret_key=self._parameters.database['SECRET_KEY'],
-                            secure=True
-                            )
-        return minioClient
-
-    def _upload_file(self, client):
-        self._make_bucket(client)
-
-        Encoder = S3UnicodeTextFileLoaderTask()
-        encoded_data = Encoder._encode(self._parameters.data)
-
-        for data, table in encoded_data, self._parameters.variables['TABLE_NAMES']:
-            client.put_object(
-                self._paramters.database['BUCKET'],
-                table,
-                data=BytesIO(data),
-                length=len(data),
-                content_type='application/csv'
-            )
-
-    def _make_bucket(self, client):
-        if client.bucket_exists(self._paramters.database['BUCKET']):
-            self._logger.info('Bucket exists')
-
-        else:
-            client.make_bucket(self._paramters.database['BUCKET'])
-            self._logger.info('Bucket created')
+class PPDFileLoader(S3FileLoaderTask, LoaderTask):
+    def __init__(self, parameters):
+        self.s3 = boto3.client('s3',
+                               endpoint_url=self._parameters.variables['ENDPOINT_URL'],
+                               aws_access_key_id=self._parameters.variables['ACCESS_KEY'],
+                               aws_secret_access_key=self._parameters.variable['SECRET_KEY'],
+                               region_name='us-west-2'
+                               )
