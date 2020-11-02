7549df67fa0334d1b0d49ea318abcc19fa58ec58
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-11-02 12:12:51 -0600

    Refactored named dictionary
    Former-commit-id: 9d04a292c29de80bf9120b6917424abf68949aea

--- Source/Python/datalabs/etl/oneview/iqvia/column.py
+++ Source/Python/datalabs/etl/oneview/iqvia/column.py
@@ -1,49 +1,46 @@
 """Oneview Physician Table Columns"""
 
-columns = [
-    {
-        'IMS_ORG_ID': 'id',
-        'BUSINESS_NAME': 'name',
-        'DBA_NAME': 'doing_business_as',
-        'PHYSICAL_ADDR_1': 'physical_address_1',
-        'PHYSICAL_ADDR_2': 'physical_address_2',
-        'PHYSICAL_CITY': 'physical_city',
-        'PHYSICAL_STATE': 'physical_state',
-        'PHYSICAL_ZIP': 'physical_zipcode',
-        'POSTAL_ADDR_1': 'postal_address_1',
-        'POSTAL_ADDR_2': 'postal_address_2',
-        'POSTAL_CITY': 'postal_city',
-        'POSTAL_STATE': 'postal_state',
-        'POSTAL_ZIP': 'postal_zipcode',
-        'PHONE': 'phone',
-        'FAX': 'fax',
-        'WEBSITE': 'website',
-        'OWNER_STATUS': 'owner_status',
-        'PROFIT_STATUS': 'profit_status'
-    },
-    {
-        'PROFESSIONAL_ID': 'id',
-        'ME': 'medical_education_number',
-        'FIRST_NAME': 'first_name',
-        'MIDDLE_NAME': 'middle_name',
-        'LAST_NAME': 'last_name',
-        'GEN_SUFFIX': 'suffix',
-        'DESIGNATION': 'designation',
-        'GENDER': 'gender',
-        'ROLE': 'role',
-        'PRIMARY_SPEC': 'primary_specialty',
-        'SECONDARY_SPEC': 'secondary_specialty',
-        'TERTIARY_SPEC': 'tertiary_specialty',
-        'PRIMARY_PROF_CODE': 'primary_professional_code',
-        'PRIMARY_PROF_DESC': 'primary_professional_description',
-        'STATUS_DESC': 'status_description'
-    },
-    {
-        'AFFIL_TYPE_ID': 'id',
-        'IMS_ORG_ID': 'business',
-        'PROFESSIONAL_ID': 'provider',
-        'AFFIL_TYPE_DESC': 'description',
-        'AFFIL_IND': 'primary',
-        'AFFIL_RANK': 'rank'
-    }
-]
+business_columns = {
+    'IMS_ORG_ID': 'id',
+    'BUSINESS_NAME': 'name',
+    'DBA_NAME': 'doing_business_as',
+    'PHYSICAL_ADDR_1': 'physical_address_1',
+    'PHYSICAL_ADDR_2': 'physical_address_2',
+    'PHYSICAL_CITY': 'physical_city',
+    'PHYSICAL_STATE': 'physical_state',
+    'PHYSICAL_ZIP': 'physical_zipcode',
+    'POSTAL_ADDR_1': 'postal_address_1',
+    'POSTAL_ADDR_2': 'postal_address_2',
+    'POSTAL_CITY': 'postal_city',
+    'POSTAL_STATE': 'postal_state',
+    'POSTAL_ZIP': 'postal_zipcode',
+    'PHONE': 'phone',
+    'FAX': 'fax',
+    'WEBSITE': 'website',
+    'OWNER_STATUS': 'owner_status',
+    'PROFIT_STATUS': 'profit_status'}
+provider_columns = {
+    'PROFESSIONAL_ID': 'id',
+    'ME': 'medical_education_number',
+    'FIRST_NAME': 'first_name',
+    'MIDDLE_NAME': 'middle_name',
+    'LAST_NAME': 'last_name',
+    'GEN_SUFFIX': 'suffix',
+    'DESIGNATION': 'designation',
+    'GENDER': 'gender',
+    'ROLE': 'role',
+    'PRIMARY_SPEC': 'primary_specialty',
+    'SECONDARY_SPEC': 'secondary_specialty',
+    'TERTIARY_SPEC': 'tertiary_specialty',
+    'PRIMARY_PROF_CODE': 'primary_professional_code',
+    'PRIMARY_PROF_DESC': 'primary_professional_description',
+    'STATUS_DESC': 'status_description'
+}
+provider_affiliation_columns = {
+    'AFFIL_TYPE_ID': 'id',
+    'IMS_ORG_ID': 'business',
+    'PROFESSIONAL_ID': 'provider',
+    'AFFIL_TYPE_DESC': 'description',
+    'AFFIL_IND': 'primary',
+    'AFFIL_RANK': 'rank'
+}
--- Source/Python/datalabs/etl/oneview/iqvia/transform.py
+++ Source/Python/datalabs/etl/oneview/iqvia/transform.py
@@ -1,8 +1,8 @@
 """ Oneview PPD Transformer"""
 import logging
 
-from datalabs.etl.oneview.ppd.column import columns
-from datalabs.etl.oneview.transform import TransformerTask
+from   datalabs.etl.oneview.ppd.column import business_columns, provider_columns, provider_affiliation_columns
+from   datalabs.etl.oneview.transform import TransformerTask
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -11,4 +11,4 @@ LOGGER.setLevel(logging.DEBUG)
 
 class IQVIATransformer(TransformerTask):
     def _get_columns(self):
-        return columns
+        return [business_columns, provider_columns, provider_affiliation_columns]
