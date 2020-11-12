dc5465bf244c02c0d8991f75712c294df0a53a1f
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-11-12 17:28:43 -0600

    Configured credentialing addresses ETL
    Former-commit-id: 67bada0d0dc01b9bf495e2f3ab2ce997f8c02261

---
+++ Source/Python/datalabs/etl/oneview/credentialing/address/extract.py
@@ -0,0 +1,16 @@
+""" Oneview credentialing addresses extractor """
+
+import logging
+
+from datalabs.etl.sftp.extract import SFTPUnicodeTextFileExtractorTask
+
+
+class CredentialingAddressesExtractor(SFTPUnicodeTextFileExtractorTask):
+    def _extract(self):
+        with self._get_sftp(self._parameters.variables) as sftp:
+            file_paths = self._get_file_paths(sftp)
+            logging.info('Extracting the following files via SFTP: %s', file_paths)
+
+            data = [self._extract_file(sftp, file) for file in file_paths]
+
+        return data
