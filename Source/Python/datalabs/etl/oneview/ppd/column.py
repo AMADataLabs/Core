bfbd8c77eb6ea23856f75c1b1173a7f85dac38fa
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-11-16 14:59:20 -0600

    Updated PPD ETL
    Former-commit-id: ce5051986f1a25d2d331a42d465a27913841305e

--- Source/Python/datalabs/etl/oneview/ppd/column.py
+++ Source/Python/datalabs/etl/oneview/ppd/column.py
@@ -43,7 +43,7 @@ columns = {
     'PREFERREDPHONENUMBER': 'telephone_number',
     'PENDINGDEAD_IND': 'presumed_dead',
     'FAXNUMBER': 'fax_number',
-    'TOPCODE': 'type_of_practice_code',
+    'TOPCODE': 'type_of_practice',
     'PECODE': 'present_employment',
     'PRIMSPECIALTY': 'primary_specialty',
     'SECONDARYSPECIALTY': 'secondary_specialty',
