9737efc8b1f18178d60cbbb43b893c1e04565f8a
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-11-30 02:26:41 -0600

    Credentialing final ETL
    Former-commit-id: 8813bdd70a9177b1d941c4441e9ec1088d73117f

--- Source/Python/datalabs/etl/oneview/credentialing/transform.py
+++ Source/Python/datalabs/etl/oneview/credentialing/transform.py
@@ -1,9 +1,10 @@
 """Oneview Credentialing Table Columns"""
-
 import logging
+import pandas
 
-from   datalabs.etl.oneview.credentialing.column import CUSTOMER_COLUMNS, PRODUCT_COLUMNS, ORDER_COLUMNS
-from   datalabs.etl.oneview.transform import TransformerTask
+from datalabs.etl.oneview.credentialing.column import CUSTOMER_COLUMNS, PRODUCT_COLUMNS, ORDER_COLUMNS, \
+    CUSTOMER_AND_ADDRESSES_COLUMNS
+from datalabs.etl.oneview.transform import TransformerTask
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -18,4 +19,13 @@ class CredentialingTransformer(TransformerTask):
 class CredentialingFinalTransformerTask(TransformerTask):
     def _transform(self):
         dataframes = [self._dataframe_to_csv(csv) for csv in self._parameters.data]
-        return dataframes
+        merged_dataframes = [self._merge_dataframes(dataframes)]
+
+        return merged_dataframes
+
+    def _merge_dataframes(self, dataframes):
+        new_df = pandas.merge(dataframes[1], dataframes[0], on='number')
+        return [new_df, dataframes[2], dataframes[3]]
+
+    def _get_columns(self):
+        return [CUSTOMER_AND_ADDRESSES_COLUMNS, PRODUCT_COLUMNS, ORDER_COLUMNS]
