579c1d9049e038444b9a1b316e9c6aef8affa98a
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-11-30 09:53:48 -0600

    Revert "Credentialing final ETL"
    This reverts commit 9737efc8b1f18178d60cbbb43b893c1e04565f8a [formerly 8813bdd70a9177b1d941c4441e9ec1088d73117f].
    
    
    Former-commit-id: 2aaae98e78285daa9582e861acfe7ca57f6981ef

--- Source/Python/datalabs/etl/oneview/credentialing/transform.py
+++ Source/Python/datalabs/etl/oneview/credentialing/transform.py
@@ -14,18 +14,3 @@ LOGGER.setLevel(logging.DEBUG)
 class CredentialingTransformer(TransformerTask):
     def _get_columns(self):
         return [CUSTOMER_COLUMNS, PRODUCT_COLUMNS, ORDER_COLUMNS]
-
-
-class CredentialingFinalTransformerTask(TransformerTask):
-    def _transform(self):
-        dataframes = [self._dataframe_to_csv(csv) for csv in self._parameters.data]
-        merged_dataframes = [self._merge_dataframes(dataframes)]
-
-        return merged_dataframes
-
-    def _merge_dataframes(self, dataframes):
-        new_df = pandas.merge(dataframes[1], dataframes[0], on='number')
-        return [new_df, dataframes[2], dataframes[3]]
-
-    def _get_columns(self):
-        return [CUSTOMER_AND_ADDRESSES_COLUMNS, PRODUCT_COLUMNS, ORDER_COLUMNS]
