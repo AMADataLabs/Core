1e046060c642c09309d3d039e6154043acbec166
rnajeeb <rudainah.najeeb@ama-assn.org>
2020-12-08 13:47:05 -0600

    Present Employment ETL
    Former-commit-id: a25581735cebe558ec0c500a3e045fa10c3d0937

---
+++ Source/Python/datalabs/etl/oneview/reference/transform.py
@@ -0,0 +1,24 @@
+""" OneView Reference Transformer"""
+import logging
+
+from   datalabs.etl.oneview.ppd.column import MPA_COLUMNS, TOP_COLUMNS, PE_COLUMNS
+from   datalabs.etl.oneview.transform import TransformerTask
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+
+class MajorProfessionalActivityTransformerTask(TransformerTask):
+    def _get_columns(self):
+        return [MPA_COLUMNS]
+
+
+class TypeOfPracticeTransformerTask(TransformerTask):
+    def _get_columns(self):
+        return [TOP_COLUMNS]
+
+
+class PresentEmployment(TransformerTask):
+    def _get_columns(self):
+        return [PE_COLUMNS]
