27df54dbd2bf52b20a7e81f65e8e560a532c781a
Peter Lane <plane@ama-assn.org>
2021-01-13 16:52:35 -0600

    Remove commented-out entrypoint lines.
    Former-commit-id: d6275224dfcd5195a7fe4ef2c1be667092e94a95

--- Build/Airflow/entrypoint.sh
+++ Build/Airflow/entrypoint.sh
@@ -6,8 +6,6 @@ if [ x"$USER_ID" != x"0" -a x"$USER_ID" != x"1001" ]; then
     NSS_WRAPPER_PASSWD=/tmp/passwd.nss_wrapper
     NSS_WRAPPER_GROUP=/etc/group
     cat /etc/passwd | sed -e 's/^airflow:/builder:/' > $NSS_WRAPPER_PASSWD
-    # cat /etc/passwd | sed -e 's/^runner:/airflow:/' -e 's/${HOME}/entrypoint.sh$/\/bin\/bash/' > $NSS_WRAPPER_PASSWD
-    # cat /etc/passwd | sed -e 's/^runner:/airflow:/' > $NSS_WRAPPER_PASSWD
     echo "airflow:x:$USER_ID:0:Airflow,,,:/home/airflow:/bin/bash" >> $NSS_WRAPPER_PASSWD
     export NSS_WRAPPER_PASSWD
     export NSS_WRAPPER_GROUP
