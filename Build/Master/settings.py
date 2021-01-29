ba9d845684a12b3a290047755ebb9582968c2be1
Peter Lane <plane@ama-assn.org>
2021-01-29 12:49:37 -0600

    Updated pip version. Restricted Jupyter Notebook version to <6.1.6 to avoid tornado module error. Added support to master settings.py for specifying the .env file to use.
    Former-commit-id: b063922e87b1c1625861251c5fa6ebbdaadf3cb6

--- Build/Master/settings.py
+++ Build/Master/settings.py
@@ -4,7 +4,11 @@ import sys
 
 import dotenv
 
-dotenv.load_dotenv()
+dotenv_path = os.environ.get('DOTENV_PATH')
+if dotenv_path:
+    dotenv.load_dotenv(dotenv_path=dotenv_path)
+else:
+    dotenv.load_dotenv()
 
 for p in os.environ.get('DATALABS_PYTHONPATH', '').split(os.pathsep)[::-1]:
     sys.path.insert(0, p)
