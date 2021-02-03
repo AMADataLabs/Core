a7a680826d20ce26f5b8a5bf53ff7f896d55620c
Peter Lane <plane@ama-assn.org>
2021-02-03 14:47:25 -0600

    Add XCOM test DAG.
    Former-commit-id: 091e158af55773506fca956966ba3ef857ea8d24

---
+++ Source/Python/datalabs/airflow/dag/test_xcom.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.utils.dates import days_ago
+
+with DAG(
+    dag_id='example_kubernetes_operator',
+    default_args={'owner': 'airflow'},
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['example'],
+) as dag:
+    write_xcom = KubernetesPodOperator(
+        namespace='default',
+        image='alpine',
+        cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
+        name="write-xcom",
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="write-xcom",
+        get_logs=True,
+    )
+
+    pod_task_xcom_result = BashOperator(
+        bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
+        task_id="pod_task_xcom_result",
+    )
