8c4eda3b6757fbb0a707f1e8e829208c16452184
rnajeeb <rudainah.najeeb@ama-assn.org>
2021-02-08 13:51:13 -0600

    Refactored dag file
    Former-commit-id: 041f506090be567f583cb177eff3d0f20b1a8afd

--- Source/Python/datalabs/airflow/dag/oneview/ingest.py
+++ Source/Python/datalabs/airflow/dag/oneview/ingest.py
@@ -1,12 +1,20 @@
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.utils.dates import days_ago
-from kubernetes.client import models as k8s
+import os
 
-extractor_config = [
-    k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name='oneview-type-of-practice')),
-]
+from   airflow import DAG
+from   kubernetes.client import models as k8s
+from   airflow.operators.python import PythonOperator
+from   airflow.utils.dates import days_ago
+
+from   datalabs.plugin import import_plugin
+
+
+def task():
+    task_class = import_plugin(os.environ['TASK_CLASS'])
+    task_wrapper_class = import_plugin(os.environ['TASK_WRAPPER_CLASS'])
+    task = task_wrapper_class(task_class)
+
+    return task.run()
+
 
 with DAG(
         dag_id='type_of_practice',
@@ -16,48 +24,91 @@ with DAG(
         tags=['TypeOfPractice'],
 ) as dag:
 
-    extractor = KubernetesPodOperator(
-        namespace='default',
-        image='alpine',
-        cmds=['python', 'task.py'],
-        env_from=extractor_config,
-        env=dict(TASK_CLASS=datalabs.etl.jdbc.extract.JDBCExtractorTask),
-        do_xcom_push=True,
-        is_delete_operator_pod=True,
-        in_cluster=True,
-        task_id="write-xcom",
-        get_logs=True,
+    extractor = PythonOperator(
+        task_id="type_of_practice_extractor",
+        python_callable=task,
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            image="docker-registry.default.svc:5000/hsg-data-labs-dev/airflow-worker:1.0.0",
+                            env=[
+                                k8s.V1EnvFromSource(name="TASK_CLASS",
+                                                    value="datalabs.etl.jdbc.extract.JDBCExtractorTask"
+                                                    )
+                            ],
+                            env_var=[
+                                k8s.V1EnvFromSource(
+                                    config_map_ref=k8s.V1ConfigMapEnvSource(name='oneview-type-of-practice')
+                                ),
+                                k8s.V1EnvFromSource(
+                                    secret_ref=k8s.V1ConfigMapEnvSource(name='aims-secret')
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ),
+        },
     )
 
-    transformer = KubernetesPodOperator(
-        namespace='default',
-        image='alpine',
-        cmds=['python', 'task.py'],
-        env_from=extractor_config,
-        env=dict(TASK_CLASS=datalabs.etl.s3.load.S3UnicodeTextFileLoaderTask),
-        do_xcom_push=True,
-        is_delete_operator_pod=True,
-        in_cluster=True,
-        task_id="write-xcom",
-        get_logs=True,
+    transformer = PythonOperator(
+        task_id="type_of_practice_transformer",
+        python_callable=task,
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            image="docker-registry.default.svc:5000/hsg-data-labs-dev/airflow-worker:1.0.0",
+                            env=[
+                                k8s.V1EnvFromSource(name="TASK_CLASS",
+                                                    value="datalabs.etl.s3.load.S3UnicodeTextFileLoaderTask"
+                                                    )
+                            ],
+                            env_var=[
+                                k8s.V1EnvFromSource(
+                                    config_map_ref=k8s.V1ConfigMapEnvSource(name='oneview-type-of-practice')
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ),
+        },
     )
 
-    loader = KubernetesPodOperator(
-        namespace='default',
-        image='alpine',
-        cmds=['python', 'task.py'],
-        env_from=extractor_config,
-        env=dict(TASK_CLASS=datalabs.etl.s3.load.S3UnicodeTextFileLoaderTask),
-        do_xcom_push=True,
-        is_delete_operator_pod=True,
-        in_cluster=True,
-        task_id="write-xcom",
-        get_logs=True,
-    )
-
-    pod_task_xcom_result = BashOperator(
-        bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
-        task_id="pod_task_xcom_result",
+    loader = PythonOperator(
+        task_id="type_of_practice_loader",
+        python_callable=task,
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            image="docker-registry.default.svc:5000/hsg-data-labs-dev/airflow-worker:1.0.0",
+                            env=[
+                                k8s.V1EnvFromSource(name="TASK_CLASS",
+                                                    value="datalabs.etl.s3.load.S3UnicodeTextFileLoaderTask"
+                                                    )
+                            ],
+                            env_var=[
+                                k8s.V1EnvFromSource(
+                                    config_map_ref=k8s.V1ConfigMapEnvSource(name='oneview-type-of-practice')
+                                ),
+                                k8s.V1EnvFromSource(
+                                    secret_ref=k8s.V1ConfigMapEnvSource(name='minio-secret')
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ),
+        },
     )
 
 extractor >> loader >> transformer
