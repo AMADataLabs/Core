547250ce7e2bf5eb7336c8c328909dc13026c662
rnajeeb <rudainah.najeeb@ama-assn.org>
2021-02-10 08:11:22 -0600

    Refactored dag
    Former-commit-id: e93592cb4a4cb50f322bff8d91a1a88b9c027600

--- Source/Python/datalabs/airflow/dag/oneview/ingest.py
+++ Source/Python/datalabs/airflow/dag/oneview/ingest.py
@@ -1,5 +1,4 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.utils.dates import days_ago
 from kubernetes.client import models as k8s
@@ -7,10 +6,10 @@ from kubernetes.client import models as k8s
 etl_config = [
     k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name='oneview-type-of-practice'))
 ]
-minio_secret = [
+minio = [
     k8s.V1EnvFromSource(secret_ref=k8s.V1ConfigMapEnvSource(name='minio-secret'))
 ]
-aims_secret = [
+aims = [
     k8s.V1EnvFromSource(secret_ref=k8s.V1ConfigMapEnvSource(name='aims-secret'))
 ]
 
@@ -19,14 +18,14 @@ with DAG(
         default_args={'owner': 'airflow'},
         schedule_interval=None,
         start_date=days_ago(2),
-        tags=['TypeOfPractice'],
+        tags=['OneView'],
 ) as dag:
 
     extractor = KubernetesPodOperator(
-        namespace='default',
-        image='alpine',
+        namespace='hsg-data-labs-dev',
+        image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
         cmds=['python', 'task.py'],
-        env_from=[etl_config, aims_secret],
+        env_from=[etl_config, aims],
         env=dict(TASK_CLASS=datalabs.etl.jdbc.extract.JDBCExtractorTask),
         do_xcom_push=True,
         is_delete_operator_pod=True,
@@ -36,8 +35,8 @@ with DAG(
     )
 
     transformer = KubernetesPodOperator(
-        namespace='default',
-        image='alpine',
+        namespace='hsg-data-labs-dev',
+        image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
         cmds=['python', 'task.py'],
         env_from=etl_config,
         env=dict(TASK_CLASS=datalabs.etl.oneview.reference.transform.TypeOfPracticeTransformerTask),
@@ -49,21 +48,16 @@ with DAG(
     )
 
     loader = KubernetesPodOperator(
-        namespace='default',
-        image='alpine',
+        namespace='hsg-data-labs-dev',
+        image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
         cmds=['python', 'task.py'],
-        env_from=[etl_config, minio_secret],
+        env_from=[etl_config, minio],
         env=dict(TASK_CLASS=datalabs.etl.s3.load.S3UnicodeTextFileLoaderTask),
         do_xcom_push=True,
         is_delete_operator_pod=True,
         in_cluster=True,
         task_id="write-xcom",
         get_logs=True,
-    )
-
-    pod_task_xcom_result = BashOperator(
-        bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
-        task_id="pod_task_xcom_result",
     )
 
 extractor >> loader >> transformer
