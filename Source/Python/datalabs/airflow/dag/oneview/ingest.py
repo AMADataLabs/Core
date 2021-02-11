3e0860d1b5ecaf52f126c6139172bf10ba811bba
rnajeeb <rudainah.najeeb@ama-assn.org>
2021-02-11 03:54:47 -0600

    OneView, Airflow DAG
    Former-commit-id: 669d1dfa8fdce653f4e18c04bf2c486b142a8542

--- Source/Python/datalabs/airflow/dag/oneview/ingest.py
+++ Source/Python/datalabs/airflow/dag/oneview/ingest.py
@@ -22,10 +22,10 @@ with DAG(
         tags=['OneView'],
 ) as dag:
 
-    extractor = KubernetesPodOperator(
+    type_of_practice_extractor = KubernetesPodOperator(
         namespace='hsg-data-labs-dev',
         image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
-        cmds=['python', 'task.py'],
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
         env_from=[etl_config, aims],
         env=dict(TASK_CLASS=datalabs.etl.jdbc.extract.JDBCExtractorTask),
         do_xcom_push=True,
@@ -35,12 +35,12 @@ with DAG(
         get_logs=True,
     )
 
-    transformer = KubernetesPodOperator(
+    type_of_practice_transformer = KubernetesPodOperator(
         namespace='hsg-data-labs-dev',
         image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
-        cmds=['python', 'task.py'],
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
         env_from=etl_config,
-        env=dict(TASK_CLASS=datalabs.etl.oneview.reference.transform.TypeOfPracticeTransformerTask),
+        env=dict(TASK_WRAPPER_CLASS=datalabs.etl.kubernetes.ETLComponentTaskWrapper),
         do_xcom_push=True,
         is_delete_operator_pod=True,
         in_cluster=True,
@@ -48,12 +48,12 @@ with DAG(
         get_logs=True,
     )
 
-    loader = KubernetesPodOperator(
+    type_of_practice_loader = KubernetesPodOperator(
         namespace='hsg-data-labs-dev',
         image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
-        cmds=['python', 'task.py'],
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
         env_from=[etl_config, minio],
-        env=dict(TASK_CLASS=datalabs.etl.s3.load.S3UnicodeTextFileLoaderTask),
+        env=dict(TASK_WRAPPER_CLASS=datalabs.etl.kubernetes.ETLComponentTaskWrapper),
         do_xcom_push=True,
         is_delete_operator_pod=True,
         in_cluster=True,
@@ -61,4 +61,84 @@ with DAG(
         get_logs=True,
     )
 
-extractor >> loader >> transformer
+    specialty_merge_extractor = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_from=[etl_config, aims],
+        env=dict(TASK_CLASS=datalabs.etl.jdbc.extract.JDBCExtractorTask),
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="specialty_merge_extractor",
+        get_logs=True,
+    )
+
+    specialty_merge_transformer = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_from=etl_config,
+        env=dict(TASK_WRAPPER_CLASS=datalabs.etl.kubernetes.ETLComponentTaskWrapper),
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="specialty_merge_transformer",
+        get_logs=True,
+    )
+
+    specialty_merge_loader = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_from=[etl_config, minio],
+        env=dict(TASK_WRAPPER_CLASS=datalabs.etl.kubernetes.ETLComponentTaskWrapper),
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="specialty_merge_loader",
+        get_logs=True,
+    )
+
+    specialty_extractor = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_from=[etl_config, aims],
+        env=dict(TASK_CLASS=datalabs.etl.jdbc.extract.JDBCExtractorTask),
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="specialty_extractor",
+        get_logs=True,
+    )
+
+    specialty_transformer = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_from=etl_config,
+        env=dict(TASK_WRAPPER_CLASS=datalabs.etl.kubernetes.ETLComponentTaskWrapper),
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="specialty_transformer",
+        get_logs=True,
+    )
+
+    specialty_loader = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image='docker-registry.default.svc:5000/hsg-data-labs-dev/datalabs-master:1.0.0',
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_from=[etl_config, minio],
+        env=dict(TASK_WRAPPER_CLASS=datalabs.etl.kubernetes.ETLComponentTaskWrapper),
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="specialty_loader",
+        get_logs=True,
+    )
+
+type_of_practice_extractor >> type_of_practice_transformer >> type_of_practice_loader
+specialty_merge_extractor >> specialty_merge_transformer >> specialty_merge_loader
+specialty_extractor >> specialty_transformer >> specialty_loader
