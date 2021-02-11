3444c773bb3fbb99e98f07092b56cc5f24c0ca57
Peter Lane <plane@ama-assn.org>
2021-02-11 10:20:34 -0600

    Add marshmallow library explicitly to the Master requirements. Fix VariableTree bug.
    Former-commit-id: 0971a3d3579a100d358e777f15d89fa4eb879d96

--- Source/Python/datalabs/access/environment.py
+++ Source/Python/datalabs/access/environment.py
@@ -10,7 +10,6 @@ LOGGER.setLevel(logging.INFO)
 class VariableTree:
     def __init__(self, root, separator=None):
         self._root = root
-        self._separator = separator or '_'
 
     def __repr__(self):
         return str(self._root)
@@ -18,13 +17,14 @@ class VariableTree:
     @classmethod
     def generate(cls, separator=None):
         ''' Generate a VariableTree from all current environment variables. '''
+        separator = separator or '_'
         root = dict()
 
         # pylint: disable=protected-access
         for name, value in os.environ._data.items():
-            cls._create_branch(root, name.decode('utf-8'), value.decode('utf-8'))
+            cls._create_branch(root, name.decode('utf-8'), value.decode('utf-8'), separator)
 
-        return VariableTree(root, separator)
+        return VariableTree(root)
 
     def get_value(self, branches: list):
         branch = self._root
@@ -53,10 +53,10 @@ class VariableTree:
 
 
     @classmethod
-    def _create_branch(cls, trunk, name, value):
+    def _create_branch(cls, trunk, name, value, separator):
         LOGGER.debug('Name: %s', name)
-        if self._separator in name:
-            prefix, suffix = name.split(self._separator', 1)
+        if separator in name:
+            prefix, suffix = name.split(separator, 1)
             LOGGER.debug('Prefix: %s\tSuffix: %s', prefix, suffix)
 
             if prefix not in trunk:
@@ -66,7 +66,7 @@ class VariableTree:
                 )
 
             if prefix and suffix:
-                cls._create_branch(trunk[prefix]['branches'], suffix, value)
+                cls._create_branch(trunk[prefix]['branches'], suffix, value, separator)
         else:
             if name not in trunk:
                 trunk[name] = dict(
