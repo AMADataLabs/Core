a654dc5bea69fbbc7f50bb70db2e5bfb44ab48e4
Peter Lane <plane@ama-assn.org>
2021-02-19 10:47:38 -0600

    Add unit tests for TaskParameterSchemaMixin.
    Former-commit-id: 89607802967767cd2dcbf420b93c5e77eded38fe

--- Test/Python/test/datalabs/etl/test_task.py
+++ Test/Python/test/datalabs/etl/test_task.py
@@ -1,10 +1,12 @@
 """ source: datalabs.etl.task """
+from   dataclasses import dataclass
 import os
 
 import mock
 import pytest
 
 import datalabs.etl.task as task
+from   datalabs.task import add_schema
 
 
 # pylint: disable=redefined-outer-name, protected-access
@@ -30,6 +32,51 @@ def test_etl_task_wrapper(environment_variables):
     assert wrapper.task._loader.data == 'True'
 
 
+def test_get_validated_parameters_returns_proper_object(parameters):
+    parameters.transformer.variables.pop('TASK_CLASS')
+    task = DummyTask(parameters.transformer)
+
+    assert isinstance(task._parameters, TaskParameters)
+    assert hasattr(task._parameters, 'cooley')
+    assert task._parameters.cooley == 'Boyz'
+    assert hasattr(task._parameters, 'high')
+    assert task._parameters.high == 'II'
+    assert hasattr(task._parameters, 'harmony')
+    assert task._parameters.harmony == 'Men'
+
+
+def test_get_validated_parameters_preserves_input_data(parameters):
+    parameters.transformer.variables.pop('TASK_CLASS')
+    parameters.transformer.data = ['East', 'Coast', 'Family']
+    task = DummyTask(parameters.transformer)
+
+    assert isinstance(task._parameters, TaskParameters)
+    assert hasattr(task._parameters, 'data')
+    assert task._parameters.data == ['East', 'Coast', 'Family']
+
+
+
+
+class DummyTask(task.ETLComponentTask, task.TaskParameterSchemaMixin):
+    def __init__(self, parameters):
+        super().__init__(parameters)
+
+        self._parameters = self._get_validated_parameters(TaskParameters)
+
+    def run(self):
+        self._data = self._parameters.data
+
+
+@add_schema
+@dataclass
+# pylint: disable=too-many-instance-attributes
+class TaskParameters:
+    cooley: str
+    high: str
+    harmony: str
+    data: object
+
+
 @pytest.fixture
 def parameters():
     return task.ETLParameters(
@@ -37,7 +84,12 @@ def parameters():
             variables=dict(TASK_CLASS='test.datalabs.etl.test_extract.Extractor', thing=True)
         ),
         transformer=task.ETLComponentParameters(
-            variables=dict(TASK_CLASS='test.datalabs.etl.test_transform.Transformer')
+            variables=dict(
+                TASK_CLASS='test.datalabs.etl.test_transform.Transformer',
+                COOLEY='Boyz',
+                HIGH='II',
+                HARMONY='Men'
+            )
         ),
         loader=task.ETLComponentParameters(
             variables=dict(TASK_CLASS='test.datalabs.etl.test_load.Loader')
@@ -53,6 +105,9 @@ def environment_variables():
     os.environ['EXTRACTOR__thing'] = 'True'
 
     os.environ['TRANSFORMER__TASK_CLASS'] = 'test.datalabs.etl.test_transform.Transformer'
+    os.environ['TRANSFORMER__COOLEY'] = 'Boyz'
+    os.environ['TRANSFORMER__HIGH'] = 'II'
+    os.environ['TRANSFORMER__HARMONY'] = 'Men'
 
     os.environ['LOADER__TASK_CLASS'] = 'test.datalabs.etl.test_load.Loader'
     os.environ['EXTRACTOR__DATABASE_HOST'] = 'ping.pong.com'
