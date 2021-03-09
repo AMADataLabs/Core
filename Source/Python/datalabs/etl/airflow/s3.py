5cad5b9b6f0d70d1270379f6ee0b1aa481e783b5
rnajeeb <rudainah.najeeb@ama-assn.org>
2021-03-09 15:36:00 -0600

    S3 Cache loader to handle bytes data
    Former-commit-id: 988d22bed5cfd0b5d39e50c584d8c238eba7b542

--- Source/Python/datalabs/etl/airflow/s3.py
+++ Source/Python/datalabs/etl/airflow/s3.py
@@ -1,14 +1,18 @@
 ''' Task Data Cache implementation for using S3 '''
 from   datalabs.etl.airflow.task import TaskDataCache
-from   datalabs.etl.s3.extract import S3UnicodeTextFileExtractorTask
-from   datalabs.etl.s3.load import S3UnicodeTextFileLoaderTask
+from   datalabs.etl.s3.extract import S3UnicodeTextFileExtractorTask, S3FileExtractorTask
+from   datalabs.etl.s3.load import S3UnicodeTextFileLoaderTask, S3FileLoaderTask
 
 
 class S3TaskDataCache(TaskDataCache):
     def extract_data(self):
         ''' Pull cached data from files on S3, assuming is in CSV format.'''
         cache_parameters = self._parameters
-        cache_extractor = S3UnicodeTextFileExtractorTask(cache_parameters)
+
+        if isinstance(self._parameters['data'], bytes):
+            cache_extractor = S3FileExtractorTask(cache_parameters)
+        else:
+            cache_extractor = S3UnicodeTextFileExtractorTask(cache_parameters)
 
         cache_extractor.run()
 
@@ -17,7 +21,11 @@ class S3TaskDataCache(TaskDataCache):
     def load_data(self, output_data):
         self._parameters['data'] = output_data
         cache_parameters = self._parameters
-        cache_loader = S3UnicodeTextFileLoaderTask(cache_parameters)
+
+        if isinstance(self._parameters['data'], bytes):
+            cache_loader = S3FileLoaderTask(cache_parameters)
+        else:
+            cache_loader = S3UnicodeTextFileLoaderTask(cache_parameters)
 
         cache_loader.run()
 
