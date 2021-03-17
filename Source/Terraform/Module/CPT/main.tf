0ab552b24a4cb359b1261911d1a6eda6d5155a05
Peter Lane <plane@ama-assn.org>
2021-03-17 12:01:39 -0500

    Add instance of the TE RDS module.
    Former-commit-id: 5f0951cd2dee6dfda9baca9052a0d0db923d0a95

--- Source/Terraform/Module/CPT/main.tf
+++ Source/Terraform/Module/CPT/main.tf
@@ -1,3 +1,81 @@
+data "aws_vpc" "datalake" {
+    tags = {
+        Name = "Data Lake VPC"
+    }
+}
+
+
+data "aws_route_table" "datalake" {
+  vpc_id = data.aws_vpc.datalake.id
+}
+
+
+resource "aws_subnet" "cpt_api_database_20" {
+    vpc_id                  = data.aws_vpc.datalake.id
+    cidr_block              = "172.31.20.0/24"
+    availability_zone       = "us-east-1a"
+
+    tags = merge(local.tags, {Name = "${var.project} API Subnet 1"})
+}
+
+
+resource "aws_route_table_association" "cpt_api_database_20" {
+    subnet_id      = aws_subnet.cpt_api_database_20.id
+    route_table_id = data.aws_route_table.datalake.id
+}
+
+
+resource "aws_subnet" "cpt_api_database_21" {
+    vpc_id                  = data.aws_vpc.datalake.id
+    cidr_block              = "172.31.21.0/24"
+    availability_zone       = "us-east-1b"
+
+    tags = merge(local.tags, {Name = "${var.project} API Subnet 2"})
+}
+
+
+resource "aws_route_table_association" "cpt_api_database_21" {
+    subnet_id      = aws_subnet.cpt_api_database_21.id
+    route_table_id = data.aws_route_table.datalake.id
+}
+
+
+module "cpt_api_database" {
+    source                          = "git::ssh://git@bitbucket.ama-assn.org:7999/te/terraform-aws-rds.git?ref=development"
+    # db_instance_class               = var.rds_instance_class
+    db_instance_storage_type        = var.rds_storage_type
+    db_instance_port                = local.rds_port
+    db_instance_allocated_storage   = 20
+    max_allocated_storage           = 1000
+    db_engine                       = local.rds_engine
+    use_engine_version              = true
+    db_engine_version               = local.rds_engine_version
+    database_username               = local.database_username
+    database_password               = local.database_password
+    db_subnet_list                  = [aws_subnet.cpt_api_database_20.id, aws_subnet.cpt_api_database_21.id]
+
+    # should default to false
+    create_initial_db               = false
+    use_snapshot                    = false
+
+    app_name                        = "cpt-api"
+    app_environment                 = lower(data.aws_ssm_parameter.account_environment.value)
+
+    tag_name                        = "${var.project} API Database"
+    tag_environment                 = local.tags["Env"]
+    tag_contact                     = local.tags["Contact"]
+    tag_systemtier                  = local.tags["SystemTier"]
+    tag_drtier                      = local.tags["DRTier"]
+    tag_dataclassification          = local.tags["DataClassification"]
+    tag_budgetcode                  = local.tags["BudgetCode"]
+    tag_owner                       = local.tags["Owner"]
+    tag_projectname                 = var.project
+    tag_notes                       = ""
+    tag_eol                         = local.tags["EOL"]
+    tag_maintwindow                 = local.tags["MaintenanceWindow"]
+
+}
+
 resource "aws_db_instance" "cpt_api_database" {
     identifier                    = local.rds_instance_name
     instance_class                = var.rds_instance_class
@@ -6,7 +84,7 @@ resource "aws_db_instance" "cpt_api_database" {
     name                          = local.database_name
     allocated_storage             = 20
     engine                        = local.rds_engine
-    engine_version                = "11.10"
+    engine_version                = local.rds_engine_version
     parameter_group_name          = "default.postgres11"
     max_allocated_storage         = 1000
     publicly_accessible           = true
@@ -422,7 +500,8 @@ locals {
     database_secret     = jsondecode(data.aws_secretsmanager_secret_version.database.secret_string)
     rds_instance_name   = local.database_secret["dbinstanceIdentifier"]
     rds_engine          = local.database_secret["engine"]
-    rds_port          = local.database_secret["port"]
+    rds_engine_version  = "11.10"
+    rds_port            = local.database_secret["port"]
     database_name       = local.database_secret["dbname"]
     database_username   = local.database_secret["username"]
     database_password   = local.database_secret["password"]
