0ee52c443fd7ca68c344abf55a9ab98b0c556639
rnajeeb <rudainah.najeeb@ama-assn.org>
2021-03-23 09:52:37 -0500

    Update transformer with abstract methods
    Former-commit-id: 1a0e59c6a1c384a7abd4abff609befc6e482b20e

--- Source/Python/datalabs/etl/oneview/reference/transform.py
+++ Source/Python/datalabs/etl/oneview/reference/transform.py
@@ -57,14 +57,9 @@ class SpecialtyMergeTransformerTask(TransformerTask):
 
 
 class FederalInformationProcessingStandardCountyTransformerTask(TransformerTask):
-    def _transform(self):
-        fips_data = self._make_dataframe()
-        self._parameters['data'] = [self.set_columns(df).to_csv() for df in fips_data]
-
-        return super()._transform()
-
-    def _make_dataframe(self):
-        return [pandas.read_excel(StringIO(data), skiprows=4) for data in self._parameters['data']]
+    def _to_dataframe(self):
+        fips_data = [pandas.read_excel(BytesIO(data), skiprows=4) for data in self._parameters['data']]
+        return [self.set_columns(df) for df in fips_data]
 
     @classmethod
     def set_columns(cls, fips_data):
