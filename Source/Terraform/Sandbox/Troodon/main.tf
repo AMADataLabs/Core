b3906298c5dbe9947e980b94f5b59551cb500dfa
Peter Lane <plane@ama-assn.org>
2021-03-25 14:01:19 -0500

    Initial, unharmonized import of Troodon CodeBuild role and polciy.
    Former-commit-id: 2be36d429daa337aa0a0dc00c0839a0e452f67b0

--- Source/Terraform/Sandbox/Troodon/main.tf
+++ Source/Terraform/Sandbox/Troodon/main.tf
@@ -12,6 +12,7 @@ terraform {
     }
 }
 
+
 resource "aws_codebuild_project" "troodon" {
     name          = "CodeBuildProject-GskTYS0yOv1X"
     service_role  = "arn:aws:iam::644454719059:role/SC-644454719059-pp-iho6z7zkmlz22-CodeBuildRole-1TC3J9IPK4TAQ"
@@ -36,4 +37,89 @@ resource "aws_codebuild_project" "troodon" {
         packaging = "NONE"
         path = "terraformbuilds"
     }
+}
+
+
+resource "aws_iam_role" "troodon" {
+    name = "SC-644454719059-pp-iho6z7zkmlz22-CodeBuildRole-1TC3J9IPK4TAQ"
+
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy" "troodon" {
+    role = aws_iam_role.example.troodon
+
+    policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ],
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateNetworkInterface",
+        "ec2:DescribeDhcpOptions",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:DeleteNetworkInterface",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeVpcs"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateNetworkInterfacePermission"
+      ],
+      "Resource": [
+        "arn:aws:ec2:us-east-1:123456789012:network-interface/*"
+      ],
+      "Condition": {
+        "StringEquals": {
+          "ec2:Subnet": [
+            "${aws_subnet.example1.arn}",
+            "${aws_subnet.example2.arn}"
+          ],
+          "ec2:AuthorizedService": "codebuild.amazonaws.com"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.example.arn}",
+        "${aws_s3_bucket.example.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
 }
