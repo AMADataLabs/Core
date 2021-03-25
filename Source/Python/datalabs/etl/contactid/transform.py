71e5f2f9e7ac5fe27a5f9578b8aead775ba170f4
Nikhil Khatri <nikhil.khatri@ama-assn.org>
2021-03-25 13:54:41 -0500

    added Id to contacts
    Former-commit-id: 7cd20bfbc31462b668ba05df9a0b7f1c0c05626e

--- Source/Python/datalabs/etl/contactid/transform.py
+++ Source/Python/datalabs/etl/contactid/transform.py
@@ -23,13 +23,9 @@ class ContactIDMergeTransformerTask(etl.TransformerTask, ABC):
 
         LOGGER.info(sfmc_contacts.head(2))
 
+        csv_data = [self._dataframe_to_csv(data) for data in [sfmc_contacts, api_orders, active_subscription, users]]
 
-        #selected_data = self._select_columns(dataframes)
-        #renamed_data = self._rename_columns(selected_data)
-
-        #csv_data = [self._dataframe_to_csv(data) for data in renamed_data]
-
-        #return [data.encode('utf-8', errors='backslashreplace') for data in csv_data]
+        return [data.encode('utf-8', errors='backslashreplace') for data in csv_data]
 
     def _to_dataframe(self):
         return [pandas.read_csv(BytesIO(file)) for file in self._parameters['data']]
@@ -37,30 +33,17 @@ class ContactIDMergeTransformerTask(etl.TransformerTask, ABC):
     def _assign_id_to_contacts(self, sfmc_contacts):
         sfmc_contacts["HSContactID"] = np.nan
         emppid = -1
-        for ind in org_manager.index:
-            if (sfmc_contacts['EMPPID'][ind] != emppid):
+        for index in sfmc_contacts.index:
+            if (sfmc_contacts['EMPPID'][index] != emppid):
                 id = uuid.uuid1()
-                org_manager['HSContactID'][ind] = id.int
+                sfmc_contacts['HSContactID'][index] = id.int
             else:
-                prev_ind = ind - 1
-                sfmc_contacts['HSContactID'][ind] = org_manager['HSContactID'][prev_ind]
-            emppid = (sfmc_contacts['EMAIL'][ind]
-'''    
-    def _select_columns(self, dataset):
-        names = [list(column_map.keys()) for column_map in self._get_columns()]
-        LOGGER.info(names)
-        return [data[name] for name, data in zip(names, dataset)]
+                prev_index = index - 1
+                sfmc_contacts['HSContactID'][index] = sfmc_contacts['HSContactID'][prev_index]
+            emppid = (sfmc_contacts['HSContactID'][index]
 
-    def _rename_columns(self, dataset):
-        column_maps = self._get_columns()
 
-        return [data.rename(columns=column_map) for data, column_map in zip(dataset, column_maps)]
-
-    @abstractmethod
-    def _get_columns(self):
-        return []
 
     @classmethod
     def _dataframe_to_csv(cls, data):
         return data.to_csv(index=False, quoting=csv.QUOTE_NONNUMERIC)
-'''
