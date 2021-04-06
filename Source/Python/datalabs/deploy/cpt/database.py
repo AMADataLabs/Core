53d9b38fcc2aaf0d15f6b6075999b394660ddb36
Peter Lane <plane@ama-assn.org>
2021-04-06 10:33:46 -0500

    Initial go at a database creation build step for CPT.
    Former-commit-id: 5d8340fc6387f1c9e3c73a1869cd33c478dc494e

---
+++ Source/Python/datalabs/deploy/cpt/database.py
@@ -0,0 +1,20 @@
+import os
+
+import datalabs.deploy.database as database
+
+
+def create_unless_exists():
+    parmameters = dict(
+        name=os.environ.get('DATABASE_NAME'),
+        backend=os.environ.get('DATABASE_BACKEND'),
+        host=os.environ.get('DATABASE_HOST'),
+        port=os.environ.get('DATABASE_PORT'),
+        username=os.environ.get('DATABASE_USERNAME'),
+        password=os.environ.get('DATABASE_PASSWORD')
+    )
+
+    if not database.exists(parameters):
+        name = parameters['name']
+        parameters['name'] = os.environ.get('DATABASE_NAME_ADMIN')
+
+        database.create(parameters, name)
