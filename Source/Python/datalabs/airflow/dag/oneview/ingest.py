1408f3884f1e21a446627f940b09fc863ae79803
rnajeeb <rudainah.najeeb@ama-assn.org>
2021-04-13 16:03:09 -0500

    Update oneview build image
    Former-commit-id: 2f1add00d39a02da6cac416ffab5910494ea4830

--- Source/Python/datalabs/airflow/dag/oneview/ingest.py
+++ Source/Python/datalabs/airflow/dag/oneview/ingest.py
@@ -12,7 +12,7 @@ AIMS_SECRET = Secret('env', None, 'oneview-etl-aims')
 ODS_SECRET = Secret('env', None, 'oneview-etl-ods')
 SFTP_SECRET = Secret('env', None, 'oneview-etl-sftp')
 DATABASE_SECRET = Secret('env', None, 'oneview-etl-database')
-DOCKER_IMAGE = 'docker-registry.default.svc:5000/hsg-data-labs-dev/oneview-etl:1.2.3'
+DOCKER_IMAGE = 'docker-registry.default.svc:5000/hsg-data-labs-dev/oneview-etl:1.2.4'
 
 ONEVIEW_ETL_DAG = DAG(
     dag_id='oneview',
