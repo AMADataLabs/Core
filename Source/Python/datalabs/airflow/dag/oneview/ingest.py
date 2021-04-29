03721882247ed0152729deb8745677784ee9b027
rnajeeb <rudainah.najeeb@ama-assn.org>
2021-04-29 14:30:49 -0500

    Added Melissa extraction
    Former-commit-id: c1460c580146a2681031818ff0c4615e59eae624

--- Source/Python/datalabs/airflow/dag/oneview/ingest.py
+++ Source/Python/datalabs/airflow/dag/oneview/ingest.py
@@ -204,6 +204,21 @@ with ONEVIEW_ETL_DAG:
         get_logs=True,
     )
 
+    EXTRACT_MELISSA = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image=DOCKER_IMAGE,
+        name="extract_melissa",
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_from=[ETL_CONFIG],
+        secrets=[ODS_SECRET, MINIO_SECRET],
+        env_vars=dict(TASK_CLASS='datalabs.etl.jdbc.extract.JDBCExtractorTask'),
+        do_xcom_push=False,
+        is_delete_operator_pod=False,
+        in_cluster=True,
+        task_id="extract_melissa",
+        get_logs=True,
+    )
+
     CREATE_PHYSICIAN_TABLE = KubernetesPodOperator(
         namespace='hsg-data-labs-dev',
         image=DOCKER_IMAGE,
@@ -506,3 +521,4 @@ EXTRACT_PHYSICIAN_RACE_ETHNICITY >> CREATE_PHYSICIAN_RACE_ETHNICITY_TABLE >> LOA
 # CREATE_RESIDENCY_PROGRAM_TABLES >> CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE
 # CREATE_PHYSICIAN_TABLE >> CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE
 # CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE >> LOAD_TABLES_INTO_DATABASE
+EXTRACT_MELISSA
