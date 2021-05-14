ed5fe27715d8acf8d4e8b8d624e2cb1beb0360d8
Peter Lane <plane@ama-assn.org>
2021-05-14 18:06:59 -0500

    Add scheduler S3 bucket, SNS data topic, and DynamoDB state tables. Add in-progress scheduler transformer task.
    Former-commit-id: b4d17d815c474ccf7bdbe5eac3bf9541d282ace9

--- Source/Terraform/Sandbox/DataLake/main.tf
+++ Source/Terraform/Sandbox/DataLake/main.tf
@@ -527,7 +527,7 @@ module "sns_scheduler_data" {
     topic_name      = "scheduler_data-${var.environment}"
     region          = var.region
     account_id      = data.aws_caller_identity.account.account_id
-    s3_bucket_name  = module.s3_processed_data.bucket_id
+    s3_bucket_name  = module.s3_scheduler_data.bucket_id
   }
 
   name = "scheduler_data-${var.environment}"
@@ -1098,6 +1098,73 @@ module "datanow_efs" {
 #         Department                          = local.department
 #     }
 # }
+
+
+#####################################################################
+# Datalake - DynamoDB Instances and Tables
+#####################################################################
+
+resource "aws_dynamodb_table" "scheduler_locks" {
+    name            = "${var.project}-${var.environment}-scheduler-locks"
+    billing_mode    = "PAY_PER_REQUEST"
+    # read_capacity   = 10
+    # write_capacity  = 2
+    hash_key        = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+
+    tags = merge(local.tags, {Name = "Data Labs DAG Scheduler Lock Table"})
+}
+
+resource "aws_dynamodb_table" "dag_state" {
+    name            = "${var.project}-${var.environment}-dag-state"
+    billing_mode    = "PAY_PER_REQUEST"
+    # read_capacity  = 10
+    # write_capacity = 2
+    hash_key       = "name"
+
+    attribute {
+        name = "name"
+        type = "S"
+    }
+
+    ttl {
+      attribute_name = "ttl"
+      enabled        = true
+    }
+
+    tags = merge(local.tags, {Name = "Data Labs DAG State Table"})
+}
+
+resource "aws_dynamodb_table" "task_state" {
+    name            = "${var.project}-${var.environment}-task-state"
+    billing_mode    = "PAY_PER_REQUEST"
+    # read_capacity  = 10
+    # write_capacity = 2
+    hash_key       = "name"
+    range_key      = "DAG"
+
+    attribute {
+        name = "name"
+        type = "S"
+    }
+
+    attribute {
+        name = "DAG"
+        type = "S"
+    }
+
+    ttl {
+      attribute_name = "ttl"
+      enabled        = true
+    }
+
+    tags = merge(local.tags, {Name = "Data Labs Task State Table"})
+}
+
 
 
 # aws_lb.etcd.dns_name
