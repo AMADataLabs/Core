a6cb7f0729a9cf02e6676459b4d25fa12a2ca923
rnajeeb <rudainah.najeeb@ama-assn.org>
2021-05-19 16:39:00 -0500

    Updated linking table config
    Former-commit-id: 26ab1dff9260c282bbad1806ac7e9367fc3a9928

--- Source/Python/datalabs/airflow/dag/oneview/ingest.py
+++ Source/Python/datalabs/airflow/dag/oneview/ingest.py
@@ -420,53 +420,54 @@ with ONEVIEW_ETL_DAG:
         get_logs=True,
     )
 
-#     CREATE_CREDENTIALING_CUSTOMER_INSTITUTION_TABLE = KubernetesPodOperator(
-#         namespace='hsg-data-labs-dev',
-#         image=DOCKER_IMAGE,
-#         name="create_credentialing_customer_institution_table",
-#         cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
-        # env_vars=dict(
-        #     TASK_CLASS='datalabs.etl.oneview.link.transform.CredentialingCustomerInstitutionTransformerTask'
-        # ),
-#         do_xcom_push=False,
-#         is_delete_operator_pod=True,
-#         in_cluster=True,
-#         task_id="create_credentialing_customer_institution_table",
-#         get_logs=True,
-#     )
-#
-#     CREATE_CREDENTIALING_CUSTOMER_BUSINESS_TABLE = KubernetesPodOperator(
-#         namespace='hsg-data-labs-dev',
-#         image=DOCKER_IMAGE,
-#         name="create_credentialing_customer_business_table",
-#         cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
-#         env_vars={
-#            **BASE_ENVIRONMENT,
-#            **dict(TASK_CLASS='datalabs.etl.oneview.link.transform.CredentialingCustomerBusinessTransformerTask')
-#         },
-#         do_xcom_push=False,
-#         is_delete_operator_pod=True,
-#         in_cluster=True,
-#         task_id="create_credentialing_customer_business_table",
-#         get_logs=True,
-#     )
-#
-#     CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE = KubernetesPodOperator(
-#         namespace='hsg-data-labs-dev',
-#         image=DOCKER_IMAGE,
-#         name="create_residency_program_physician_table",
-#         cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
-#         env_vars={
-#            **BASE_ENVIRONMENT,
-#            **dict(TASK_CLASS='datalabs.etl.oneview.link.transform.ResidencyProgramPhysicianTransformerTask')
-#         },
-#         do_xcom_push=False,
-#         is_delete_operator_pod=True,
-#         in_cluster=True,
-#         task_id="create_residency_program_physician_table",
-#         get_logs=True,
-#     )
-#
+    CREATE_CREDENTIALING_CUSTOMER_INSTITUTION_TABLE = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image=DOCKER_IMAGE,
+        name="create_credentialing_customer_institution_table",
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_vars={
+            **BASE_ENVIRONMENT,
+            **dict(TASK_CLASS='datalabs.etl.oneview.link.transform.CredentialingCustomerInstitutionTransformerTask')
+        },
+        do_xcom_push=False,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="create_credentialing_customer_institution_table",
+        get_logs=True,
+    )
+
+    CREATE_CREDENTIALING_CUSTOMER_BUSINESS_TABLE = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image=DOCKER_IMAGE,
+        name="create_credentialing_customer_business_table",
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_vars={
+           **BASE_ENVIRONMENT,
+           **dict(TASK_CLASS='datalabs.etl.oneview.link.transform.CredentialingCustomerBusinessTransformerTask')
+        },
+        do_xcom_push=False,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="create_credentialing_customer_business_table",
+        get_logs=True,
+    )
+
+    CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image=DOCKER_IMAGE,
+        name="create_residency_program_physician_table",
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_vars={
+           **BASE_ENVIRONMENT,
+           **dict(TASK_CLASS='datalabs.etl.oneview.link.transform.ResidencyProgramPhysicianTransformerTask')
+        },
+        do_xcom_push=False,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="create_residency_program_physician_table",
+        get_logs=True,
+    )
+
     LOAD_PHYSICIAN_TABLE_INTO_DATABASE = KubernetesPodOperator(
         namespace='hsg-data-labs-dev',
         image=DOCKER_IMAGE,
@@ -558,6 +559,19 @@ with ONEVIEW_ETL_DAG:
         get_logs=True,
     )
 
+    LOAD_LINKING_TABLES_INTO_DATABASE = KubernetesPodOperator(
+        namespace='hsg-data-labs-dev',
+        image=DOCKER_IMAGE,
+        name="load_linking_tables_into_database",
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_vars={**BASE_ENVIRONMENT, **dict(TASK_CLASS='datalabs.etl.orm.load.ORMLoaderTask')},
+        do_xcom_push=False,
+        is_delete_operator_pod=False,
+        in_cluster=True,
+        task_id="load_linking_tables_into_database",
+        get_logs=True,
+    )
+
     MIGRATE_DATABASE = KubernetesPodOperator(
         namespace='hsg-data-labs-dev',
         image=DOCKER_IMAGE,
@@ -592,13 +606,13 @@ EXTRACT_CREDENTIALING_ADDRESSES >> MERGE_CREDENTIALING_ADDRESSES_INTO_CUSTOMER_T
 CREATE_CREDENTIALING_CUSTOMER_PRODUCT_AND_ORDER_TABLES >> MERGE_CREDENTIALING_ADDRESSES_INTO_CUSTOMER_TABLE
 # MERGE_CREDENTIALING_ADDRESSES_INTO_CUSTOMER_TABLE # >> LOAD_CREDENTIALING_TABLES_INTO_DATABASE
 EXTRACT_PHYSICIAN_RACE_ETHNICITY >> CREATE_PHYSICIAN_RACE_ETHNICITY_TABLE # >> LOAD_RACE_ETHNICITY_TABLE_INTO_DATABASE
-# MERGE_CREDENTIALING_ADDRESSES_INTO_CUSTOMER_TABLE >> CREATE_CREDENTIALING_CUSTOMER_INSTITUTION_TABLE
-# CREATE_RESIDENCY_PROGRAM_TABLES >> CREATE_CREDENTIALING_CUSTOMER_INSTITUTION_TABLE
-# CREATE_CREDENTIALING_CUSTOMER_INSTITUTION_TABLE # >> LOAD_TABLES_INTO_DATABASE
-# MERGE_CREDENTIALING_ADDRESSES_INTO_CUSTOMER_TABLE >> CREATE_CREDENTIALING_CUSTOMER_BUSINESS_TABLE
-# CREATE_BUSINESS_AND_PROVIDER_TABLES >> CREATE_CREDENTIALING_CUSTOMER_BUSINESS_TABLE
-# CREATE_CREDENTIALING_CUSTOMER_BUSINESS_TABLE # >> LOAD_TABLES_INTO_DATABASE
-# CREATE_RESIDENCY_PROGRAM_TABLES >> CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE
-# CREATE_PHYSICIAN_TABLE >> CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE
-# CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE >> LOAD_TABLES_INTO_DATABASE
+MERGE_CREDENTIALING_ADDRESSES_INTO_CUSTOMER_TABLE >> CREATE_CREDENTIALING_CUSTOMER_INSTITUTION_TABLE
+CREATE_RESIDENCY_PROGRAM_TABLES >> CREATE_CREDENTIALING_CUSTOMER_INSTITUTION_TABLE
+# CREATE_CREDENTIALING_CUSTOMER_INSTITUTION_TABLE # >> LOAD_LINKING_TABLES_INTO_DATABASE
+MERGE_CREDENTIALING_ADDRESSES_INTO_CUSTOMER_TABLE >> CREATE_CREDENTIALING_CUSTOMER_BUSINESS_TABLE
+CREATE_BUSINESS_AND_PROVIDER_TABLES >> CREATE_CREDENTIALING_CUSTOMER_BUSINESS_TABLE
+# CREATE_CREDENTIALING_CUSTOMER_BUSINESS_TABLE # >> LOAD_LINKING_TABLES_INTO_DATABASE
+CREATE_RESIDENCY_PROGRAM_TABLES >> CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE
+CREATE_PHYSICIAN_TABLE >> CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE
+# CREATE_RESIDENCY_PROGRAM_PHYSICIAN_TABLE >> LOAD_LINKING_TABLES_INTO_DATABASE
 EXTRACT_MELISSA >> CREATE_MELISSA_TABLES # >> LOAD_MELISSA_TABLES_INTO_DATABASE
