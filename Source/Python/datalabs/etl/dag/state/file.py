0e55cabb463ba7bf123b64e47cdae598e86428e8
Peter Lane <plane@ama-assn.org>
2021-05-20 13:44:04 -0500

    Fix issues with file-based DAG state implementation.
    Former-commit-id: 2f9288ae52ef99028d47480c9ca8b33d4504b7b8

--- Source/Python/datalabs/etl/dag/state/file.py
+++ Source/Python/datalabs/etl/dag/state/file.py
@@ -1,10 +1,13 @@
 """ File-based DAG state classes. """
+from   dataclasses import dataclass
 from   datetime import datetime
+import os
 from   pathlib import Path
 
 import pytz
 
 from   datalabs.etl.dag.state.base import State, Status
+from   datalabs.parameter import add_schema
 
 
 @add_schema
@@ -15,9 +18,11 @@ class DAGStateParameters:
 
 
 class DAGState(State):
-    PARAMETER_CLASS = FileStateParameters
+    PARAMETER_CLASS = DAGStateParameters
 
-    @abstractmethod
+    def connect(self):
+        pass
+
     def get_status(self, name: str, execution_time: datetime):
         state_path = self._generate_state_path(name, execution_time)
         status = Status.Unknown
@@ -30,14 +35,16 @@ class DAGState(State):
 
         return status
 
-    @abstractmethod
     def set_status(self, name: str, execution_time: datetime, status: Status):
         state_path = self._generate_state_path(name, execution_time)
+
+        if not state_path.parent.exists():
+            os.makedirs(state_path.parent)
 
         with open(state_path, 'w') as file:
             file.write(status.value)
 
-    def _generate_state_path(cls, name: str, execution_time: datetime):
+    def _generate_state_path(self, name: str, execution_time: datetime):
         execution_time = execution_time.astimezone(pytz.timezone("UTC"))
         datestamp = execution_time.strftime('%Y-%m-%d')
         timestamp = execution_time.strftime('%H%M%S')
