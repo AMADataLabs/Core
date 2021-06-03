8f8b026f63e61a5e52e82670b7a45194ccee9355
Garrett <garrett.lappe@ama-assn.org>
2021-06-03 13:55:45 -0500

    Added parameter class for email loader
    Former-commit-id: 0e8cb71f91ad5389733cd844cc9596b7704d3591

--- Source/Python/datalabs/analysis/dbl/load.py
+++ Source/Python/datalabs/analysis/dbl/load.py
@@ -1,20 +1,32 @@
+from dataclasses import dataclass
 import os
 
+from datalabs.parameter import add_schema
 from datalabs.messaging.email_message import send_email
 from datalabs.etl.load import LoaderTask
-from datalabs.etl.fs.load import LocalUnicodeTextFileLoaderTask
+from datalabs.etl.fs.load import LocalUnicodeTextFileLoaderTask  # tbd
+
+
+@add_schema
+@dataclass
+# pylint: disable=too-many-instance-attributes
+class DBLReportEmailLoaderParameters:
+    to: str
+    cc: str
+    subject: str
+    body: str
+    from_account: str
 
 
 class DBLReportEmailLoaderTask(LoaderTask):
-    def __init__(self, parameters):
-        super().__init__(parameters)
+    PARAMETER_CLASS = DBLReportEmailLoaderParameters
 
     def _load(self):
         send_email(
-            to=self._parameters['to'],
-            cc=self._parameters['cc'],
-            subject=self._parameters['subject'],
-            body=self._parameters['body'],
-            from_account=self._parameters['from_account'],
+            to=self._parameters.to,
+            cc=self._parameters.cc,
+            subject=self._parameters.subject,
+            body=self._parameters.body,
+            from_account=self._parameters.from_account,
             attachments=self._parameters['data']
         )
