f9e1a5c9312bdec19b21b5eecbe680cceb230282
Peter Lane <plane@ama-assn.org>
2021-06-03 14:10:08 -0500

    Copy generic version of deployment helper scripts from CPT. Update Pipelines for OneView deployments to Sandbox.
    Former-commit-id: 9b74c78a32e781b87ba995932630783626b9bb5c

--- Source/Python/datalabs/deploy/database.py
+++ Source/Python/datalabs/deploy/database.py
@@ -23,3 +23,20 @@ def create(parameters, name):
     with Database.from_parameters(parameters) as database:
         database.execute("commit")
         database.execute(f'create database {name}')
+
+
+def create_unless_exists():
+    parameters = dict(
+        name=os.environ.get('DATABASE_NAME'),
+        backend=os.environ.get('DATABASE_BACKEND'),
+        host=os.environ.get('DATABASE_HOST'),
+        port=os.environ.get('DATABASE_PORT'),
+        username=os.environ.get('DATABASE_USERNAME'),
+        password=os.environ.get('DATABASE_PASSWORD')
+    )
+
+    if not database.exists(parameters):
+        name = parameters['name']
+        parameters['name'] = os.environ.get('DATABASE_NAME_ADMIN')
+
+        database.create(parameters, name)
