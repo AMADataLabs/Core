880300f54c7d58212abc5939a81348539c33e857
Peter Lane <plane@ama-assn.org>
2021-07-13 16:51:35 -0500

    Do not include the event in the DAG task parameters.
    Former-commit-id: e5451a94ef595e4d7ec4d13ab3c91d3ed28e0ea9

--- Source/Python/datalabs/etl/dag/awslambda.py
+++ Source/Python/datalabs/etl/dag/awslambda.py
@@ -45,8 +45,6 @@ class DAGTaskWrapper(task.DAGTaskWrapper):
     def _get_dag_task_parameters(self):
         dag_task_parameters = super()._get_dag_task_parameters()
 
-        dag_task_parameters.update(self._parameters)
-
         if self._runtime_parameters.get("type") == 'Task':
             dynamodb_loader = DynamoDBEnvironmentLoader(dict(
                 table=os.environ["DYNAMODB_CONFIG_TABLE"],
