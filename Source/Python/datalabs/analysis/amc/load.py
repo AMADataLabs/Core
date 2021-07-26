ba00b309246981829796074740d6a246ee3e8635
Garrett <garrett.lappe@ama-assn.org>
2021-07-26 13:54:33 -0500

    Added AMC address report loader utilizing SMTP's send_email. Testing required
    Former-commit-id: 59b777f1bd8e45342dba80dab5cf0c6f789cbd9e

---
+++ Source/Python/datalabs/analysis/amc/load.py
@@ -0,0 +1,38 @@
+""" AMC Flagged Addresses Report loader task """
+from dataclasses import dataclass
+from datetime import datetime
+
+# pylint: disable=import-error, invalid-name
+from datalabs.etl.smtploader import SMTPLoader
+from datalabs.messaging.email_message import Attachment, send_email
+from datalabs.parameter import add_schema
+
+
+@add_schema
+@dataclass
+# pylint: disable=too-many-instance-attributes
+class AMCLoaderParameters:
+    to: str or [str]
+    cc: str or [str]
+    body: str
+    from_account: str
+    data: [Attachment]
+
+
+class AMCReportSMTPLoader(SMTPLoader):
+    PARAMETER_CLASS = AMCLoaderParameters
+
+    def _load(self):
+        date = str(datetime.now().date())  # 'YYYY-MM-DD'
+        name = f'AMC_flagged_addresses_{date}.xlsx'
+        file = Attachment(name=name, data=self._data[0])
+
+        send_email(
+            to=self._parameters.to,
+            cc=self._parameters.cc,
+            subject=f'AMC Sweep Results - {date}',
+            body=self._parameters.body,
+            attachments=[file],
+            from_account=self._parameters.from_account
+
+        )
