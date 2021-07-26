5bb245585557091a8b168c60a836b7013940236a
Garrett <garrett.lappe@ama-assn.org>
2021-07-26 00:43:37 -0500

    Fixed imports, pylint issues, and parameter attribute naming
    Former-commit-id: 564de28fba5bfba6e8014b091023526b37b37575

--- Source/Python/datalabs/analysis/amc/transform.py
+++ Source/Python/datalabs/analysis/amc/transform.py
@@ -1,9 +1,10 @@
-''' AMC Flagged Addresses Report generation task '''
-from   dataclasses import dataclass
+""" AMC Flagged Addresses Report generation task """
+from dataclasses import dataclass
 
-from   datalabs.analysis.amc.address import AddressReportParameters
-from   datalabs.etl.transform import TransformerTask
-from   datalabs.parameter import add_schema
+# pylint: disable=import-error, invalid-name
+from datalabs.analysis.amc.address import AMCAddressFlagger
+from datalabs.etl.transform import TransformerTask
+from datalabs.parameter import add_schema
 
 
 @add_schema
@@ -14,16 +15,17 @@ class FlaggedAddressReportParameters:
     cc: str
     subject: str
     body: str
-    from: str
+    from_account: str
     attachments: str
     data: object = None
 
 
+# pylint: disable=no-self-use
 class FlaggedAddressReportTask(TransformerTask):
-    PARAMETER_CLASS = FlaggedAddressReportParameters
+    # PARAMETER_CLASS = FlaggedAddressReportParameters  # these parameters are required for loader, not transformer
 
     def _transform(self):
-        flagger = AMCAddressFlagger(this._parameters)
+        flagger = AMCAddressFlagger()
 
         # run() must return the report data as a list of byte strings
         return flagger.run()
