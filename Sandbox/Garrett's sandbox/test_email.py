5651ca1969efbfe7841251800ca6594fa75abae7
Garrett Lappe <garrett.lappe@ama-assn.org>
2021-08-03 09:36:59 -0500

    Updated email examples and added sample attachment files
    Former-commit-id: 9d8d90580e1d3ea87e651b2f36c8d83bcc5d7c85

--- Sandbox/Garrett's sandbox/test_email.py
+++ Sandbox/Garrett's sandbox/test_email.py
@@ -1,10 +1,65 @@
+""" Just some examples around send_email and Attachments """
+
 from io import BytesIO
 from datalabs.messaging.email_message import send_email, Attachment
 
 
-a = Attachment(file_path='dog.jpg')
-with open('test_attachment.txt', 'rb') as f:
-    a2 = Attachment(name='test_att.txt', data=BytesIO(f.read()))
+def test_manual_attachment_email():
+    """ Attachment option 1 - specify path to a local file """
+    a = Attachment(file_path='test_email/dog.jpg')
 
-send_email(to='garrett.lappe@ama-assn.org', from_account='garrett.lappe@ama-assn.org', subject='test',
-           attachments=[a, a2])
+    """ Attachment option 2 - specify attachment name and data directly """
+    with open('test_email/test_attachment.txt', 'rb') as f:
+        a2 = Attachment(
+            name='test_att.txt',
+            data=f
+        )
+
+        send_email(
+            to='garrett.lappe@ama-assn.org',
+            from_account='garrett.lappe@ama-assn.org',
+            subject='test',
+            attachments=[a, a2]
+        )
+
+
+""" EXAMPLE EMAIL LOADER TASK"""
+
+from dataclasses import dataclass
+from datalabs.parameter import add_schema
+from datalabs.etl.load import LoaderTask
+
+@add_schema
+@dataclass
+# pylint: disable=too-many-instance-attributes
+class SomeEmailLoaderParameters:
+    to: str or [str]
+    cc: str or [str]
+    data = None
+
+class SomeEmailLoader(LoaderTask):
+    PARAMETER_CLASS = SomeEmailLoaderParameters
+    def _load(self):
+        attachment = Attachment(name='test.jpg', data=self._parameters.data[0])
+        send_email(
+            to=self._parameters.to,
+            cc=self._parameters.cc,
+            attachments=[attachment],
+            from_account='datalabs@ama-assn.org',
+            subject='Test from email loader'
+        )
+
+
+def test_loader():
+    with open(
+            'test_email/dog.jpg',
+            'rb'
+    ) as f:
+        loader = SomeEmailLoader(
+            parameters={
+                'to': '<your_email_address_here>',
+                'cc': '',
+                'data': [f]
+            }
+        )
+        loader._load()
