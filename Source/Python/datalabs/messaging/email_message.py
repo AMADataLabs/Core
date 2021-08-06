59bcfa5ed62387e424c84808a50d3d1e00efdbe7
Garrett Lappe <garrett.lappe@ama-assn.org>
2021-08-06 11:29:04 -0500

    Updated email_message.py to be in line with current master branch
    Former-commit-id: 6011c87610be9712083496e350c682434726daf3

--- Source/Python/datalabs/messaging/email_message.py
+++ Source/Python/datalabs/messaging/email_message.py
@@ -1,14 +1,52 @@
+""" Sends an email using AMA SMTP configuration """
+from io import BytesIO
 import os
 import smtplib
 from email.message import EmailMessage
 
 
-# will clean up in the future or something probably, just needs to get in to support DBL report stuff
-def send_email(to, subject, cc=None, body=None, attachments=None, from_account=None, html_content=None):
+class Attachment:
+    def __init__(self, name=None, file_path=None, data=None):
+        """
+        :param name: string - name of attachment file as it appears in the email
+        :param file_path: string - absolute or relative path to file to add as attachment
+        :param data: BytesIO object of data to add as attachment. If specified, must also specify name
+        """
+        self.name = name
+        self.data: BytesIO = data
+
+        if file_path is not None:
+            if not os.path.exists(file_path):
+                raise FileNotFoundError(file_path)
+            with open(file_path, 'rb') as file:
+                self.data = BytesIO(file.read())
+            # if name is None, determine filename automatically from file path
+            if self.name in [None, '', '/']:
+                self.name = file_path.replace('\\', '/').split('/')[-1]
+        elif data is not None:
+            assert self.data.readable()
+
+        assert self.name is not None
+        assert self.data is not None
+        assert self.data.readable()
+
+
+# pylint: disable=too-many-arguments, invalid-name
+def send_email(to, subject, cc=None, body=None, attachments: [Attachment] = None, from_account=None, html_content=None):
+    """
+    :param to: string of ';' delimited email addresses or list of email addresses
+    :param subject: string
+    :param cc: string of ';' delimited email addresses or list of email addresses
+    :param body: string
+    :param attachments: list of Attachment objects (see above)
+    :param from_account: string of account to send email from
+    :param html_content: html data to insert
+    :return: None
+    """
     with smtplib.SMTP('amamx.ama-assn.org') as smtp:
         msg = EmailMessage()
         msg['To'] = to
-        if 'cc' is not None:
+        if cc not in [None, '']:
             msg['Cc'] = cc
         msg['Subject'] = subject
         if body is not None:
@@ -16,20 +54,10 @@ def send_email(to, subject, cc=None, body=None, attachments=None, from_account=N
         if html_content is not None:
             msg.add_alternative(f"{html_content}", subtype='html')
         if attachments is not None:
-            if isinstance(attachments, str):
-                attachments = {attachments}
-            if not hasattr(attachments, '__iter__'):
-                raise ValueError('attachments parameter must be an iterable')
-            attachments = set(attachments)  # do not attach same file multiple times
-            for attachment in attachments:
-                if not os.path.exists(attachment):
-                    raise FileNotFoundError(f'File path {attachment} not found.')
-                with open(attachment, 'rb') as f:
-                    data = f.read()
-                attachment = attachment.replace('\\', '/')
+            for attachment in set(attachments):
                 msg.add_attachment(
-                    data,
-                    filename=attachment.split('/')[-1],
+                    attachment.data.read(),
+                    filename=attachment.name,
                     maintype='application',
                     subtype='octet-stream'
                 )
@@ -37,8 +65,8 @@ def send_email(to, subject, cc=None, body=None, attachments=None, from_account=N
         if from_account is None:
             from_account = os.environ.get('AMA_EMAIL_ADDRESS')
             if from_account is None:
-                raise EnvironmentError(f'from_account parameter not specified and environment variable '
-                                       f'not set - cannot determine email address to send email message from.')
+                raise EnvironmentError('from_account parameter not specified and environment variable '
+                                       'not set - cannot determine email address to send email message from.')
         msg['From'] = from_account
 
         smtp.send_message(msg)
