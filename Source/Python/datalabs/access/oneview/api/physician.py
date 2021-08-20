db50a950515de93d5cabad8f945b2ff59fe3af7c
Daniel Ro <daniel.ro@ama-assn.org>
2021-08-20 14:48:16 -0500

    Updated API Lambda Function per PR Review comments from Peter
    Former-commit-id: 008bcb3dc2a46362b981f486d1e8b496a937047c

--- Source/Python/datalabs/access/oneview/api/physician.py
+++ Source/Python/datalabs/access/oneview/api/physician.py
@@ -3,7 +3,7 @@ from   abc import abstractmethod
 import logging
 
 from   sqlalchemy import or_, func
-from sqlalchemy.orm import defer, undefer
+from   sqlalchemy.orm import defer, undefer
 
 from   datalabs.access.api.task import APIEndpointTask, ResourceNotFound
 from   datalabs.model.masterfile.oneview import Physician
@@ -13,7 +13,7 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
 
-class BasePhysicianEndpointTask(APIEndpointTask):
+class PhysiciansEndpointTask(APIEndpointTask):
     def _run(self, database):
         LOGGER.debug('Parameters: %s', self._parameters)
         self._set_parameter_defaults()
@@ -25,6 +25,12 @@ class BasePhysicianEndpointTask(APIEndpointTask):
 
         self._response_body = self._generate_response_body(query.all(), fields)
 
+        if not self._response_body:
+            raise ResourceNotFound('No data exists for the given column filters')
+
+        self._response_body = self._response_body[0]
+
+
     def _set_parameter_defaults(self):
         pass
 
@@ -32,67 +38,47 @@ class BasePhysicianEndpointTask(APIEndpointTask):
     def _query_for_physicians(cls, database):
         return database.query(Physician)
 
-    @abstractmethod
     def _filter(self, query):
-        pass
 
-    @classmethod
-    def _generate_response_body(cls, rows, fields):
+        query_params = self._parameters.query
+        return_fields = query_params.pop("field", None)
 
-        output = []
-
-        # If return fields is not specified
-        if len(fields) == 0:
-            for row in rows:
-                d = vars(row)
-
-                # vars(row) has the SQLAlchemy State Instance as a key, so this removes it manually
-                # If there is some way to take the Query and return a dict more eloquently, that would be nice
-                d.pop("_sa_instance_state")
-                output.append(d)
-        else:
-            for row in rows:
-                d = {}
-                for column in fields:
-                    d[column] = str(getattr(row, column))
-                output.append(d)
-
-        return output
-
-class PysicianEndpointTask(BasePhysicianEndpointTask):
-    def _run(self, database):
-        super()._run(database)
-
-        if not self._response_body:
-            raise ResourceNotFound('No data exists for the given modifier')
-
-        self._response_body = self._response_body[0]
-
-    def _filter(self, query):
-        fields = self._parameters.query
-
-        query, return_fields = self._filter_by_fields(query, fields)
+        query = self._filter_by_fields(query, query_params)
+        if return_fields is not None:
+            query = self._filter_return_fields(query, return_fields)
 
         # pylint: disable=singleton-comparison
         return query, return_fields
 
     @classmethod
-    def _filter_by_fields(cls, query, fields):
+    def _generate_response_body(cls, rows, return_fields):
+        columns = return_fields or [column.name for column in Physician.__table__.columns]
+        output = []
 
-        return_fields = []
+        # If return fields is not specified
+        for row in rows:
+            row_data = {column: getattr(row, column) for column in columns}
 
-        if "field" in fields:
-            # Remove all columns from SELECT statment
-            query = query.options(defer('*'))
+            output.append(row_data)
 
-            return_fields = fields.pop("field")
+        return output
 
-            # Add back in specified fields to SELECT
-            for f in return_fields:
-                query = query.options(undefer(getattr(Physician, f)))
-
+    @classmethod
+    def _filter_by_fields(cls, query, query_params):
         # Add WHERE filters to query
-        for attr, value in fields.items():
-            query = query.filter(func.lower(getattr(Physician, attr)) == func.lower(value))
+        for field, value in query_params.items():
+            query = query.filter(func.lower(getattr(Physician, field)) == func.lower(value))
 
-        return query, return_fields
+        return query
+
+    @classmethod
+    def _filter_return_fields(cls, query, return_fields):
+        # Remove all columns from SELECT statement
+        query = query.options(defer('*'))
+
+        # Add back in specified fields to SELECT
+        for field in return_fields:
+            query = query.options(undefer(getattr(Physician, field)))
+
+        return query
+
