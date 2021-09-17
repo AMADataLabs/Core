e61e7da0eebc8fe64a73f29e76f41ced8b1cef63
Peter Lane <plane@ama-assn.org>
2021-09-17 18:32:13 -0500

    Remove explicit use of dask in OneView transformers.
    Former-commit-id: 1114e541c25f8050e1c084787ac4da36cf0cd7ac

--- Source/Python/datalabs/etl/oneview/melissa/transform.py
+++ Source/Python/datalabs/etl/oneview/melissa/transform.py
@@ -1,10 +1,10 @@
 """ Oneview Melissa Transformer"""
 import logging
-import dask.array
 
-from   datalabs.etl.oneview.transform import TransformerTask
+import pandas
 
 import datalabs.etl.oneview.melissa.column as columns
+from   datalabs.etl.oneview.transform import TransformerTask
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -13,28 +13,25 @@ LOGGER.setLevel(logging.INFO)
 
 class MelissaTransformerTask(TransformerTask):
     def _preprocess_data(self, data):
-        zip_code_data = data[0]
-        area_code_data = data[2]
+        zip_code_data, county_data, area_code_data, census_data, cbsa_data, zip_code_cbsa_data, msa_data = data
 
         zip_codes = self._generate_zip_code_primary_keys(zip_code_data)
         zip_codes['FIPS_CD'] = zip_codes['FIPS_CD'].str.lstrip('0')
 
         area_codes = self._generate_area_code_primary_keys(area_code_data)
 
-        return [zip_codes, data[1], area_codes, data[3], data[4], data[5], data[6]]
+        return [zip_codes, county_data, area_codes, census_data, cbsa_data, zip_code_cbsa_data, msa_data]
 
     @classmethod
     def _generate_zip_code_primary_keys(cls, data):
-        zip_code_primary_keys = dask.array.array([str(column['ZIP']) + str(column['CITY_CD'])
-                                                  for index, column in data.iterrows()])
+        zip_code_primary_keys = [str(column['ZIP']) + str(column['CITY_CD']) for index, column in data.iterrows()]
         data['id'] = zip_code_primary_keys
 
         return data
 
     @classmethod
     def _generate_area_code_primary_keys(cls, data):
-        area_code_primary_keys = dask.array.array([str(column['AREA_CD']) + str(column['PREFIX'])
-                                                   for index, column in data.iterrows()])
+        area_code_primary_keys = [str(column['AREA_CD']) + str(column['PREFIX']) for index, column in data.iterrows()]
         data['id'] = area_code_primary_keys
 
         return data
