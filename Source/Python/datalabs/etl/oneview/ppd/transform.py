c8e7a76e9a928b5804b5e27b6923318e3b74f0a1
Peter Lane <plane@ama-assn.org>
2021-09-18 13:06:35 -0500

    Fix scalable dataframe mixin override.
    Former-commit-id: 6ca17ee1c56c8b131ac882f9e0f6be07639950d3

--- Source/Python/datalabs/etl/oneview/ppd/transform.py
+++ Source/Python/datalabs/etl/oneview/ppd/transform.py
@@ -57,7 +57,7 @@ class NPITransformerTask(TransformerTask):
 
 class PPDTransformerTask(TransformerTask):
     def _csv_to_dataframe(self, data: bytes, on_disk, **kwargs) -> pandas.DataFrame:
-        return super()._csv_to_dataframe(data, sep='|', dtype=str, **kwargs)
+        return super()._csv_to_dataframe(data, on_disk, sep='|', dtype=str, **kwargs)
 
     def _preprocess_data(self, data):
         ppd, race_ethnicity, medical_student = data
