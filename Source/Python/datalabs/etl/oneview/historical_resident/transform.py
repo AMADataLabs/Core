3c81f1b862617fda1f631caebd4b0739b3ce7af1
rnajeeb <rudainah.najeeb@ama-assn.org>
2021-10-06 09:38:55 -0500

    Updated historical resident etl
    Former-commit-id: df0c0d2569a1d3a1cd5e5b4148dc679247e4fb59

--- Source/Python/datalabs/etl/oneview/historical_resident/transform.py
+++ Source/Python/datalabs/etl/oneview/historical_resident/transform.py
@@ -14,5 +14,12 @@ class HistoricalResidentTransformerTask(TransformerTask):
     def _csv_to_dataframe(cls, data, on_disk, **kwargs):
         return super()._csv_to_dataframe(data, on_disk, sep='|')
 
+    def _preprocess_data(self, data):
+        historical_resident = data[0]
+
+        primary_keys = list(range(len(historical_resident)))
+
+        historical_resident['id'] = primary_keys
+
     def _get_columns(self):
         return [column.HISTORICAL_RESIDENCY]
