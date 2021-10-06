0efe7a9d6e46cf96620783009f3c6bebc13273b7
Hadi Modares <Hadi.Modares@ama-assn.org>
2021-10-06 11:20:16 -0500

    Added transformer node for ADDRESS_LOAD_AGGREAGATION DA
    Former-commit-id: 5f9a8ccf1b7eb58bc9c6fc2ac9d3019cd3997f76

--- Source/Python/datalabs/airflow/dag/analysis/batchload/aggregate.py
+++ Source/Python/datalabs/airflow/dag/analysis/batchload/aggregate.py
@@ -52,4 +52,14 @@ with ADDRESS_LOAD_AGGREGATION_DAG:
         env_vars={**BASE_ENVIRONMENT, **dict(TASK_CLASS='datalabs.etl.sftp.extract.SFTPFileExtractorTask')},
     )
 
-EXTRACT_ADDRESS_LOAD
+with ADDRESS_LOAD_AGGREGATION_DAG:
+    TRANSFROM_ADDRESS_LOAD = KubernetesPodOperator(
+        name="transform_address_load",
+        task_id="transform_address_load",
+        cmds=['python', 'task.py', '{{ task_instance_key_str }}'],
+        env_from=[ETL_CONFIG],
+        secrets=[EFT_SECRET],
+        env_vars={**BASE_ENVIRONMENT, **dict(TASK_CLASS='datalabs.analysis.address.batchload.transform.AddressLoadFileAggregationTransformerTask')},
+    )
+
+EXTRACT_ADDRESS_LOAD >> TRANSFROM_ADDRESS_LOAD
