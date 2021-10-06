c002dc00cdfca9910abbedb033bd30401d5f9bf5
Peter Lane <plane@ama-assn.org>
2021-10-06 11:09:21 -0500

    Update historical resident pre-computed PK. General OneView transformer cleanup.
    Former-commit-id: af69e92f9b3ec7b080c999283b4ee656da91abf2

--- Test/Python/test/datalabs/etl/oneview/test_transform.py
+++ Test/Python/test/datalabs/etl/oneview/test_transform.py
@@ -9,7 +9,7 @@ import pandas
 import pytest
 
 os.environ['ENABLE_FEATURE_DASK'] = 'True'
-from   datalabs.etl.oneview.transform import ScalableTransformerMixin
+from   datalabs.etl.oneview.transform import ScalableTransformerMixin  # pylint: disable=wrong-import-position
 
 
 # pylint: disable=protected-access, redefined-outer-name
