12d4adb50d04021e449d6d7315d21df92135a9f6
Peter Lane <plane@ama-assn.org>
2021-10-16 13:05:31 -0500

    Add debug logging to zip transformer.
    Former-commit-id: 42363a6626b0ef6c38e2c596f1c86018e826d14c

--- Source/Python/datalabs/etl/archive/transform.py
+++ Source/Python/datalabs/etl/archive/transform.py
@@ -15,10 +15,10 @@ class ZipTransformerTask(TransformerTask):
     def _transform(self) -> 'Transformed Data':
         zip_data = BytesIO()
         filename_data_tuples = pickle.loads(self._parameters['data'][0])
-        LOGGER.debug(f'Data to zip: {filename_data_tuples}')
 
         with ZipFile(zip_data, 'w') as zip_file:
             for file, data in filename_data_tuples:
+                LOGGER.debug(f'Adding {len(data)} byte file {file} to the archive...')
                 zip_file.writestr(file, data)
 
         return [bytes(zip_data.getbuffer())]
