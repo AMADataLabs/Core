07e0e68f625970faf3cf75a317c57cc703497960
Peter Lane <plane@ama-assn.org>
2021-10-27 21:55:49 -0500

    Reimplement concatenation task without using Pandas.
    Former-commit-id: f5f5488cd890df414bcc8b1c0917a015dc4d6355

---
+++ Test/Python/test/datalabs/etl/manipulate/test_transform.py
@@ -0,0 +1,45 @@
+""" source: datalabs.etl.manipulate.transform """
+from   io import BytesIO
+
+import pandas
+import pytest
+
+from datalabs.etl.manipulate.transform import ConcatenateTransformerTask
+
+
+def test_concatenation_works_on_indexed_csv(split_indexed_data):
+    task = ConcatenateTransformerTask(dict(execution_time='2021-01-01 00:00:00', data=split_indexed_data))
+    task.run()
+
+    assert len(task.data) == 1
+
+    concatenated_data = pandas.read_csv(BytesIO(task.data[0]), dtype=object)
+
+    assert len(concatenated_data) == 4
+
+
+def test_concatenation_works_on_unindexed_csv(split_unindexed_data):
+    task = ConcatenateTransformerTask(dict(execution_time='2021-01-01 00:00:00', data=split_unindexed_data))
+    task.run()
+
+    assert len(task.data) == 1
+
+    concatenated_data = pandas.read_csv(BytesIO(task.data[0]), dtype=object)
+
+    assert len(concatenated_data) == 4
+
+
+@pytest.fixture
+def split_indexed_data():
+    return [
+        b',COLUMN1,COLUMN2,COLUMN3\n0,INS00000004,INTALERE,\n1,INS00000008,CHILDRENS HOSPITAL ASSOCIATION,\n',
+        b',COLUMN1,COLUMN2,COLUMN3\n0,INS00000010,FUNKY TOWN,\n1,INS00000011,FOREVERMORE DANCE,\n'
+    ]
+
+
+@pytest.fixture
+def split_unindexed_data():
+    return [
+        b'COLUMN1,COLUMN2,COLUMN3\nINS00000004,INTALERE,\nINS00000008,CHILDRENS HOSPITAL ASSOCIATION,\n',
+        b'COLUMN1,COLUMN2,COLUMN3\nINS00000010,FUNKY TOWN,\nINS00000011,FOREVERMORE DANCE,\n'
+    ]
