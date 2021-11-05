9d9b229c0bdf17b5710a31bca536b27c2060a8f4
Peter Lane <plane@ama-assn.org>
2021-11-05 09:01:38 -0500

    Remove unused, extra release transformer. Reverse the order of ORMLoader sequence to delete, update, and then add records.
    Former-commit-id: 8f3ae18bb28f4847fbdbb4bea395dee7d7f1a1dc

--- Source/Python/datalabs/etl/cpt/release/transform.py
+++ Source/Python/datalabs/etl/cpt/release/transform.py
@@ -1,15 +1,9 @@
 ''' CPT release data transformers. '''
-from   dataclasses import dataclass
-from   datetime import date, datetime
-import json
-
-from   dateutil.parser import isoparse
 import pandas
 
 from   datalabs.etl.cpt.release.type import RELEASE_TYPES
 from   datalabs.etl.csv import CSVWriterMixin
 from   datalabs.etl.transform import TransformerTask
-from   datalabs.parameter import add_schema
 
 
 class ReleaseTypesTransformerTask(CSVWriterMixin, TransformerTask):
