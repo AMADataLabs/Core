ef68fef8921610d244a4d6e8f0c1f5c30b2747da
Peter Lane <plane@ama-assn.org>
2021-11-18 22:53:09 -0600

    Fix migration scripts. Remove old view from OneView DB model.
    Former-commit-id: cde92dc678566acdaa00aba69b459cc4ea1fd225

--- Build/OneView/alembic/env.py
+++ Build/OneView/alembic/env.py
@@ -10,9 +10,10 @@ from alembic import context
 from datalabs.access.credentials import Credentials
 from datalabs.access.database import Configuration
 from datalabs.access.orm import Database
-from datalabs.model.masterfile.oneview import BASE, PHYSICIAN_VIEW, PHYSICIAN_PROVIDER_VIEW
+# from datalabs.model.masterfile.oneview import BASE, PHYSICIAN_VIEW, PHYSICIAN_PROVIDER_VIEW
+from datalabs.model.masterfile.oneview import BASE
 
-register_entities([PHYSICIAN_VIEW, PHYSICIAN_PROVIDER_VIEW])
+# register_entities([PHYSICIAN_VIEW, PHYSICIAN_PROVIDER_VIEW])
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
