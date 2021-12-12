a41dd9e64b0bfaf24e249457025b6fa2a58e6c31
Hadi Modares <Hadi.Modares@ama-assn.org>
2021-12-12 17:48:01 -0600

    Added a missing transform.py file
    Former-commit-id: 1b56ad346bb6c238ada58d0c44cffe1f46794822

---
+++ Source/Python/datalabs/analysis/ppma/license_movement/transform.py
@@ -0,0 +1,40 @@
+""" License Movement PPMA update pipeline - Transformer """
+from dataclasses import dataclass
+from io import BytesIO
+import pandas as pd
+
+# pylint: disable=import-error,unused-import,unused-variable
+from datalabs.analysis.ppma.license_movement.finder import LicenseMovementFinder
+from datalabs.etl.transform import TransformerTask
+from datalabs.parameter import add_schema
+
+
+@add_schema
+@dataclass
+class LicenseMovementTransformerParameter:
+    data: list
+
+
+class LicenseMovementTransformerTask(TransformerTask):
+    PARAMETER_CLASS = LicenseMovementTransformerParameter
+
+    def _transform(self):
+        dataframes = [pd.read_csv(BytesIO(file)) for file in self._parameters.data]
+        mismatch_data = dataframes[0]
+        old_ppma_data = dataframes[1]
+        credentialing_data = dataframes[2]
+        ppd = dataframes[3]
+
+        finder = LicenseMovementFinder()
+
+        data = finder.find_potential_updates(data=mismatch_data, old_ppma_data=old_ppma_data)
+        data = finder.filter_to_allowed_states(data=data)
+        data = finder.filter_on_credentialing_zip_distance(data=data, credentialing_data=credentialing_data, ppd=ppd)
+        data = finder.filter_to_valid_credentialing_data(data=data)
+
+        batch_data = finder.format_batch_load_file(data=data)
+
+        output = BytesIO()
+        batch_data.to_csv(output, sep='|', index=False)
+
+        return [output.getvalue()]
