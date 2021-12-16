9912592b5793dbd4b1a621353dd46af8a372b6f3
Peter Lane <plane@ama-assn.org>
2021-12-16 12:22:26 -0600

    Reorganized OneView data model files.
    Former-commit-id: fd17ada4b78dbe9693416871aaabcea5056d9e73

---
+++ Source/Python/datalabs/model/masterfile/oneview/__init__.py
@@ -0,0 +1,46 @@
+''' OneView database model module. '''
+from datalabs.model.masterfile.oneview.content import \
+    BASE, \
+    Physician, \
+    ResidencyProgram, \
+    ResidencyProgramPersonnelMember, \
+    ResidencyProgramInstitution, \
+    Business, \
+    Provider, \
+    ProviderAffiliation, \
+    CredentialingCustomer, \
+    CredentialingOrder, \
+    ZipCode, \
+    County, \
+    AreaCode, \
+    Census, \
+    CoreBasedStatisticalAreaMelissa, \
+    ZipCodeCoreBasedStatisticalArea, \
+    MetropolitanStatisticalArea, \
+    HistoricalResident, \
+    IqviaUpdate, \
+    CredentialingCustomerInstitution, \
+    CredentialingCustomerBusiness, \
+    ResidencyProgramPhysician, \
+    CorporateParentBusiness, \
+    SubsidiaryOwnerBusiness, \
+    CredentialingProduct, \
+    TypeOfPractice, \
+    PresentEmployment, \
+    MajorProfessionalActivity, \
+    FederalInformationProcessingStandardCounty, \
+    CoreBasedStatisticalArea, \
+    Specialty, \
+    State, \
+    ClassOfTradeClassification, \
+    ClassOfTradeSpecialty, \
+    ClassOfTradeFacilityType, \
+    ProviderAffiliationGroup, \
+    ProviderAffiliationType, \
+    ProfitStatus, \
+    OwnerStatus, \
+    MedicalSchool
+from datalabs.model.masterfile.oneview.user_interface import \
+    PHYSICIAN_MATERIALIZED_VIEW, \
+    PHYSICIAN_PROVIDER_MATERIALIZED_VIEW
+from datalabs.model.masterfile.oneview.datanow import PHYSICIAN_DATA_VIEW
