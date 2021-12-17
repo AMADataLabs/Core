e7ceefe74da9757781c241070f6a3738e519b562
Hadi Modares <Hadi.Modares@ama-assn.org>
2021-12-17 10:21:26 -0600

    Changed schedule interval to None for now
    Former-commit-id: 59538ccc2c9d4c40b26f39881e3fba0d02f3e55a

--- Source/Python/datalabs/airflow/dag/licensemovement/process_trigger.py
+++ Source/Python/datalabs/airflow/dag/licensemovement/process_trigger.py
@@ -37,7 +37,7 @@ LICENSE_MOVEMENT_DAG = DAG(
         in_cluster=True,
         get_logs=True,
     ),
-    schedule_interval="0 0 12 ? * 4",
+    schedule_interval=None,
     start_date=datetime(2021, 12, 4),
     tags=['LICENSE_MOVEMENT'],
     # catchup=True
