cfc0b966fbb3a8cbf3557b880141f43d098052b0
Peter Lane <plane@ama-assn.org>
2022-01-24 17:02:21 -0600

    Finish Java project bundler.
    Former-commit-id: 134db714b17108f32444b4a8bea703105608d64c

--- Script/bundle_java_project.py
+++ Script/bundle_java_project.py
@@ -9,7 +9,8 @@ import shutil
 import sys
 from   zipfile import ZipFile
 
-from datalabs.build.bundle.java import JavaSourceBundle
+from   datalabs.build.bundle.java import JavaSourceBundle
+from   datalabs.common.setup import FileGeneratorFilenames, SimpleFileGenerator
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -19,30 +20,34 @@ LOGGER.setLevel(logging.INFO)
 class ProjectBundler(ABC):
     def __init__(self, repository_path):
         self._repository_path = repository_path
-        self._shared_source_path = Path(os.path.join(self._repository_path, 'Source', 'Python')).resolve()
+        self._shared_source_path = Path(os.path.join(self._repository_path, 'Source', 'Java')).resolve()
         self._build_path = Path(os.path.join(self._repository_path, 'Build')).resolve()
 
 
-    def bundle(self, project, target_path, extra_files, **kwargs):
+    def bundle(self, project, version, target_path, extra_files, **kwargs):
         target_path = target_path or Path(os.path.join(self._build_path, project, project)).resolve()
 
-        self._build_bundle(project, Path(target_path), extra_files, **kwargs)
+        self._build_bundle(project, version, Path(target_path), extra_files, **kwargs)
 
     @abstractmethod
-    def _build_bundle(self, project, target_path, extra_files, **kwargs):
+    def _build_bundle(self, project, version, target_path, extra_files, **kwargs):
         pass
 
 
 class LocalProjectBundler(ProjectBundler):
-    def _build_bundle(self, project, target_path, extra_files, **kwargs):
+    def _build_bundle(self, project, version, target_path, extra_files, **kwargs):
         if os.path.exists(target_path) and not kwargs['in_place']:
             LOGGER.info('=== Removing Old Target Directory ===')
             shutil.rmtree(target_path)
+        LOGGER.debug(kwargs)
 
         os.makedirs(target_path, exist_ok=True)
 
         LOGGER.info('=== Copying Build Files ===')
         self._copy_build_files(project, target_path)
+
+        LOGGER.info('=== Generating Project Object Model ===')
+        self._render_project_object_model_file(project, version, target_path)
 
         LOGGER.info('=== Copying Source Files ===')
         self._copy_source_files(project, target_path)
@@ -50,25 +55,28 @@ class LocalProjectBundler(ProjectBundler):
         LOGGER.info('=== Copying Extra Files ===')
         self._copy_extra_files(extra_files, target_path)
 
-        LOGGER.info('=== Copying Lambda Function Handler ===')
-        self._copy_lambda_function_handler(target_path)
-
-        LOGGER.info('=== Copying Local Task Script ===')
-        self._copy_local_task_script(target_path)
-
-        LOGGER.info('=== Creating Zip Archive ===')
-        self._zip_bundle_directory(project, target_path)
+        LOGGER.info('=== Creating Jar Archive ===')
+        self._jar_source_directory(project, target_path)
 
     def _copy_build_files(self, project, target_path):
-        self._copy_alembic_files(project, target_path)
+        shutil.copy(os.path.join(self._build_path, 'Master', 'pom.xml.jinja'), os.path.join(target_path, 'pom.xml.jinja'))
 
-        shutil.copy(os.path.join(self._build_path, 'Master', 'settings.py'), os.path.join(target_path, 'settings.py'))
+    def _render_project_object_model_file(self, project, version, target_path):
+        template_path = os.path.join(target_path, 'pom.xml.jinja')
+        result_path = os.path.join(target_path, 'pom.xml')
+
+        if not os.path.exists(result_path):
+            LOGGER.info(f'Generating file {str(result_path)} from template {str(template_path)}')
+            filenames = FileGeneratorFilenames(template=Path(template_path), output=Path(result_path))
+
+            file_generator = SimpleFileGenerator(filenames, project=project, version=version)
+            file_generator.generate()
 
     def _copy_source_files(self, project, target_path):
         modspec_path = os.path.join(self._build_path, project, 'modspec.yaml')
-        bundle = PythonSourceBundle.from_file(modspec_path)
+        bundle = JavaSourceBundle.from_file(modspec_path)
 
-        return bundle.copy(self._shared_source_path, target_path)
+        return bundle.copy(self._shared_source_path, os.path.join(target_path, 'src', 'main'))
 
     def _copy_extra_files(self, files, target_path):
         files = files or []
@@ -76,33 +84,8 @@ class LocalProjectBundler(ProjectBundler):
         for file in files:
             shutil.copy(file, os.path.join(target_path, file))
 
-    def _copy_lambda_function_handler(self, target_path):
-        shutil.copy(os.path.join(self._build_path, 'Master', 'awslambda.py'), os.path.join(target_path, 'awslambda.py'))
-
-    def _copy_local_task_script(self, target_path):
-        shutil.copy(os.path.join(self._build_path, 'Master', 'task.py'), os.path.join(target_path, 'task.py'))
-
-    def _zip_bundle_directory(self, project, target_path):
-        archive_path = target_path.resolve().with_suffix('.zip')
-
-        if os.path.exists(archive_path):
-            os.remove(archive_path)
-
-        with ZipFile(archive_path, 'w') as archive:
-            for contents in os.walk(target_path):
-                self._archive_contents(archive, target_path, contents)
-
-    def _copy_alembic_files(self, project, target_path):
-        alembic_path = os.path.join(self._build_path, project, 'alembic')
-        target_alembic_path = os.path.join(target_path, 'alembic')
-
-        self._remove_alembic_files(target_alembic_path)
-
-        if os.path.exists(alembic_path):
-            shutil.copytree(os.path.join(alembic_path, 'versions'), os.path.join(target_alembic_path, 'versions'))
-            shutil.copyfile(os.path.join(alembic_path, 'script.py.mako'), os.path.join(target_alembic_path, 'env.py'))
-            shutil.copyfile(os.path.join(alembic_path, 'env.py'), os.path.join(target_alembic_path, 'script.py.mako'))
-            shutil.copyfile(alembic_path + '.ini', target_alembic_path + '.ini')
+    def _jar_source_directory(self, project, target_path):
+        os.system(f'mvn -f {os.path.join(target_path, "pom.xml")} package')
 
     def _archive_contents(self, archive, target_path, contents):
         root, dirs, files = contents
@@ -118,83 +101,6 @@ class LocalProjectBundler(ProjectBundler):
             archive.write(os.path.join(root, f), arcname=os.path.join(relative_root, f))
 
 
-    def _remove_alembic_files(self, target_alembic_path):
-        versions_path = os.path.join(target_alembic_path, 'versions')
-        env_path = os.path.join(target_alembic_path, 'env.py')
-        mako_path = os.path.join(target_alembic_path, 'script.py.mako')
-
-        if os.path.exists(versions_path):
-            shutil.rmtree(versions_path)
-
-        if os.path.exists(env_path):
-            os.remove(env_path)
-
-        if os.path.exists(mako_path):
-            os.remove(mako_path)
-
-
-class ServerlessProjectBundler(ProjectBundler):
-    def _build_bundle(self, project, target_path, extra_files, **kwargs):
-        from   docker import from_env
-
-        docker = from_env()
-
-        image, _ = self._generate_serverless_artifact(docker, project)
-
-        self._save_serverless_artifact(docker, image, project, target_path)
-
-
-    def _generate_serverless_artifact(self, docker, project):
-        LOGGER.info('=== Generating Serverless Artifact ===')
-        dockerfile_path = os.path.join(self._repository_path, 'Build', 'Master', 'Dockerfile.bundle')
-
-        return docker.images.build(
-            path=self._repository_path,
-            tag='datalabs-serverless-bundler',
-            dockerfile=dockerfile_path,
-            buildargs=dict(PROJECT=f'{project}'),
-            quiet=False
-        )
-
-    def _save_serverless_artifact(self, docker, image, project, target_path):
-        import tarfile
-
-        LOGGER.info('=== Saving Serverless Artifact ===')
-        artifact_path = target_path.resolve().with_suffix('.zip')
-
-        archive = DockerArchive(docker, image.tags[0], '/Build/Bundle.zip')
-        tarball = tarfile.open(fileobj=archive)
-        artifact = tarball.extractfile('Bundle.zip')
-
-        with open(artifact_path, 'wb') as f:
-            for chunk in artifact:
-                f.write(chunk)
-
-
-class DockerArchive(io.BytesIO):
-    def __init__(self, docker, image: str, path: str):
-        self._docker = docker
-        self._image = image
-        self._path = path
-
-        super().__init__(self._load())
-
-    def _load(self):
-        chunks = []
-        container = self._docker.containers.run(self._image, '/bin/bash', detach=True)
-        stream, _ = container.get_archive(self._path)
-
-        for chunk in stream:
-            chunks.append(chunk)
-
-        try:
-            container.kill()
-        except Exception:
-            pass
-
-        return b''.join(chunks)
-
-
 
 if __name__ == '__main__':
     return_code = 0
@@ -204,10 +110,11 @@ if __name__ == '__main__':
         help='Specify the target directory into which files will be bundled (default Build/<PROJECT>/<PROJECT>)')
     ap.add_argument('-i', '--in-place', action='store_true', default=False,
         help='Do not pre-clean the target directory.')
-    ap.add_argument('-s', '--serverless', action='store_true', default=False,
-        help='Create a zip archive of the bundle for serverless deployment.')
     ap.add_argument(
         '-f', '--file', action='append', required=False, help='Include the given file in the bundle.'
+    )
+    ap.add_argument(
+        '-v', '--version', default="1.0.0", help='Version of the bundle (default 1.0.0).'
     )
     ap.add_argument('project', help='Name of the project.')
     args = vars(ap.parse_args())
@@ -219,13 +126,11 @@ if __name__ == '__main__':
         repository_path = os.path.join(script_base_path, '..')
         project_bundler = None
 
-        if args['serverless']:
-            project_bundler = ServerlessProjectBundler(repository_path)
-        else:
-            project_bundler = LocalProjectBundler(repository_path)
+        project_bundler = LocalProjectBundler(repository_path)
 
         return_code = project_bundler.bundle(
             args['project'],
+            args['version'],
             target_path=args['directory'],
             extra_files=args['file'],
             in_place=args['in_place']
