966266c64eb1196cfd790f29b027a52f35581092
rnajeeb <rudainah.najeeb@ama-assn.org>
2022-01-26 13:43:46 -0600

    Updated unit test with dag test
    Former-commit-id: 4d4cfa7894ac9a30eee0bff19980e5d279893dcb

--- Test/Python/test/datalabs/etl/dag/execute/test_ecs.py
+++ Test/Python/test/datalabs/etl/dag/execute/test_ecs.py
@@ -3,7 +3,7 @@ import os
 
 import pytest
 
-from   datalabs.etl.dag.execute.ecs import FargateTaskExecutorTask
+from   datalabs.etl.dag.execute.ecs import FargateTaskExecutorTask, FargateDAGExecutorTask
 
 
 @pytest.mark.skipif(
@@ -11,18 +11,39 @@ from   datalabs.etl.dag.execute.ecs import FargateTaskExecutorTask
     reason="Normally skip integration tests to increase testing speed."
 )
 # pylint: disable=redefined-outer-name, protected-access, unused-argument
-def test_fargate_task_executor(runtime_parameters):
-    task = FargateTaskExecutorTask(runtime_parameters)
+def test_fargate_task_executor(task_runtime_parameters):
+    task = FargateTaskExecutorTask(task_runtime_parameters)
+
+    task.run()
+
+
+@pytest.mark.skipif(
+    os.getenv('RUN_INTEGRATION_TESTS') != 'True',
+    reason="Normally skip integration tests to increase testing speed."
+)
+# pylint: disable=redefined-outer-name, protected-access, unused-argument
+def test_fargate_dag_executor(dag_runtime_parameters):
+    task = FargateDAGExecutorTask(dag_runtime_parameters)
 
     task.run()
 
 
 @pytest.fixture
-def runtime_parameters():
+def task_runtime_parameters():
     return dict(
         dag="SOME_DAG",
         job_queue="ecs-scheduler-job-queue",
         job_definition="ecs-scheduler-job-definition",
         execution_time="2021-01-01T00:00:00.000000",
         task="SOME_TASK"
+    )
+
+
+@pytest.fixture
+def dag_runtime_parameters():
+    return dict(
+        dag="SOME_DAG",
+        job_queue="ecs-scheduler-job-queue",
+        job_definition="ecs-scheduler-job-definition",
+        execution_time="2021-01-01T00:00:00.000000"
     )
