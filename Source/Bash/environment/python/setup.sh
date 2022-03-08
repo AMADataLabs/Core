65a3282b73c276054815c7e3a09ab8fcd4a4cc4e
Peter Lane <plane@ama-assn.org>
2022-03-08 12:46:38 -0600

    More build script debugging.
    Former-commit-id: 1b4e6ef0bac90f65c4b7eb94db27dbb718d0756d

--- Source/Bash/environment/python/setup.sh
+++ Source/Bash/environment/python/setup.sh
@@ -12,6 +12,8 @@ setup_python_virtual_environment() {
 create_python_virtual_environment() {
     environment_path=$1
 
+    ls $environment_path
+    mkdir -p $environment_path
     /usr/bin/env python3.7 -m venv $environment_path
     echo "Done creating virtual environment"
 }
@@ -20,12 +22,7 @@ create_python_virtual_environment() {
 install_python_virtual_environment_dependencies() {
     environment_path=$1
 
-    echo Still using $(which python3.7)
-    current_path=$PATH
-    export VIRTUAL_ENV=${PWD}/Environment/Master/BitBucketPipelines
-    echo Adjusting path to use $VIRTUAL_ENV/bin/python3.7
-    export PATH="$VIRTUAL_ENV/bin:$PATH"
-    echo Now using $(which python3.7)
+    . $environment_path/bin/activate
 
     echo "--- Upgrading pip ---"
     pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --upgrade pip
@@ -33,6 +30,5 @@ install_python_virtual_environment_dependencies() {
     echo "--- Installing Python packages from requirements file $environment_path/requirements.txt ---"
     pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r $environment_path/requirements.txt
 
-    unsetenv VIRTUAL_ENV
-    export PATH=$current_path
+    deactivate
 }
