03259091a8e2ef06375efce3e7725fabb9dd745b
Peter Lane <plane@ama-assn.org>
2022-03-09 16:58:07 -0600

    Add IVL foundations app stack.
    Former-commit-id: 3ccaf3cc784b9029a87f6d2abe5b4aa77cfc31b4

---
+++ Source/Terraform/Foundations/ivl/environment.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    region = "us-east-1"
+    version = "~> 3.0"
+}
+
+
+terraform {
+    backend "s3" {
+        bucket          = "ama-hsg-datalabs-datalake-terraform-state-sandbox"
+        key             = "Foundations/ivl.tfstate"
+        region          = "us-east-1"
+        dynamodb_table  = "hsg-datalabs-terraform-locks"
+    }
+
+    required_providers {
+        aws = "3.37"
+    }
+}
+
+
+data "terraform_remote_state" "infrastructure" {
+  backend = "s3"
+
+  config = {
+    bucket          = "ama-hsg-datalabs-datalake-terraform-state-sandbox"
+    key             = "Foundations/ivl.tfstate"
+    region          = "us-east-1"
+    dynamodb_table  = "hsg-datalabs-terraform-locks"
+  }
+}
