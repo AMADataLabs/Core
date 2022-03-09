aa80995b942adb47611b2c2f76356fa19a2d30c8
Peter Lane <plane@ama-assn.org>
2022-03-08 18:11:35 -0600

    Cleanup HelloWorldJava Terraform. Fix HelloWorldJava DAG config Lambda names. Re-add setup of Python dependencies in Python bundle creation script.
    Former-commit-id: aa0cbb072c7ccc14722bcccfc3352fd7e5066efd

--- Source/Terraform/JavaLambda/sbx/compatibility.tf
+++
@@ -1,7 +0,0 @@
-
-resource "aws_acm_certificate" "cert" {
-  domain_name               = "*.cptapi.local"
-  certificate_authority_arn = "arn:aws:acm-pca:us-east-1:644454719059:certificate-authority/266baa99-df15-4981-8a20-70634c1a88ce"
-
-  tags = merge(local.tags, {Name = "${var.project}-${local.environment}-private-zone-cert"})
-}
