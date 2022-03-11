e192ba57df3fe9fccb9943a3f442da3828cf0823
rnajeeb <rudainah.najeeb@ama-assn.org>
2022-03-11 17:42:37 -0600

    debugging oneview api
    Former-commit-id: bdce8eae2ee889daec1f16f09cf102fc28bbb2a1

--- Source/Python/datalabs/access/oneview/api/physician.py
+++ Source/Python/datalabs/access/oneview/api/physician.py
@@ -37,8 +37,6 @@ class PhysiciansEndpointTask(APIEndpointTask):
         return_fields = query_params.pop("field", None)
 
         query, filter_conditions = self._filter_by_fields(query, query_params)
-        if return_fields is not None:
-            query = self._filter_return_fields(query, return_fields)
 
         # pylint: disable=singleton-comparison
         return query, return_fields, query_params, filter_conditions
@@ -46,12 +44,13 @@ class PhysiciansEndpointTask(APIEndpointTask):
     @classmethod
     def _generate_response_body(cls, rows, return_fields, query_params, filter_conditions):
         # pylint: disable=no-member
-        columns = return_fields or [column.key for column in Physician.__table__.columns]
+        if return_fields is None:
+            return_fields = [column.key for column in Physician.__table__.columns]
         output = []
 
         # If return fields is not specified
         for row in rows:
-            row_data = {column: getattr(row, column) for column in columns}
+            row_data = {column: getattr(row, column) for column in return_fields}
             output.append(row_data)
 
         return output
@@ -72,21 +71,8 @@ class PhysiciansEndpointTask(APIEndpointTask):
         return query, filter_conditions
 
     @classmethod
-    def _filter_return_fields(cls, query, return_fields):
-        # Remove all columns from SELECT statement
-        query = query.options(defer('*'))
-
-        # Add back in specified fields to SELECT
-        for field in return_fields:
-            query = query.options(undefer(getattr(Physician, field)))
-
-        return query
-
-    @classmethod
     def _query_for_values(cls, values, field, filter_conditions):
         for value in values:
-            if value.isnumeric() is False:
-                func.lower(value)
             filter_conditions += [(func.lower(getattr(Physician, field)) == value)]
 
         return filter_conditions
