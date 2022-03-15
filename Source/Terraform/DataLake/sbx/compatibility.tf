4a0242d018acbc49ed90b3afd6b6237c9d0319e8
Peter Lane <plane@ama-assn.org>
2022-03-15 16:22:16 -0500

    Remove DataLake sandbox stack (moved to data-lake TE module repository).
    Former-commit-id: 4e08566285153e885dec361c2e066fb8f4d42a2e

--- Source/Terraform/DataLake/sbx/compatibility.tf
+++
@@ -1,7 +0,0 @@
-# Additional resources that are not part of the TE DataLake stack
-
-resource "aws_ecr_repository" "datanow" {
-  name = "datanow"
-
-  tags = merge(local.tags, { Name = "Data Labs Data Lake DataNow Container Repository" })
-}
