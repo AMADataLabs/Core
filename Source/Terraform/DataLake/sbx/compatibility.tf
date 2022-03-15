886fb18ae0acc1c72a0753b2d87cd35005956bc6
Peter Lane <plane@ama-assn.org>
2022-03-15 14:20:56 -0500

    Move legacy DataLake module to new Legacy stack.
    Former-commit-id: 641d7a1269a863853f9414cc9197939bdf518385

--- Source/Terraform/DataLake/sbx/compatibility.tf
+++ Source/Terraform/DataLake/sbx/compatibility.tf
@@ -1,11 +1,5 @@
 # Additional resources that are not part of the TE DataLake stack
 
-module "datalabs_terraform_state" {
-  source  = "../../Module/DataLake"
-  project = "DataLake"
-}
-
-
 resource "aws_ecr_repository" "datanow" {
   name = "datanow"
 
