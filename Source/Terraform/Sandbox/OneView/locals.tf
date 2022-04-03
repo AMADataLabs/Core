9f5aa7bff897d3f51856bdaf70fc4f73e38ed352
rnajeeb <rudainah.najeeb@ama-assn.org>
2022-04-02 21:50:52 -0500

    OneView reindexing tasks as AWS Batch jobs
    Former-commit-id: 786bee586a4fe27fc91a79c7be44a79ca192e163

--- Source/Terraform/Sandbox/OneView/locals.tf
+++ Source/Terraform/Sandbox/OneView/locals.tf
@@ -1,11 +1,41 @@
 locals {
+    accounts = {
+    sbx = "644454719059"
+    dev = "191296302136"
+    tst = "194221139997"
+    stg = "340826698851"
+    prd = "285887636563"
+    }
+
+    region = "us-east-1"
+
+    host_suffixes = {
+    sbx = "-sbx"
+    dev = "-dev"
+    tst = "-test"
+    stg = "-intg"
+    prd = ""
+    }
+
     system_tier         = "Application"
     na                  = "N/A"
     budget_code         = "PBW"
     owner               = "DataLabs"
+    project             = "OneView"
+
+    environment = data.aws_ssm_parameter.account_environment.value
+    account     = lookup(local.accounts, local.environment)
+    ecr_account = local.environment == "sbx" ? local.account : "394406051370"
+
+    dag_lambda            = "${local.project}-${local.environment}-OneViewDag"
+    task_lambda           = "${local.project}-${local.environment}-OneViewDag"
+    task_job              = "OneViewDag"
+    s3_lambda_bucket      = "ama-${local.environment}-datalake-lambda-us-east-1"
+    dynamodb_config_table = "DataLake-configuration-${local.environment}"
+
     tags                = {
         Name                = "Data Labs OneView Parameter"
-        Env                 = data.aws_ssm_parameter.account_environment.value
+        Env                 = local.environment
         Contact             = data.aws_ssm_parameter.contact.value
         SystemTier          = local.system_tier
         DRTier              = local.na
--- Source/Terraform/Sandbox/OneView/main.tf
+++ Source/Terraform/Sandbox/OneView/main.tf
@@ -127,3 +127,129 @@ module "etl_lambda" {
     tag_eol                 = local.tags["EOL"]
     tag_maintwindow         = local.tags["MaintenanceWindow"]
 }
+
+
+module "batch_compute_environment" {
+  source = "../../Module/BatchComputeEnvironment"
+
+  name            = local.task_job
+  project         = local.project
+  environment     = local.environment
+  security_groups = [module.batch_sg.security_group_id]
+  subnets         = data.terraform_remote_state.infrastructure.outputs.subnet_ids
+
+  tag_contact            = local.contact
+  tag_budgetcode         = local.budget_code
+  tag_owner              = local.owner
+  tag_systemtier         = "0"
+  tag_drtier             = "0"
+  tag_dataclassification = "N/A"
+  tag_notes              = "N/A"
+  tag_eol                = "N/A"
+  tag_maintwindow        = "N/A"
+}
+
+module "batch_job_queue" {
+  source = "../../Module/BatchJobQueue"
+
+  name                = local.task_job
+  project             = local.project
+  environment         = local.environment
+  compute_environment = module.batch_compute_environment.arn
+
+  tag_contact            = local.contact
+  tag_budgetcode         = local.budget_code
+  tag_owner              = local.owner
+  tag_systemtier         = "0"
+  tag_drtier             = "0"
+  tag_dataclassification = "N/A"
+  tag_notes              = "N/A"
+  tag_eol                = "N/A"
+  tag_maintwindow        = "N/A"
+}
+
+module "batch_job" {
+  source = "../../Module/BatchJobDefinition"
+
+  name                  = local.task_job
+  project               = local.project
+  environment           = local.environment
+  service_role          = module.batch_compute_environment.service_role.arn
+  ecr_account           = local.ecr_account
+  image                 = "hello_world_java"
+  image_version         = "1.0.0"
+  resource_requirements = <<EOF
+[
+    {"type": "VCPU", "value": "1"},
+    {"type": "MEMORY", "value": "2048"}
+]
+EOF
+  environment_vars      = <<EOF
+[
+    {
+        "name": "TASK_WRAPPER_CLASS",
+        "value": "datalabs.etl.dag.ecs.DAGTaskWrapper"
+    },
+    {
+        "name": "TASK_RESOLVER_CLASS",
+        "value": "datalabs.etl.dag.resolve.TaskResolver"
+    },
+    {
+        "name": "DYNAMODB_CONFIG_TABLE",
+        "value": "${local.project}-configuration-${local.environment}"
+    }
+]
+EOF
+
+  tag_contact            = local.contact
+  tag_budgetcode         = local.budget_code
+  tag_owner              = local.owner
+  tag_systemtier         = "0"
+  tag_drtier             = "0"
+  tag_dataclassification = "N/A"
+  tag_notes              = "N/A"
+  tag_eol                = "N/A"
+  tag_maintwindow        = "N/A"
+}
+
+module "batch_sg" {
+  source      = "app.terraform.io/AMA/security-group/aws"
+  version     = "1.0.0"
+  name        = "${local.project}-${local.environment}-hello-world-java-batch-sg"
+  description = "Security group for Lambda VPC interfaces"
+  vpc_id      = data.terraform_remote_state.infrastructure.outputs.vpc_id[0]
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = "443"
+      to_port     = "443"
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.96.64.0/20,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,199.164.8.1/32"
+    },
+    {
+      from_port   = "5432"
+      to_port     = "5432"
+      protocol    = "tcp"
+      description = "PostgreSQL ports"
+      cidr_blocks = "10.96.64.0/20,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,199.164.8.1/32"
+    },
+    {
+      from_port   = "3306"
+      to_port     = "3306"
+      protocol    = "tcp"
+      description = "MySQL ports"
+      cidr_blocks = "10.96.64.0/20,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,199.164.8.1/32"
+    },
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = "-1"
+      to_port     = "-1"
+      protocol    = "-1"
+      description = "outbound ports"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
