d63061e36c6b38dc95edbe1eaadbc9e8f809ff5c
Peter Lane <plane@ama-assn.org>
2022-05-03 16:29:02 -0500

    Break up watermark DAG into a coordination and per-user DAGs.
    Former-commit-id: 18c27e4495a089e92f5d2ccb77b1dedbcfff9bb9

--- Source/Python/datalabs/etl/dag/cpt/files/watermark.py
+++ Source/Python/datalabs/etl/dag/cpt/files/watermark.py
@@ -1,24 +1,28 @@
 ''' CPT files distribution watermarking DAG module '''
 import datalabs.etl.dag.dag as dag
 from   datalabs.etl.archive.transform import UnzipTransformerTask, ZipTransformerTask
-from   datalabs.etl.s3.extract import S3FileExtractorTask, S3DirectoryListingExtractorTask
+from   datalabs.etl.control import DAGNotificationFactoryTask
+from   datalabs.etl.s3.extract import S3FileExtractorTask
 from   datalabs.etl.s3.load import S3FileReplicatorTask
 from   datalabs.etl.pdf.transform import SignedPDFTransformerTask
+from   datalabs.etl.sns.load import SNSMessageLoaderTask
 
 
-class DAG(dag.DAG):
+class CoordinationDAG(dag.DAG):
     EXTRACT_USER_IDS: S3FileExtractorTask
-    EXTRACT_RELEASE_FILES: UnzipTransformerTask
-    EXTRACT_RELEASE_FILE_LIST: S3DirectoryListingExtractorTask
-    REPLICATE_TEXT_FILES: S3FileReplicatorTask
-    SIGN_PDFS: SignedPDFTransformerTask
-    CREATE_WATERMARKED_DISTRIBUTIONS: ZipTransformerTask
+    SCHEDULE_USER_DAGS: DAGRepeaterTask
+    NOTIFY_DAG_PROCESSOR: SNSMessageLoaderTask
 
+
+class UserDAG(dag.DAG):
+    EXTRACT_RELEASE_FILES: UnzipTransformerTask
+    SIGN_PDFS: SignedPDFTransformerTask
+    CREATE_WATERMARKED_DISTRIBUTION: ZipTransformerTask
 
 
 # pylint: disable=pointless-statement
-DAG.EXTRACT_USER_IDS >> DAG.REPLICATE_TEXT_FILES
 
-DAG.EXTRACT_RELEASE_FILES >> DAG.EXTRACT_RELEASE_FILE_LIST >> DAG.REPLICATE_TEXT_FILES
+CoordinationDAG.EXTRACT_USER_IDS >> CoordinationDAG.SCHEDULE_USER_DAGS >> CoordinationDAG.NOTIFY_DAG_PROCESSOR
 
-DAG.REPLICATE_TEXT_FILES >> DAG.SIGN_PDFS >> DAG.CREATE_WATERMARKED_DISTRIBUTIONS
+
+UserDAG.EXTRACT_RELEASE_FILES >> UserDAG.SIGN_PDFS >> UserDAG.CREATE_WATERMARKED_DISTRIBUTION
