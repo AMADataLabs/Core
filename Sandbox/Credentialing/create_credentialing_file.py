bb1effc75ea3528602977f46acad5ba47a9fbdb6
Victoria Grose  (she/her/hers) <vigrose@ama-assn.org>
2022-05-09 16:28:25 -0500

    updated delta determination for credentialing and datagov address loads
    Former-commit-id: db8b2a80cc83d4b6085991a8100514ba6a5c726a

--- Sandbox/Credentialing/create_credentialing_file.py
+++ Sandbox/Credentialing/create_credentialing_file.py
@@ -1,12 +1,13 @@
 '''Functions to extract datamart data'''
 import os
-from datetime import date
+from datetime import date, datetime
 import pandas as pd
 import pyodbc
 import settings
 from dateutil.relativedelta import relativedelta
 from datalabs.access.datamart import DataMart
 import logging
+import useful_functions as use
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -73,32 +74,70 @@ def get_results():
     all_results = pd.merge(addresses, results, on='CUSTOMER_NBR')
     return all_results
 
+def get_previous_date():
+    out = os.environ.get('OUTPUT_FOLDER')
+    previous_file = use.get_newest(out, 'address_load_profile_orders')
+    previous_datetime = datetime.strptime(previous_file[-14:-4], "%Y-%m-%d")
+    previous_date = previous_datetime.date()
+    return previous_date
+
 def generate_file():
     '''Generate file for loading'''
     today = str(date.today())
     all_results = get_results()
     out = os.environ.get('OUTPUT_FOLDER')
-    cols = ['me#',
+    previous_date = get_previous_date()
+    all_results = all_results[(all_results.FULL_DT) > previous_date]
+    all_results = all_results.drop_duplicates(['ME','CUSTOMER_KEY'])
+    acceptable_customers = ['Ambulatory Care',
+                            'Hospital',
+                            'Group',
+                            'Government',
+                            'Health Related',
+                            'Government',
+                            'Long Term Care',
+                            'Managed Care']
+    all_results = all_results[all_results.CUSTOMER_CATEGORY_DESC.isin(acceptable_customers)]
+    col_rename = {
+        'ADDRESS_LINE_1':'addr_line_1',
+        'CITY':'addr_city',
+        'STATE':'addr_state',
+        'ADDRESS_LINE_2':'addr_line_2'
+    }
+    all_results = all_results.rename(columns=col_rename)
+    cols = ['entity_id',
+            'me#',
+            'comm_id',
+            'usage',
             'load_type',
             'addr_type',
             'addr_line_1',
             'addr_line_2',
+            'addr_line_3',
             'addr_city',
             'addr_state',
             'addr_zip',
+            'addr_plus4',
+            'addr_country',
             'source',
             'source_dtm'
             ]
     all_results['me#'] = fix_me(all_results.ME)
+    all_results['addr_zip'] = [use.fix_zipcode(x) for x in all_results.ZIPCODE]
+    all_results['addr_plus4'] = [x if len(str(x))==9 else '' for x in all_results.ZIPCODE]
     all_results['load_type'] = 'A'
     all_results['addr_type'] = 'OF'
     all_results['source'] = 'CRED-ADDR'
     all_results['source_dtm'] = today
+    all_results['entity_id'] = ''
+    all_results['comm_id'] = ''
+    all_results['usage'] = ''
+    all_results['addr_country'] = ''
     all_results['addr_line_3'] = ''
     all_results[cols].sample(1000).to_csv(f'{out}Credentialing_Addresses_Sample_{today}.csv',
                                           index=False)
 
-    all_results.to_csv(f'{out}Credentialing_Addresses_{today}.csv', index=False)
+    all_results[cols].to_csv(f'{out}address_load_profile_orders_{today}.csv', index=False)
     all_results.to_csv(f'{out}Credentialing_Addresses_latest.csv', index=False)
 
 def generate_triangulation_file():
@@ -121,4 +160,4 @@ def generate_triangulation_file():
     new_results.to_csv(f'{out}Credentialing_Addresses_{str(today)}.csv', index=False)
 
 if __name__ == "__main__":
-    generate_triangulation_file()
+    generate_file()
