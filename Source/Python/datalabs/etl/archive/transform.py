c8dc961727e8db999121fc0ef4b074845cd14795
Peter Lane <plane@ama-assn.org>
2022-05-20 13:09:42 -0500

    Fix unzip transformer. Implement PDF signing transformer.
    Former-commit-id: f8e271f7244ea46fa520b288d323b8ee015f570b

--- Source/Python/datalabs/etl/archive/transform.py
+++ Source/Python/datalabs/etl/archive/transform.py
@@ -5,8 +5,8 @@ import logging
 import pickle
 from   zipfile import ZipFile
 
-from datalabs.etl.transform import TransformerTask
-from datalabs.etl.extract import FileExtractorTask, IncludeNamesMixin
+from   datalabs.etl.transform import TransformerTask
+from   datalabs.etl.extract import FileExtractorTask
 from   datalabs.parameter import add_schema
 
 logging.basicConfig()
@@ -20,13 +20,14 @@ LOGGER.setLevel(logging.INFO)
 class UnzipTransformerParameters:
     data: object
     files: str = None
-    include_names: str = None
-    include_datestamp: str = None
     execution_time: str = None
 
 
-class UnzipTransformerTask(IncludeNamesMixin, FileExtractorTask):
+class UnzipTransformerTask(FileExtractorTask):
     PARAMETER_CLASS = UnzipTransformerParameters
+    @property
+    def include_names(self):
+        return True
 
     def _get_client(self) -> 'Context Manager':
         return ZipFiles(self._parameters.data)
@@ -64,7 +65,7 @@ class ZipTransformerTask(TransformerTask):
                 LOGGER.debug('Adding %s byte file %s to the archive...', len(data), file)
                 zip_file.writestr(file, data)
 
-        return [bytes(zip_data.getbuffer())]
+        return bytes(zip_data.getbuffer())
 
 
 class ZipFiles:
--- Source/Python/datalabs/etl/pdf/transform.py
+++ Source/Python/datalabs/etl/pdf/transform.py
@@ -1,9 +1,13 @@
 """ AWS S3 Loader """
 from   dataclasses import dataclass
+from   io import BytesIO
 import logging
+import pickle
 
 from   datalabs.etl.task import ExecutionTimeMixin
+from   datalabs.etl.transform import TransformerTask
 from   datalabs.parameter import add_schema
+from   datalabs.tool.pdf import PDFSigner
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -13,14 +17,49 @@ LOGGER.setLevel(logging.DEBUG)
 @add_schema
 @dataclass
 # pylint: disable=too-many-instance-attributes
-class SignedPDFTransformerParameters:
-    base_path: str
-    credentials_file: str
+class PDFSigningTransformerParameters:
     credentials_password: str
     recipient: str
     data: object
     execution_time: str=None
 
 
-class SignedPDFTransformerTask(ExecutionTimeMixin):
-    PARAMETER_CLASS = SignedPDFTransformerParameters
+class PDFSigningTransformerTask(ExecutionTimeMixin, TransformerTask):
+    PARAMETER_CLASS = PDFSigningTransformerParameters
+
+    def _transform(self):
+        credentials, packed_data = self._parameters.data
+        named_files = pickle.loads(packed_data)
+
+        named_pdfs = self._extract_pdfs(named_files)
+
+        named_signed_pdfs = [self._sign_pdf(named_pdf, credentials) for named_pdf in named_pdfs]
+
+        signed_files = list(self._replace_files(named_files, named_signed_pdfs))
+
+        return [pickle.dumps(signed_files)]
+
+    @classmethod
+    def _extract_pdfs(cls, named_files):
+        return [named_file for named_file in named_files if named_file[0].endswith('.pdf')]
+
+    def _sign_pdf(self, named_pdf, credentials):
+        unsigned_pdf_file = BytesIO(named_pdf[1])
+        signed_pdf_file = BytesIO()
+        credentials_file = BytesIO(credentials)
+        password = self._parameters.credentials_password
+        recipient = self._parameters.recipient
+
+        PDFSigner.sign(unsigned_pdf_file, signed_pdf_file, credentials_file, password, recipient)
+
+        return named_pdf[0], bytes(signed_pdf_file.getbuffer())
+
+    @classmethod
+    def _replace_files(cls, named_files, named_updated_files):
+        named_updated_file_map = {name:file for name, file in named_updated_files}
+
+        for name, file in named_files:
+            if name in named_updated_file_map:
+                yield (name, named_updated_file_map[name])
+            else:
+                yield (name, file)
