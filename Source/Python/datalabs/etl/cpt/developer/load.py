a9c4de159694133d1322bb674974014457ec58aa
viviansun0213 <Ronghua.Sun@ama-assn.org>
2022-06-13 17:44:32 -0500

    DL-2555 lint fix
    Former-commit-id: f20112dc2d9273b5d32270925cc6ce4b59eb3863

--- Source/Python/datalabs/etl/cpt/developer/load.py
+++ Source/Python/datalabs/etl/cpt/developer/load.py
@@ -1,12 +1,8 @@
 """ Email Report loader task """
 from   dataclasses import dataclass
-from   datetime import datetime
-from   io import BytesIO
-import pickle
 
 # pylint: disable=import-error, invalid-name
 from   datalabs.etl.load import LoaderTask
-from   datalabs.messaging.email_message import Attachment, send_email
 from   datalabs.parameter import add_schema
 
 
