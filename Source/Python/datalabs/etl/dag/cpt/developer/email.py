829d452e391a44322b986a01930e6617a9df3d04
viviansun0213 <Ronghua.Sun@ama-assn.org>
2022-06-17 11:19:08 -0500

    DL-2670 fix
    Former-commit-id: 8ffae225fb4168cfb390b1f60421aff2fc3d3e81

--- Source/Python/datalabs/etl/dag/cpt/developer/email.py
+++ Source/Python/datalabs/etl/dag/cpt/developer/email.py
@@ -1,11 +1,10 @@
 ''' DAG definition for the Email Report ETL. '''
 from   datalabs.etl.dag.dag import DAG
-from   datalabs.etl.orm.load import ORMLoaderTask
 from   datalabs.etl.cpt.developer.transform import EmailReportGeneratorTask
 from   datalabs.etl.cpt.developer.load import EmailReportSMTPLoaderTask
 
 class EmailReportDAG(DAG):
-    EXTRACT_EMAILS: ORMLoaderTask
+    EXTRACT_EMAILS: SQLExtractorTask
     CREATE_EMAIL_REPORT: EmailReportGeneratorTask
     SEND_EMAIL_REPORT: EmailReportSMTPLoaderTask
 
