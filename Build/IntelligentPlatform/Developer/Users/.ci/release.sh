e8c1d5bf6e175163b22640db04408ab1a2dd486c
viviansun0213 <Ronghua.Sun@ama-assn.org>
2022-06-21 09:27:49 -0500

    DL-2672 create a CPT Developer Program Emails report DAG: Build Configuration 1
    Former-commit-id: c20d95c908b25175ae9b7ad24ea1245a5d368a4a

---
+++ Build/IntelligentPlatform/Developer/Users/.ci/release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -u
+
+ENVIRONMENT=${1:-""}
+
+
+main() {
+    declare -A VERSIONS=(
+        [sbx]=dev
+        [dev]=dev
+        [tst]=1.1.0
+        [prd]=1.0.0
+    )
+
+    if [[ "$ENVIRONMENT" != "" ]]; then
+        export RELEASE_VERSION="${VERSIONS[$ENVIRONMENT]}"
+    fi
+
+    export RELEASE_TYPE="Python"
+    export RELEASE_NAME="datalabs-developer-email-etl"
+    export RELEASE_DESCRIPTION="Developer Emails ETL DAG plugin"
+}
+
+
+main
