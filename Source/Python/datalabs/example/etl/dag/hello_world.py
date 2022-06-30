95778ee1150a96e37730c21b31c462d910b955d3
Magizh Shankar Umashankar <mu9@illinois.edu>
2022-06-30 11:01:09 +0530

    Nameing HelloWorld DAG task
    Former-commit-id: 28df7aaee2dbc6c56cbd4aeca5173b1f026f8364

--- Source/Python/datalabs/example/etl/dag/hello_world.py
+++ Source/Python/datalabs/example/etl/dag/hello_world.py
@@ -1,14 +1,11 @@
 ''' Definition for the HelloWorld DAG. '''
 from   datalabs.etl.dag.dag import DAG
-from   datalabs.etl.task import DummyTask
 from   datalabs.example.etl.hello_world.task import HelloWorldTask
 
 
 
 class HelloWorldDAG(DAG):
-    DUMMY_EXTRACTOR: DummyTask
-    HELLO_WORLD_TRANSFORMER: HelloWorldTask
-    DUMMY_LOADER: DummyTask
+    PRINT_HELLO_WORLD: HelloWorldTask
 
 # pylint: disable=pointless-statement
-HelloWorldDAG.DUMMY_EXTRACTOR >> HelloWorldDAG.HELLO_WORLD_TRANSFORMER >> HelloWorldDAG.DUMMY_LOADER
+HelloWorldDAG.PRINT_HELLO_WORLD
