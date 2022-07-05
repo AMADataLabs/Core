4f40135d454cb16f487c4d308b8ca83c50b98f27
Magizh Shankar Umashankar <mu9@illinois.edu>
2022-07-05 20:48:14 +0530

    webhook done
    Former-commit-id: 0e17a2d73b93ab930467bee6ee387097f8243b8d

--- Source/Python/datalabs/etl/dag/notify/webhook.py
+++ Source/Python/datalabs/etl/dag/notify/webhook.py
@@ -25,5 +25,6 @@ class StatusWebHookNotifier():
             )
         }
         LOGGER.info('Message %s', message)
-        requests.post(self.web_hooks, data=json.dumps(message), headers={'Content-Type': 'application/json'})
+        for web_hook in self.web_hooks:
+            requests.post(web_hook, data=json.dumps(message), headers={'Content-Type': 'application/json'})
         LOGGER.info('WEB HOOK NOTIFICATION SENT TO %s', self.web_hooks)
