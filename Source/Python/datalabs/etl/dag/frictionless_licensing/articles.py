163e71a6a20886cda36cd39bbade9f1a63312bda
rnajeeb <rudainah.najeeb@ama-assn.org>
2022-07-07 19:19:05 -0500

    Refactored name
    Former-commit-id: 9f9b82d4a74cc2954db79e62378044d01d1b8623

--- Source/Python/datalabs/etl/dag/frictionless_licensing/articles.py
+++ Source/Python/datalabs/etl/dag/frictionless_licensing/articles.py
@@ -5,10 +5,10 @@ from   datalabs.etl.orm.load import ORMLoaderTask
 
 
 class ArticlesDAG(DAG):
-    EXTRACT_PLATFORM_ARTICLES: JDBCExtractorTask
-    CREATE_PLATFORM_ARTICLES: 'datalabs.etl.cpt.transform.PlatformTransformerTask'
-    LOAD_PLATFORM_ARTICLES: ORMLoaderTask
+    EXTRACT_ACTIVE_ARTICLES: JDBCExtractorTask
+    CREATE_ACTIVE_ARTICLES: 'datalabs.etl.cpt.organization.transform.PlatformTransformerTask'
+    LOAD_ACTIVE_ARTICLES: ORMLoaderTask
 
 
 # pylint: disable=pointless-statement
-ArticlesDAG.EXTRACT_PLATFORM_ARTICLES >> ArticlesDAG.CREATE_PLATFORM_ARTICLES >> ArticlesDAG.LOAD_PLATFORM_ARTICLES
+ArticlesDAG.EXTRACT_ACTIVE_ARTICLES >> ArticlesDAG.CREATE_ACTIVE_ARTICLES >> ArticlesDAG.LOAD_ACTIVE_ARTICLES
