66271d0772311e395742d3cb6bd858f1295bba8f
Peter Lane <Peter.Lane@ama-assn.org>
2022-07-27 11:21:14 -0500

    Fix uncaught style standard violation.
    Former-commit-id: 80d60c096685cfe757dc24ba734919c684277a4f

--- Source/Python/datalabs/etl/intelligent_platform/licensing/transform.py
+++ Source/Python/datalabs/etl/intelligent_platform/licensing/transform.py
@@ -35,8 +35,10 @@ class LicensedOrganizationsTransformerTask(CSVReaderMixin, CSVWriterMixin, Trans
 
         frictionless_licensing_organizations = frictionless_licensing_organizations.drop_duplicates()
 
-        frictionless_licensing_organizations["id"] = frictionless_licensing_organizations.apply(self._generate_id,
-                                                                                                axis=1)
+        frictionless_licensing_organizations["id"] = frictionless_licensing_organizations.apply(
+            self._generate_id,
+            axis=1
+        )
 
         return [self._dataframe_to_csv(frictionless_licensing_organizations)]
 
