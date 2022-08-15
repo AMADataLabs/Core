c6c5144298f33d6b144a63e10fbf60bd22d09383
Peter Lane <Peter.Lane@ama-assn.org>
2022-08-15 16:45:23 -0500

    Update versions for AIP packages on tst.
    Former-commit-id: 7dc8b5002ad5533e98a3d22cafa8726675146f3a

--- Build/IntelligentPlatform/Developer/Users/.ci/release.sh
+++ Build/IntelligentPlatform/Developer/Users/.ci/release.sh
@@ -9,7 +9,7 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.1.0
+        [tst]=1.2.0
         [prd]=1.1.0
     )
 
--- Build/IntelligentPlatform/Licensing/.ci/release.sh
+++ Build/IntelligentPlatform/Licensing/.ci/release.sh
@@ -9,8 +9,7 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.0.1
-        [itg]=
+        [tst]=1.1.0
         [prd]=1.0.1
     )
 
