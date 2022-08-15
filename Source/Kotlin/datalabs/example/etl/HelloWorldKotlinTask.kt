4bca44294b70d4d8c50f26583a0983527cb87fcd
Peter Lane <Peter.Lane@ama-assn.org>
2022-08-15 01:07:23 -0500

    Add example Kotlin task.
    Former-commit-id: bc0cd687baf94aa4b261eaae1cb4a0cd1c7675ce

--- Source/Kotlin/datalabs/example/etl/HelloWorldKotlinTask.kt
+++ Source/Kotlin/datalabs/example/etl/HelloWorldKotlinTask.kt
@@ -3,6 +3,9 @@ package datalabs.example.etl
 import java.util.Vector
 import kotlin.collections.Map
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import datalabs.parameter.Optional
 import datalabs.parameter.KParameters
 import datalabs.task.Task
@@ -20,6 +23,8 @@ open class HelloWorldKotlinTask(
     parameters: Map<String, String>,
     data: Vector<ByteArray>
 ) : Task(parameters, data, HelloWorldKotlinParameters::class.java) {
+    internal Logger logger = LoggerFactory.getLogger(HelloWorldKotlinTask::class.java);
+
     override fun run() : Vector<ByteArray>? {
         return null
     }
