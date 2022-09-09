50d4d6f604174cf68402efd1cff3313997ce8b33
viviansun0213 <Ronghua.Sun@ama-assn.org>
2022-09-09 10:07:16 -0500

    DL-2311 fix bugs
    Former-commit-id: 4eff22660afb3078f7ac023ef979bab0bf969af5

--- Source/Python/datalabs/etl/cpt/files.py
+++ Source/Python/datalabs/etl/cpt/files.py
@@ -3,27 +3,40 @@ searches the processed data S3 bucket for the most recent (current) and
 next most recent (prior) release distribution zip files
 """
 from   dataclasses import dataclass
+
+from   dateutil.parser import isoparse
+
 from   datalabs.parameter import add_schema
-from   datalabs.etl.s3.extract import S3FileExtractorTask
+from   datalabs.access.aws import AWSClient
+from   datalabs.etl.extract import FileExtractorTask
+from   datalabs.etl.task import ETLException
+
 
 @add_schema
 @dataclass
 # pylint: disable=too-many-instance-attributes
 class ReleaseFilesListExtractorParameters:
+    bucket: str
     base_path: str
     link_files_zip: str = None
     execution_time: str = None
 
+
 # pylint: disable=too-many-ancestors
-class ReleaseFilesListExtractorTask(S3FileExtractorTask):
+class ReleaseFilesListExtractorTask(FileExtractorTask):
     PARAMETER_CLASS = ReleaseFilesListExtractorParameters
 
+    def _get_client(self):
+        return AWSClient(
+            's3'
+        )
+
     def _get_files(self):
-        current_base_path = self._get_latest_path()
-        prior_base_path = self._get_second_latest_path()
+        current_base_path = self._get_two_latest_path()[0]
+        prior_base_path = self._get_two_latest_path()[1]
         files, current_files, prior_files = [], [], []
 
-        if self._parameters.link_files_zip is not None:
+        if self._parameters.link_files_zip is not None and (current_base_path):
             files = self._parameters.link_files_zip.split(',')
         else:
             raise ValueError('"files" parameter must contain the list of files to extract.')
@@ -34,39 +47,89 @@ class ReleaseFilesListExtractorTask(S3FileExtractorTask):
             files = current_files + prior_files
         elif current_base_path:
             files = ['/'.join((current_base_path, file.strip())) for file in files]
-        else:
-            files = []
 
         return files
 
-    def _get_latest_path(self):
+
+    def _extract(self):
+        data = None
+
+        with self._get_client() as client:
+            self._client = client
+            files = self._get_files()
+            data = self._extract_files(files)
+
+        return data
+
+    # pylint: disable=logging-fstring-interpolation
+    # pylint: disable=arguments-differ
+    def _extract_file(self, file):
+        data = None
+
+        try:
+            response = self._client.get_object(Bucket=self._parameters.bucket, Key=file)
+        except Exception as exception:
+            raise ETLException(
+                f"Unable to get file '{file}' from S3 bucket '{self._parameters.bucket}'"
+            ) from exception
+
+        data = response['Body'].read()
+
+        return data
+
+    def _extract_files(self, files):
+        data = [self._extract_file(file) for file in files ]
+
+        return data
+
+    def _get_two_latest_path(self):
         release_folder = self._get_release_folder()
         path = self._parameters.base_path
+        result_path = [None, None]
 
         if (release_folder is not None) and (path is not None):
-            path = '/'.join((self._parameters.base_path, release_folder))
+            # most recent date to execution_date
+            result_path[0] = '/'.join((self._parameters.base_path, release_folder[1]))
+            # next most recent date to execution_date
+            result_path[1] = '/'.join((self._parameters.base_path, release_folder[0]))
 
-        if path.startswith('/'):
-            path = path[1:]
+        if result_path[0].startswith('/'):
+            result_path[0] = result_path[0][1:]
+        if result_path[1].startswith('/'):
+            result_path[1] = result_path[1][1:]
 
-        return path
+        return result_path
 
-    def _get_second_latest_path(self):
-        release_folder = self._get_prior_release_folder()
-        path = self._parameters.base_path
+    def _get_release_folder(self):
+        release_folder = self._get_execution_date()
+        result = []
+        list_files = sorted(self._list_files(self._parameters.base_path))
 
-        if (release_folder is not None) and (path is not None):
-            path = '/'.join((self._parameters.base_path, release_folder))
+        if release_folder is not None:
+            result = [d for d in list_files if d < release_folder]
+            result = result[-2:]
+        else:
+            result = list_files[-2:]
 
-        if path.startswith('/'):
-            path = path[1:]
+        return result
 
-        return path
+    def _get_execution_date(self):
+        execution_time = self._parameters.execution_time
+        execution_date = None
 
-    def _get_prior_release_folder(self):
-        release_folders = sorted(
-            super()._list_files(self._parameters.base_path)
-        )
-        release_folder = release_folders[-2]
+        if execution_time:
+            execution_date = isoparse(execution_time).date().strftime('%Y%m%d')
 
-        return release_folder
+        return execution_date
+
+    def _list_files(self, path):
+        response = self._client.list_objects_v2(Bucket=self._parameters.bucket, Prefix=path)
+        objects = {x['Key'].split('/', 3)[2] for x in response['Contents']}
+
+        if  '' in objects:
+            objects.remove('')
+
+        return objects
+
+    def _resolve_wildcard(self, file):
+        pass
--- Test/Python/test/datalabs/etl/cpt/files.py
+++ Test/Python/test/datalabs/etl/cpt/files.py
@@ -1,91 +1,73 @@
-"""
-Local test for searching the processed data S3 bucket for the most recent (current)
-and next most recent (prior) release distribution zip files
-local directoy list:
-xxx/data/cpt/files//20220830
-xxx/data/cpt/files//20220829
-xxx/data/cpt/files//20220826
-xxx/data/cpt/files//20220825
-xxx/data/cpt/files//20220824
-"""
-import glob
-import os
-import  datalabs.etl.cpt.files as s3
+""" source: datalabs.etl.s3.extract """
+import datetime
 import mock
+import pytest
+from   dateutil.tz import tzutc
 
-# pylint: disable=redefined-outer-name, disable=C0103
-# local directoy list"
-drive_name = "OneDrive - American Medical Association"
-directory = "/mnt/c/Users/rsun/"+drive_name+"/data/cpt/files"
-directory_empty = "/mnt/c/Users/rsun/"+drive_name+"/data/cpt/files1"
+from   datalabs.etl.cpt.files import ReleaseFilesListExtractorTask
 
-# pylint: disable=redefined-outer-name, protected-access, disable=C0103
-#mock the results of the directory listing
-def get_two_latest_path(directory) -> list:
-    with mock.patch('boto3.client'):
-        list_of_files = glob.glob(f'{directory}/*')
-        latest_res, second_latest_res = '', ''
 
-        if len(list_of_files) >= 2:
-            latest_res = max(list_of_files, key=os.path.getctime)
-            list_of_files_noMax = [i for i in list_of_files if i != latest_res]
-            second_latest_res = max(list_of_files_noMax)
-            path = [latest_res, second_latest_res]
-        elif len(list_of_files) == 1:
-            path = [latest_res, None]
-        elif len(list_of_files) == 0:
-            path = [None, None]
+# pylint: disable=redefined-outer-name, protected-access, invalid-name
+def test_mocked_list_dir_files(parameters, object_listing):
+    task = ReleaseFilesListExtractorTask(parameters)
+    files = None
+    with mock.patch("boto3.client") as client:
+        client.return_value.list_objects_v2.return_value = object_listing
 
-    return [list_of_files, path]
+        with task._get_client() as s3:
+            task._client = s3
+            files = list(task._list_files("AMA/CPT/"))
 
-# pylint: disable=redefined-outer-name, protected-access, disable=C0103
-def _get_local_files(task, directory):
-    base_path = get_two_latest_path(directory)[1]
-    current_base_path = base_path[0]
-    prior_base_path = base_path[1]
-    files, current_files, prior_files = [], [], []
+        # list all files in directory AMA/CPT/
+        assert len(files) == 3
+        assert "20220825" in files
+        assert "20220825" in files
+        assert "20211112" in files
 
-    if task._parameters.link_files_zip is not None:
-        files = task._parameters.link_files_zip.split(',')
-    else:
-        raise ValueError('"files" parameter must contain the list of files to extract.')
 
-    if (current_base_path is not None) and (prior_base_path is not None):
-        current_files = ['/'.join((current_base_path, file.strip())) for file in files]
-        prior_files = ['/'.join((prior_base_path, file.strip())) for file in files]
-        files = current_files + prior_files
-    elif current_base_path is not None:
-        files = ['/'.join((current_base_path, file.strip())) for file in files]
-    else:
-        files = []
+# pylint: disable=redefined-outer-name, protected-access, invalid-name
+def test_mocked_get_files(parameters, object_listing):
+    task = ReleaseFilesListExtractorTask(parameters)
+    files = [None, None]
+    with mock.patch("boto3.client") as client:
+        client.return_value.list_objects_v2.return_value = object_listing
+        with task._get_client() as s3:
+            task._client = s3
+            files = task._get_files()
 
-    return files
+        # list current&prior release zip files in directory AMA/CPT/
+        assert len(files) == 2
+        assert "AMA/CPT/20220825/foo.txt" in files[0]
+        assert "AMA/CPT/20220824/foo.txt" in files[1]
 
-# pylint: disable=redefined-outer-name, protected-access, disable=C0103
-def ReleaseFilesListExtractorTaskTests(parameters):
-    with mock.patch('boto3.client'):
-        parameters['base_path'] = directory
-        parameters['link_files_zip'] = 'files_test.zip'
-        parameters['execution_time'] = '20220830'
 
-        task = s3.ReleaseFilesListExtractorTask(parameters)
-        result_files = _get_local_files(task, directory)
-        #assert that correct run directories are chosen and correct zip file paths are generated
-        list_of_dir = get_two_latest_path(directory)[0]
-        current_files = result_files[0]
-        prior_files =  result_files[1]
-        assert len(list_of_dir) == 5
-        assert len(result_files) == 2
+# pylint: disable=redefined-outer-name, protected-access, invalid-name
+def test_mocked_list_extract_files(parameters, object_listing):
+    task = ReleaseFilesListExtractorTask(parameters)
+    data = [None,None]
+    with mock.patch("boto3.client") as client:
+        client.return_value.list_objects_v2.return_value = object_listing
+        with task._get_client() as s3:
+            task._client = s3
+            data = task._extract()
 
-        drive_name = "OneDrive - American Medical Association"
-        assert current_files =='/mnt/c/Users/rsun/'+drive_name+'/data/cpt/files/20220830/files_test.zip'
-        assert prior_files =='/mnt/c/Users/rsun/'+drive_name+'/data/cpt/files/20220829/files_test.zip'
+        # 2 files to be extracted
+        assert len(data) == 2
 
-        # empty directory
-        list_of_files_empty = get_two_latest_path(directory_empty)[0]
-        assert len(list_of_files_empty) == 0
-        result_empty = _get_local_files(task, directory_empty)
-        # empty directory with zip files
-        assert len(result_empty) == 0
 
-ReleaseFilesListExtractorTaskTests(parameters = {})
+# pylint: disable=redefined-outer-name, protected-access, invalid-name
+@pytest.fixture
+
+def parameters():
+    return dict(
+        BUCKET='jumanji',
+        BASE_PATH='AMA/CPT',
+        LINK_FILES_ZIP='foo.txt',
+        EXECUTION_TIME='20220901'
+            )
+
+
+# pylint: disable=line-too-long
+@pytest.fixture
+def object_listing():
+    return {'ResponseMetadata': {'RequestId': 'ESQCJTX5EGF0MNA9', 'HostId': '31z7Cwa0tgCRjKNBS06hucoN3cfzVgoB/anrjDcmZ7KgkG2Zm3aFip55Q7UigG/DWp11z8CmpLw=', 'HTTPStatusCode': 200, 'HTTPHeaders': {'x-amz-id-2': '31z7Cwa0tgCRjKNBS06hucoN3cfzVgoB/anrjDcmZ7KgkG2Zm3aFip55Q7UigG/DWp11z8CmpLw=', 'x-amz-request-id': 'ESQCJTX5EGF0MNA9', 'date': 'Wed, 31 Aug 2022 18:25:24 GMT', 'x-amz-bucket-region': 'us-east-1', 'content-type': 'application/xml', 'transfer-encoding': 'chunked', 'server': 'AmazonS3'}, 'RetryAttempts': 0}, 'IsTruncated': False, 'Contents': [{'Key': 'AMA/CPT/20211112/foo.txt', 'LastModified': datetime.datetime(2021, 11, 12, 17, 29, 46, tzinfo=tzutc()), 'ETag': '"d41d8cd98f00b204e9800998ecf8427e"', 'Size': 0, 'StorageClass': 'STANDARD'}, {'Key': 'AMA/CPT/20220825/foo.txt', 'LastModified': datetime.datetime(2022, 8, 25, 15, 28, 27, tzinfo=tzutc()), 'ETag': '"beff53cfb86ba56a577298131a0907b8"', 'Size': 23, 'StorageClass': 'STANDARD'}, {'Key': 'AMA/CPT/20220824/foo.txt', 'LastModified': datetime.datetime(2022, 8, 24, 20, 56, 31, tzinfo=tzutc()), 'ETag': '"516401d00004232cc330234548ed4812"', 'Size': 70, 'StorageClass': 'STANDARD'}, {'Key': 'AMA/CPT/20211112/foo.txt', 'LastModified': datetime.datetime(2021, 11, 12, 17, 29, 46, tzinfo=tzutc()), 'ETag': '"1159a7c22f53f054d41c8ca1169d5b7c"', 'Size': 14, 'StorageClass': 'STANDARD'}], 'Name': 'ama-sbx-datalake-ingest-us-east-1', 'Prefix': 'AMA/Test/', 'MaxKeys': 1000, 'EncodingType': 'url', 'KeyCount': 4}
