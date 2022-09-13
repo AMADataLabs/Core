c5f8f3760b118f0447b6aad89067df9ec28facb0
Peter Lane <Peter.Lane@ama-assn.org>
2022-09-13 11:39:01 -0500

    Update upgrade-python script. Update AMC DAG definition.
    Former-commit-id: 54bcaa028f0d9604af4a5e83f20366748e81eb08

--- Source/Python/datalabs/etl/dag/masterfile/address_flagging_report.py
+++ Source/Python/datalabs/etl/dag/masterfile/address_flagging_report.py
@@ -1,12 +1,11 @@
 ''' AMC address flagging DAG definition. '''
 from   datalabs.analysis.amc.transform import AMCAddressFlaggingTransformerTask
 from   datalabs.etl.dag import dag
-from   datalabs.etl.sql.jdbc.extract import JDBCExtractorTask
 from   datalabs.analysis.amc.load import AMCReportSMTPLoaderTask
 
 
 class DAG(dag.DAG):
-    EXTRACT_AMC: JDBCExtractorTask
+    EXTRACT_AMC: "datalabs.etl.sql.SqlExtractorTask"
     FLAG_ADDRESSES: AMCAddressFlaggingTransformerTask
     EMAIL_ADDRESS_REPORT: AMCReportSMTPLoaderTask
 
