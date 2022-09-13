6bac2afea0c484e1d0c72b7d770b8e2986c2f039
Peter Lane <Peter.Lane@ama-assn.org>
2022-09-13 13:13:34 -0500

    Build AMC extractor code bundle.
    Former-commit-id: 8ffda83a1f74f9ce033a638cdae85d3b1fc27526

--- Build/AMC/Extract/.ci/release.sh
+++ Build/AMC/Extract/.ci/release.sh
@@ -9,9 +9,9 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.0.1
-        [itg]=1.0.1
-        [prd]=1.0.1
+        [tst]=1.0.0
+        [itg]=1.0.0
+        [prd]=1.0.0
     )
 
     if [[ "$ENVIRONMENT" != "" ]]; then
@@ -19,8 +19,8 @@ main() {
     fi
 
     export RELEASE_TYPE="Python"
-    export RELEASE_NAME="datalabs-amc"
-    export RELEASE_DESCRIPTION="AMC Address Flagging Report DAG plugin"
+    export RELEASE_NAME="datalabs-amc-extract"
+    export RELEASE_DESCRIPTION="AMC Address Flagging Report Extraction DAG plugin"
 }
 
 
