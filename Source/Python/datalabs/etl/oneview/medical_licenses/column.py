4cf67d3dbb0c72a5612776d7bc9ce0bae6dca262
rnajeeb <rudainah.najeeb@ama-assn.org>
2022-09-13 15:12:56 -0500

    Updated medical license transformer
    Former-commit-id: 79e7567a98eb85772aa35ab4b11e36ddc3c46866

--- Source/Python/datalabs/etl/oneview/medical_licenses/column.py
+++ Source/Python/datalabs/etl/oneview/medical_licenses/column.py
@@ -3,6 +3,7 @@
 MEDICAL_LICENSES_COLUMNS = {
     'LIC_NBR': 'number',
     'meNumber': 'medical_education_number',
+    'LIC_STATE': 'state',
     'ISS_DT': 'issue_date',
     'EXP_DT': 'expiry_date',
     'RNW_DT': 'renew_date',
