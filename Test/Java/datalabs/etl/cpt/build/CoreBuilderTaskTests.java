7e3410454869fab9f53c604a9eecd18e096e4e70
Peter Lane <Peter.Lane@ama-assn.org>
2022-09-16 16:32:38 -0500

    Add DtkAccess behavior check unit test.
    Former-commit-id: 1a02a3298ed2758dbd5a43fdc5db78d51a1781c1

---
+++ Test/Java/datalabs/etl/cpt/build/CoreBuilderTaskTests.java
@@ -0,0 +1,43 @@
+package datalabs.etl.cpt.build;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+
+import org.ama.dtk.DtkAccess;
+import datalabs.etl.dag.notify.sns.DagNotifier;
+
+
+class CoreBuilderTaskTests {
+    protected static final Logger LOGGER = LoggerFactory.getLogger(CoreBuilderTaskTests.class);
+
+    @BeforeEach
+    void beforeEach() {
+    }
+
+    @Test
+    @DisplayName("Test DtkAccess.load() will throw an exception if the file arguments are invalid.")
+    void dtkAccessLoadThrowsExceptionForInvalidFilePaths() {
+        DtkAccess link = new DtkAccess();
+        boolean success = false;
+
+        try {
+            link.load("bogus/path", "fake/path");
+        } catch (java.lang.Exception exception) {
+            success = true;
+        }
+
+        Assertions.assertTrue(success);
+    }
+}
