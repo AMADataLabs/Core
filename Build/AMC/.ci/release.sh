67b7b983a317087cd0212d91842033b650e5bb52
Peter Lane <Peter.Lane@ama-assn.org>
2022-09-20 09:30:14 -0500

    Update AMC component version. Add script to get latest component version in CodeArtifact.

--- Build/AMC/.ci/release.sh
+++ Build/AMC/.ci/release.sh
@@ -9,8 +9,8 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.0.1
-        [itg]=1.0.1
+        [tst]=1.1.0
+        [itg]=1.1.0
         [prd]=1.0.1
     )
 
