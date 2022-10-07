ad0d2a383ef02f97258da7abfd75db2e4489429e
Peter Lane <Peter.Lane@ama-assn.org>
2022-10-07 00:09:45 -0500

    Update OneView ETL package version on tst and prd.

--- Build/OneView/ETL/.ci/release.sh
+++ Build/OneView/ETL/.ci/release.sh
@@ -9,8 +9,8 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.2.3
-        [prd]=1.2.3
+        [tst]=1.3.0
+        [prd]=1.3.0
     )
 
     if [[ "$ENVIRONMENT" != "" ]]; then
