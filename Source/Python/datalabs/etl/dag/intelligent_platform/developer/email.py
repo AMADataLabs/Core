a0bb1c228cd01190f808623c42f8f240821d7b5f
Peter Lane <Peter.Lane@ama-assn.org>
2022-10-13 15:03:55 -0500

    Fix developer emails DAG task class. Fix DAG config.

--- Source/Python/datalabs/etl/dag/intelligent_platform/developer/email.py
+++ Source/Python/datalabs/etl/dag/intelligent_platform/developer/email.py
@@ -1,11 +1,12 @@
 ''' DAG definition for the Email Report ETL. '''
 from   datalabs.etl.dag import dag
 from   datalabs.etl.intelligent_platform.developer.email.load import EmailReportSMTPLoaderTask
+from   datalabs.etl.sql.sqlalchemy import SQLAlchemyExtractorTask
 
 
 @dag.register(name="DEVELOPER_EMAILS")
 class DAG(dag.DAG):
-    EXTRACT_EMAILS: "SQLExtractorTask"
+    EXTRACT_EMAILS: SQLAlchemyExtractorTask
     SEND_EMAIL_REPORT: EmailReportSMTPLoaderTask
 
 
