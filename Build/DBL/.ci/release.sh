867a5b515dbe00a8d2bb64a847e1ad912ba210cf
Peter Lane <Peter.Lane@ama-assn.org>
2022-10-24 10:55:14 -0500

    Update DBL target versions for tst and prd.

--- Build/DBL/.ci/release.sh
+++ Build/DBL/.ci/release.sh
@@ -9,8 +9,8 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.1.1
-        [prd]=1.1.1
+        [tst]=1.1.2
+        [prd]=1.1.2
     )
 
     if [[ "$ENVIRONMENT" != "" ]]; then
