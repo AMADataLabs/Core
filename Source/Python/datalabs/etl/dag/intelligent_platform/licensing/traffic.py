fcc14eb270eb9d93f1d8c375bc32dc98802c8507
Yogesh Vaze <Yogesh.Vaze@INFOVISIONLABS.COM>
2022-11-24 14:48:35 +0530

    Made changes to the sql

--- Source/Python/datalabs/etl/dag/intelligent_platform/licensing/traffic.py
+++ Source/Python/datalabs/etl/dag/intelligent_platform/licensing/traffic.py
@@ -1,14 +1,18 @@
 ''' DAG definition for the Intelligent Platform Licensing ETL. '''
 from   datalabs.etl.dag import dag
 from   datalabs.etl.sql.sqlalchemy.extract import SQLAlchemyExtractorTask
-from   datalabs.etl.intelligent_platform.licensing.traffic.load import TrafficReportSMTPLoaderTask
-
+from   datalabs.etl.smtp.load import SMTPFileLoaderTask
+from   datalabs.etl.orm.load import ORMLoaderTask
 
 @dag.register(name="LICENSING_TRAFFIC")
 class DAG(dag.DAG):
     EXTRACT_TRAFFIC: SQLAlchemyExtractorTask
-    SEND_TRAFFIC_REPORT: TrafficReportSMTPLoaderTask
-
+    SEND_TRAFFIC_REPORT: SMTPFileLoaderTask
+    EXTRACT_COUNTS: SQLAlchemyExtractorTask
+    LOAD_COUNTS_TABLE: ORMLoaderTask
+    SEND_APPLICANT_DATA: SMTPFileLoaderTask
 
 # pylint: disable=pointless-statement
 DAG.EXTRACT_TRAFFIC >> DAG.SEND_TRAFFIC_REPORT
+DAG.EXTRACT_COUNTS >> DAG.LOAD_COUNTS_TABLE
+DAG.EXTRACT_COUNTS >> DAG.SEND_APPLICANT_DATA
