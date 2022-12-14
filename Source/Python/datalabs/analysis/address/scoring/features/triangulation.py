44243279b910c30cf97b06a6ab4ce484bb832e1d
Garrett <garrett.lappe@ama-assn.org>
2022-12-14 01:04:34 -0600

    Huuuuuuuuuuuuuge performance increase in processing time by refactoring feature generation checks using Pandas

--- Source/Python/datalabs/analysis/address/scoring/features/triangulation.py
+++ Source/Python/datalabs/analysis/address/scoring/features/triangulation.py
@@ -10,6 +10,48 @@ pd.set_option('max_columns', None)
 REQUIRED_COLUMNS = ['ME', 'ADDRESS_KEY']
 
 
+ADDRESS_STANDARDIZATIONS = {
+    'AVENUE': 'AVE',
+    'SUITE': 'STE',
+    'ROAD': 'RD',
+    'PLACE': 'PL',
+    'NORTH': 'N',
+    'EAST': 'E',
+    'SOUTH': 'S',
+    'WEST': 'W',
+    'BOULEVARD': 'BLVD',
+    'STREET': 'ST',
+    'DRIVE': 'DR',
+    'HIGHWAY': 'HWY',
+    'CULVERT': 'CT',
+    'PARKWAY': 'PKWY',
+
+    'CENTER': 'CTR',
+    'UNIVERSITY': 'UNI',
+    'UNIV': 'UNI',
+    'HOSPITAL': 'HOSP',
+    'HOSPIT': 'HOSP',
+}
+
+def standardize_address_words(text):
+    tokens = text.split()
+    result = []
+    for token in tokens:
+        if token in ADDRESS_STANDARDIZATIONS:
+            result.append(ADDRESS_STANDARDIZATIONS[token])
+        else:
+            result.append(token)
+    return ' '.join(result)
+
+def standardize_address_in_me_address_key(me_address_key):
+    sections = me_address_key.split('_')
+
+    if len(sections) != 3:
+        return me_address_key  # data is messed up.
+    me, address, zip = sections
+    address = standardize_address_words(address)
+    return f'{me}_{address}_{zip}'
+
 class TriangulationDataSource:
     def __init__(self, file_or_buffer, source_name):
         self.file_or_buffer = file_or_buffer
@@ -26,44 +68,55 @@ def add_triangulation_features(base_data: pd.DataFrame, triangulation_data: Tria
     # 1. find if base_data ME-address_key pair exists in triangulation
     # 2. find if OTHER address_key values exist for each ME-address_key pair
     base_data.columns = [col.upper() for col in base_data.columns]
-    base_pairs = base_data[['ME', 'ADDRESS_KEY']].drop_duplicates()
-    base_pairs['ADDRESS_KEY'] = base_pairs['ADDRESS_KEY'].fillna('').astype(str)
+
+    base_data['ME10'] = base_data['ME'].apply(lambda x: x[:10])  # IQVIA and Symphony ME numbers are only first 10 chars
+    if 'ADDRESS_KEY' not in base_data.columns.values:
+        base_data['ADDRESS_KEY'] = base_data['ADDR_LINE2'].fillna('').apply(
+            str.upper).apply(standardize_address_words) + '_' + base_data['ZIP'].fillna('')
+
+    base_data['ME_ADDRESS_KEY'] = base_data['ME10'] + '_' + base_data['ADDRESS_KEY']
+    base_data['ME_ADDRESS_KEY'] = base_data['ME_ADDRESS_KEY'].apply(str.upper)
 
     if 'AS_OF_DATE' in triangulation_data.data.columns:
         triangulation_data.data = triangulation_data.data[triangulation_data.data['AS_OF_DATE'] <= as_of_date]
 
     triangulation_pairs = triangulation_data.data[['ME', 'ADDRESS_KEY']].drop_duplicates()
+
+
+
     triangulation_pairs['ADDRESS_KEY'] = triangulation_pairs['ADDRESS_KEY'].fillna('').astype(str)
 
-    base_pairs = [
-        f"{me}_{key}" for me, key in tqdm(zip(base_pairs['ME'].values, base_pairs['ADDRESS_KEY'].values), total=base_pairs.shape[0])
-    ]
-    triangulation_pairs = [
-        f"{me}_{key}" if key != '' else None for me, key in tqdm(
-            zip(triangulation_pairs['ME'].values, triangulation_pairs['ADDRESS_KEY'].values),
-            total=triangulation_pairs.shape[0]
-        )
-    ]
+    triangulation_pairs['ME_ADDRESS_KEY'] = triangulation_pairs['ME'].fillna('').astype(str) + '_' + \
+                                            triangulation_pairs['ADDRESS_KEY']
 
-    # this below line is quite slow, need to optimize / use another strategy
-    triangulation_agreement = {base_pair: base_pair in triangulation_pairs for base_pair in tqdm(base_pairs)}
+    triangulation_pairs['ME_ADDRESS_KEY'] = triangulation_pairs['ME_ADDRESS_KEY'].apply(
+        str.upper
+    ).apply(
+        standardize_address_in_me_address_key
+    )
 
-    # if ME exists in both base and triangulation data but the PAIR does not exist in triangulation, then
-    # some other pair must exist for that ME
-    triangulation_other = {
-        base_pair: base_pair.split('_')[0] in triangulation_data.data['ME'].values and
-        base_pair not in triangulation_pairs for base_pair in tqdm(base_pairs)
-    }
-    feature_data = base_data[['ENTITY_ID', 'ME', 'ADDRESS_KEY']].drop_duplicates()
-    feature_data['KEY'] = [
-        f"{me}_{key}" for me, key in tqdm(zip(feature_data['ME'].values, feature_data['ADDRESS_KEY'].values), total=feature_data.shape[0])
-    ]
-    feature_data[f'TRIANGULATION_{triangulation_data.source_name}_AGREEMENT'] = feature_data['KEY'].apply(
-        lambda x: triangulation_agreement[x]
-    ).fillna(0).astype(int)
-    feature_data[f'TRIANGULATION_{triangulation_data.source_name}_OTHER'] = feature_data['KEY'].apply(
-        lambda x: triangulation_other[x]
-    ).fillna(0).astype(int)
+    # only keep triangulation data with actual address keys populated (with zip code)
+    triangulation_pairs = triangulation_pairs[triangulation_pairs['ADDRESS_KEY'].apply(lambda x: not x.endswith('_'))]
+
+    agreement_colname = f'TRIANGULATION_{triangulation_data.source_name}_AGREEMENT'
+    other_colname = f'TRIANGULATION_{triangulation_data.source_name}_OTHER'
+
+    base_data[agreement_colname] = 0
+    base_data.loc[
+        base_data['ME_ADDRESS_KEY'].isin(triangulation_pairs['ME_ADDRESS_KEY']),
+        agreement_colname
+    ] = 1
+    print(agreement_colname, base_data[agreement_colname].sum())
+
+    base_data[other_colname] = 0
+    base_data.loc[
+        (base_data['ME10'].isin(triangulation_pairs['ME'])) &
+        (~base_data[agreement_colname]),
+        other_colname
+    ] = 1
+    print(other_colname, base_data[other_colname].sum())
+
+    feature_data = base_data[['ENTITY_ID', 'ME', 'ADDRESS_KEY', agreement_colname, other_colname]].drop_duplicates()
     return feature_data
 
 
