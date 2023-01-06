0a860de432677f655ab6e608ab76690907af863b
Peter Lane <Peter.Lane@ama-assn.org>
2023-01-06 15:48:45 -0600

    Set parameter class for base TriggerHandlerTask.

--- Source/Python/datalabs/etl/dag/trigger/handler/task.py
+++ Source/Python/datalabs/etl/dag/trigger/handler/task.py
@@ -16,6 +16,8 @@ class TriggerHandlerParameters:
 
 
 class TriggerHandlerTask(Task, ABC):
+    PARAMETER_CLASS = TriggerHandlerParameters
+
     def run(self) -> "list<bytes>":
         notifier = SNSDAGNotifier(self._parameters["dag_topic_arn"])
 
