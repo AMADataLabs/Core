ce6c5021f15e9453429f2b0215d7d68f4f37e7a3
Peter Lane <Peter.Lane@ama-assn.org>
2023-01-06 17:18:17 -0600

    Advance scheduler to tst and itg.

--- Build/Scheduler/.ci/release.sh
+++ Build/Scheduler/.ci/release.sh
@@ -9,8 +9,8 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.4.1
-        [itg]=1.4.1
+        [tst]=2.0.0
+        [itg]=2.0.0
         [prd]=1.4.1
     )
 
