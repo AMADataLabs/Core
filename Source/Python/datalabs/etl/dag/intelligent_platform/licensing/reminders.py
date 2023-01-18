b6cbc1ef3b4f975d79143fae24dc8cc6cc95ecb1
Peter Lane <Peter.Lane@ama-assn.org>
2023-01-18 10:46:22 -0600

    Adjust import for module name change.

--- Source/Python/datalabs/etl/dag/intelligent_platform/licensing/reminders.py
+++ Source/Python/datalabs/etl/dag/intelligent_platform/licensing/reminders.py
@@ -1,7 +1,7 @@
 ''' DAG definition for the Intelligent Platform Licensing ETL. '''
 from datalabs.etl.dag import dag
 from datalabs.etl.intelligent_platform.licensing.reminders.email import ReminderEmailTask
-from datalabs.etl.intelligent_platform.licensing.reminders.update_reminders import IncrementRemindersTask
+from datalabs.etl.intelligent_platform.licensing.reminders.increment_reminders import IncrementRemindersTask
 from datalabs.etl.orm.load import ORMLoaderTask
 from datalabs.etl.sql.sqlalchemy.extract import SQLAlchemyExtractorTask
 
