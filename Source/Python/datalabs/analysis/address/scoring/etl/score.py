7740a69c23a87293679abb8af113db17a26c92d2
Peter Lane <Peter.Lane@ama-assn.org>
2023-01-20 18:31:59 -0600

    Cleanup Address Scoring score module.

--- Source/Python/datalabs/analysis/address/scoring/etl/score.py
+++ Source/Python/datalabs/analysis/address/scoring/etl/score.py
@@ -1,20 +1,28 @@
 """ Applies a pre-trained model to score processed address feature data """
-from   datetime import datetime
-import os
+from dataclasses import dataclass
 import pickle as pk
 
-import numpy as np
 import pandas as pd
 from   sklearn.preprocessing import MinMaxScaler
-from   xgboost import XGBClassifier
 
 from   datalabs.analysis.address.scoring.etl.feature import REQUIRED_FEATURES
 from   datalabs.analysis.address.scoring.etl.column \
     import INFO_COLUMNS, FILLMAX_COLUMNS, FILLNEG1_COLUMNS, FILL1_COLUMNS
-from   datalabs.etl.transform import TransformerTask
+from   datalabs.etl.csv import CSVReaderMixin, CSVWriterMixin
+from   datalabs.parameter import add_schema
+from   datalabs.task import Task
 
 
-class AddressScoringTransformerTask(TransformerTask):
+@add_schema
+@dataclass
+# pylint: disable=too-many-instance-attributes
+class AddressScoringTransformerParameters:
+    execution_time: str = None
+
+
+class AddressScoringTransformerTask(CSVReaderMixin, CSVWriterMixin, Task):
+    PARAMETER_CLASS = AddressScoringTransformerParameters
+
     def run(self) -> 'list<bytes>':
         pd.set_option('max_columns', None)
         model = pk.loads(self._data[0])
@@ -28,7 +36,7 @@ class AddressScoringTransformerTask(TransformerTask):
         if 'me10' in aggregate_features.columns.values:  # lingering from triangulation
             del aggregate_features['me10']
 
-        found_info_columns = [column for column in INFO_COLUMNS if col in aggregate_features.columns]
+        found_info_columns = [column for column in INFO_COLUMNS if column in aggregate_features.columns]
         info_data = aggregate_features[found_info_columns]
 
         aggregate_features.set_index(found_info_columns, inplace=True)
@@ -40,7 +48,7 @@ class AddressScoringTransformerTask(TransformerTask):
                 aggregate_features[column] = aggregate_features[column].fillna(0).astype(int)
 
         for column in REQUIRED_FEATURES:
-            if col not in aggregate_features.columns:
+            if column not in aggregate_features.columns:
                 aggregate_features[column] = 0
         predictions = model.predict_proba(aggregate_features[REQUIRED_FEATURES])
         predictions = [p[1] for p in predictions]
@@ -48,7 +56,8 @@ class AddressScoringTransformerTask(TransformerTask):
 
         return info_data
 
-    def _scale_columns(self, data: pd.DataFrame):
+    @classmethod
+    def _scale_columns(cls, data: pd.DataFrame):
         for column in data.columns.values:
             if 'count' in column or 'frequency' in column or '_age' in column or 'years' in column:
                 scaler = MinMaxScaler()
@@ -57,8 +66,8 @@ class AddressScoringTransformerTask(TransformerTask):
 
         return data
 
-    def _resolve_null_values(self, data: pd.DataFrame):
-
+    @classmethod
+    def _resolve_null_values(cls, data: pd.DataFrame):
         for column in FILLNEG1_COLUMNS:
             data[column].fillna(-1, inplace=True)
 
@@ -68,16 +77,17 @@ class AddressScoringTransformerTask(TransformerTask):
         for column in data.columns.values:
             try:
                 data[column] = data[column].astype(float)
-            except:
+            except:  # pylint: disable=bare-except
                 pass
 
         for column in FILLMAX_COLUMNS:
-            mx = data[column].dropna().max()
-            data[column].fillna(mx, inplace=True)
+            max_value = data[column].dropna().max()
+            data[column].fillna(max_value, inplace=True)
 
         return data.fillna(0)
 
-    def _resolve_feature_structure(self, data: pd.DataFrame):
+    @classmethod
+    def _resolve_feature_structure(cls, data: pd.DataFrame):
         # removes any extra features generated (likely source-related columns) and sorts column order
         resolved_data = pd.DataFrame()
         resolved_data.index = data.index
