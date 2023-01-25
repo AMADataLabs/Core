53dc8efab36dfebed50d819c7472786486f8872e
Peter Lane <Peter.Lane@ama-assn.org>
2023-01-25 09:36:50 -0600

    Update licensing package versions.

--- Build/IntelligentPlatform/Licensing/.ci/release.sh
+++ Build/IntelligentPlatform/Licensing/.ci/release.sh
@@ -9,8 +9,8 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.6.2
-        [prd]=1.6.2
+        [tst]=1.7.0
+        [prd]=1.7.0
     )
 
     if [[ "$ENVIRONMENT" != "" ]]; then
