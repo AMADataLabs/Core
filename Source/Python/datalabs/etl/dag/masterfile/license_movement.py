7f94e3d43375a30f43174f93890bf34809e44105
viviansun0213 <Ronghua.Sun@ama-assn.org>
2023-02-03 09:29:57 -0600

    revert dag

--- Source/Python/datalabs/etl/dag/masterfile/license_movement.py
+++ Source/Python/datalabs/etl/dag/masterfile/license_movement.py
@@ -1,20 +1,18 @@
 ''' DAG definition for License Movement Process '''
 from   datalabs.analysis.ppma.license_movement.transform import LicenseMovementTransformerTask
 from   datalabs.etl.dag import dag
-from   datalabs.etl.dag.dag import register, Repeat
-from   datalabs.etl.manipulate.transform import ConcatenateTransformerTask
+from   datalabs.etl.dag.dag import register
 from   datalabs.etl.sftp.extract import SFTPFileExtractorTask
 from   datalabs.etl.sftp.load import SFTPFileLoaderTask
+from   datalabs.etl.sql.extract import SQLExtractorTask
 
 
 @register(name="LICENSE_MOVEMENT")
 class DAG(dag.DAG):
     EXTRACT_PPD: SFTPFileExtractorTask
     EXTRACT_CREDENTIALING_ADDRESSES: SFTPFileExtractorTask
-    EXTRACT_OLD_PPMA: Repeat("SqlExtractorTask", 20)
-    CONCATENATE_OLD_PPMA: ConcatenateTransformerTask
-    EXTRACT_MISMATCHES: Repeat("SqlExtractorTask", 60)
-    CONCATENATE_MISMATCHES: ConcatenateTransformerTask
+    EXTRACT_OLD_PPMA: SQLExtractorTask
+    EXTRACT_MISMATCHES: SQLExtractorTask
     CREATE_BATCH_LOAD_FILE: LicenseMovementTransformerTask
     LOAD_BATCH_LOAD_FILE: SFTPFileLoaderTask
 
@@ -22,12 +20,6 @@ class DAG(dag.DAG):
 # pylint: disable=pointless-statement, expression-not-assigned
 DAG.EXTRACT_PPD >> DAG.CREATE_BATCH_LOAD_FILE
 DAG.EXTRACT_CREDENTIALING_ADDRESSES >> DAG.CREATE_BATCH_LOAD_FILE
-DAG.sequence('EXTRACT_OLD_PPMA')
-DAG.last('EXTRACT_OLD_PPMA') \
-        >> DAG.CONCATENATE_OLD_PPMA \
-        >> DAG.CREATE_BATCH_LOAD_FILE
-DAG.sequence('EXTRACT_MISMATCHES')
-DAG.last('EXTRACT_MISMATCHES') \
-        >> DAG.CONCATENATE_MISMATCHES \
-        >> DAG.CREATE_BATCH_LOAD_FILE
+DAG.EXTRACT_OLD_PPMA >> DAG.CREATE_BATCH_LOAD_FILE
+DAG.EXTRACT_MISMATCHES >> DAG.CREATE_BATCH_LOAD_FILE
 DAG.CREATE_BATCH_LOAD_FILE >> DAG.LOAD_BATCH_LOAD_FILE
