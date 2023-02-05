13b49d89b0f1d16bbb42ef3fb4a6d9de9dfd5c41
viviansun0213 <Ronghua.Sun@ama-assn.org>
2023-02-05 14:13:16 -0600

    mismatch n=30

--- Source/Python/datalabs/etl/dag/masterfile/license_movement.py
+++ Source/Python/datalabs/etl/dag/masterfile/license_movement.py
@@ -1,7 +1,8 @@
 ''' DAG definition for License Movement Process '''
 from   datalabs.analysis.ppma.license_movement.transform import LicenseMovementTransformerTask
 from   datalabs.etl.dag import dag
-from   datalabs.etl.dag.dag import register
+from   datalabs.etl.dag.dag import register, Repeat
+from   datalabs.etl.manipulate.transform import ConcatenateTransformerTask
 from   datalabs.etl.sftp.extract import SFTPFileExtractorTask
 from   datalabs.etl.sftp.load import SFTPFileLoaderTask
 from   datalabs.etl.sql.extract import SQLExtractorTask
@@ -12,7 +13,8 @@ class DAG(dag.DAG):
     EXTRACT_PPD: SFTPFileExtractorTask
     EXTRACT_CREDENTIALING_ADDRESSES: SFTPFileExtractorTask
     EXTRACT_OLD_PPMA: SQLExtractorTask
-    EXTRACT_MISMATCHES: SQLExtractorTask
+    EXTRACT_MISMATCHES: Repeat("SqlExtractorTask", 30)
+    CONCATENATE_MISMATCHES: ConcatenateTransformerTask
     CREATE_BATCH_LOAD_FILE: LicenseMovementTransformerTask
     LOAD_BATCH_LOAD_FILE: SFTPFileLoaderTask
 
@@ -21,5 +23,8 @@ class DAG(dag.DAG):
 DAG.EXTRACT_PPD >> DAG.CREATE_BATCH_LOAD_FILE
 DAG.EXTRACT_CREDENTIALING_ADDRESSES >> DAG.CREATE_BATCH_LOAD_FILE
 DAG.EXTRACT_OLD_PPMA >> DAG.CREATE_BATCH_LOAD_FILE
-DAG.EXTRACT_MISMATCHES >> DAG.CREATE_BATCH_LOAD_FILE
+DAG.sequence('EXTRACT_MISMATCHES')
+DAG.last('EXTRACT_MISMATCHES') \
+        >> DAG.CONCATENATE_MISMATCHES \
+        >> DAG.CREATE_BATCH_LOAD_FILE
 DAG.CREATE_BATCH_LOAD_FILE >> DAG.LOAD_BATCH_LOAD_FILE
