c022430ea9d4685021004e5b4a95b39dc25a81d9
viviansun0213 <Ronghua.Sun@ama-assn.org>
2023-02-07 10:51:02 -0600

    old_ppma without mismatch

--- Source/Python/datalabs/etl/dag/masterfile/license_movement.py
+++ Source/Python/datalabs/etl/dag/masterfile/license_movement.py
@@ -1,20 +1,20 @@
 ''' DAG definition for License Movement Process '''
 from   datalabs.analysis.ppma.license_movement.transform import LicenseMovementTransformerTask
 from   datalabs.etl.dag import dag
-from   datalabs.etl.dag.dag import register, Repeat
+from   datalabs.etl.dag.dag import register
 from   datalabs.etl.manipulate.transform import ConcatenateTransformerTask
 from   datalabs.etl.sftp.extract import SFTPFileExtractorTask
 from   datalabs.etl.sftp.load import SFTPFileLoaderTask
-#from   datalabs.etl.sql.extract import SQLExtractorTask
+from   datalabs.etl.sql.extract import SQLExtractorTask
 
 
 @register(name="LICENSE_MOVEMENT")
 class DAG(dag.DAG):
     EXTRACT_PPD: SFTPFileExtractorTask
     EXTRACT_CREDENTIALING_ADDRESSES: SFTPFileExtractorTask
-    #EXTRACT_OLD_PPMA: SQLExtractorTask
-    EXTRACT_MISMATCHES: Repeat("SqlExtractorTask", 2)
-    CONCATENATE_MISMATCHES: ConcatenateTransformerTask
+    EXTRACT_OLD_PPMA: SQLExtractorTask
+    #EXTRACT_MISMATCHES: Repeat("SqlExtractorTask", 2)
+    #CONCATENATE_MISMATCHES: ConcatenateTransformerTask
     CREATE_BATCH_LOAD_FILE: LicenseMovementTransformerTask
     LOAD_BATCH_LOAD_FILE: SFTPFileLoaderTask
 
@@ -22,9 +22,9 @@ class DAG(dag.DAG):
 # pylint: disable=pointless-statement, expression-not-assigned
 DAG.EXTRACT_PPD >> DAG.CREATE_BATCH_LOAD_FILE
 DAG.EXTRACT_CREDENTIALING_ADDRESSES >> DAG.CREATE_BATCH_LOAD_FILE
-#DAG.EXTRACT_OLD_PPMA >> DAG.CREATE_BATCH_LOAD_FILE
-DAG.sequence('EXTRACT_MISMATCHES')
-DAG.last('EXTRACT_MISMATCHES') \
-        >> DAG.CONCATENATE_MISMATCHES \
-        >> DAG.CREATE_BATCH_LOAD_FILE
+DAG.EXTRACT_OLD_PPMA >> DAG.CREATE_BATCH_LOAD_FILE
+#DAG.sequence('EXTRACT_MISMATCHES')
+#DAG.last('EXTRACT_MISMATCHES') \
+#        >> DAG.CONCATENATE_MISMATCHES \
+#        >> DAG.CREATE_BATCH_LOAD_FILE
 DAG.CREATE_BATCH_LOAD_FILE >> DAG.LOAD_BATCH_LOAD_FILE
