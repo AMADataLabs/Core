f65f41729262122dd2da8aebc47e03bc3222a4a8
Peter Lane <Peter.Lane@ama-assn.org>
2023-03-01 14:13:58 -0600

    Build dev version of CPT files Lambda bundles.

--- Build/CPT/Files/Build/.ci/release.sh
+++ Build/CPT/Files/Build/.ci/release.sh
@@ -9,8 +9,8 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-#        [tst]=1.0.0
-#        [prd]=1.0.0
+        [tst]=0.2.0
+        [prd]=0.2.0
     )
 
     if [[ "$ENVIRONMENT" != "" ]]; then
