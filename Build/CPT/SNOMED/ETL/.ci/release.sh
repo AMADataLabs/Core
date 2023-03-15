bb4f5105e3731e7bbd4a563c9f0fe6344695d795
rnajeeb <rudainah.najeeb@ama-assn.org>
2023-03-15 15:44:14 -0500

    Create snomed etl build config

---
+++ Build/CPT/SNOMED/ETL/.ci/release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -u
+
+ENVIRONMENT=${1:-""}
+
+
+main() {
+    declare -A VERSIONS=(
+        [sbx]=dev
+        [dev]=dev
+        [tst]=0.1.0
+        [prd]=0.1.0
+    )
+
+    if [[ "$ENVIRONMENT" != "" ]]; then
+        export RELEASE_VERSION="${VERSIONS[$ENVIRONMENT]}"
+    fi
+
+    export RELEASE_TYPE="Python"
+    export RELEASE_NAME="datalabs-cpt-snomed-etl"
+    export RELEASE_DESCRIPTION="CPT SNOMED ETL DAG definition and tasks"
+}
+
+
+main
