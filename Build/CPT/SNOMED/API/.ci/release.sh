143885a6d2fe41be770cf7553a799c7ffdceb2f4
Nikhil <nikhil.trivedi@ama-assn.org>
2023-03-15 17:31:01 -0500

    first commit - SNOMED-CPT-API

---
+++ Build/CPT/SNOMED/API/.ci/release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -u
+
+ENVIRONMENT=${1:-""}
+
+
+main() {
+    declare -A VERSIONS=(
+        [sbx]=dev
+        [dev]=dev
+        [tst]=1.2.3
+        [itg]=1.2.3
+        [prd]=1.2.3
+    )
+
+    if [[ "$ENVIRONMENT" != "" ]]; then
+        export RELEASE_VERSION="${VERSIONS[$ENVIRONMENT]}"
+    fi
+
+    export RELEASE_TYPE="Python"
+    export RELEASE_NAME="datalabs-snomed-cpt-api"
+    export RELEASE_DESCRIPTION="SNOMED CPT API"
+}
+
+
+main
