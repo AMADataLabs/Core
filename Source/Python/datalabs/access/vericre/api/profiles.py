054e7011be6e7e096ac3b2d89c17e3c4ba78b21a
Gary WU <>
2023-03-17 16:12:01 -0500

    initial configuration for /profiles endpoint without reaching RDS & S3

---
+++ Source/Python/datalabs/access/vericre/api/profiles.py
@@ -0,0 +1,44 @@
+""" Release endpoint classes."""
+from   dataclasses import dataclass
+import logging
+
+import boto3
+from   botocore.exceptions import ClientError
+
+from   datalabs.access.api.task import APIEndpointTask, InternalServerError
+from   datalabs.parameter import add_schema
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+
+# pylint: disable=too-many-instance-attributes
+@add_schema(unknowns=True)
+@dataclass
+class ProfilesEndpointParameters:
+    path: dict
+    query: dict
+    authorization: dict
+    # bucket: str
+    # base_path: str
+    # url_duration: str
+    unknowns: dict=None
+
+
+class ProfilesEndpointTask(APIEndpointTask):
+    PARAMETER_CLASS = ProfilesEndpointParameters
+
+    def __init__(self, parameters: dict, data: "list<bytes>"=None):
+        super().__init__(parameters, data)
+
+        self._s3 = boto3.client('s3')
+
+    def run(self):
+        print("profile app initial===")
+
+        self._response_body = self._generate_response_body("body-profile")
+
+    @classmethod
+    def _generate_response_body(cls, msg):
+        return f'profile app response{msg}'
