a5788f37d8d440276af9aa5dc13d9f2b56129b66
Nikhil <nikhil.trivedi@ama-assn.org>
2023-04-13 11:47:46 -0500

    requested edits

--- Source/Python/datalabs/access/cpt/api/snomed.py
+++ Source/Python/datalabs/access/cpt/api/snomed.py
@@ -1,11 +1,12 @@
 """ Release endpoint classes. """
+from   collections import defaultdict
 from   dataclasses import dataclass
 import logging
 
-from   datalabs.access.api.task import APIEndpointTask, ResourceNotFound, InvalidRequest
-from   datalabs.parameter import add_schema
+from   datalabs.access.api.task import APIEndpointTask, InvalidRequest, ResourceNotFound 
 from   datalabs.access.aws import AWSClient
-from collections import defaultdict
+from   datalabs.parameter import add_schema
+
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -93,48 +94,39 @@ class MapSearchEndpointTask(APIEndpointTask):
     PARAMETER_CLASS = MapSearchEndpointParameters
 
     def run(self):
-        maxResults = int(self._parameters.query.get("results")[0]) if self._parameters.query.get("results") else 50
-        index = int(self._parameters.query.get("index")[0]) if self._parameters.query.get("index") else 0
-        keywords = self._parameters.query.get("keyword")
+        max_results, index, keywords = self._get_query_parameters(self._parameters.query)
 
-        if maxResults < 1:
+        with AWSClient("dynamodb") as db:
+            maps = self._get_maps(keywords, db)
+
+        filtered_maps = self._filter_maps(maps, self._parameters.query.get("category"))
+
+        self._response_body = self._generate_response(filtered_maps, index, max_results)
+
+    def _get_query_parameters(self, parameters):
+
+        max_results = int(parameters.get("results")[0]) if parameters.get("results") else 50
+        index = int(parameters.get("index")[0]) if parameters.get("index") else 0
+        keywords = parameters.get("keyword")
+
+        if max_results < 1:
             raise InvalidRequest("Results must be greater 0.")
         if index < 0:
             raise InvalidRequest("Index must be 0 or greater.")
 
-        with AWSClient("dynamodb") as db:
-            maps = self._getMappings(keywords, db)
+        return max_results, index, keywords
 
-        filtered_maps = self._filter_maps(maps, self._parameters.query.get("category"))
-
-        self._response_body = self._generate_response(filtered_maps, index, maxResults)
-
-    def _generate_response(self, filtered_maps, index, maxResults):
-        items = {}
-
-        items["maps"] = filtered_maps[index:index + maxResults]
-        items["available"] = len(filtered_maps)
-
-        if len(filtered_maps) >= maxResults - index and index + maxResults < len(filtered_maps):
-            items["next"] = "/snomed/maps/cpt?index=" + str(index + maxResults) + "&results=" + str(maxResults)
-
-        return items
-
-    def _getMappings(self, keywords, db):
-        maps = []
+    def _get_maps(self, keywords, db):
+        mapping_set = {}
 
         if keywords:
             for keyword in keywords:
-                mapping_references = db.execute_statement(
-                    Statement=f"SELECT * FROM \"CPT-API-snomed-sbx\".\"SearchIndex\" WHERE sk = 'KEYWORD:{keyword}'"       
-                ) 
-
-                mapping_set = self._get_mappings_from_references(mapping_references["Items"], db)
-                maps.extend([self._generate_map(mappings) for mappings in mapping_set.values()])
+                mapping_references = self._get_mapping_references_by_keyword(db, keyword)
+                mapping_set.update(self._get_mappings_from_references(mapping_references["Items"], db))
         else:
             mapping_set = self._get_all_mappings(db)
-            maps = [self._generate_map(mappings) for mappings in mapping_set.values()]         
-
+            
+        maps = [self._generate_map(mappings) for mappings in mapping_set.values()]  
         maps.sort(key=lambda x: x["concept"])
 
         return maps
@@ -142,13 +134,39 @@ class MapSearchEndpointTask(APIEndpointTask):
     def _filter_maps(self, maps, categories):
 
         if categories:
-            for currMap in maps:
-                mappings = currMap["mappings"]
-                filtered_mappings = [m for m in mappings if m["category"] in categories]
-                currMap["mappings"] = filtered_mappings
+            for map in maps:
+                map["mappings"] = [m for m in map["mappings"] if m["category"] in categories]
 
         return maps
-   
+
+    def _generate_response(self, filtered_maps, index, max_results):
+        items = dict(
+            maps=filtered_maps[index:index + max_results],
+            available=len(filtered_maps)
+        )
+
+        if len(filtered_maps) >= max_results - index and index + max_results < len(filtered_maps):
+            items["next"] = "/snomed/maps/cpt?index=" + str(index + max_results) + "&results=" + str(max_results)
+
+        return items
+
+    def _get_mapping_references_by_keyword(self, db, keyword):
+        mapping_references = db.execute_statement(
+            Statement=f"SELECT * FROM \"CPT-API-snomed-sbx\".\"SearchIndex\" WHERE sk = 'KEYWORD:{keyword}'"       
+        ) 
+
+        return mapping_references
+
+    def _get_mappings_from_references(self, keyword_items, db):
+        mappings = defaultdict(list)
+
+        for search_item in keyword_items:
+            mapping = self._get_mapping_from_reference(search_item['pk']['S'], db)
+
+            mappings[mapping["pk"]["S"]].append(mapping)
+
+        return mappings
+
     def _get_all_mappings(self, db):
         mappings = defaultdict(list)
 
@@ -191,16 +209,6 @@ class MapSearchEndpointTask(APIEndpointTask):
         )
 
         return results["Items"][0]
-
-    def _get_mappings_from_references(self, keyword_items, db):
-        mappings = defaultdict(list)
-
-        for search_item in keyword_items:
-            mapping = self._get_mapping_from_reference(search_item['pk']['S'], db)
-
-            mappings[mapping["pk"]["S"]].append(mapping)
-
-        return mappings
 
     def _paginate(self, db, statement):  
         results = db.execute_statement(Statement=statement)
