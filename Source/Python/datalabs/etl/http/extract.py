f6ba8f9ebbbe03d8a6a6daa00edfb0f74dc6738e
chris <chwillia@ama-assn.org>
2023-04-27 17:41:55 -0500

    Create basic auth header if username and password and both included

--- Source/Python/datalabs/etl/http/extract.py
+++ Source/Python/datalabs/etl/http/extract.py
@@ -13,6 +13,8 @@ from   datalabs.parameter import add_schema
 class HTTPFileExtractorParameters:
     urls: str
     execution_time: str = None
+    username: str = None
+    password: str = None
 
 
 class HTTPFileExtractorTask(IncludeNamesMixin, FileExtractorTask):
@@ -22,7 +24,10 @@ class HTTPFileExtractorTask(IncludeNamesMixin, FileExtractorTask):
         return self._parameters.urls.split(',')
 
     def _get_client(self):
-        return requests.Session()
+        client = requests.Session()
+        if self._parameters.username and self._parameters.password:
+            client.auth = (self._parameters.username, self._parameters.password)
+        return client
 
     def _resolve_wildcard(self, file):
         if '*' in file:
@@ -42,6 +47,8 @@ class HTTPFileExtractorTask(IncludeNamesMixin, FileExtractorTask):
 # pylint: disable=too-many-instance-attributes
 class HTTPFileListExtractorParameters:
     execution_time: str = None
+    username: str = None
+    password: str = None
 
 
 class HTTPFileListExtractorTask(HTTPFileExtractorTask):
@@ -50,7 +57,14 @@ class HTTPFileListExtractorTask(HTTPFileExtractorTask):
     def _get_files(self):
         return list(itertools.chain.from_iterable(self._parse_url_lists(self._data)))
 
+    def _get_client(self):
+        client = super()._get_client()
+        if self._parameters.username and self._parameters.password:
+            client.auth = (self._parameters.username, self._parameters.password)
+        return client
+
     @classmethod
     def _parse_url_lists(cls, data):
+        print("chris the data is x", data)
         for url_list in data:
             yield [url.decode().strip() for url in url_list.split(b'\n')]
