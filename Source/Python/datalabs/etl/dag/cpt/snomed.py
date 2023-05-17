386155f45b3c1fa0529efc41afd115d26bd00915
rnajeeb <rudainah.najeeb@ama-assn.org>
2023-05-17 11:28:55 -0500

    Updated etl config

--- Source/Python/datalabs/etl/dag/cpt/snomed.py
+++ Source/Python/datalabs/etl/dag/cpt/snomed.py
@@ -6,8 +6,8 @@ from   datalabs.etl.dag import dag
 class DAG(dag.DAG):
     EXTRACT_SNOMED_FILE: "datalabs.etl.s3.extract.S3FileExtractorTask"
     CREATE_SNOMED_MAPPINGS: "datalabs.etl.cpt.snomed.transform.SNOMEDMappingTransformerTask"
-    # LOAD_SNOMED_MAPPINGS: "datalabs.etl.dynamodb.load.DynamoDBLoaderTask"
+    LOAD_SNOMED_MAPPINGS: "datalabs.etl.cpt.snomed.load.DynamoDBLoaderTask"
 
 
 # pylint: disable=pointless-statement
-DAG.EXTRACT_SNOMED_FILE >> DAG.CREATE_SNOMED_MAPPINGS # >> DAG.LOAD_SNOMED_MAPPINGS
+DAG.EXTRACT_SNOMED_FILE >> DAG.CREATE_SNOMED_MAPPINGS  >> DAG.LOAD_SNOMED_MAPPINGS
