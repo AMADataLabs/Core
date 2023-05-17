5b0e3aea34f7c84486cf758a395e508111258871
rnajeeb <rudainah.najeeb@ama-assn.org>
2023-05-17 10:45:24 -0500

    Updated extractor

--- Source/Python/datalabs/etl/dag/cpt/snomed.py
+++ Source/Python/datalabs/etl/dag/cpt/snomed.py
@@ -4,7 +4,7 @@ from   datalabs.etl.dag import dag
 
 @dag.register(name="SNOMED_CPT")
 class DAG(dag.DAG):
-    EXTRACT_SNOMED_FILE: "datalabs.etl.sftp.extract.SFTPFileExtractorTask"
+    EXTRACT_SNOMED_FILE: "datalabs.etl.s3.extract.S3FileExtractorTask"
     CREATE_SNOMED_MAPPINGS: "datalabs.etl.cpt.snomed.transform.SNOMEDMappingTransformerTask"
     # LOAD_SNOMED_MAPPINGS: "datalabs.etl.dynamodb.load.DynamoDBLoaderTask"
 
