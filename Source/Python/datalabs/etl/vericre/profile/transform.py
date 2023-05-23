859c64119a26d0856aa3d2b3fd072d1ce2d50b7f
chris <chwillia@ama-assn.org>
2023-05-23 03:53:33 -0500

    updates to pr comments

--- Source/Python/datalabs/etl/vericre/profile/transform.py
+++ Source/Python/datalabs/etl/vericre/profile/transform.py
@@ -1,46 +1,52 @@
-from datalabs.task import Task
-import urllib3
 import json
-import os
 from typing import List
+from dataclasses import dataclass
+from datalabs.parameter import add_schema
+from datalabs.task import Task
 
-class CAQHStatusURLListTranformerTask(Task):
+@add_schema
+@dataclass
+class CAQHStatusURLListTransformerParameters:
+    host: str = None
+    organization: str = None
 
-    USERNAME = os.environ.get('USERNAME')
-    PASSWORD = os.environ.get('PASSWORD')
+class CAQHStatusURLListTransformerTask(Task):
+    """
+    Task to transform CAQH status URL list.
+    """
+    PARAMETER_CLASS = CAQHStatusURLListTransformerParameters
 
-    def get_caqh_profile_status_url(self):
-
-        ama_masterfile = json.loads(self._data[0].decode())
-        
-        host = os.environ.get('HOST')
-        organization_id = os.environ.get('ORGANIZATION')
-        
-        urls = []
-        for item in ama_masterfile['item']:
-            query_params = item['request']['url']['query']
-            
-            npi_provider_ids = []
-            for param in query_params:
-                if param['key'] == 'NPI_Provider_Id':
-                    npi_provider_ids.append(param['value'])
-            
-            for npi_provider_id in npi_provider_ids:
-                url_template = "https://{host}/RosterAPI/api/providerstatusbynpi?Product=PV&Organization_Id={organization_id}&NPI_Provider_Id={npi_provider_id}"
-                urls.append(url_template.format(
-                    host=host,
-                    organization_id=organization_id,
-                    npi_provider_id=npi_provider_id
-                ))
-        
-        return urls
-    
     def run(self) -> List[str]:
-      
-        urls = self.get_caqh_profile_status_url() 
-        
+        urls = self._get_caqh_profile_status_url()
+
         encoded_urls = '\n'.join(urls).encode()
-        
+
         return [encoded_urls]
+
+    def _get_caqh_profile_status_url(self):
+        urls = []
+
+        for dict_bytes_object in self._data:
+            dict_data_list = json.loads(dict_bytes_object.decode())
+
+            if isinstance(dict_data_list, dict):
+                npi_code = dict_data_list.get('npi', {}).get('npiCode')
+
+                host = self._parameters.host
+                organization_id = self._parameters.organization
+                url_template = f"https://{host}/RosterAPI/api/providerstatusbynpi?Product=PV&Organization_Id={organization_id}&NPI_Provider_Id={npi_code}"
+                urls.append(url_template)
+
+            if isinstance(dict_data_list, list):
+                for data in dict_data_list:
+                    if isinstance(data, dict):
+                        npi_code = data.get('npi', {}).get('npiCode')
+
+                        host = self._parameters.host
+                        organization_id = self._parameters.organization
+                        url_template = f"https://{host}/RosterAPI/api/providerstatusbynpi?Product=PV&Organization_Id={organization_id}&NPI_Provider_Id={npi_code}"
+                        urls.append(url_template)
+
+        return urls
 
 
--- Test/Python/test/datalabs/etl/test_caqh_status_url_list.py
+++ Test/Python/test/datalabs/etl/test_caqh_status_url_list.py
@@ -1,29 +1,26 @@
-import pytest
-from datalabs.etl.vericre.profile.transform import CAQHStatusURLListTranformerTask
-from datalabs.task import Task
+import json
+from dataclasses import asdict
+from datalabs.etl.vericre.profile.transform import CAQHStatusURLListTransformerTask
+from datalabs.etl.vericre.profile.transform import CAQHStatusURLListTransformerParameters
 
-from unittest.mock import patch
 
-class TestCAQHStatusURLListTransformerTask:
-    @pytest.fixture
-    def task(self):
-        task = CAQHStatusURLListTranformerTask(Task)
-        task._data = [
-            b'{"item": [{"request": {"url": {"query": [{"key": "organizationId", "value": "6166"}, {"key": "caqhProviderId", "value": "16038675"}, {"key": "attestationDate", "value": "08/14/2022"}]},"response": []}},{"name": "Get Status By NPI","request": {"method": "GET","header": [],"url": {"query": [{"key": "Product","value": "PV"},{"key": "Organization_Id","value": "6166"},{"key": "NPI_Provider_Id","value": "1356425755"}]}},"response": []}]}'
-        ]
-        return task
-    
-    @patch('os.environ.get')
-    def test_get_caqh_profile_status_url(self, mock_environ_get, task):
-        mock_environ_get.side_effect = lambda x: {
-            'HOST': 'proview-demo.nonprod.caqh.org',
-            'ORGANIZATION': '6166'
-        }.get(x)
-        
-        expected_urls = [
-            "https://proview-demo.nonprod.caqh.org/RosterAPI/api/providerstatusbynpi?Product=PV&Organization_Id=6166&NPI_Provider_Id=1356425755"
-        ]
-        
-        result = task.get_caqh_profile_status_url()
-        
-        assert result == expected_urls
+def test_caqh_status_url_list_transformer_task():
+    with open('ama_masterfile.json', 'r') as file:
+        input_data = json.load(file)
+
+    encoded_data = [json.dumps(data).encode() for data in input_data]
+
+    parameters = CAQHStatusURLListTransformerParameters(host='example.org', organization='123')
+    parameters_dict = asdict(parameters)
+    task = CAQHStatusURLListTransformerTask(parameters_dict)
+    task._data = encoded_data
+
+    result = task.run()
+
+    expected_urls = [
+        b'https://example.org/RosterAPI/api/providerstatusbynpi?Product=PV&Organization_Id=123&NPI_Provider_Id=123456009\n'
+        b'https://example.org/RosterAPI/api/providerstatusbynpi?Product=PV&Organization_Id=123&NPI_Provider_Id=123456008\n'
+        b'https://example.org/RosterAPI/api/providerstatusbynpi?Product=PV&Organization_Id=123&NPI_Provider_Id=123456007'
+    ]
+
+    assert result == expected_urls
