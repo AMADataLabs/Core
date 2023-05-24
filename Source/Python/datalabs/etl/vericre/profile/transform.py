36caced49d78fcff0e9be87a94fcc4a9b355cdcc
viviansun0213 <Ronghua.Sun@ama-assn.org>
2023-05-24 13:04:10 -0500

    fix lint

--- Source/Python/datalabs/etl/vericre/profile/transform.py
+++ Source/Python/datalabs/etl/vericre/profile/transform.py
@@ -1,14 +1,13 @@
-import uuid
+"Tranformer Task for AMAMetadata"
 from datalabs.etl.csv import CSVReaderMixin, CSVWriterMixin
 from datalabs.etl.vericre.profile.column import AMA_PROFILE_COLUMNS
 from datalabs.task import Task
-from typing import List, Dict, Any
 
 
 class AMAMetadataTranformerTask(CSVReaderMixin, CSVWriterMixin, Task):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    
+
     def run(self):
         ama_profiles = self._csv_to_dataframe(self._data[0], encoding="latin")
 
