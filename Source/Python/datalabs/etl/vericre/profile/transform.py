096a89b47d21c079fdd6ffe09f0761399683c2a3
chris <chwillia@ama-assn.org>
2023-06-04 21:08:22 -0500

    implementation and tests are working

--- Source/Python/datalabs/etl/vericre/profile/transform.py
+++ Source/Python/datalabs/etl/vericre/profile/transform.py
@@ -7,15 +7,12 @@ from   typing import List
 
 from   datalabs.parameter import add_schema
 from   datalabs.task import Task
-import pdb
-
 
 @add_schema
 @dataclass
 class CAQHStatusURLListTransformerParameters:
     host: str
     organization: str
-
 
 class CAQHStatusURLListTransformerTask(Task):
     PARAMETER_CLASS = CAQHStatusURLListTransformerParameters
@@ -51,8 +48,6 @@ class CAQHProfileURLListTranformerTask(Task):
 
         packed_data = self._data
 
-        pdb.set_trace()
-
         urls = self._generate_urls(packed_data, host, organization)
 
         return urls
@@ -61,7 +56,6 @@ class CAQHProfileURLListTranformerTask(Task):
     def _parse_pickle_data(self, data):
         decoded_data = [pickle.loads(pickled_dataset) for pickled_dataset in data]
 
-        # pdb.set_trace()
         decoded_data = decoded_data[0][0][1].decode()
 
         parsed_data = json.loads(decoded_data)
@@ -73,39 +67,16 @@ class CAQHProfileURLListTranformerTask(Task):
         ]
 
         return active_provider_ids
-
-    # @classmethod
-    # def _generate_urls(cls, statuses, host, organization):
-    #     pdb.set_trace()
-    #     return [cls._generate_url(status, host, organization) for status in statuses]
-
+    
+    # pylint: disable=trailing-whitespace
     @classmethod
-    # def _generate_urls(cls, statuses, host, organization):
-    #     parsed_statuses = pickle.loads(statuses[0])
-    #     active_statuses = []
-
-    #     for status in parsed_statuses:
-    #         status_dict = status[1]  # Extract the dictionary from the tuple,
-    #         decoded_data = pickle.loads(status_dict)
-
-    #         roster_status = decoded_data[0]["roster_status"]
-    #         pdb.set_trace()
-
-    #         if roster_status == "ACTIVE":
-    #             active_statuses.append(status_dict)
-    #         else:
-    #             print(f"Ignoring status with roster status: {roster_status}")
-    #     # pdb.set_trace()
-    #     value = [cls._generate_url(status, host, organization) for status in active_statuses]
-    #     # pdb.set_trace()
-
-    #     return value
     def _generate_urls(cls, statuses, host, organization):
         parsed_statuses = pickle.loads(statuses[0])
+
         active_statuses = []
 
         for status in parsed_statuses:
-            status_dict = status[1]  # Extract the dictionary from the tuple,
+            status_dict = status[1]  
             decoded_data = pickle.loads(status_dict)
             
             for data in decoded_data:
@@ -114,25 +85,23 @@ class CAQHProfileURLListTranformerTask(Task):
                 caqh_provider_id =  data["caqh_provider_id"]
                 provider_status_date = data["provider_status_date"]
 
-                # pdb.set_trace()
                 if roster_status == "ACTIVE":
                     active_statuses.append(caqh_provider_id)
                 else:
                     print(f"Ignoring status with roster status: {roster_status}")
-        # pdb.set_trace()
-        value = [cls._generate_url(caqh_provider_id, provider_status_date, host, organization) for caqh_provider_id in active_statuses]
-        # pdb.set_trace()
+
+        value = [cls._generate_url(caqh_provider_id, provider_status_date, host, organization) 
+                 for caqh_provider_id in active_statuses]
 
         return value
 
     @classmethod
     def _generate_url(cls, caqh_provider_id, provider_status_date, host, organization):
-        # pdb.set_trace()
         base_url = "https://" + host + "/credentialingapi/api/v8/entities"
+
         organization_parameter = "organizationId=" + str(organization)
         provider_parameters = "caqhProviderId=" + caqh_provider_id
         attestation_date = datetime.strptime(provider_status_date, '%Y%m%d').strftime("%m/%d/%Y")
         attestation_parameter = "attestationDate=" + attestation_date
-        # pdb.set_trace()
 
         return f"{base_url}?{organization_parameter}&{provider_parameters}&{attestation_parameter}".encode()
--- Test/Python/test/datalabs/etl/vericre/profile/test_transform.py
+++ Test/Python/test/datalabs/etl/vericre/profile/test_transform.py
@@ -1,10 +1,8 @@
 """ source: datalabs.etl.vericre.profile.transform """
 import json
-
 import pytest
 
-from   datalabs.etl.vericre.profile.transform import CAQHStatusURLListTransformerTask, CAQHProfileURLListTranformerTask
-import pdb
+from datalabs.etl.vericre.profile.transform import CAQHStatusURLListTransformerTask, CAQHProfileURLListTranformerTask
 
 # pylint: disable=redefined-outer-name, protected-access
 def test_caqh_status_url_list_transformer_task(fixture_input_data):
@@ -26,9 +24,8 @@ def test_caqh_status_url_list_transformer_task(fixture_input_data):
         dict(host=host, organization=organization_id), data=fixture_input_data)
 
     result = task.run()
-    pdb.set_trace()
-    assert result == expected_urls
 
+    assert result == expected_urls
 
 # pylint: disable=redefined-outer-name
 def test_caqh_url_list_transformer_task(fixture_input_data_status):
@@ -36,8 +33,8 @@ def test_caqh_url_list_transformer_task(fixture_input_data_status):
 
     organization_id = '6167'
     url_1 = (
-        f"https://example.org/credentialingapi/api/v8/entities?"
-        f"organizationId=6167&caqhProviderId=16038676&attestationDate=08/14/2022"
+        "https://example.org/credentialingapi/api/v8/entities?"
+        "organizationId=6167&caqhProviderId=16038676&attestationDate=08/14/2022"
     )
 
     expected_urls = [url_1.encode()]
@@ -72,7 +69,9 @@ def fixture_input_data():
 
     return [json.dumps(npi_profiles_list).encode()]
 
-# pylint: disable=function-redefined, missing-final-newline
+# pylint: disable=function-redefined, missing-final-newline, line-too-long
 @pytest.fixture
 def fixture_input_data_status():
-    return [b'\x80\x04\x95M\x01\x00\x00\x00\x00\x00\x00]\x94\x8c\x1b./caqh_profile_statuses.pkl\x94B#\x01\x00\x00\x80\x04\x95\x18\x01\x00\x00\x00\x00\x00\x00]\x94(}\x94(\x8c\x0forganization_id\x94\x8c\x046166\x94\x8c\x10caqh_provider_id\x94\x8c\x0816038675\x94\x8c\rroster_status\x94\x8c\rNOT ON ROSTER\x94\x8c\x0fprovider_status\x94\x8c\x13Expired Attestation\x94\x8c\x14provider_status_date\x94\x8c\x0820220814\x94\x8c\x17provider_practice_state\x94\x8c\x02IL\x94\x8c\x13provider_found_flag\x94\x8c\x01Y\x94u}\x94(h\x02\x8c\x046167\x94h\x04\x8c\x0816038676\x94h\x06\x8c\x06ACTIVE\x94h\x08h\th\nh\x0bh\x0ch\rh\x0eh\x0fue.\x94\x86\x94a.']
+    return [
+        b'\x80\x04\x95M\x01\x00\x00\x00\x00\x00\x00]\x94\x8c\x1b./caqh_profile_statuses.pkl\x94B#\x01\x00\x00\x80\x04\x95\x18\x01\x00\x00\x00\x00\x00\x00]\x94(}\x94(\x8c\x0forganization_id\x94\x8c\x046166\x94\x8c\x10caqh_provider_id\x94\x8c\x0816038675\x94\x8c\rroster_status\x94\x8c\rNOT ON ROSTER\x94\x8c\x0fprovider_status\x94\x8c\x13Expired Attestation\x94\x8c\x14provider_status_date\x94\x8c\x0820220814\x94\x8c\x17provider_practice_state\x94\x8c\x02IL\x94\x8c\x13provider_found_flag\x94\x8c\x01Y\x94u}\x94(h\x02\x8c\x046167\x94h\x04\x8c\x0816038676\x94h\x06\x8c\x06ACTIVE\x94h\x08h\th\nh\x0bh\x0ch\rh\x0eh\x0fue.\x94\x86\x94a.'
+    ]
