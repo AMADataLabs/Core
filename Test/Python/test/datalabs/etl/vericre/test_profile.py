67ca5d458182901850d1fd633246dc43299d9219
chris <chwillia@ama-assn.org>
2023-06-14 13:25:57 -0500

    updated test

--- Test/Python/test/datalabs/etl/vericre/test_profile.py
+++ Test/Python/test/datalabs/etl/vericre/test_profile.py
@@ -1,7 +1,17 @@
 ''' Source: datalabs.etl.dag.vericre.profile '''
+import os
+import pytest
 
 # pylint: disable=import-outside-toplevel, unused-import
 def test_cpt_api_dag_imports():
     import datalabs.etl.dag.vericre.profile
 
     assert True
+
+# pylint: disable=no-name-in-module
+def test_cpt_api_dag_imports_with_exception():
+    os.environ["DL_3436"] = "True"
+
+    with pytest.raises(ImportError):
+        from datalabs.etl.vericre.profile.transform import AMAProfilesTransformerTask
+        from datalabs.etl.vericre.profile.transform import VeriCreProfileSynchronizerTask
