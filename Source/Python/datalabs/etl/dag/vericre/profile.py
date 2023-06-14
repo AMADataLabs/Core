f63deeb1c7dbeb56fa78384f88957a43e94dfbf4
chris <chwillia@ama-assn.org>
2023-06-14 04:20:32 -0500

    test is working

--- Source/Python/datalabs/etl/dag/vericre/profile.py
+++ Source/Python/datalabs/etl/dag/vericre/profile.py
@@ -1,27 +1,23 @@
-''' DAG definition for the Email Report ETL. '''
-from   datalabs.etl.dag import dag
-from   datalabs import feature
-from   datalabs.etl.intelligent_platform.developer.email.load import EmailReportSMTPLoaderTask
-from   datalabs.etl.sql.sqlalchemy.extract import SQLAlchemyExtractorTask
-from   datalabs.etl.vericre.profile.transform import CAQHProfileURLListTranformerTask
-from   datalabs.etl.http.extract import HTTPFileListExtractorTask
-from   datalabs.etl.sftp.extract import SFTPFileExtractorTask
+from datalabs import feature
+from datalabs.etl.dag import dag
+from datalabs.etl.http.extract import HTTPFileListExtractorTask
+from datalabs.etl.qldb.load import QLDBLoaderTask
+from datalabs.etl.vericre.profile.transform import CAQHProfileURLListTranformerTask
+from datalabs.etl.vericre.profile.transform import CAQHStatusURLListTransformerTask
+from datalabs.etl.sftp.extract import SFTPFileExtractorTask
 if feature.enabled("DL_3436"):
-    from    datalabs.etl.vericre.profile.transform import AMAProfilesTransformerTask
-    from    datalabs.etl.vericre.profile.transform import VeriCreProfileSynchronizerTask
+    from datalabs.etl.vericre.profile.transform import AMAProfilesTransformerTask
+    from datalabs.etl.vericre.profile.transform import VeriCreProfileSynchronizerTask
 
 
-from   datalabs.etl.vericre.profile.transform import CAQHProfileURLListTranformerTask
-from   datalabs.etl.vericre.profile.transform import CAQHStatusURLListTransformerTask
-from   datalabs.etl.qldb.load import QLDBLoaderTask
-
-@dag.register(name="DEVELOPER_EMAILS")
-
+@dag.register(name="PROFILES")
 class DAG(dag.DAG):
     EXTRACT_AMA_PHYSICIAN_PROFILES: SFTPFileExtractorTask
-    CREATE_AMA_PROFILE_TABLE: AMAProfilesTransformerTask
+    if feature.enabled("DL_3436"):
+        CREATE_AMA_PROFILE_TABLE: AMAProfilesTransformerTask
     LOAD_AMA_PROFILES_TO_LEDGER: QLDBLoaderTask
-    SYNC_PROFILES_TO_DATABASE: VeriCreProfileSynchronizerTask
+    if feature.enabled("DL_3436"):
+        SYNC_PROFILES_TO_DATABASE: VeriCreProfileSynchronizerTask
 
     CREATE_CAQH_STATUS_URLS: CAQHStatusURLListTransformerTask
     EXTRACT_CAQH_PHYSICIAN_STATUSES: HTTPFileListExtractorTask
@@ -32,15 +28,9 @@ class DAG(dag.DAG):
     LOAD_CAQH_PROFILES_TO_LEDGER: QLDBLoaderTask
 
 
-# pylint: disable=pointless-statement
-DAG.EXTRACT_AMA_PHYSICIAN_PROFILES >> \
-    DAG.CREATE_AMA_PROFILE_TABLE
-DAG.LOAD_AMA_PROFILES_TO_LEDGER >> \
-    DAG.SYNC_PROFILES_TO_DATABASE
-DAG.CREATE_CAQH_STATUS_URLS >> \
-    DAG.EXTRACT_CAQH_PHYSICIAN_STATUSES >> \
-    DAG.CREATE_CAQH_PROFILE_URLS >> \
-    DAG.EXTRACT_CAQH_PHYSICIAN_PROFILES >> \
-    DAG.CREATE_CAQH_PROFILE_TABLE >> \
-    DAG.LOAD_CAQH_PROFILES_TO_LEDGER
+DAG.EXTRACT_AMA_PHYSICIAN_PROFILES >> (
+    DAG.CREATE_AMA_PROFILE_TABLE >> DAG.LOAD_AMA_PROFILES_TO_LEDGER if feature.enabled("DL_3436") else DAG.LOAD_AMA_PROFILES_TO_LEDGER
+) >> (
+    DAG.SYNC_PROFILES_TO_DATABASE >> DAG.CREATE_CAQH_STATUS_URLS if feature.enabled("DL_3436") else DAG.CREATE_CAQH_STATUS_URLS
+) >> DAG.EXTRACT_CAQH_PHYSICIAN_STATUSES >> DAG.CREATE_CAQH_PROFILE_URLS >> DAG.EXTRACT_CAQH_PHYSICIAN_PROFILES >> DAG.CREATE_CAQH_PROFILE_TABLE >> DAG.LOAD_CAQH_PROFILES_TO_LEDGER
 
