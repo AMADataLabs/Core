c3f0b70afda56d86b3775bf8eefa904ff50d98be
Nikhil <nikhil.trivedi@ama-assn.org>
2023-06-20 12:23:43 -0500

    requested changes

--- Source/Python/datalabs/etl/vericre/profile/transform.py
+++ Source/Python/datalabs/etl/vericre/profile/transform.py
@@ -84,24 +84,25 @@ class CAQHProfileTransformerTask(Task):
 
     def run(self):
       profiles = [x[1] for x in pickle.loads(self._data[0])]
-      qldb_profiles = []
       current_date = isoparse(self._parameters.execution_time)
-
-      for profile in profiles:
-          result_dict = self.xml_to_dict(profile)
-          qldb_profle = self.create_qldb_profile(result_dict["Provider"], current_date)
-          qldb_profiles.append(qldb_profle)
+      qldb_profiles = self.create_vericre_profile_from_caqh_profile(profiles, current_date)
 
       return [json.dumps(qldb_profiles).encode()]
 
-    def xml_to_dict(self, xml_string):
-      xml_dict = xmltodict.parse(xml_string)
-      return xml_dict
+    def create_vericre_profile_from_caqh_profile(self, profiles, current_date):
+      qldb_profiles = []
+
+      for profile in profiles:
+          result_dict = xmltodict.parse(profile)
+          qldb_profle = self.create_qldb_profile(result_dict["Provider"], current_date)
+          qldb_profiles.append(qldb_profle)
+
+      return qldb_profiles
 
     def create_qldb_profile(self, profile, current_date):
       transformed_profile = {
         'FutureBoardExamDate': self.create_future_board_exam_date(profile["Specialty"]),
-        'TaxID': self.create_tax_ID(profile["Practice"]["Tax"]),
+        'TaxID': self.create_tax_id(profile["Practice"]["Tax"]),
         'WorkHistory': self.create_work_history(profile["WorkHistory"]),
         'FirstName': profile.get("FirstName"),
         'OtherName': self.create_other_name(profile["OtherName"]),
@@ -116,11 +117,13 @@ class CAQHProfileTransformerTask(Task):
         'Insurance': self.create_insurance(profile["Insurance"], current_date),
         'MedicaidProviderFlag': profile.get("MedicaidProviderFlag"),
         'ProviderAddress': self.create_provider_address(profile["ProviderAddress"]),
-        'ProviderCDS': self.create_provider_CDS(profile["ProviderCDS"]),
+        'ProviderCDS': self.create_provider_cds(profile["ProviderCDS"]),
         'LastName': profile.get("LastName"),
         'ProviderType': profile.get("ProviderType"),
         'ECFMGIssueDate': profile.get("ECFMGIssueDate")
       }
+
+      return transformed_profile
 
     def create_future_board_exam_date(self, specialties):
       future_board_exam_date = ''
@@ -132,18 +135,19 @@ class CAQHProfileTransformerTask(Task):
 
       return future_board_exam_date
 
-    def create_tax_ID(self, taxes):
-        tax_ID = ""
+    def create_tax_id(self, taxes):
+        tax_id = ""
 
         for tax in taxes:
           if tax.get("TaxType", {}).get("TaxTypeDescription") == "Individual":
-            tax_ID = tax.get("TaxID", "")
+            tax_id = tax.get("TaxID", "")
             break
 
-        return tax_ID
+        return tax_id
 
     def create_work_history(self, work_histories):
-      [work_history.pop("@ID") for work_history in work_histories]
+      for work_history in work_histories:
+        work_history.pop("@ID")
 
       return sorted(work_histories, key=lambda x: x["StartDate"])
 
@@ -153,7 +157,9 @@ class CAQHProfileTransformerTask(Task):
       return other_name
 
     def create_previous_insurance(self, insurances, current_date):
-      [insurance.pop("@ID") for insurance in insurances]
+      for insurance in insurances:
+        insurance.pop("@ID")
+
       previous_insurances = [insurance for insurance in insurances if datetime.fromisoformat(insurance["EndDate"]) <= current_date]
 
       return sorted(previous_insurances, key=lambda x: x["StartDate"])
@@ -167,7 +173,9 @@ class CAQHProfileTransformerTask(Task):
       return provider_medicare
 
     def create_time_gap(self, time_gaps):
-      [time_gap.pop("@ID") for time_gap in time_gaps]
+      for time_gap in time_gaps:
+        time_gap.pop("@ID")
+
       time_gaps = [time_gap for time_gap in time_gaps if time_gap["GapExplanation"] != "Academic/Training leave"]
 
       return sorted(time_gaps, key=lambda x: x["StartDate"])
@@ -182,12 +190,12 @@ class CAQHProfileTransformerTask(Task):
 
       return provider_address
 
-    def create_provider_CDS(self, all_provider_CDS):
-      if isinstance(all_provider_CDS, dict):
-        all_provider_CDS.pop("@ID")
-      elif isinstance(all_provider_CDS, list):
-        for provider_CDS in all_provider_CDS:
-          provider_CDS.pop("@ID")
-        all_provider_CDS = sorted(all_provider_CDS, key=lambda x: x["IssueDate"])
+    def create_provider_cds(self, all_provider_cds):
+      if isinstance(all_provider_cds, dict):
+        all_provider_cds = [all_provider_cds]
 
-      return all_provider_CDS
+      for provider_cds in all_provider_cds:
+        provider_cds.pop("@ID")
+      modified_all_provider_cds = sorted(all_provider_cds, key=lambda x: x["IssueDate"])
+
+      return modified_all_provider_cds
