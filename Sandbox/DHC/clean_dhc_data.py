c3eff3f36375c6212d03fee073851a210bd83a57
Victoria Grose  (she/her/hers) <vigrose@ama-assn.org>
2023-07-03 09:39:49 -0500

    dhc and datagov load file updates

--- Sandbox/DHC/clean_dhc_data.py
+++ Sandbox/DHC/clean_dhc_data.py
@@ -6,7 +6,7 @@ import os
 import pandas as pd
 import settings
 from pathlib import Path
-from datalabs.access.edw import EDW
+import pyodbc
 
 #Set today
 TODAY = str(date.today())[0:7]
@@ -14,64 +14,46 @@ TODAY = str(date.today())[0:7]
 #Make dataframes
 print('Reading Files...')
 
-DOWNLOADS = os.getenv('DOWNLOADS') 
+DOWNLOADS = os.getenv('DOWNLOAD_FOLDER') 
 file_list = []
 ALL_DHC = pd.DataFrame()
 for file in os.listdir(DOWNLOADS):
-    if file.startswith('ContactInfo0422'):
+    if file.startswith('DQ-'):
         file_name = f'{DOWNLOADS}/{file}'
         print(file_name)
         new_file = pd.read_csv(file_name, error_bad_lines=False)
         ALL_DHC = pd.concat([ALL_DHC, new_file])
+print(ALL_DHC.columns)
 
 
 #Set file locations
 DHC_OUT_DIR = os.getenv('DHC_OUT_FOLDER')
-LOCAL_DHC_OUT_DIR = os.getenv('LOCAL_DHC_OUT_FOLDER')
+# LOCAL_DHC_OUT_DIR = os.getenv('LOCAL_DHC_OUT_FOLDER')
 
-print('Reading EDW...')
-#Write queries
-ME_QUERY = \
-    """
-    SELECT DISTINCT
-    P.PARTY_ID,
-    P.KEY_VAL AS ME
-    FROM
-    AMAEDW.PARTY_KEY P
-    WHERE
-    P.KEY_TYPE_ID = 18
-    AND
-    P.ACTIVE_IND = 'Y'
-    """
+def get_npi_to_me():
+    password = os.environ.get('CREDENTIALS_EDW_PASSWORD')
+    username = os.environ.get('CREDENTIALS_EDW_USERNAME')
+    query = os.environ.get('QUERY')
+    w = "DSN=PRDDW; UID={}; PWD={}".format(username, password)
+    AMAEDW = pyodbc.connect(w)
+    npi_to_me = pd.read_sql(con=AMAEDW, sql=query)
+    return npi_to_me
 
-NPI_QUERY = \
-    """
-    SELECT DISTINCT
-    P.PARTY_ID,
-    P.KEY_VAL AS NPI
-    FROM
-    AMAEDW.PARTY_KEY P
-    WHERE
-    P.KEY_TYPE_ID = 38
-    AND
-    P.ACTIVE_IND = 'Y'
-    """
-#Execute queries
-with EDW() as edw:
-    NPI = edw.read(NPI_QUERY)
-    ME = edw.read(ME_QUERY)
-
-#Make id conversion table
-NPI_TO_ME = pd.merge(NPI, ME, on='PARTY_ID')[['NPI', 'ME']]
+def append_me(new_download):
+    print('Appending ME numbers')
+    me_npi = get_npi_to_me()
+    new_download['NPI_NBR'] = [str(int(x)) for x in new_download.NPI]
+    with_mes = pd.merge(new_download, me_npi, on='NPI_NBR', how='left')
+    with_mes.drop(columns = ['NPI_NBR'])
+    return with_mes
 
 print('Cleaning...')
-# print(len(ALL_DHC))
-# ALL_DHC = ALL_DHC.drop_duplicates()
-# print(len(ALL_DHC))
+print(len(ALL_DHC))
+ALL_DHC = ALL_DHC.drop_duplicates()
+print(len(ALL_DHC))
 
 #Add ME
-ALL_DHC['NPI'] = ALL_DHC['NPI'].astype(str)
-DHC = pd.merge(NPI_TO_ME, ALL_DHC, on='NPI', how='right')
+ALL_DHC = append_me(ALL_DHC)
 
 #Fix phones
 ALL_DHC.columns = [c.replace(' ','_') for c in ALL_DHC.columns.values]
@@ -80,6 +62,6 @@ ALL_DHC['Phone_Number'] = ALL_DHC['Phone_Number'].apply(lambda x: x.replace('.',
 
 #Save
 print('Saving...')
-DHC.to_csv(f'{LOCAL_DHC_OUT_DIR}/DHC_{TODAY}.csv', index=False)
-DHC.to_csv(f'{DHC_OUT_DIR}/DHC_{TODAY}.csv', index=False)
+# ALL_DHC.to_csv(f'{LOCAL_DHC_OUT_DIR}/DHC_{TODAY}.csv', index=False)
+ALL_DHC.to_csv(f'{DHC_OUT_DIR}/DHC_{TODAY}.csv', index=False)
 
