23f63ce0294c14bb4beb946e76a3ca75e8437a13
rnajeeb <rudainah.najeeb@ama-assn.org>
2023-07-10 14:37:51 -0500

    Splitter task stalling

--- Source/Python/datalabs/etl/dag/cpt/snomed.py
+++ Source/Python/datalabs/etl/dag/cpt/snomed.py
@@ -7,7 +7,7 @@ class DAG(dag.DAG):
     EXTRACT_SNOMED_FILE: "datalabs.etl.s3.extract.S3FileExtractorTask"
     CREATE_SNOMED_MAPPINGS: "datalabs.etl.cpt.snomed.transform.SNOMEDMappingTransformerTask"
     SPLIT_MAPPINGS: "datalabs.etl.manipulate.transform.SplitTransformerTask"
-    LOAD_SNOMED_MAPPINGS: Repeat("datalabs.etl.cpt.snomed.load.DynamoDBLoaderTask", 3)
+    LOAD_SNOMED_MAPPINGS: Repeat("datalabs.etl.cpt.snomed.load.DynamoDBLoaderTask", 4)
 
 
 # pylint: disable=pointless-statement
