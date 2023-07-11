041c7133156db6e88265b4ab9c95971003d1ba7b
rnajeeb <rudainah.najeeb@ama-assn.org>
2023-07-11 11:17:47 -0500

    Splitter task updated for json

--- Source/Python/datalabs/etl/dag/cpt/snomed.py
+++ Source/Python/datalabs/etl/dag/cpt/snomed.py
@@ -6,7 +6,7 @@ from   datalabs.etl.dag import dag, Repeat
 class DAG(dag.DAG):
     EXTRACT_SNOMED_FILE: "datalabs.etl.s3.extract.S3FileExtractorTask"
     CREATE_SNOMED_MAPPINGS: "datalabs.etl.cpt.snomed.transform.SNOMEDMappingTransformerTask"
-    SPLIT_MAPPINGS: "datalabs.etl.manipulate.transform.SplitTransformerTask"
+    SPLIT_MAPPINGS: "datalabs.etl.manipulate.transform.JSONSplitTransformerTask"
     LOAD_SNOMED_MAPPINGS: Repeat("datalabs.etl.cpt.snomed.load.DynamoDBLoaderTask", 4)
 
 
