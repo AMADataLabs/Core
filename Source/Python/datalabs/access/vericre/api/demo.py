f5b9da9eee22e139472dd51e0fac3c94c10b9d5f
Gary WU <>
2023-07-12 17:31:05 -0500

    add an endpoint for Lambada invoke testing

--- Source/Python/datalabs/access/vericre/api/demo.py
+++ Source/Python/datalabs/access/vericre/api/demo.py
@@ -1,7 +1,6 @@
 """ Release endpoint classes."""
 from   dataclasses import dataclass
 import logging
-import threading
 
 import requests
 
@@ -31,24 +30,12 @@ class DemoEndpointTask(APIEndpointTask):
     def run(self):
         LOGGER.debug('Parameters: %s', self._parameters)
 
-        # thread = threading.Thread(target=self._demo_for_threading)
-        # thread.start()
-
-        requests.get(f'https://{self._parameters.vericre_alb_domain}', verify=False)
+        requests.get(f'https://{self._parameters.vericre_alb_domain}', verify=False, timeout=0.1)
 
         response_result = f"---{self._parameters.vericre_alb_domain}"
         LOGGER.info('do something with response_result')
 
         self._response_body = self._generate_response_body(response_result)
-
-    # def _demo_for_threading(self):
-    #     LOGGER.info('Demo for threading start...')
-        
-    #     api_invoke_result = requests.get(f'https://{self._parameters.vericre_alb_domain}', verify=False)
-    #     LOGGER.info('API invoke result: %s', api_invoke_result.content)
-
-    #     for num in range(100):
-    #         LOGGER.info('Demo for threading counter: %s', num)
 
     @classmethod
     def _generate_response_body(cls, response_result):
