6fcb8eda13418195b99a45c764e8d92922e4c50e
Gary WU <>
2023-07-12 12:52:27 -0500

    add a demo endpoint for threading

--- Source/Python/datalabs/access/vericre/api/demo.py
+++ Source/Python/datalabs/access/vericre/api/demo.py
@@ -2,7 +2,6 @@
 from   dataclasses import dataclass
 import logging
 import threading
-import time
 
 import requests
 
@@ -42,11 +41,8 @@ class DemoEndpointTask(APIEndpointTask):
 
     def _demo_for_threading(self):
         LOGGER.info('Demo for threading start...')
-        for i in range(1000):
-            LOGGER.info('Demo for threading: %s', i)
-            
-        # api_invoke_result = requests.get(f'https://{self._parameters.backend_domain}', verify=False)
-        # LOGGER.info('API invoke result: %s', api_invoke_result.content)
+        api_invoke_result = requests.get(f'https://{self._parameters.backend_domain}', verify=False)
+        LOGGER.info('API invoke result: %s', api_invoke_result.content)
 
     @classmethod
     def _generate_response_body(cls, response_result):
