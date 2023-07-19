82924da3e8ba325ef679042773e3299ddd1a160e
Peter Lane <Peter.Lane@ama-assn.org>
2023-07-19 11:05:03 -0500

    Update CPT API versions.

--- Build/CPT/API/Endpoint/.ci/release.sh
+++ Build/CPT/API/Endpoint/.ci/release.sh
@@ -9,9 +9,9 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.2.3
-        [itg]=1.2.3
-        [prd]=dev
+        [tst]=2.0.0
+        [itg]=2.0.0
+        [prd]=2.0.0
     )
 
     if [[ "$ENVIRONMENT" != "" ]]; then
