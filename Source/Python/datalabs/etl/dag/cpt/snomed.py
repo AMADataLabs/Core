3d1fea3b1b1fa778f7cca898ae93128de4f6be08
rnajeeb <rudainah.najeeb@ama-assn.org>
2023-07-21 11:07:04 -0500

    Update split count

--- Source/Python/datalabs/etl/dag/cpt/snomed.py
+++ Source/Python/datalabs/etl/dag/cpt/snomed.py
@@ -7,9 +7,8 @@ class DAG(dag.DAG):
     EXTRACT_SNOMED_FILE: "datalabs.etl.s3.extract.S3FileExtractorTask"
     CREATE_SNOMED_MAPPINGS: "datalabs.etl.cpt.snomed.transform.SNOMEDMappingTransformerTask"
     SPLIT_MAPPINGS: "datalabs.etl.manipulate.transform.JSONSplitTransformerTask"
-    LOAD_SNOMED_MAPPINGS: Repeat("datalabs.etl.cpt.snomed.load.DynamoDBLoaderTask", 10)
+    LOAD_SNOMED_MAPPINGS: "datalabs.etl.cpt.snomed.load.DynamoDBLoaderTask"
 
 
 # pylint: disable=pointless-statement
-DAG.EXTRACT_SNOMED_FILE >> DAG.CREATE_SNOMED_MAPPINGS >> DAG.SPLIT_MAPPINGS >> DAG.first('LOAD_SNOMED_MAPPINGS')
-DAG.sequence('LOAD_SNOMED_MAPPINGS')
+DAG.EXTRACT_SNOMED_FILE >> DAG.CREATE_SNOMED_MAPPINGS >> DAG.SPLIT_MAPPINGS >> DAG.LOAD_SNOMED_MAPPINGS_0
