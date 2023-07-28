88f964ce774bd74d843f9812a64f65a931c7c1ee
chris <chwillia@ama-assn.org>
2023-07-28 17:39:57 -0500

    updates

---
+++ Build/DEUS/API/.ci/release.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -u
+ENVIRONMENT=${1:-""}
+main() {
+    declare -A VERSIONS=(
+        [sbx]=dev
+        [dev]=dev
+        [tst]=1.0.0
+        [itg]=1.0.0
+        [prd]=1.0.0
+    )
+    if [[ "$ENVIRONMENT" != "" ]]; then
+        export RELEASE_VERSION="${VERSIONS[$ENVIRONMENT]}"
+    fi
+    export RELEASE_TYPE="Python"
+    export RELEASE_NAME="datalabs-deus-api"
+    export RELEASE_DESCRIPTION="DEUS backend API"
+}
+main
