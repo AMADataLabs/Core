64d0683af4decd9b7c5e425c38e9485169e222a7
Peter Lane <Peter.Lane@ama-assn.org>
2023-08-18 23:44:07 -0500

    Add missing module.

---
+++ Source/Python/datalabs/util/profile.py
@@ -0,0 +1,22 @@
+''' Performance profiling utilities. '''
+from   datetime import datetime
+import logging
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.info)
+
+
+def run_time_logger(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now().isoformat()
+        LOGGER.info(f"start: {start} @{func.__name__}")
+
+        result = func(*args, **kwargs)
+
+        end = datetime.now().isoformat()
+        LOGGER.info(f"end: {end} @{func.__name__}")
+
+        return result
+
+    return wrapper
