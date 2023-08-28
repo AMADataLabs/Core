ba64b4b83e2851cdc810286f9229a1155cbc2320
Peter Lane <Peter.Lane@ama-assn.org>
2023-08-27 20:46:12 -0500

    Update DBL target versions.

--- Build/DBL/.ci/release.sh
+++ Build/DBL/.ci/release.sh
@@ -9,8 +9,8 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=2.0.0
-        [prd]=2.0.0
+        [tst]=2.0.1
+        [prd]=2.0.1
     )
 
     if [[ "$ENVIRONMENT" != "" ]]; then
