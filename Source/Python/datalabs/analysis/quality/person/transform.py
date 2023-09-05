ff95302199455f950c2bdb01e0631247e108ac82
Jay Sharma <jay.sharma@ama-assn.org>
2023-09-05 11:27:31 -0500

    Made changes as per the pull request

--- Source/Python/datalabs/analysis/quality/person/transform.py
+++ Source/Python/datalabs/analysis/quality/person/transform.py
@@ -1,11 +1,13 @@
+''' Person transformer class for creating Person entitiy '''
+# pylint: disable=import-error
 import logging
-import pandas
 import json
+import pandas
 
-from  dataclasses import dataclass
+# pylint: disable=wrong-import-order
+from dataclasses import dataclass
 from datalabs.analysis.quality.transform import TransformerTask
 from datalabs.parameter import add_schema
-
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -18,49 +20,45 @@ class PersonTransformerParameters:
     metadata: str = None
 
 
-
 class PersonTransformerTask(TransformerTask):
-
     PARAMETER_CLASS = PersonTransformerParameters
-    
-    
+
+    # pylint: disable=no-self-use
     def _parse_metadata(self, metadata):
-        
         json_metadata = json.loads(metadata)
 
-        return {list(d[1].keys())[0]:{"index": d[0], "seperator": list(d[1].values())[0]} for d in enumerate(json_metadata)}
+        return {list(d[1].keys())[0]: {"index": d[0], "seperator": list(d[1].values())[0]}
+                for d in enumerate(json_metadata)}
 
-  
     def _preprocess(self, dataset):
+        dataset = [self._convert_columns_to_lower_case(
+            data) for data in dataset]
 
-        dataset = [self._to_lowercase(data) for data in dataset]
-        
         return dataset
-        
-    @classmethod        
-    def _to_lowercase(cls, data):
-       data.rename(columns=lambda x: x.lower(), inplace=True)
-       return data
-        
+
+    @classmethod
+    def _convert_columns_to_lower_case(cls, data):
+        data.rename(columns=lambda x: x.lower(), inplace=True)
+
+        return data
+
     def _create_entity(self, dataset):
-    
         race_ethnicity, medical_education_num, person_type, party_ids, entity_ids, person_details, person_name = dataset
-        
         ids = pandas.merge(entity_ids, party_ids, on='party_id')
-        person_data = pandas.merge(ids, medical_education_num, left_on='me', right_on='medical_education_number', how='right')
-        person_data = pandas.merge(person_data, person_type, on='entity_id' ,how='left').drop_duplicates()
-        person_data = pandas.merge(person_data, person_name, on='party_id',how='left').drop_duplicates()
-        person_data = pandas.merge(person_data, race_ethnicity, on='medical_education_number', how='left')
-        person_data = pandas.merge(person_data, person_details, on='party_id',how='left').drop_duplicates()
-        
+        person_data = pandas.merge(
+            ids, medical_education_num, left_on='me', right_on='medical_education_number', how='right')
+        person_data = pandas.merge(
+            person_data, person_type, on='entity_id', how='left').drop_duplicates()
+        person_data = pandas.merge(
+            person_data, person_name, on='party_id', how='left').drop_duplicates()
+        person_data = pandas.merge(
+            person_data, race_ethnicity, on='medical_education_number', how='left')
+        person_data = pandas.merge(
+            person_data, person_details, on='party_id', how='left').drop_duplicates()
+
         return [person_data]
 
-
     def _postprocess(self, dataset):
-
         dataset[0].drop(columns=['me', 'med_edu_nbr'], inplace=True)
-        
+
         return dataset
-
-
-
