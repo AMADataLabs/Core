63b545332dce4b1ee9264b370de3150f5568f4ca
Peter Lane <Peter.Lane@ama-assn.org>
2023-09-08 18:34:20 -0500

    Initial build configurations for Express DAG packages.

---
+++ Build/ExpressDAG/Python/.ci/release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -u
+
+ENVIRONMENT=${1:-""}
+
+
+main() {
+    declare -A VERSIONS=(
+        [sbx]=dev
+        [dev]=dev
+        [tst]=0.1.0.dev0
+        [itg]=0.1.0.dev0
+        [prd]=0.1.0.dev0
+    )
+
+    if [[ "$ENVIRONMENT" != "" ]]; then
+        export RELEASE_VERSION="${VERSIONS[$ENVIRONMENT]}"
+    fi
+
+    export RELEASE_TYPE="Python"
+    export RELEASE_NAME="datalabs-express-dag-python"
+    export RELEASE_DESCRIPTION="Express DAG Python base package"
+}
+
+
+main
