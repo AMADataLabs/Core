3f6f829f650ca155ff069cad5ed8daab26212d79
Jay Sharma <jay.sharma@ama-assn.org>
2023-09-22 15:57:46 -0500

    test commit

--- Source/Python/datalabs/analysis/quality/education.py
+++ Source/Python/datalabs/analysis/quality/education.py
@@ -5,6 +5,7 @@ import logging
 import pandas
 
 # pylint: disable=wrong-import-order
+from numpy import nan
 from datetime import datetime
 from datalabs.task import Task
 from datalabs.etl.csv import CSVReaderMixin, CSVWriterMixin
@@ -30,27 +31,9 @@ class EducationTransformerTask(Task, CSVReaderMixin, CSVWriterMixin):
         return [self._csv_to_dataframe(data, sep=",") for data in dataset]
 
     def _preprocess_data(self, dataset):
-        preprocessed_data = [self._columns_to_lower(data) for data in dataset]
+        dataset = [self._columns_to_lower(data) for data in dataset]
 
-        return preprocessed_data
-
-    @classmethod
-    def _columns_to_lower(self, data):
-        data.rename(columns=lambda x: x.lower(), inplace=True)
-
-        return data
-
-    def _create_entity(self, preprocessed_data):
-        (
-            medical_education_number,
-            ppd_party_ids,
-            school_ids,
-            school_address_ids,
-            school_attendees_details,
-            graduate_education_details,
-            physician_graduation_year,
-            hospital_names,
-        ) = preprocessed_data
+        ppd_party_ids, hospital_names, school_ids, graduate_education_details, *rest = dataset
 
         ppd_party_ids = ppd_party_ids[["party_id", "me"]]
 
@@ -63,6 +46,24 @@ class EducationTransformerTask(Task, CSVReaderMixin, CSVWriterMixin):
 
         graduate_education_details = graduate_education_details.drop_duplicates("party_id")
 
+        return [ppd_party_ids, hospital_names, school_ids, graduate_education_details] + rest
+
+    @classmethod
+    def _columns_to_lower(self, data):
+        return data.rename(columns=lambda x: x.lower())
+
+    def _create_entity(self, preprocessed_data):
+        (
+            ppd_party_ids,
+            hospital_names,
+            school_ids,
+            graduate_education_details,
+            medical_education_number,
+            school_address_ids,
+            school_attendees_details,
+            physician_graduation_year,
+        ) = preprocessed_data
+
         extra_school_ids = school_ids[~school_ids.school_id.isin(school_address_ids.school_id)]
 
         all_school_ids = pandas.concat([school_address_ids, extra_school_ids])
@@ -73,6 +74,7 @@ class EducationTransformerTask(Task, CSVReaderMixin, CSVWriterMixin):
             left_on="me",
             right_on="medical_education_number",
         )
+
         school_address_information = pandas.merge(
             all_school_ids,
             hospital_names,
@@ -80,6 +82,7 @@ class EducationTransformerTask(Task, CSVReaderMixin, CSVWriterMixin):
             right_on="party_hospital_id",
             how="left",
         ).drop_duplicates()
+
         school_attended_information = pandas.merge(
             school_attendees_details,
             school_address_information,
@@ -87,61 +90,60 @@ class EducationTransformerTask(Task, CSVReaderMixin, CSVWriterMixin):
             right_on="party_id_school",
             how="left",
         ).drop_duplicates()
+
         graduate_education_information = pandas.merge(
             graduate_education_details, hospital_names, on="party_hospital_id", how="left"
         ).drop_duplicates()
 
-        school_attended_information = self._reformat_graduate_dates(school_attended_information)
+        school_attended_information, graduate_education_information = self._reformat_dates(
+            school_attended_information, graduate_education_information
+        )
 
         school_attended_information = self._add_county_and_status_codes(school_attended_information)
 
         school_attended_information = self._grab_latest_school_attended(school_attended_information)
 
-        education = pandas.merge(all_ppd_ids, physician_graduation_year, on="party_id", how="left")
-        education = pandas.merge(education, school_attended_information, on="party_id", how="left")
-        education = pandas.merge(
-            education,
-            graduate_education_information,
-            on="party_id",
-            how="left",
-            suffixes=["_SCH", "_GME"],
+        return self._merge_sub_entities(
+            [
+                all_ppd_ids,
+                physician_graduation_year,
+                school_attended_information,
+                graduate_education_information,
+            ]
         )
 
-        return [education]
-
     @classmethod
-    def _reformat_graduate_dates(self, school_attended_information):
-        date_list = []
-        for row in school_attended_information.itertuples():
-            try:
-                new_date = (
-                    datetime.strptime(str(row.GRAD_DT), "%d-%b-%Y").date().strftime("%d-%m-%Y")
-                )
-            except ValueError:
-                new_date = None
-            date_list.append(new_date)
-        school_attended_information["grad_date"] = date_list
-        school_attended_information = school_attended_information.sort_values("grad_date")
+    def _reformat_dates(self, school_attended_information, graduate_education_information):
+        # fmt:off
+        school_attended_information["grad_dt"] = school_attended_information["grad_dt"].replace(nan, None)
+        graduate_education_information["begin_dt"] = graduate_education_information["begin_dt"].replace(nan, None)
+        graduate_education_information["end_dt"] = graduate_education_information["end_dt"].replace(nan, None)
+        # fmt:on
 
-        return school_attended_information
+        school_attended_information["grad_date"] = school_attended_information["grad_dt"].apply(
+            lambda x: datetime.strptime(str(x), "%d-%b-%Y").strftime("%Y-%m-%d") if x else None
+        )
+
+        graduate_education_information["begin_date"] = graduate_education_information[
+            "begin_dt"
+        ].apply(lambda x: datetime.strptime(str(x), "%d-%b-%Y").strftime("%Y-%m-%d") if x else None)
+
+        graduate_education_information["end_date"] = graduate_education_information["end_dt"].apply(
+            lambda x: datetime.strptime(str(x), "%d-%b-%Y").strftime("%Y-%m-%d") if x else None
+        )
+
+        return school_attended_information, graduate_education_information
 
     @classmethod
     def _add_county_and_status_codes(self, school_attended_information):
-        countries = []
-        statuses = []
-        for row in school_attended_information.itertuples():
-            country = 0
-            status = 0
-            if row.country_id == "6705":
-                country = "1"
-            if row.sts_type_id == "9":
-                status = "2"
-            if row.sts_type_id == "54":
-                status = "1"
-            countries.append(country)
-            statuses.append(status)
-        school_attended_information["country"] = countries
-        school_attended_information["status"] = statuses
+        school_attended_information["country"] = school_attended_information["country_id"].apply(
+            lambda x: "1" if str(x) == "6705" else "0"
+        )
+
+        school_attended_information["status"] = school_attended_information["sts_type_id"].apply(
+            lambda x: "2" if str(x) == "9" else "1" if str(x) == "54" else "0"
+        )
+
         school_attended_information = school_attended_information.sort_values(
             ["status", "country", "grad_date"]
         )
@@ -153,9 +155,11 @@ class EducationTransformerTask(Task, CSVReaderMixin, CSVWriterMixin):
         singular_school_attended = school_attended_information.drop_duplicates(
             "party_id", keep=False
         )
+
         multiple_school_attended = school_attended_information[
             school_attended_information.duplicated("party_id", keep=False)
         ]
+
         latest_school_attended = multiple_school_attended.drop_duplicates("party_id", keep="last")
         school_attended_information = pandas.concat(
             [singular_school_attended, latest_school_attended]
@@ -163,11 +167,34 @@ class EducationTransformerTask(Task, CSVReaderMixin, CSVWriterMixin):
 
         return school_attended_information
 
+    @classmethod
+    def _merge_sub_entities(self, sub_entities):
+        (
+            all_ppd_ids,
+            physician_graduation_year,
+            school_attended_information,
+            graduate_education_information,
+        ) = sub_entities
+
+        education = pandas.merge(all_ppd_ids, physician_graduation_year, on="party_id", how="left")
+
+        education = pandas.merge(education, school_attended_information, on="party_id", how="left")
+
+        education = pandas.merge(
+            education,
+            graduate_education_information,
+            on="party_id",
+            how="left",
+            suffixes=["_SCH", "_GME"],
+        )
+
+        return [education]
+
     # pylint: disable=no-self-usegme_singular
     def _postprocess(self, entities):
-        entities[0].drop(columns=["me", "grad_dt"], inplace=True)
-
-        return entities
+        return [
+            entities[0].drop(columns=["me", "grad_dt", "begin_dt", "end_dt"]).replace(nan, None)
+        ]
 
     def _pack(self, postprocessed_data):
         return [
