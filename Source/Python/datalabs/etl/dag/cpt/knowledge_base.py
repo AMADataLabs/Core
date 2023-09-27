d4ef2b9a7581612a0ad0e7568beb2f26f306cd27
Yogesh Vaze <yogesh.vaze@vinzglobal.com>
2023-09-27 18:03:07 +0530

    Corrected DAG Name

--- Source/Python/datalabs/etl/dag/cpt/knowledge_base.py
+++ Source/Python/datalabs/etl/dag/cpt/knowledge_base.py
@@ -2,7 +2,7 @@
 from   datalabs.etl.dag import dag
 
 
-@dag.register(name="CPT_KNOWLEDGE_BASE_ETL")
+@dag.register(name="KNOWLEDGE_BASE_ETL")
 class DAG(dag.DAG):
     EXTRACT_KNOWLEDGE_BASE_FILE: "datalabs.etl.s3.extract.S3FileExtractorTask"
     CREATE_KNOWLEDGE_BASE_DATA: "datalabs.etl.cpt.knowledge_base.transform.DataTransformerTask"
