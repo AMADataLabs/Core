f4434efcd5428ce5fd7ad55a7f18ba49c2da6306
Yogesh Vaze <yogesh.vaze@vinzglobal.com>
2023-09-28 11:13:54 +0530

    Changed the imports

--- Source/Python/datalabs/etl/dag/cpt/knowledge_base.py
+++ Source/Python/datalabs/etl/dag/cpt/knowledge_base.py
@@ -1,15 +1,15 @@
 ''' DAG definition for KnowledgeBase-CPT API ETL '''
 from   datalabs.etl.dag import dag
-from   datalabs.etl.cpt.knowledge_base import transform
-from   datalabs.etl.cpt.knowledge_base import load
+from   datalabs.etl.cpt.knowledge_base.load import OpensearchLoaderTask
+from   datalabs.etl.cpt.knowledge_base.transform import DataTransformerTask
 
 
 
 @dag.register(name="KNOWLEDGE_BASE_ETL")
 class DAG(dag.DAG):
     EXTRACT_KNOWLEDGE_BASE_FILE: "datalabs.etl.s3.extract.S3FileExtractorTask"
-    CREATE_KNOWLEDGE_BASE_DATA: "transform.DataTransformerTask"
-    LOAD_KNOWLEDGE_BASE_DATA: "load.OpensearchLoaderTask"
+    CREATE_KNOWLEDGE_BASE_DATA: "DataTransformerTask"
+    LOAD_KNOWLEDGE_BASE_DATA: "OpensearchLoaderTask"
 
 
 # pylint: disable=pointless-statement
