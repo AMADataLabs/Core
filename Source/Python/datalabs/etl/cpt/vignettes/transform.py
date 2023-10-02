067b6e55cf5512835f311340b792b1d2be228e28
Rudainah <rudainah.najeeb@ama-assn.org>
2023-10-02 15:16:19 -0500

    Refactored

--- Source/Python/datalabs/etl/cpt/vignettes/transform.py
+++ Source/Python/datalabs/etl/cpt/vignettes/transform.py
@@ -17,12 +17,9 @@ LOGGER.setLevel(logging.INFO)
 class VignettesTransformerTask(Task):
     def run(self):
         LOGGER.debug(self._data)
+        vignettes, codes, hcpcs_codes, administrative_codes = self._parse_data(self._data)
 
-        vignettes, codes, hcpcs_codes, administrative_codes = self._parse_data()
-
-        matched_vignettes = self._match_codes_to_concepts(vignettes, codes)
-        matched_vignettes = self._match_hcpcs_codes_to_concepts(matched_vignettes, hcpcs_codes)
-        matched_vignettes = self._match_administrative_code_to_concepts(matched_vignettes, administrative_codes)
+        matched_vignettes = self._match_datasets(vignettes, codes, hcpcs_codes, administrative_codes)
 
         cleaned_vignettes = self._clean_data(matched_vignettes)
 
@@ -30,25 +27,34 @@ class VignettesTransformerTask(Task):
 
         return [json.dumps(vignettes_mappings).encode()]
 
-    def _parse_data(self):
-        parsed_vignettes = pandas.read_csv(BytesIO(self._data[0]), delimiter='|', encoding='latin-1', skiprows=31,
-                                           header=None, names=['cpt_code','long_descriptor','typical_patient',
-                                                               'pre_service_info','intra_service_info',
-                                                               'post_service_info', 'ruc_reviewed_date',
-                                                               'survey_specialty'
-                                                               ]
-                                           )
-        parsed_codes = pandas.read_excel(self._data[1]).rename(
+    @classmethod
+    def _parse_data(cls, data):
+        parsed_vignettes = pandas.read_csv(
+            BytesIO(data[0]), delimiter='|', encoding='latin-1', skiprows=31, header=None, names=[
+                'cpt_code','long_descriptor','typical_patient', 'pre_service_info','intra_service_info',
+                'post_service_info', 'ruc_reviewed_date', 'survey_specialty'
+                ]
+            )
+        parsed_codes = pandas.read_excel(data[1]).rename(
             columns={'Concept Id': 'concept_id', 'CPT Code': 'cpt_code'}
             )
-        parsed_hcpcs_codes = pandas.read_csv(BytesIO(self._data[2]), delimiter='\t').rename(
+        parsed_hcpcs_codes = pandas.read_csv(BytesIO(data[2]), delimiter='\t').rename(
             columns={'Concept Id': 'concept_id','HCPCS II Code': 'cpt_code'}
             )
-        parsed_administrative_codes = pandas.read_excel(self._data[3]).rename(
+        parsed_administrative_codes = pandas.read_excel(data[3]).rename(
             columns={'Concept Id': 'concept_id','Code': 'cpt_code'}
             )
 
         return parsed_vignettes, parsed_codes, parsed_hcpcs_codes, parsed_administrative_codes
+
+    def _match_datasets(self, vignettes, codes, hcpcs_codes, administrative_codes):
+        matched_vignettes = self._match_codes_to_concepts(vignettes, codes)
+
+        matched_vignettes = self._match_hcpcs_codes_to_concepts(matched_vignettes, hcpcs_codes)
+
+        matched_vignettes = self._match_administrative_code_to_concepts(matched_vignettes, administrative_codes)
+
+        return matched_vignettes
 
     def _match_codes_to_concepts(self, data, codes):
         matched_vignettes = pandas.merge(data, codes, on='cpt_code', how='left')
