729ddb7b7b07a8569c1cb56eafa1b935d1a0a06f
Peter Lane <Peter.Lane@ama-assn.org>
2023-10-04 10:48:02 -0500

    Update target versions for VeriCre Profiles API.

--- Build/VeriCre/API/Profile/.ci/release.sh
+++ Build/VeriCre/API/Profile/.ci/release.sh
@@ -9,9 +9,9 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.0.7
-        [itg]=1.0.7
-        [prd]=1.0.7
+        [tst]=1.0.9
+        [itg]=1.0.9
+        [prd]=1.0.9
     )
 
     if [[ "$ENVIRONMENT" != "" ]]; then
