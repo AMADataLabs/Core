6f7dd437d7d0d5bd870f48cc7c7c251422c51391
Peter Lane <Peter.Lane@ama-assn.org>
2023-10-06 23:32:23 -0500

    Replace HelloWorld DAG task class annotations with string annotations.

--- Source/Python/datalabs/example/etl/dag/hello_world.py
+++ Source/Python/datalabs/example/etl/dag/hello_world.py
@@ -2,16 +2,15 @@
     Definition for the HelloWorld DAG.
 '''
 from   datalabs.etl.dag.dag import DAG, register
-from   datalabs.example.etl.hello_world.task import HelloWorldTask
 
 
 
 @register(name="HELLO_WORLD")
 class HelloWorldDAG(DAG):
-    PRINT_HELLO_WORLD: HelloWorldTask
-    PRINT_HELLO_WORLD1: HelloWorldTask
-    PRINT_HELLO_WORLD2: HelloWorldTask
-    PRINT_HELLO_WORLD3: HelloWorldTask
+    PRINT_HELLO_WORLD: "datalabs.example.etl.hello_world.task.HelloWorldTask"
+    PRINT_HELLO_WORLD1: "datalabs.example.etl.hello_world.task.HelloWorldTask"
+    PRINT_HELLO_WORLD2: "datalabs.example.etl.hello_world.task.HelloWorldTask"
+    PRINT_HELLO_WORLD3: "datalabs.example.etl.hello_world.task.HelloWorldTask"
 
 # pylint: disable=pointless-statement
 HelloWorldDAG.PRINT_HELLO_WORLD >> HelloWorldDAG.PRINT_HELLO_WORLD1 >> HelloWorldDAG.PRINT_HELLO_WORLD2
