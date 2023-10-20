a14cefef63d5ef9dad3d38a4fdef04f49e99ddfe
Nikhil <nikhil.trivedi@ama-assn.org>
2023-10-19 21:12:19 -0500

    revert change

--- Source/Python/datalabs/etl/vericre/profile/transform.py
+++ Source/Python/datalabs/etl/vericre/profile/transform.py
@@ -288,8 +288,7 @@ class MedicalSchoolsTransformerTask(CSVReaderMixin, CSVWriterMixin, Task):
             = aggregated_medical_schools.groupby("entityId")["medicalSchools"].apply(list).reset_index()
 
         aggregated_medical_schools['medicalSchools'] = aggregated_medical_schools['medicalSchools'].apply(
-            # lambda x: sorted(x, key=lambda item: str(item['graduateDate']))
-            lambda x: sorted(x, key=lambda item: str(item.get('graduateDate', ''))) # TODO: Nikhil Trivedi change
+            lambda x: sorted(x, key=lambda item: str(item['graduateDate']))
         )
 
         return aggregated_medical_schools
