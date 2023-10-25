190860c01beb017b26c9cb852ae1697792ee402c
Yogesh Vaze <yogesh.vaze@vinzglobal.com>
2023-10-25 20:51:37 +0530

    Corrected import

--- Source/Python/datalabs/access/cpt/api/knowledge_base.py
+++ Source/Python/datalabs/access/cpt/api/knowledge_base.py
@@ -14,7 +14,7 @@ from   datalabs.access.api.task import APIEndpointTask, InvalidRequest, Resource
 from   datalabs.access.aws import AWSClient
 from   datalabs.parameter import add_schema
 from   datalabs.access.cpt.api.authorize import PRODUCT_CODE_KB
-from   datalabs.access.cpt.api.knowledge_base_authorizer as KnowledgeBaseAuthorizer
+import datalabs.access.cpt.api.knowledge_base_authorizer as knowledgebaseauthorizer
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -50,7 +50,7 @@ class MapSearchEndpointTask(APIEndpointTask):
     PARAMETER_CLASS = MapSearchEndpointParameters
 
     def run(self):
-        authorized = KnowledgeBaseAuthorizer.authorized(self._parameters.authorization["authorizations"])
+        authorized = knowledgebaseauthorizer.authorized(self._parameters.authorization["authorizations"])
         if authorized:
             try:
                 search_parameters = self._get_search_parameters(self._parameters.query)
