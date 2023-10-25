6655dd82148c038d1a4fc9eefb604995ce1cf712
Yogesh Vaze <yogesh.vaze@vinzglobal.com>
2023-10-25 20:36:36 +0530

    Fixed import of authorizer

--- Source/Python/datalabs/access/cpt/api/knowledge_base.py
+++ Source/Python/datalabs/access/cpt/api/knowledge_base.py
@@ -50,7 +50,7 @@ class MapSearchEndpointTask(APIEndpointTask):
     PARAMETER_CLASS = MapSearchEndpointParameters
 
     def run(self):
-        authorized = KnowledgeBaseAuthorizer._authorized(self._parameters.authorization["authorizations"])
+        authorized = KnowledgeBaseAuthorizer.authorized(self._parameters.authorization["authorizations"])
         if authorized:
             try:
                 search_parameters = self._get_search_parameters(self._parameters.query)
@@ -614,81 +614,81 @@ class OpenSearchDataImporter:
         LOGGER.info(f"indices (After import): {indices}")
 
 
-class KnowledgeBaseAuthorizer:
-    @classmethod
-    def _authorized(cls, authorizations):
-        LOGGER.info(f"_authorized method of KnowledgeBaseAuthorizer class called.")
-        authorized_years = cls._get_authorized_years(authorizations)
-        LOGGER.info(f"Authorized years: {authorized_years}")
-        authorized = False
-        code_set = cls._get_current_year_code_set()
-        LOGGER.info(f"Code Set: {code_set}")
-        LOGGER.info(f"Authorizations: {str(authorizations)}")
-        if datetime.now().year in authorized_years:
-            authorized = True
-
-        return authorized
-
-    @classmethod
-    def _get_current_year_code_set(cls):
-        return f'{PRODUCT_CODE_KB}{str(datetime.now().year)[2:]}'
-
-    @classmethod
-    def _get_authorized_years(cls, authorizations):
-        '''Get year from authorizations which are of one of the form:
-            {PRODUCT_CODE}YY: ISO-8601 Timestamp
-           For example,
-            {PRODUCT_CODE}23: 2023-10-11T00:00:00-05:00
-        '''
-        cpt_api_authorizations = {key:value for key, value in authorizations.items() if cls._is_cpt_kb_product(key)}
-        current_time = datetime.now(timezone.utc)
-        authorized_years = []
-
-        for product_code, period_of_validity in cpt_api_authorizations.items():
-            authorized_years += cls._generate_authorized_years(product_code, period_of_validity, current_time)
-
-        return authorized_years
-
-    @classmethod
-    def _is_cpt_kb_product(cls, product):
-        return product.startswith(PRODUCT_CODE_KB)
-
-    @classmethod
-    def _generate_authorized_years(cls, product_code, period_of_validity, current_time):
-        period_of_validity["start"] = datetime.fromisoformat(period_of_validity["start"]).astimezone(timezone.utc)
-        period_of_validity["end"] = datetime.fromisoformat(period_of_validity["end"]).astimezone(timezone.utc)
-        authorized_years = []
-
-        if (
-                product_code.startswith(PRODUCT_CODE_KB)
-                and current_time >= period_of_validity["start"] <= current_time <= period_of_validity["end"]
-        ):
-            authorized_years += cls._generate_years_from_product_code(PRODUCT_CODE_KB, product_code)
-
-        return authorized_years
-
-    @classmethod
-    def _generate_years_from_period(cls, period, current_time):
-        years = list(range(period["start"].year, current_time.year + 1))
-
-        if period["end"] <= current_time:
-            years.pop()
-
-        return years
-
-    @classmethod
-    def _generate_years_from_product_code(cls, base_product_code, product_code):
-        authorized_years = []
-
-        try:
-            authorized_years.append(cls._parse_authorization_year(base_product_code, product_code))
-        except ValueError:
-            pass
-
-        return authorized_years
-
-    @classmethod
-    def _parse_authorization_year(cls, base_product_code, product_code):
-        two_digit_year = product_code[len(base_product_code):]
-
-        return int('20' + two_digit_year)
+# class KnowledgeBaseAuthorizer:
+#     @classmethod
+#     def _authorized(cls, authorizations):
+#         LOGGER.info(f"_authorized method of KnowledgeBaseAuthorizer class called.")
+#         authorized_years = cls._get_authorized_years(authorizations)
+#         LOGGER.info(f"Authorized years: {authorized_years}")
+#         authorized = False
+#         code_set = cls._get_current_year_code_set()
+#         LOGGER.info(f"Code Set: {code_set}")
+#         LOGGER.info(f"Authorizations: {str(authorizations)}")
+#         if datetime.now().year in authorized_years:
+#             authorized = True
+#
+#         return authorized
+#
+#     @classmethod
+#     def _get_current_year_code_set(cls):
+#         return f'{PRODUCT_CODE_KB}{str(datetime.now().year)[2:]}'
+#
+#     @classmethod
+#     def _get_authorized_years(cls, authorizations):
+#         '''Get year from authorizations which are of one of the form:
+#             {PRODUCT_CODE}YY: ISO-8601 Timestamp
+#            For example,
+#             {PRODUCT_CODE}23: 2023-10-11T00:00:00-05:00
+#         '''
+#         cpt_api_authorizations = {key:value for key, value in authorizations.items() if cls._is_cpt_kb_product(key)}
+#         current_time = datetime.now(timezone.utc)
+#         authorized_years = []
+#
+#         for product_code, period_of_validity in cpt_api_authorizations.items():
+#             authorized_years += cls._generate_authorized_years(product_code, period_of_validity, current_time)
+#
+#         return authorized_years
+#
+#     @classmethod
+#     def _is_cpt_kb_product(cls, product):
+#         return product.startswith(PRODUCT_CODE_KB)
+#
+#     @classmethod
+#     def _generate_authorized_years(cls, product_code, period_of_validity, current_time):
+#         period_of_validity["start"] = datetime.fromisoformat(period_of_validity["start"]).astimezone(timezone.utc)
+#         period_of_validity["end"] = datetime.fromisoformat(period_of_validity["end"]).astimezone(timezone.utc)
+#         authorized_years = []
+#
+#         if (
+#                 product_code.startswith(PRODUCT_CODE_KB)
+#                 and current_time >= period_of_validity["start"] <= current_time <= period_of_validity["end"]
+#         ):
+#             authorized_years += cls._generate_years_from_product_code(PRODUCT_CODE_KB, product_code)
+#
+#         return authorized_years
+#
+#     @classmethod
+#     def _generate_years_from_period(cls, period, current_time):
+#         years = list(range(period["start"].year, current_time.year + 1))
+#
+#         if period["end"] <= current_time:
+#             years.pop()
+#
+#         return years
+#
+#     @classmethod
+#     def _generate_years_from_product_code(cls, base_product_code, product_code):
+#         authorized_years = []
+#
+#         try:
+#             authorized_years.append(cls._parse_authorization_year(base_product_code, product_code))
+#         except ValueError:
+#             pass
+#
+#         return authorized_years
+#
+#     @classmethod
+#     def _parse_authorization_year(cls, base_product_code, product_code):
+#         two_digit_year = product_code[len(base_product_code):]
+#
+#         return int('20' + two_digit_year)
--- Source/Python/datalabs/access/cpt/api/knowledge_base_authorizer.py
+++ Source/Python/datalabs/access/cpt/api/knowledge_base_authorizer.py
@@ -1,18 +1,7 @@
 """ Release endpoint classes. """
 import logging
-import time
-import uuid
-import boto3
-from   requests_aws4auth import AWS4Auth
-from   dataclasses import dataclass
 from   datetime import datetime, timezone
 
-from   opensearchpy import OpenSearch, RequestsHttpConnection, NotFoundError
-from   opensearchpy.helpers import bulk
-
-from   datalabs.access.api.task import APIEndpointTask, InvalidRequest, ResourceNotFound, InternalServerError
-from   datalabs.access.aws import AWSClient
-from   datalabs.parameter import add_schema
 from   datalabs.access.cpt.api.authorize import PRODUCT_CODE_KB
 
 logging.basicConfig()
@@ -21,81 +10,80 @@ LOGGER.setLevel(logging.DEBUG)
 
 
 # pylint: disable=too-many-instance-attributes
-class KnowledgeBaseAuthorizer:
-    @classmethod
-    def _authorized(cls, authorizations):
-        LOGGER.info(f"_authorized method of KnowledgeBaseAuthorizer class called.")
-        authorized_years = cls._get_authorized_years(authorizations)
-        LOGGER.info(f"Authorized years: {authorized_years}")
-        authorized = False
-        code_set = cls._get_current_year_code_set()
-        LOGGER.info(f"Code Set: {code_set}")
-        LOGGER.info(f"Authorizations: {str(authorizations)}")
-        if datetime.now().year in authorized_years:
-            authorized = True
 
-        return authorized
+def authorized(authorizations):
+    LOGGER.info(f"_authorized method of KnowledgeBaseAuthorizer class called.")
+    authorized_years = get_authorized_years(authorizations)
+    LOGGER.info(f"Authorized years: {authorized_years}")
+    authorized = False
+    code_set = get_current_year_code_set()
+    LOGGER.info(f"Code Set: {code_set}")
+    LOGGER.info(f"Authorizations: {str(authorizations)}")
+    if datetime.now().year in authorized_years:
+        authorized = True
 
-    @classmethod
-    def _get_current_year_code_set(cls):
-        return f'{PRODUCT_CODE_KB}{str(datetime.now().year)[2:]}'
+    return authorized
 
-    @classmethod
-    def _get_authorized_years(cls, authorizations):
-        '''Get year from authorizations which are of one of the form:
-            {PRODUCT_CODE}YY: ISO-8601 Timestamp
-           For example,
-            {PRODUCT_CODE}23: 2023-10-11T00:00:00-05:00
-        '''
-        cpt_api_authorizations = {key:value for key, value in authorizations.items() if cls._is_cpt_kb_product(key)}
-        current_time = datetime.now(timezone.utc)
-        authorized_years = []
 
-        for product_code, period_of_validity in cpt_api_authorizations.items():
-            authorized_years += cls._generate_authorized_years(product_code, period_of_validity, current_time)
+def get_current_year_code_set():
+    return f'{PRODUCT_CODE_KB}{str(datetime.now().year)[2:]}'
 
-        return authorized_years
 
-    @classmethod
-    def _is_cpt_kb_product(cls, product):
-        return product.startswith(PRODUCT_CODE_KB)
+def get_authorized_years(authorizations):
+    '''Get year from authorizations which are of one of the form:
+        {PRODUCT_CODE}YY: ISO-8601 Timestamp
+       For example,
+        {PRODUCT_CODE}23: 2023-10-11T00:00:00-05:00
+    '''
+    cpt_api_authorizations = {key: value for key, value in authorizations.items() if is_cpt_kb_product(key)}
+    current_time = datetime.now(timezone.utc)
+    authorized_years = []
 
-    @classmethod
-    def _generate_authorized_years(cls, product_code, period_of_validity, current_time):
-        period_of_validity["start"] = datetime.fromisoformat(period_of_validity["start"]).astimezone(timezone.utc)
-        period_of_validity["end"] = datetime.fromisoformat(period_of_validity["end"]).astimezone(timezone.utc)
-        authorized_years = []
+    for product_code, period_of_validity in cpt_api_authorizations.items():
+        authorized_years += generate_authorized_years(product_code, period_of_validity, current_time)
 
-        if (
-                product_code.startswith(PRODUCT_CODE_KB)
-                and current_time >= period_of_validity["start"] <= current_time <= period_of_validity["end"]
-        ):
-            authorized_years += cls._generate_years_from_product_code(PRODUCT_CODE_KB, product_code)
+    return authorized_years
 
-        return authorized_years
 
-    @classmethod
-    def _generate_years_from_period(cls, period, current_time):
-        years = list(range(period["start"].year, current_time.year + 1))
+def is_cpt_kb_product(product):
+    return product.startswith(PRODUCT_CODE_KB)
 
-        if period["end"] <= current_time:
-            years.pop()
 
-        return years
+def generate_authorized_years(product_code, period_of_validity, current_time):
+    period_of_validity["start"] = datetime.fromisoformat(period_of_validity["start"]).astimezone(timezone.utc)
+    period_of_validity["end"] = datetime.fromisoformat(period_of_validity["end"]).astimezone(timezone.utc)
+    authorized_years = []
 
-    @classmethod
-    def _generate_years_from_product_code(cls, base_product_code, product_code):
-        authorized_years = []
+    if (
+            product_code.startswith(PRODUCT_CODE_KB)
+            and current_time >= period_of_validity["start"] <= current_time <= period_of_validity["end"]
+    ):
+        authorized_years += generate_years_from_product_code(PRODUCT_CODE_KB, product_code)
 
-        try:
-            authorized_years.append(cls._parse_authorization_year(base_product_code, product_code))
-        except ValueError:
-            pass
+    return authorized_years
 
-        return authorized_years
 
-    @classmethod
-    def _parse_authorization_year(cls, base_product_code, product_code):
-        two_digit_year = product_code[len(base_product_code):]
+def generate_years_from_period(period, current_time):
+    years = list(range(period["start"].year, current_time.year + 1))
 
-        return int('20' + two_digit_year)
+    if period["end"] <= current_time:
+        years.pop()
+
+    return years
+
+
+def generate_years_from_product_code(base_product_code, product_code):
+    authorized_years = []
+
+    try:
+        authorized_years.append(parse_authorization_year(base_product_code, product_code))
+    except ValueError:
+        pass
+
+    return authorized_years
+
+
+def parse_authorization_year(base_product_code, product_code):
+    two_digit_year = product_code[len(base_product_code):]
+
+    return int('20' + two_digit_year)
