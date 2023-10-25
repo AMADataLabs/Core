d59e7d65b3a611b068e89f7e0bfce346565591d1
Rudainah <rudainah.najeeb@ama-assn.org>
2023-10-25 15:55:11 -0500

    Implemented 400 & 401

--- Source/Python/datalabs/access/cpt/api/vignettes.py
+++ Source/Python/datalabs/access/cpt/api/vignettes.py
@@ -2,7 +2,7 @@
 import logging
 from   dataclasses import dataclass
 
-from   datalabs.access.api.task import APIEndpointTask, ResourceNotFound
+from   datalabs.access.api.task import APIEndpointTask, ResourceNotFound, InvalidRequest
 from   datalabs.access.aws import AWSClient
 from   datalabs.parameter import add_schema
 
@@ -27,9 +27,23 @@ class MapLookupEndpointTask(APIEndpointTask):
     PARAMETER_CLASS = MapLookupEndpointParameters
 
     def run(self):
-        mappings = self._get_mappings_for_code(self._parameters.path["cpt_code"])
+        cpt_code, additional_information = self._get_query_parameters()
+        mappings = self._get_mappings_for_code(cpt_code)
 
-        self._response_body = self._generate_response(mappings)
+        self._response_body = self._generate_response(mappings, additional_information)
+
+    def _get_query_parameters(self):
+        if not self._parameters.path["cpt_code"] or not self._parameters.path["cpt_code"].isnumeric():
+            raise InvalidRequest("Bad Request", 400)
+
+        if self._parameters.query.get('additional_information'):
+            if not (
+                self._parameters.query.get('additional_information')[0].upper() == 'TRUE' or
+                self._parameters.query.get('additional_information')[0].upper() == 'FALSE'
+            ):
+                raise InvalidRequest("Bad Request", 400)
+
+        return self._parameters.path["cpt_code"], self._parameters.query.get('additional_information')
 
     def _get_mappings_for_code(self, code):
         with AWSClient("dynamodb") as dynamodb:
@@ -38,11 +52,11 @@ class MapLookupEndpointTask(APIEndpointTask):
             )
 
         if results["Items"] == []:
-            raise ResourceNotFound("No Vignette for the given CPT Code")
+            raise ResourceNotFound("No data available")
 
         return results["Items"]
 
-    def _generate_response(self, mappings):
+    def _generate_response(self, mappings, additional_information):
         response = {
                 "cpt_code": mappings[0]['pk']['S'].replace("CPT CODE:", ""),
                 "typical_patient": mappings[0]['typical_patient']['S'],
@@ -51,10 +65,7 @@ class MapLookupEndpointTask(APIEndpointTask):
                 "post_service_info": mappings[0]['post_service_info']['S']
         }
 
-        if (
-            self._parameters.query.get('additional_information') and
-            self._parameters.query.get('additional_information')[0].upper() == 'TRUE'
-        ):
+        if (additional_information and additional_information[0].upper() == 'TRUE'):
             response.update(
                 {
                     "ruc_reviewed_date": mappings[0]['ruc_reviewed_date']['S'],
