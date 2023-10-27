d6fbed96dbb8118925450680c6dc406ab736bc54
Killian Nolan <killian.nolan@ama-assn.org>
2023-10-27 11:34:05 -0700

    Testing a change on SBX - new token api.

--- Source/Python/datalabs/access/vericre/api/tokenapi.py
+++ Source/Python/datalabs/access/vericre/api/tokenapi.py
@@ -1,13 +1,11 @@
-import json
 from dataclasses import dataclass
 
 import urllib3
 import logging
-import urllib.parse
 
-from datalabs.access.api.task import APIEndpointTask, InternalServerError
+from datalabs.access.api.task import APIEndpointTask
 from datalabs.parameter import add_schema
-from datalabs.util.profile import run_time_logger
+from datalabs.util.profile import get_ama_access_token
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -33,42 +31,10 @@ class AMATokenEndpointTask(APIEndpointTask, HttpClient):
 
     def run(self):
         LOGGER.debug('Parameters in AMATokenEndpointTask: %s', self._parameters)
+        grant_type = self._parameters.query["grant_type"][0]
+        client_id = self._parameters.query["client_id"][0]
+        client_secret = self._parameters.query["client_secret"][0]
 
-        access_token = self._get_ama_access_token()
-        LOGGER.debug("access_token: %s", access_token)
-        self._generate_response(access_token)
+        token_json = get_ama_access_token(self, grant_type, client_id, client_secret)
 
-    @run_time_logger
-    def _get_ama_access_token(self):
-        token_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-
-        token_fields = {
-            "grant_type": self._parameters.query["grant_type"][0],
-            "client_id": self._parameters.query["client_id"][0],
-            "client_secret": self._parameters.query["client_secret"][0],
-        }
-
-        token_body = urllib.parse.urlencode(token_fields)
-        LOGGER.debug("token_body: %s", token_body)
-        token_response = self._request_ama_token(token_headers, token_body)
-
-        if token_response.status != 200:
-            raise InternalServerError(
-                f'Internal Server error caused by: {token_response.data}, status: {token_response.status}'
-            )
-
-        token_json = json.loads(token_response.data.decode("utf-8"))
-
-        return token_json
-
-    def _generate_response(self, response):
-        self._response_body = response
-
-    @run_time_logger
-    def _request_ama_token(self, token_headers, token_body):
-        return self.HTTP.request(
-            'POST',
-            self._parameters.token_url,
-            headers=token_headers,
-            body=token_body
-        )
+        self._response_body = token_json
