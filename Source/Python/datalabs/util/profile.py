c43c045659ec1f594e0ad9d9ddae9d30488b214f
Killian Nolan <killian.nolan@ama-assn.org>
2023-11-01 13:15:07 -0700

    VER-812 - Centralising token logic

--- Source/Python/datalabs/util/profile.py
+++ Source/Python/datalabs/util/profile.py
@@ -1,6 +1,10 @@
 ''' Performance profiling utilities. '''
-from   datetime import datetime
+import json
+import urllib
+from datetime import datetime
 import logging
+
+from datalabs.access.api.task import InternalServerError
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -20,3 +24,30 @@ def run_time_logger(func):
         return result
 
     return wrapper
+
+
+def get_ama_access_token(self, grant_type, client_id, client_secret):
+    token_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    token_fields = {
+        "grant_type": grant_type,
+        "client_id": client_id,
+        "client_secret": client_secret
+    }
+    token_body = urllib.parse.urlencode(token_fields)
+
+    token_response = _request_ama_token(self, token_headers, token_body)
+
+    if token_response.status != 200:
+        raise InternalServerError(
+            f'Internal Server error caused by: {token_response.data}, status: {token_response.status}'
+        )
+    return json.loads(token_response.data)
+
+
+def _request_ama_token(self, token_headers, token_body):
+    return self.HTTP.request(
+        'POST',
+        self._parameters.token_url,
+        headers=token_headers,
+        body=token_body
+    )
