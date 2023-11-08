1a441299850350109bf1a4172672adf16fbbfdf1
viviansun0213 <Ronghua.Sun@ama-assn.org>
2023-11-07 22:29:49 -0600

    Implement _get_dag_parameters TriggerHandlerTask & fix test error

--- Source/Python/datalabs/etl/dag/trigger/handler/pause.py
+++ Source/Python/datalabs/etl/dag/trigger/handler/pause.py
@@ -1,7 +1,10 @@
 """ DAG Scheduler trigger handler. """
+from   datalabs.access.aws import AWSClient
+from   datalabs.access.cpt.api.snomed import MapSearchEndpointTask
 from   datalabs.etl.dag.notify.sns import SNSDAGNotifier
 from   datalabs.etl.dag.trigger.handler import task
 
+# pylint: disable=no-member, protected-access
 class TriggerHandlerTask(task.TriggerHandlerTask):
     def _get_dag_parameters(self, trigger_parameters: dict, event: dict) -> dict:
         '''
@@ -16,6 +19,7 @@ class TriggerHandlerTask(task.TriggerHandlerTask):
         YET_ANOTHER_DAG         2023-10-20T00:00:00     123456
         '''
         paused_dags = {}
+        current_rows = []
 
         # Paused DAG table: trigger_parameters["PAUSED_DAG_TABLE"]
         #
@@ -29,6 +33,20 @@ class TriggerHandlerTask(task.TriggerHandlerTask):
         #     SOME_OTHER_DAG={"execution_time": "2023-11-05T00:00:00"},
         #     YET_ANOTHER_DAG={"execution_time": "2023-10-20T00:00:00"}
         # )
+
+        with AWSClient("dynamodb", **self._connection_parameters()) as dynamodb:
+            results = MapSearchEndpointTask._paginate(
+                dynamodb,
+                f"SELECT dag_id, execution_time FROM \"{self._parameters.table}\""
+            )
+
+            current_rows = [(x["dag_id"]["S"], x["execution_time"]["S"]) for x in results]
+
+            for dag_id, execution_time in current_rows:
+                if dag_id in paused_dags:
+                    paused_dags[dag_id]["execution_time"] = [paused_dags[dag_id]["execution_time"]] + [execution_time]
+                else:
+                    paused_dags[dag_id]=(dict(execution_time=execution_time))
 
         return paused_dags
 
