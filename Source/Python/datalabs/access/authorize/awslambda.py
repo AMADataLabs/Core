c5815ed380ee89e4def826a824dac6aecab1bcab
Peter Lane <peter.lane@ama-assn.org>
2023-11-10 23:13:26 +0000

    Revert "Fix authorizer (pull request #595)"

--- Source/Python/datalabs/access/authorize/awslambda.py
+++ Source/Python/datalabs/access/authorize/awslambda.py
@@ -1,10 +1,9 @@
 """ API endpoint-specific Lambda function Task wrapper. """
 import logging
 import os
-from typing import Any, Dict
 
-from datalabs.access.authorize.task import AuthorizerTaskException
-from datalabs.awslambda import TaskWrapper
+from   datalabs.access.authorize.task import AuthorizerParameters, AuthorizerTaskException
+from   datalabs.awslambda import TaskWrapper
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -13,35 +12,35 @@ LOGGER.setLevel(logging.DEBUG)
 
 class AuthorizerLambdaTaskWrapper(TaskWrapper):
     def _get_task_parameters(self):
-        self._parameters["headers"] = {key.lower(): value for key, value in self._parameters["headers"].items()}
+        self._parameters["headers"] = {key.lower():value for key, value in self._parameters["headers"].items()}
         token = self._get_authorization_token()
-        LOGGER.info("Bearer token: %s", token)
+        LOGGER.info('Bearer token: %s', token)
 
         return self._get_authorization_parameters(token)
 
     def _get_authorization_token(self):
         token = self._parameters["headers"].get("authorization").strip()
 
-        if not token.startswith("Bearer "):
+        if not token.startswith('Bearer '):
             raise AuthorizerTaskException('Invalid bearer token: "{token}"')
 
         return token
 
-    def _get_authorization_parameters(self, token) -> dict:
+    def _get_authorization_parameters(self, token):
         LOGGER.debug("Authorization Lambda event:\n%s", self._parameters)
-        return dict(
-            token=token.split(" ")[1],
-            endpoint=self._parameters.get("methodArn"),
-            passport_url=os.environ.get("PASSPORT_URL"),
+        return AuthorizerParameters(
+            token=token.split(' ')[1],
+            endpoint=self._parameters.get('methodArn'),
+            passport_url=os.environ.get('PASSPORT_URL'),
             customer=self._parameters["headers"].get("x-customer-nbr"),
         )
 
-    def _handle_exception(self, exception: AuthorizerTaskException) -> Dict[str, Any]:
-        LOGGER.exception("An error occurred during authorization")
+    def _handle_exception(self, exception: AuthorizerTaskException) -> (int, dict):
+        LOGGER.exception('An error occurred during authorization')
         return dict(message=exception.message)
 
-    def _handle_success(self) -> Dict[str, Any]:
-        LOGGER.info("Authorization:\n%s", self.task.authorization)
+    def _handle_success(self) -> (int, dict):
+        LOGGER.info('Authorization:\n%s', self.task.authorization)
         return self.task.authorization
 
     # pylint: disable=unused-argument
--- Source/Python/datalabs/access/authorize/task.py
+++ Source/Python/datalabs/access/authorize/task.py
@@ -1,35 +1,29 @@
 """Authorize Task"""
-from dataclasses import dataclass
+from   dataclasses import dataclass
 import logging
 import json
-from typing import Dict, List, Optional
-
 import requests
 
-from datalabs.parameter import add_schema
-from datalabs.task import Task, TaskException
+from   datalabs.task import Task, TaskException
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
 
-@add_schema
 @dataclass
 class AuthorizerParameters:
     token: str
     passport_url: str
     endpoint: str
-    customer: Optional[str] = None
+    customer: str = None
 
 
 class AuthorizerTask(Task):
-    PARAMETER_CLASS = AuthorizerParameters
-
-    def __init__(self, parameters: dict, data: Optional[List[bytes]] = None):
+    def __init__(self, parameters: AuthorizerParameters, data: "list<bytes>"=None):
         super().__init__(parameters, data)
 
-        self._authorization: Dict[str, str] = {}
+        self._authorization = {}
         self._session = requests.Session()
 
     @property
@@ -42,13 +36,16 @@ class AuthorizerTask(Task):
 
     def run(self):
         headers = {
-            "Authorization": "Bearer " + self._parameters.token,
+            'Authorization': 'Bearer ' + self._parameters.token,
         }
 
         if self._parameters.customer:
             headers["x-customer-nbr"] = self._parameters.customer
 
-        response = self._session.post(self._parameters.passport_url, headers=headers)
+        response = self._session.post(
+            self._parameters.passport_url,
+            headers=headers
+        )
         LOGGER.debug("Passport Response: \n%s", response)
 
         if response.status_code in (200, 401):
@@ -57,39 +54,53 @@ class AuthorizerTask(Task):
 
             self._authorization = self._authorize(entitlements)
         else:
-            raise AuthorizerTaskException(f"Unable to authorize: {response.text}")
+            raise AuthorizerTaskException(f'Unable to authorize: {response.text}')
 
     def _authorize(self, entitlements):
         active_subscriptions = self._get_active_subscriptions(entitlements)
 
         customer_number = self._parameters.customer if self._parameters.customer else entitlements.get("customerNumber")
         policy = None
-        context = dict(customerNumber=customer_number, customerName=entitlements.get("customerName"))
+        context = dict(
+            customerNumber=customer_number,
+            customerName=entitlements.get("customerName")
+        )
 
         if active_subscriptions is not None:
-            policy = self._generate_policy(effect="Allow")
+            policy = self._generate_policy(effect='Allow')
             context.update(self._generate_context_from_subscriptions(active_subscriptions))
         else:
-            policy = self._generate_policy(effect="Deny")
+            policy = self._generate_policy(effect='Deny')
 
-        return {"principalId": "username", "context": context, "policyDocument": policy}
+        return {
+            "principalId": "username",
+            "context": context,
+            "policyDocument": policy
+        }
 
     @classmethod
     def _get_active_subscriptions(cls, entitlements):
-        subscriptions = entitlements.get("subscriptionsList")
+        subscriptions = entitlements.get('subscriptionsList')
 
         if subscriptions:
             subscriptions = [s for s in subscriptions if s.get("agreementStatus") == "A"]
 
         return subscriptions
 
+
     def _generate_policy(self, effect):
-        base, stage, action, _ = self._parameters.endpoint.split("/", 3)
-        resource = "/".join((base, stage, action, "*"))
+        base, stage, action, _ = self._parameters.endpoint.split('/', 3)
+        resource = '/'.join((base, stage, action, '*'))
 
         return {
             "Version": "2012-10-17",
-            "Statement": [{"Action": "execute-api:Invoke", "Effect": effect, "Resource": resource}],
+            "Statement": [
+                {
+                    "Action": "execute-api:Invoke",
+                    "Effect": effect,
+                    "Resource": resource
+                }
+            ]
         }
 
     @classmethod
@@ -98,10 +109,14 @@ class AuthorizerTask(Task):
 
         for subscription in subscriptions:
             context[subscription.get("productCode")] = json.dumps(
-                {"start": subscription.get("startDate"), "end": subscription.get("accessEndDt")}
+                {
+                    "start": subscription.get("startDate"),
+                    "end": subscription.get("accessEndDt")
+                }
             )
 
         return context
+
 
 
 class AuthorizerTaskException(TaskException):
