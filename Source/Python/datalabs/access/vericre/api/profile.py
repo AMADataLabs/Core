428d2ef2e3bc9d8d5db9d7408430a0b06f465f77
Gary WU <>
2023-11-30 17:23:46 -0600

    remove unnecessary pylint description

--- Source/Python/datalabs/access/vericre/api/profile.py
+++ Source/Python/datalabs/access/vericre/api/profile.py
@@ -1,17 +1,17 @@
 """ Release endpoint classes."""
-from   dataclasses import dataclass, asdict
-from   collections import defaultdict
+from dataclasses import dataclass, asdict
+from collections import defaultdict
 import json
 import logging
 import sys
 import uuid
 
-from   datalabs.access.api.task import APIEndpointTask, APIEndpointException, ResourceNotFound, InvalidRequest
-from   datalabs.access.aws import AWSClient
-from   datalabs.access.orm import Database
-from   datalabs.parameter import add_schema
-from   datalabs.util.profile import run_time_logger
-from   datalabs.access.vericre.api.option import OPTION_MAP, OPTION_VALUES_MAP
+from datalabs.access.api.task import APIEndpointTask, APIEndpointException, ResourceNotFound, InvalidRequest
+from datalabs.access.aws import AWSClient
+from datalabs.access.orm import Database
+from datalabs.parameter import add_schema
+from datalabs.util.profile import run_time_logger
+from datalabs.access.vericre.api.option import OPTION_MAP, OPTION_VALUES_MAP
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -21,34 +21,34 @@ LOGGER.setLevel(logging.DEBUG)
 # pylint: disable=too-many-instance-attributes
 @dataclass
 class ProfileRecords:
-    entity_id: str=None
-    demographics: list=None
-    ecfmg: list=None
-    medical_schools: list=None
-    medical_training: list=None
-    licenses: list=None
-    sanctions: list=None
-    dea: list=None
-    abms: list=None
-    claim: list=None
-    provider_cds: list=None
-    work_history: list=None
-    insurance: list=None
+    entity_id: str = None
+    demographics: list = None
+    ecfmg: list = None
+    medical_schools: list = None
+    medical_training: list = None
+    licenses: list = None
+    sanctions: list = None
+    dea: list = None
+    abms: list = None
+    claim: list = None
+    provider_cds: list = None
+    work_history: list = None
+    insurance: list = None
 
 
 class BaseProfileEndpointTask(APIEndpointTask):
     RECORD_SECTION_NAMES = {
-        'medicalSchools': 'medical_schools',
-        'medicalTraining': "medical_training",
-        'ProviderCDS': "provider_cds",
-        'WorkHistory': "work_history",
-        "Insurance": "insurance"
+        "medicalSchools": "medical_schools",
+        "medicalTraining": "medical_training",
+        "ProviderCDS": "provider_cds",
+        "WorkHistory": "work_history",
+        "Insurance": "insurance",
     }
 
     RESPONSE_MAX_SIZE = 5510
 
     def run(self):
-        LOGGER.debug('Parameters: %s', self._parameters)
+        LOGGER.debug("Parameters: %s", self._parameters)
 
         with Database.from_parameters(self._parameters) as database:
             self._run(database)
@@ -94,14 +94,13 @@ class BaseProfileEndpointTask(APIEndpointTask):
 
         return result
 
-    # pylint: disable=no-self-use
     @classmethod
     def _generate_response_body(cls, aggregated_records):
         return {"profiles": [asdict(object) for object in list(aggregated_records.values())]}
 
     @classmethod
     def _generate_base_query(cls):
-        return '''
+        return """
             select
                 u.ama_entity_id,
                 ff.sub_section as section_identifier,
@@ -129,7 +128,7 @@ class BaseProfileEndpointTask(APIEndpointTask):
             join form_field ff on ff.form = p.form
                 and ff.sub_section is not null
             where 1=1
-        '''
+        """
 
     # pylint: disable=unused-argument
     @classmethod
@@ -142,15 +141,15 @@ class BaseProfileEndpointTask(APIEndpointTask):
 
     @classmethod
     def _add_sorting_to_query(cls, query):
-        query = f'{query} order by u.ama_entity_id asc, ff.sub_section asc, ff.order asc'
+        query = f"{query} order by u.ama_entity_id asc, ff.sub_section asc, ff.order asc"
 
         return query
 
     @classmethod
     def _aggregate_record(cls, aggregated_records, record):
-        section_name = cls.RECORD_SECTION_NAMES.get(record['section_identifier'], record['section_identifier'])
+        section_name = cls.RECORD_SECTION_NAMES.get(record["section_identifier"], record["section_identifier"])
 
-        aggregated_records[record['ama_entity_id']].entity_id = record['ama_entity_id']
+        aggregated_records[record["ama_entity_id"]].entity_id = record["ama_entity_id"]
 
         cls._append_record_to_aggregated_records(aggregated_records, section_name, record)
 
@@ -158,12 +157,12 @@ class BaseProfileEndpointTask(APIEndpointTask):
 
     @classmethod
     def _filter_by_active_user(cls, query):
-        query = f'{query} and u.is_deleted = False and u.status = \'ACTIVE\''
+        query = f"{query} and u.is_deleted = False and u.status = 'ACTIVE'"
         return query
 
     @classmethod
     def _filter_by_hidden_form_field(cls, query):
-        query = f'{query} and ff.is_hidden = False'
+        query = f"{query} and ff.is_hidden = False"
         return query
 
     @classmethod
@@ -172,7 +171,7 @@ class BaseProfileEndpointTask(APIEndpointTask):
 
     @classmethod
     def _append_record_to_aggregated_records(cls, aggregated_records, section_name, record):
-        section_items = getattr(aggregated_records[record['ama_entity_id']], section_name)
+        section_items = getattr(aggregated_records[record["ama_entity_id"]], section_name)
 
         if section_items is None:
             section_items = []
@@ -181,25 +180,25 @@ class BaseProfileEndpointTask(APIEndpointTask):
 
         section_items.append(item)
 
-        setattr(aggregated_records[record['ama_entity_id']], section_name, section_items)
+        setattr(aggregated_records[record["ama_entity_id"]], section_name, section_items)
 
     @classmethod
     def _create_record_item(cls, record):
-        record_values = record['values']
+        record_values = record["values"]
 
-        if isinstance(record['option'], int) and str(record['option']) in OPTION_MAP:
-            record_values = cls._convert_option_values(OPTION_MAP[str(record['option'])], record['values'])
+        if isinstance(record["option"], int) and str(record["option"]) in OPTION_MAP:
+            record_values = cls._convert_option_values(OPTION_MAP[str(record["option"])], record["values"])
 
         return dict(
-            field_identifier=record['field_identifier'],
-            is_authoritative=record['is_authoritative'],
-            is_source=record['is_source'],
-            name=record['name'],
-            read_only=record['read_only'],
-            source_key=record['source_key'],
-            source_tag=record['source_tag'],
-            type=record['type'],
-            values=record_values
+            field_identifier=record["field_identifier"],
+            is_authoritative=record["is_authoritative"],
+            is_source=record["is_source"],
+            name=record["name"],
+            read_only=record["read_only"],
+            source_key=record["source_key"],
+            source_tag=record["source_tag"],
+            type=record["type"],
+            values=record_values,
         )
 
     @classmethod
@@ -238,7 +237,7 @@ class SingleProfileLookupEndpointParameters:
     database_port: str
     database_username: str
     database_password: str
-    unknowns: dict=None
+    unknowns: dict = None
 
 
 class SingleProfileLookupEndpointTask(BaseProfileEndpointTask):
@@ -271,7 +270,7 @@ class MultiProfileLookupEndpointParameters:
     database_password: str
     request_cache_table: str
     payload: dict
-    unknowns: dict=None
+    unknowns: dict = None
 
 
 class MultiProfileLookupEndpointTask(BaseProfileEndpointTask):
@@ -282,14 +281,14 @@ class MultiProfileLookupEndpointTask(BaseProfileEndpointTask):
         response_size = self._get_response_size(response_body)
         entity_ids = list(aggregated_records.keys())
 
-        LOGGER.info('Response Result size: %s KB', response_size)
+        LOGGER.info("Response Result size: %s KB", response_size)
 
         if response_size > self.RESPONSE_MAX_SIZE:
             request_id = self._save_entity_ids_to_dynamodb(self._parameters.request_cache_table, entity_ids)
 
-            response_body['profiles'] = self._extract_profiles_subset(response_body['profiles'])
+            response_body["profiles"] = self._extract_profiles_subset(response_body["profiles"])
 
-            response_body['next'] = self._generate_next_url(request_id, len(response_body['profiles']))
+            response_body["next"] = self._generate_next_url(request_id, len(response_body["profiles"]))
 
         return response_body
 
@@ -312,7 +311,7 @@ class MultiProfileLookupEndpointTask(BaseProfileEndpointTask):
                 f"The request contained {len(entity_ids)} entity IDs, but the maximum allowed is 1,000."
             )
 
-        return f'''{query} and u.ama_entity_id in ('{"','".join(entity_ids)}')'''
+        return f"""{query} and u.ama_entity_id in ('{"','".join(entity_ids)}')"""
 
     @classmethod
     def _get_response_size(cls, aggregated_records):
@@ -349,15 +348,12 @@ class MultiProfileLookupEndpointTask(BaseProfileEndpointTask):
 
     @classmethod
     def _generate_next_url(cls, request_id, index):
-        return f'/profile_api/profiles/lookup/{request_id}?index={index}'
+        return f"/profile_api/profiles/lookup/{request_id}?index={index}"
 
     @classmethod
     @run_time_logger
     def _generate_item(cls, request_id, entity_ids):
-        item = dict(
-            request_id=dict(S=request_id),
-            entity_ids=dict(S=(",".join(entity_ids)))
-        )
+        item = dict(request_id=dict(S=request_id), entity_ids=dict(S=(",".join(entity_ids))))
 
         return item
 
@@ -377,7 +373,7 @@ class MultiProfileLookupByIndexEndpointParameters:
     database_username: str
     database_password: str
     request_cache_table: str
-    unknowns: dict=None
+    unknowns: dict = None
 
 
 class MultiProfileLookupByIndexEndpointTask(MultiProfileLookupEndpointTask):
@@ -385,17 +381,17 @@ class MultiProfileLookupByIndexEndpointTask(MultiProfileLookupEndpointTask):
 
     # pylint: disable=bad-super-call
     def _generate_response_body(self, aggregated_records):
-        request_id = self._parameters.path['request_id']
-        index = int(self._parameters.query['index'][0])
+        request_id = self._parameters.path["request_id"]
+        index = int(self._parameters.query["index"][0])
         response_body = super(MultiProfileLookupEndpointTask, self)._generate_response_body(aggregated_records)
         response_size = self._get_response_size(response_body)
 
-        LOGGER.info('Response Result size: %s KB', response_size)
+        LOGGER.info("Response Result size: %s KB", response_size)
 
         if response_size > self.RESPONSE_MAX_SIZE:
-            response_body['profiles'] = self._extract_profiles_subset(response_body['profiles'])
+            response_body["profiles"] = self._extract_profiles_subset(response_body["profiles"])
 
-            response_body['next'] = self._generate_next_url(request_id, index + len(response_body['profiles']))
+            response_body["next"] = self._generate_next_url(request_id, index + len(response_body["profiles"]))
 
         return response_body
 
@@ -403,8 +399,8 @@ class MultiProfileLookupByIndexEndpointTask(MultiProfileLookupEndpointTask):
     @classmethod
     def _add_parameter_filters_to_query(cls, query, parameters):
         request_cache_table = parameters.request_cache_table
-        request_id = parameters.path['request_id']
-        index = int(parameters.query['index'][0])
+        request_id = parameters.path["request_id"]
+        index = int(parameters.query["index"][0])
         entity_ids = parameters.query.get("entity_id")
 
         if request_id:
@@ -417,26 +413,22 @@ class MultiProfileLookupByIndexEndpointTask(MultiProfileLookupEndpointTask):
     @classmethod
     def _get_entity_ids_from_dynamodb(cls, request_cache_table, request_id, index):
         response = None
-        key = dict(
-            request_id=dict(
-                S=request_id
-            )
-        )
+        key = dict(request_id=dict(S=request_id))
 
         with AWSClient("dynamodb") as dynamodb:
             response = dynamodb.get_item(TableName=request_cache_table, Key=key)
 
         entity_ids = cls._extract_entity_ids(response)
 
-        return entity_ids.split(',')[index:]
+        return entity_ids.split(",")[index:]
 
     @classmethod
     def _filter_by_entity_ids(cls, query, entity_ids):
-        return f'''{query} and u.ama_entity_id in ('{"','".join(entity_ids)}')'''
+        return f"""{query} and u.ama_entity_id in ('{"','".join(entity_ids)}')"""
 
     @classmethod
     def _extract_entity_ids(cls, response):
         if "Item" not in response or "entity_ids" not in response["Item"]:
-            raise APIEndpointException(f'Invalid DynamoDB configuration item: {json.dumps(response)}')
+            raise APIEndpointException(f"Invalid DynamoDB configuration item: {json.dumps(response)}")
 
         return response["Item"]["entity_ids"]["S"]
