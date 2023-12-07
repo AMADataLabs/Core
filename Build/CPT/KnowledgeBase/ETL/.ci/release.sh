b02adffc84ce815138f9cb18aa1a83347a2ea9b0
Peter Lane <Peter.Lane@ama-assn.org>
2023-12-07 17:02:05 -0600

    Deploy KB ETL 1.0.1 to production.

--- Build/CPT/KnowledgeBase/ETL/.ci/release.sh
+++ Build/CPT/KnowledgeBase/ETL/.ci/release.sh
@@ -9,8 +9,8 @@ main() {
     declare -A VERSIONS=(
         [sbx]=dev
         [dev]=dev
-        [tst]=1.0.1rc0
-        [prd]=1.0.0
+        [tst]=1.0.1
+        [prd]=1.0.1
     )
 
     if [[ "$ENVIRONMENT" != "" ]]; then
